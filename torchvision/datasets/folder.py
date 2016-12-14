a032807520b0c5b2c5377795bc910b6bd0f859db
Marat Dukhan <maratek@gmail.com>
2016-12-14 14:41:37 -0500

    Add support for accimage.Image
    It can be enabled by calling torchvision.set_image_backend('accimage')

--- torchvision/datasets/folder.py
+++ torchvision/datasets/folder.py
@@ -38,8 +38,25 @@ def make_dataset(dir, class_to_idx):
     return images
 
 
-def default_loader(path):
+def pil_loader(path):
     return Image.open(path).convert('RGB')
+
+
+def accimage_loader(path):
+    import accimage
+    try:
+        return accimage.Image(path)
+    except IOError:
+        # Potentially a decoding problem, fall back to PIL.Image
+        return pil_loader(path)
+
+
+def default_loader(path):
+    from torchvision import get_image_backend
+    if get_image_backend() == 'accimage':
+        return accimage_loader(path)
+    else:
+        return pil_loader(path)
 
 
 class ImageFolder(data.Dataset):
