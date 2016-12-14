0a787e0a830e1b39cbde2b224ef09755cfa79d91
Marat Dukhan <maratek@gmail.com>
2016-12-14 14:38:12 -0500

    Fix time per dataset in benchmark

--- test/preprocess-bench.py
+++ test/preprocess-bench.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
         batch = next(train_iter)
     end_time = timer()
     print("Performance: {dataset:.0f} minutes/dataset, {batch:.2f} secs/batch, {image:.2f} ms/image".format(
-        dataset=(end_time - start_time) * len(train_loader) / (batch_count * args.batchSize) / 60.0,
+        dataset=(end_time - start_time) * (float(len(train_loader)) / batch_count / 60.0),
         batch=(end_time - start_time) / float(batch_count),
         image=(end_time - start_time) / (batch_count * args.batchSize) * 1.0e+3))
 
