149760d41aab24907357b7516ef15c294c4757ac
Ludovic Denoyer <ludovic.Denoyer@lip6.fr>
2017-01-25 17:05:13 +0100

    Add Omniglot dataset loader

--- torchvision/datasets/__init__.py
+++ torchvision/datasets/__init__.py
@@ -3,9 +3,10 @@ from .folder import ImageFolder
 from .coco import CocoCaptions, CocoDetection
 from .cifar import CIFAR10, CIFAR100
 from .mnist import MNIST
+from .omniglot import OMNIGLOT
 
 __all__ = ('LSUN', 'LSUNClass',
            'ImageFolder',
            'CocoCaptions', 'CocoDetection',
            'CIFAR10', 'CIFAR100',
-           'MNIST')
+           'MNIST','OMNIGLOT')
