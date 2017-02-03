5d426c0eb06de3ab5e16dd1799fdb8b568db5ca1
Marat Dukhan <maratek@gmail.com>
2017-02-03 15:42:06 -0500

    Make all ReLU in SqueezeNet in-place

--- torchvision/models/squeezenet.py
+++ torchvision/models/squeezenet.py
@@ -14,10 +14,10 @@ class Fire(nn.Module):
         self.squeeze_activation = nn.ReLU(inplace=True)
         self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes,
                                    kernel_size=1)
-        self.expand1x1_activation = nn.ReLU()
+        self.expand1x1_activation = nn.ReLU(inplace=True)
         self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes,
                                    kernel_size=3, padding=1)
-        self.expand3x3_activation = nn.ReLU()
+        self.expand3x3_activation = nn.ReLU(inplace=True)
 
     def forward(self, x):
         x = self.squeeze_activation(self.squeeze(x))
