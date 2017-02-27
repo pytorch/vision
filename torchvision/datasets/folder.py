3c691967e269e20e4522dab5dfe57a59ac089e0d
Soumith Chintala <soumith@fb.com>
2017-02-27 16:53:50 -0500

    check for non-zero number of images in ImageFolder

--- torchvision/datasets/folder.py
+++ torchvision/datasets/folder.py
@@ -47,6 +47,9 @@ class ImageFolder(data.Dataset):
                  loader=default_loader):
         classes, class_to_idx = find_classes(root)
         imgs = make_dataset(root, class_to_idx)
+        if len(imgs) == 0:
+            raise(RuntimeError("Found 0 images in subfolders of: " + root + "\n"
+                               "Supported image extensions are: " + ",".join(IMG_EXTENSIONS)))
 
         self.root = root
         self.imgs = imgs
