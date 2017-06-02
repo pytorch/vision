17e126676b8dd9f1697b6f161ffb68d5a42ca11f
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2017-06-02 13:37:56 +0530

    document and remove unused imports in mnist.py

--- torchvision/datasets/mnist.py
+++ torchvision/datasets/mnist.py
@@ -5,12 +5,25 @@ import os
 import os.path
 import errno
 import torch
-import json
 import codecs
-import numpy as np
 
 
 class MNIST(data.Dataset):
+    """MNIST Dataset.
+
+    Args:
+        root (string): Root directory of dataset where ``processed/training.pt``
+            and  ``processed/test.pt`` exist.
+        train (bool, optional): If True, creates dataset from ``training.pt``,
+            otherwise from ``test.pt``.
+        download (bool, optional): If true, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, `transforms.RandomCrop`
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it
+    """
     urls = [
         'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz',
         'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz',
@@ -70,6 +83,7 @@ class MNIST(data.Dataset):
             os.path.exists(os.path.join(self.root, self.processed_folder, self.test_file))
 
     def download(self):
+        """Download the MNIST data if it doesn't exist in processed_folder already."""
         from six.moves import urllib
         import gzip
 
