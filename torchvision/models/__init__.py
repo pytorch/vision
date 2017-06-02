8f96e9df2973e09e984c038bf3a44558c88f784b
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2017-06-02 16:29:09 +0530

    pep8 fix

--- torchvision/models/__init__.py
+++ torchvision/models/__init__.py
@@ -37,7 +37,7 @@ You can use the following transform to normalize::
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
-An example of such normalization can be found in the imagenet example 
+An example of such normalization can be found in the imagenet example
 `here <https://github.com/pytorch/examples/blob/42e5b996718797e45c46a25c55b031e6768f8440/imagenet/main.py#L89-L101>`_
 
 ImageNet 1-crop error rates (224x224)
