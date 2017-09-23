41e6653b05972c2c8e8653a79377d731036224a3
Alykhan Tejani <alykhan.tejani@gmail.com>
2017-09-23 16:54:26 +0100

    update docstring

--- torchvision/transforms.py
+++ torchvision/transforms.py
@@ -383,12 +383,15 @@ class RandomSizedCrop(object):
 
 
 class FiveCrop(object):
-    """Crop the given PIL.Image into four corners and the central crop.
+    """Crop the given PIL.Image into four corners and the central crop.abs
 
-      Args:
-          size (sequence or int): Desired output size of the crop. If size is an
-              int instead of sequence like (h, w), a square crop (size, size) is
-              made.
+       Note: this transform returns a tuple of images and there may be a mismatch in the number of
+       inputs and targets your `Dataset` returns.
+
+       Args:
+           size (sequence or int): Desired output size of the crop. If size is an
+               int instead of sequence like (h, w), a square crop (size, size) is
+               made.
     """
 
     def __init__(self, size):
@@ -417,7 +420,13 @@ class TenCrop(object):
     """Crop the given PIL.Image into four corners and the central crop plus the
        flipped version of these (horizontal flipping is used by default)
 
+       Note: this transform returns a tuple of images and there may be a mismatch in the number of
+       inputs and targets your `Dataset` returns.
+
        Args:
+           size (sequence or int): Desired output size of the crop. If size is an
+               int instead of sequence like (h, w), a square crop (size, size) is
+               made.
            vflip bool: Use vertical flipping instead of horizontal
     """
 
