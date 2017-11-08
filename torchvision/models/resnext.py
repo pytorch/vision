f9ba7fb77171c319ccbf8f53072ceaf54dc2b23c
Andrzej Sulecki <asulecki@nvidia.com>
2017-11-08 13:34:43 +0100

    comments

--- torchvision/models/resnext.py
+++ torchvision/models/resnext.py
@@ -13,38 +13,6 @@ def conv3x3(in_planes, out_planes, stride=1):
                      padding=1, bias=False)
 
 
-#class BasicBlock(nn.Module):
-#    expansion = 1
-#
-#    def __init__(self, inplanes, planes, stride=1, downsample=None):
-#        super(BasicBlock, self).__init__()
-#        self.conv1 = conv3x3(inplanes, planes, stride)
-#        self.bn1 = nn.BatchNorm2d(planes)
-#        self.relu = nn.ReLU(inplace=True)
-#        self.conv2 = conv3x3(planes, planes)
-#        self.bn2 = nn.BatchNorm2d(planes)
-#        self.downsample = downsample
-#        self.stride = stride
-#
-#    def forward(self, x):
-#        residual = x
-#
-#        out = self.conv1(x)
-#        out = self.bn1(out)
-#        out = self.relu(out)
-#
-#        out = self.conv2(out)
-#        out = self.bn2(out)
-#
-#        if self.downsample is not None:
-#            residual = self.downsample(x)
-#
-#        out += residual
-#        out = self.relu(out)
-#
-#        return out
-
-
 class ResNeXtBottleneckC(nn.Module):
     expansion = 4
 
@@ -166,6 +134,11 @@ class ResNeXt(nn.Module):
 
 def resnext50(cardinality=32, baseWidth=4, shortcut='C', **kwargs):
     """Constructs a ResNeXt-50 model.
+
+    Args:
+        cardinality (int): Cardinality of the aggregated transform
+        baseWidth (int): Base width of the grouped convolution
+        shortcut ('A'|'B'|'C'): 'B' use 1x1 conv to downsample, 'C' use 1x1 conv on every residual connection
     """
     model = ResNeXt(ResNeXtBottleneckC, [3, 4, 6, 3], cardinality=cardinality, baseWidth=baseWidth, shortcut=shortcut, **kwargs)
     return model
@@ -173,6 +146,11 @@ def resnext50(cardinality=32, baseWidth=4, shortcut='C', **kwargs):
 
 def resnext101(cardinality=32, baseWidth=4, shortcut='C', **kwargs):
     """Constructs a ResNeXt-101 model.
+
+    Args:
+        cardinality (int): Cardinality of the aggregated transform
+        baseWidth (int): Base width of the grouped convolution
+        shortcut ('A'|'B'|'C'): 'B' use 1x1 conv to downsample, 'C' use 1x1 conv on every residual connection
     """
     model = ResNeXt(ResNeXtBottleneckC, [3, 4, 23, 3], cardinality=cardinality, baseWidth=baseWidth, shortcut=shortcut, **kwargs)
     return model
@@ -180,6 +158,11 @@ def resnext101(cardinality=32, baseWidth=4, shortcut='C', **kwargs):
 
 def resnext152(cardinality=32, baseWidth=4, shortcut='C', **kwargs):
     """Constructs a ResNeXt-152 model.
+
+    Args:
+        cardinality (int): Cardinality of the aggregated transform
+        baseWidth (int): Base width of the grouped convolution
+        shortcut ('A'|'B'|'C'): 'B' use 1x1 conv to downsample, 'C' use 1x1 conv on every residual connection
     """
     model = ResNeXt(ResNeXtBottleneckC, [3, 8, 36, 3], cardinality=cardinality, baseWidth=baseWidth, shortcut=shortcut, **kwargs)
     return model
