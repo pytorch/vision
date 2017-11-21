92620602736116002f48c88d77729925932a1559
Evgenii Zheltonozhskii <zheltonozhskiy@gmail.com>
2017-11-21 18:16:38 +0200

    Add initial realisation of ImageNet32 reader

---
+++ torchvision/datasets/imagenet_downsampled.py
@@ -0,0 +1,121 @@
+import os
+import os.path
+import pickle
+
+import numpy as np
+import torch.utils.data as data
+from PIL import Image
+from torchvision.datasets.utils import check_integrity
+
+
+class ImageNetDS(data.Dataset):
+    """`Downsampled ImageNet <https://patrykchrabaszcz.github.io/Imagenet32/>`_ Datasets.
+
+    Args:
+        root (string): Root directory of dataset where directory
+            ``ImagenetXX_train`` exists.
+        img_size (int): Dimensions of the images: 64,32,16,8
+        train (bool, optional): If True, creates dataset from training set, otherwise
+            creates from test set.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+
+    """
+    base_folder = 'Imagenet{}_train'
+    train_list = [
+        ['train_data_batch_1', ''],
+        ['train_data_batch_2', ''],
+        ['train_data_batch_3', ''],
+        ['train_data_batch_4', ''],
+        ['train_data_batch_5', ''],
+        ['train_data_batch_6', ''],
+        ['train_data_batch_7', ''],
+        ['train_data_batch_8', ''],
+        ['train_data_batch_9', ''],
+        ['train_data_batch_10', '']
+    ]
+
+    test_list = [
+        ['val_data', ''],
+    ]
+
+    def __init__(self, root, img_size, train=True, transform=None, target_transform=None):
+        self.root = os.path.expanduser(root)
+        self.transform = transform
+        self.target_transform = target_transform
+        self.train = train  # training set or test set
+        self.img_size = img_size
+
+        self.base_folder = self.base_folder.format(img_size)
+
+        # if not self._check_integrity():
+        #    raise RuntimeError('Dataset not found or corrupted.') # TODO
+
+        # now load the picked numpy arrays
+        if self.train:
+            self.train_data = []
+            self.train_labels = []
+            for fentry in self.train_list:
+                f = fentry[0]
+                file = os.path.join(self.root, self.base_folder, f)
+                with open(file, 'rb') as fo:
+                    entry = pickle.load(fo)
+                    self.train_data.append(entry['data'])
+                    self.train_labels += [label - 1 for label in entry['labels']]
+                    self.mean = entry['mean']
+
+            self.train_data = np.concatenate(self.train_data)
+            self.train_data = self.train_data.reshape((self.train_data.shape[0], 3, 32, 32))
+            self.train_data = self.train_data.transpose((0, 2, 3, 1))  # convert to HWC
+        else:
+            f = self.test_list[0][0]
+            file = os.path.join(self.root, f)
+            fo = open(file, 'rb')
+            entry = pickle.load(fo)
+            self.test_data = entry['data']
+            self.test_labels = [label - 1 for label in entry['labels']]
+            fo.close()
+            self.test_data = self.test_data.reshape((self.test_data.shape[0], 3, 32, 32))
+            self.test_data = self.test_data.transpose((0, 2, 3, 1))  # convert to HWC
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        if self.train:
+            img, target = self.train_data[index], self.train_labels[index]
+        else:
+            img, target = self.test_data[index], self.test_labels[index]
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        if self.train:
+            return len(self.train_data)
+        else:
+            return len(self.test_data)
+
+    def _check_integrity(self):
+        root = self.root
+        for fentry in (self.train_list + self.test_list):
+            filename, md5 = fentry[0], fentry[1]
+            fpath = os.path.join(root, self.base_folder, filename)
+            if not check_integrity(fpath, md5):
+                return False
+        return True
