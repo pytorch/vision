4f874cc6738adda6e06c0d320285554a8930e201
vlomonaco <vincenzo.lomonaco@outlook.com>
2017-11-22 22:27:57 -0500

    CORe50 Dataset added. Utils improved with reporthook during download.

--- torchvision/datasets/utils.py
+++ torchvision/datasets/utils.py
@@ -30,7 +30,7 @@ def reporthook(count, block_size, total_size):
     speed = int(progress_size / (1024 * duration))
     percent = min(int(count * block_size * 100 / total_size), 100)
     sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
-                    (percent, progress_size / (1024 * 1024), speed, duration))
+                     (percent, progress_size / (1024 * 1024), speed, duration))
     sys.stdout.flush()
 
 
