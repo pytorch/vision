5861f14ab215073fe2002bb1d9f8f3f0df13d39c
Martin Raison <martinraison@users.noreply.github.com>
2017-12-06 12:10:15 +0100

    EMNIST dataset + speedup *MNIST preprocessing (#334)
    * EMNIST dataset + speedup *MNIST preprocessing

--- torchvision/datasets/__init__.py
+++ torchvision/datasets/__init__.py
@@ -3,7 +3,7 @@ from .folder import ImageFolder
 from .coco import CocoCaptions, CocoDetection
 from .cifar import CIFAR10, CIFAR100
 from .stl10 import STL10
-from .mnist import MNIST, FashionMNIST
+from .mnist import MNIST, EMNIST, FashionMNIST
 from .svhn import SVHN
 from .phototour import PhotoTour
 from .fakedata import FakeData
@@ -12,5 +12,5 @@ from .semeion import SEMEION
 __all__ = ('LSUN', 'LSUNClass',
            'ImageFolder', 'FakeData',
            'CocoCaptions', 'CocoDetection',
-           'CIFAR10', 'CIFAR100', 'FashionMNIST',
+           'CIFAR10', 'CIFAR100', 'EMNIST', 'FashionMNIST',
            'MNIST', 'STL10', 'SVHN', 'PhotoTour', 'SEMEION')
