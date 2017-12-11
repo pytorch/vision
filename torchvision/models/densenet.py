4cd138f5c34a5703606e2fcffe1ddb17501c95d3
Jon Crall <erotemic@gmail.com>
2017-12-11 17:56:31 -0500

    Densenet average pool is now determined by the resolution of the final convolutional output

--- torchvision/models/densenet.py
+++ torchvision/models/densenet.py
@@ -156,6 +156,7 @@ class DenseNet(nn.Module):
     def forward(self, x):
         features = self.features(x)
         out = F.relu(features, inplace=True)
-        out = F.avg_pool2d(out, kernel_size=7, stride=1).view(features.size(0), -1)
+        kernel_size = tuple(map(int, features.shape[-2:]))
+        out = F.avg_pool2d(out, kernel_size=kernel_size, stride=1).view(features.size(0), -1)
         out = self.classifier(out)
         return out
