146d948d9ddb0890edf1500f926b82f0e8fa2100
Sri Krishna <skrish13@users.noreply.github.com>
2017-12-13 00:17:47 +0530

    Update densenet.py
    added missing weight init for densenet

--- torchvision/models/densenet.py
+++ torchvision/models/densenet.py
@@ -152,6 +152,14 @@ class DenseNet(nn.Module):
 
         # Linear layer
         self.classifier = nn.Linear(num_features, num_classes)
+        
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()        
 
     def forward(self, x):
         features = self.features(x)
