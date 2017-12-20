9889de1d9cf6604779e4f3ec1c5f4016f0b2a66f
Christopher Beckham <christopher-beckham@users.noreply.github.com>
2017-12-20 12:05:27 -0500

    Change string to comment (#374)

--- torchvision/models/resnet.py
+++ torchvision/models/resnet.py
@@ -17,7 +17,7 @@ model_urls = {
 
 
 def conv3x3(in_planes, out_planes, stride=1):
-    "3x3 convolution with padding"
+    """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
 
