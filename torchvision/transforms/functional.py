9b526dbc8adb091b4c588ef1599f7c3f2e040cdc
Francisco Massa <fvsmassa@gmail.com>
2018-01-29 22:30:17 -0800

    Fix ToTensor when PIL Image has mode F
    Fixes https://github.com/pytorch/vision/issues/399.
    
    The only case of floating point supported by PIL seems to be `F`, so this should fix it.

--- torchvision/transforms/functional.py
+++ torchvision/transforms/functional.py
@@ -59,6 +59,8 @@ def to_tensor(pic):
         img = torch.from_numpy(np.array(pic, np.int32, copy=False))
     elif pic.mode == 'I;16':
         img = torch.from_numpy(np.array(pic, np.int16, copy=False))
+    elif pic.mode == 'F':
+        img = torch.from_numpy(np.array(pic, np.float32, copy=False))
     else:
         img = torch.ByteTensor(torch.ByteStorage.from_buffer(pic.tobytes()))
     # PIL image mode: 1, L, P, I, F, RGB, YCbCr, RGBA, CMYK
