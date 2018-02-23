6e0366f847304014e8d7fcd62f712320f48b60ce
kwotsin <leeks_50996@hotmail.com>
2018-02-23 01:31:46 +0000

    added support for (float, float) input

--- torchvision/transforms/functional.py
+++ torchvision/transforms/functional.py
@@ -261,21 +261,28 @@ def crop(img, i, j, h, w):
 
 
 def center_crop(img, output_size):
-    if isinstance(output_size, float):
-        w, h = img.size
-        th, tw = int(h * output_size), int(w * output_size)
-
-        i = int(round((h - th) / 2.))
-        j = int(round((w - tw) / 2.))
-        return crop(img, i, j, th, tw)
+    w, h = img.size
+    # Case of single value provided
+    if isinstance(output_size, numbers.Number):
+        # Float case: constraint for fraction must be from 0 to 1.0
+        if isinstance(output_size, float):
+            if not 0.0 < output_size <= 1.0:
+                raise ValueError("Invalid float output size. Range is (0.0, 1.0]")
+            output_size = (output_size, output_size)
+            th, tw = int(h * output_size[0]), int(w * output_size[1])
+        elif isinstance(output_size, int):
+            output_size = (output_size, output_size)
+            th, tw = output_size
+    # Case of tuple of values provided
     else:
-        if isinstance(output_size, numbers.Number):
-            output_size = (int(output_size), int(output_size))
-        w, h = img.size
-        th, tw = output_size
-        i = int(round((h - th) / 2.))
-        j = int(round((w - tw) / 2.))
-        return crop(img, i, j, th, tw)
+        if isinstance(output_size[0], float):
+            th, tw = int(h * output_size[0]), int(w * output_size[1])
+        elif isinstance(output_size[0], int):
+            th, tw = output_size
+
+    i = int(round((h - th) / 2.))
+    j = int(round((w - tw) / 2.))
+    return crop(img, i, j, th, tw)
 
 
 def resized_crop(img, i, j, h, w, size, interpolation=Image.BILINEAR):
