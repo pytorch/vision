5a1bf0a3854eba78e06af8b58b02c54607f9320b
Francisco Massa <fvsmassa@gmail.com>
2018-04-20 09:02:50 -0600

    Fix import after move

--- torchvision/layers/_utils.py
+++ torchvision/layers/_utils.py
@@ -16,9 +16,9 @@ def _load_C_extensions(source_cpu, source_gpu=None):
         source.extend(source_gpu)
         extra_cflags = ['-DWITH_CUDA']
     source = [os.path.join(this_dir, s) for s in source]
-    extra_include_paths = [this_dir]
+    extra_include_paths = [this_dir + '/extensions']
     return load_ext('torchvision', source, extra_cflags=extra_cflags, extra_include_paths=extra_include_paths)
 
-_C = _load_C_extensions(['vision.cpp'])
+_C = _load_C_extensions(['extensions/vision.cpp'])
 
 
