f5eb0faa9658172a66ed54c4786d24df5fc48370
Francisco Massa <fvsmassa@gmail.com>
2018-04-25 08:57:57 -0600

    Replace runtime_error by AT_ERROR

--- torchvision/layers/extensions/nms.h
+++ torchvision/layers/extensions/nms.h
@@ -6,7 +6,7 @@ at::Tensor nms(const at::Tensor& dets,
                const float threshold) {
 
   if (dets.type().is_cuda())
-    std::runtime_error("NMS not implemented on the GPU");
+    AT_ERROR("NMS not implemented on the GPU");
 
   at::Tensor result = nms_cpu(dets, scores, threshold);
   return result;
