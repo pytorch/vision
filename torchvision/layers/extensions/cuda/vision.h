3fd8ab7d13e647cae946df1c4dd034c8a8b31609
Francisco Massa <fvsmassa@gmail.com>
2018-04-25 08:53:12 -0600

    Reduce memory requirements for backwards

--- torchvision/layers/extensions/cuda/vision.h
+++ torchvision/layers/extensions/cuda/vision.h
@@ -10,10 +10,13 @@ at::Tensor ROIAlign_forward_cuda(const at::Tensor& input,
                                  const int sampling_ratio);
 
 at::Tensor ROIAlign_backward_cuda(const at::Tensor& grad,
-                                  const at::Tensor& input,
                                   const at::Tensor& rois,
                                   const float spatial_scale,
                                   const int pooled_height,
                                   const int pooled_width,
+                                  const int batch_size,
+                                  const int channels,
+                                  const int height,
+                                  const int width,
                                   const int sampling_ratio);
 
--- torchvision/layers/roi_align.py
+++ torchvision/layers/roi_align.py
@@ -12,10 +12,11 @@ from ._utils import _C
 class _ROIAlign(Function):
     @staticmethod
     def forward(ctx, input, roi, output_size, spatial_scale, sampling_ratio):
-        ctx.save_for_backward(input, roi)
+        ctx.save_for_backward(roi)
         ctx.output_size = _pair(output_size)
         ctx.spatial_scale = spatial_scale
         ctx.sampling_ratio = sampling_ratio
+        ctx.input_shape = input.size()
         output = _C.roi_align_forward(input, roi, spatial_scale,
                 output_size[0], output_size[1], sampling_ratio)
         return output
@@ -23,12 +24,13 @@ class _ROIAlign(Function):
     @staticmethod
     @once_differentiable
     def backward(ctx, grad_output):
-        input, rois = ctx.saved_tensors
+        rois, = ctx.saved_tensors
         output_size = ctx.output_size
         spatial_scale = ctx.spatial_scale
         sampling_ratio = ctx.sampling_ratio
-        grad_input =  _C.roi_align_backward(grad_output, input, rois, spatial_scale,
-                output_size[0], output_size[1], sampling_ratio)
+        bs, ch, h, w = ctx.input_shape
+        grad_input =  _C.roi_align_backward(grad_output, rois, spatial_scale,
+                output_size[0], output_size[1], bs, ch, h, w, sampling_ratio)
         return grad_input, None, None, None, None
 
 
