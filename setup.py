c1098815f8439457107286fa9af39eb1e540645d
Francisco Massa <fvsmassa@gmail.com>
2018-04-26 08:32:05 -0600

    Cleanups

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import sys
 from setuptools import setup, find_packages
 from pkg_resources import get_distribution, DistributionNotFound
 import subprocess
+import glob
 
 import torch.cuda
 from torch.utils.cpp_extension import CppExtension, CUDAExtension, CUDA_HOME
@@ -72,10 +73,12 @@ pillow_req = 'pillow-simd' if get_dist('pillow-simd') is not None else 'pillow'
 requirements.append(pillow_req + pillow_ver)
 
 def get_extensions():
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    extensions_dir = os.path.join(this_dir, 'torchvision', 'csrc')
 
-    main_file = ['vision.cpp']
-    source_cpu = ['cpu/nms_cpu.cpp', 'cpu/ROIAlign_cpu.cpp']
-    source_cuda = ['cuda/ROIAlign_cuda.cu', 'cuda/ROIPool_cuda.cu']
+    main_file = glob.glob(os.path.join(extensions_dir, '*.cpp'))
+    source_cpu = glob.glob(os.path.join(extensions_dir, 'cpu', '*.cpp'))
+    source_cuda = glob.glob(os.path.join(extensions_dir, 'cuda', '*.cu'))
 
     sources = main_file + source_cpu
     extension = CppExtension
@@ -87,8 +90,6 @@ def get_extensions():
         sources += source_cuda
         define_macros += [('WITH_CUDA', None)]
     
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    extensions_dir = os.path.join(this_dir, 'torchvision', 'csrc')
     sources = [os.path.join(extensions_dir, s) for s in sources]
 
     include_dirs = [extensions_dir]
--- torchvision/layers/_utils.py
+++ torchvision/layers/_utils.py
@@ -1,29 +1,32 @@
 import os.path
+import glob
 
 import torch
 
 try:
     from torch.utils.cpp_extension import load as load_ext
+    from torch.utils.cpp_extension import CUDA_HOME
 except ImportError:
     raise ImportError("The cpp layer extensions requires PyTorch 0.4 or higher")
 
 
-def _load_C_extensions(source_cpu, source_gpu=None):
+def _load_C_extensions():
     this_dir = os.path.dirname(os.path.abspath(__file__))
     this_dir = os.path.dirname(this_dir)
     this_dir = os.path.join(this_dir, 'csrc')
-    source = source_cpu
+
+    main_file = glob.glob(os.path.join(this_dir, '*.cpp'))
+    source_cpu = glob.glob(os.path.join(this_dir, 'cpu', '*.cpp'))
+    source_cuda = glob.glob(os.path.join(this_dir, 'cuda', '*.cu'))
+
+    source = main_file + source_cpu
+
     extra_cflags = []
-    if torch.cuda.is_available() and source_gpu is not None:
-        source.extend(source_gpu)
+    if torch.cuda.is_available() and CUDA_HOME is not None:
+        source.extend(source_cuda)
         extra_cflags = ['-DWITH_CUDA']
     source = [os.path.join(this_dir, s) for s in source]
     extra_include_paths = [this_dir]
     return load_ext('torchvision', source, extra_cflags=extra_cflags, extra_include_paths=extra_include_paths)
 
-main_file = ['vision.cpp']
-source_cpu = ['cpu/nms_cpu.cpp', 'cpu/ROIAlign_cpu.cpp']
-source_cuda = ['cuda/ROIAlign_cuda.cu', 'cuda/ROIPool_cuda.cu']
-
-_C = _load_C_extensions(source_cpu + main_file, source_cuda)
-
+_C = _load_C_extensions()
