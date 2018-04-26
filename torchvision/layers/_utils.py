dbe1f60056e8f36a7a4abc267bce0a22a0c8810b
Francisco Massa <fvsmassa@gmail.com>
2018-04-26 06:47:57 -0600

    Change folder structure

--- torchvision/layers/_utils.py
+++ torchvision/layers/_utils.py
@@ -10,13 +10,15 @@ except ImportError:
 
 def _load_C_extensions(source_cpu, source_gpu=None):
     this_dir = os.path.dirname(os.path.abspath(__file__))
+    this_dir = os.path.dirname(this_dir)
+    this_dir = os.path.join(this_dir, 'csrc')
     source = source_cpu
     extra_cflags = []
     if torch.cuda.is_available() and source_gpu is not None:
         source.extend(source_gpu)
         extra_cflags = ['-DWITH_CUDA']
-    source = [os.path.join(this_dir, 'extensions', s) for s in source]
-    extra_include_paths = [this_dir + '/extensions']
+    source = [os.path.join(this_dir, s) for s in source]
+    extra_include_paths = [this_dir]
     return load_ext('torchvision', source, extra_cflags=extra_cflags, extra_include_paths=extra_include_paths)
 
 main_file = ['vision.cpp']
