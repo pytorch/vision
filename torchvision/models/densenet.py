adefd42693391628ebc6bfbb2ab63def7e795844
hamedonline <h.homaei@gmail.com>
2018-04-28 22:57:23 +0430

    Update densenet
    Update weight initialization function name to bypass the warning about future naming deprecation.

--- torchvision/models/densenet.py
+++ torchvision/models/densenet.py
@@ -209,7 +209,7 @@ class DenseNet(nn.Module):
         # Official init from torch repo.
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal(m.weight.data)
+                nn.init.kaiming_normal_(m.weight.data)
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
