d8bcd1f67cf3e3aaf3c5ce012da17f0a11bae46b
moskomule <hataya@keio.jp>
2018-04-30 09:16:26 +0900

    add nonlinearity='relu' for sure

--- torchvision/models/resnet.py
+++ torchvision/models/resnet.py
@@ -112,7 +112,7 @@ class ResNet(nn.Module):
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode="fan_out")
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
--- torchvision/models/vgg.py
+++ torchvision/models/vgg.py
@@ -47,7 +47,7 @@ class VGG(nn.Module):
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode="fan_out")
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.BatchNorm2d):
