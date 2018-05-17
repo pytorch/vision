628e90cb45cb5ba232335f6462a2e336472ea4eb
David Morton <davidlmorton@users.noreply.github.com>
2018-05-17 17:09:46 -0500

    Add metadata to some datasets (#501)
    * Add classes metadata to MNIST and FashionMNIST
    
    * Add `targets` property to MNIST and FashionMNIST
    
    * Add class metadata to CIFAR10/CIFAR100
    
    * Add `targets` property to CIFAR10/CIFAR100
    
    * Add targets attribute to DatasetFolder

--- torchvision/datasets/cifar.py
+++ torchvision/datasets/cifar.py
@@ -45,6 +45,18 @@ class CIFAR10(data.Dataset):
     test_list = [
         ['test_batch', '40351d587109b95175f43aff81a1287e'],
     ]
+    meta = {
+        'filename': 'batches.meta',
+        'key': 'label_names',
+        'md5': '5ff9c542aee3614f3951f8cda6e48888',
+    }
+
+    @property
+    def targets(self):
+        if self.train:
+            return self.train_labels
+        else:
+            return self.test_labels
 
     def __init__(self, root, train=True,
                  transform=None, target_transform=None,
@@ -99,6 +111,21 @@ class CIFAR10(data.Dataset):
             fo.close()
             self.test_data = self.test_data.reshape((10000, 3, 32, 32))
             self.test_data = self.test_data.transpose((0, 2, 3, 1))  # convert to HWC
+
+        self._load_meta()
+
+    def _load_meta(self):
+        path = os.path.join(self.root, self.base_folder, self.meta['filename'])
+        if not check_integrity(path, self.meta['md5']):
+            raise RuntimeError('Dataset metadata file not found or corrupted.' +
+                               ' You can use download=True to download it')
+        with open(path, 'rb') as infile:
+            if sys.version_info[0] == 2:
+                data = pickle.load(infile)
+            else:
+                data = pickle.load(infile, encoding='latin1')
+            self.classes = data[self.meta['key']]
+        self.class_to_idx = {_class: i for i, _class in enumerate(self.classes)}
 
     def __getitem__(self, index):
         """
@@ -187,3 +214,8 @@ class CIFAR100(CIFAR10):
     test_list = [
         ['test', 'f0ef6b0ae62326f3e7ffdfab6717acfc'],
     ]
+    meta = {
+        'filename': 'meta',
+        'key': 'fine_label_names',
+        'md5': '7973b15100ade9c7d40fb424638fde48',
+    }
