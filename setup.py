2c737dcd5abc4d4f0c8f104596549a57a451b821
Robin-des-Bois <vaaler@me.com>
2018-05-26 14:26:09 +0200

    Update setup.py
    fixed formatting issue.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
-    
+
 def pillow_requirement():
     if platform.system() == 'Darwin':
         return 'pillow >= 4.1.1, != 5.1'
