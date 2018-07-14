68aacef59d7ad5f4056aa26033cc986d32238c4a
sebastianberns <sebastianberns@gmail.com>
2018-07-14 19:36:09 +0200

    Add LA and RGBA tests

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -1185,9 +1185,16 @@ class Tester(unittest.TestCase):
         ])
         convert = transforms.ToTensor()
         
-        # Invert Grayscale
+        a = Image.fromarray(np.eye(size[0], size[1], dtype=np.uint8), mode=mode)
+        
+        # Invert L
         img = Image.fromarray(np.arange(0, 255, 16, dtype=np.uint8).reshape(size), mode=mode)
         inv = Image.fromarray(np.arange(255, 0, -16, dtype=np.uint8).reshape(size), mode=mode)
+        assert torch.equal(invert(img), convert(inv))
+        
+        # Invert LA
+        img.putalpha(a)
+        inv.putalpha(a)
         assert torch.equal(invert(img), convert(inv))
         
         # Invert RGB
@@ -1199,6 +1206,11 @@ class Tester(unittest.TestCase):
         g = Image.fromarray(np.arange(0, 255, 16, dtype=np.uint8).reshape(size), mode=mode)
         b = Image.fromarray(np.arange(128, 255, 8, dtype=np.uint8).reshape(size), mode=mode)
         inv = Image.merge('RGB', (r,g,b))
+        assert torch.equal(invert(img), convert(inv))
+        
+        # Invert RGBA
+        img.putalpha(a)
+        inv.putalpha(a)
         assert torch.equal(invert(img), convert(inv))
         
         # Checking if Invert can be printed as string
