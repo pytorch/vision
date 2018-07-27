cdf906b1f4d92c3be25cc8eec4008d14ee5236e4
Varun Agrawal <varagrawal@gmail.com>
2018-07-27 14:42:09 -0400

    update setup.py to be correct

--- setup.py
+++ setup.py
@@ -7,12 +7,9 @@ import sys
 from setuptools import setup, find_packages
 from pkg_resources import get_distribution, DistributionNotFound
 import glob
-<<<<<<< c42bd059e7431f1f6be40a29313e36951ce57842
 
 import torch.cuda
 from torch.utils.cpp_extension import CppExtension, CUDAExtension, CUDA_HOME
-=======
->>>>>>> Cleanups
 
 import torch.cuda
 from torch.utils.cpp_extension import CppExtension, CUDAExtension, CUDA_HOME
@@ -52,40 +49,6 @@ requirements = [
     'torch',
 ]
 
-
-def get_extensions():
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    extensions_dir = os.path.join(this_dir, 'torchvision', 'csrc')
-
-    main_file = glob.glob(os.path.join(extensions_dir, '*.cpp'))
-    source_cpu = glob.glob(os.path.join(extensions_dir, 'cpu', '*.cpp'))
-    source_cuda = glob.glob(os.path.join(extensions_dir, 'cuda', '*.cu'))
-
-    sources = main_file + source_cpu
-    extension = CppExtension
-
-    define_macros = []
-
-    if torch.cuda.is_available() and CUDA_HOME is not None:
-        extension = CUDAExtension
-        sources += source_cuda
-        define_macros += [('WITH_CUDA', None)]
-
-    sources = [os.path.join(extensions_dir, s) for s in sources]
-
-    include_dirs = [extensions_dir]
-
-    ext_modules = [
-        extension(
-            'torchvision._C',
-            sources,
-            include_dirs=include_dirs,
-            define_macros=define_macros
-        )
-    ]
-
-    return ext_modules
-
 pillow_ver = ' >= 4.1.1'
 pillow_req = 'pillow-simd' if get_dist('pillow-simd') is not None else 'pillow'
 requirements.append(pillow_req + pillow_ver)
@@ -98,16 +61,9 @@ def get_extensions():
     this_dir = os.path.dirname(os.path.abspath(__file__))
     extensions_dir = os.path.join(this_dir, 'torchvision', 'csrc')
 
-<<<<<<< c42bd059e7431f1f6be40a29313e36951ce57842
-    main_file = ['vision.cpp']
-    source_cpu = ['cpu/nms_cpu.cpp', 'cpu/ROIAlign_cpu.cpp']
-    source_cuda = ['cuda/ROIAlign_cuda.cu']
-    name = 'torchvision._C'
-=======
     main_file = glob.glob(os.path.join(extensions_dir, '*.cpp'))
     source_cpu = glob.glob(os.path.join(extensions_dir, 'cpu', '*.cpp'))
     source_cuda = glob.glob(os.path.join(extensions_dir, 'cuda', '*.cu'))
->>>>>>> Cleanups
 
     sources = main_file + source_cpu
     extension = CppExtension
@@ -126,7 +82,8 @@ def get_extensions():
 
     ext_modules = [
         extension(
-            name, sources,
+            'torchvision._C',
+            sources,
             include_dirs=include_dirs,
             define_macros=define_macros
         )
