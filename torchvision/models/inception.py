fe8fe7099086316f391d5427845e8e90da41f502
Bowen Bao <bowbao@microsoft.com>
2018-10-08 16:25:48 -0700

    nit

--- torchvision/models/inception.py
+++ torchvision/models/inception.py
@@ -70,11 +70,10 @@ class Inception3(nn.Module):
 
     def forward(self, x):
         if self.transform_input:
-            x = torch.cat((
-                torch.unsqueeze(x[:, 0], 1) * (0.229 / 0.5) + (0.485 - 0.5) / 0.5,
-                torch.unsqueeze(x[:, 1], 1) * (0.224 / 0.5) + (0.456 - 0.5) / 0.5,
-                torch.unsqueeze(x[:, 2], 1) * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
-                ), 1)
+            x_ch0 = torch.unsqueeze(x[:, 0], 1) * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
+            x_ch1 = torch.unsqueeze(x[:, 1], 1) * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
+            x_ch2 = torch.unsqueeze(x[:, 2], 1) * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
+            x = torch.cat((x_ch0, x_ch1, x_ch2), 1)
         # 299 x 299 x 3
         x = self.Conv2d_1a_3x3(x)
         # 149 x 149 x 32
