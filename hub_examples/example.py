55490a28763426ba6e74e539aeb7d39ba8d00035
Ailing Zhang <ailzhang@fb.com>
2018-10-22 14:44:14 -0700

    address comment

--- hub_examples/example.py
+++
@@ -1,7 +0,0 @@
-from torchvision.models.resnet import resnet18
-
-def wrapper1(pretrained=False, **kwargs):
-    model = resnet18(pretrained, **kwargs)
-    return model
-
-
--- hubconf.py
+++ hubconf.py
@@ -1,32 +1,29 @@
-# /**************************For Repo Owner************************************
-# entrypoints is a list of (func_name, module_name, checkpoint_dir).
-#   func_name: required, this function should return a model
-#   module_name: required, specifies where to load the module in the package
-#   checkpoint_url: optional, specifies where to download the checkpoint
-# dependencies is a list of dependent package names.
-# help_msp is a string, explaining what model each entrypoint points to.
-# Example repo structure:
-# repo/
-#   hubconf.py
-#   a/
-#     __init__.py
-#     b/
-#       __init__.py
-#       c.py
-# For callable `x` defined in c.py, make sure you can do `import a.b.c.x` when
-# starting an interactive python shell in repo/.
-# The corresponding entrypoint is ('x', 'a.b.c', <checkpoint_url>).
-# ****************************************************************************/
+import torch.utils.model_zoo as model_zoo
 
-entrypoints = [
-    ('wrapper1',
-     'hub_examples.example',
-     'https://download.pytorch.org/models/resnet18-5c106cde.pth'),
-    ('resnet18',
-     'torchvision.models.restnet18',
-     'https://download.pytorch.org/models/resnet18-5c106cde.pth')
-    ]
+# Optional list of dependencies required by the package
 dependencies = ['torch', 'math']
-help_msg = ("/****** Hub Help Section ******/\n"
-            "hello world\n"
-            "/******  End of Message  ******/")
+
+def wrapper1(pretrained=False, *args, **kwargs):
+    """
+    pretrained (bool): a recommended kwargs for all entrypoints
+    args & kwargs are arguments for the function
+    """
+    from torchvision.models.resnet import resnet18
+    model = resnet18(*args, **kwargs)
+    checkpoint = 'https://download.pytorch.org/models/resnet18-5c106cde.pth'
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(checkpoint, progress=False))
+    return model
+
+def wrapper2(pretrained=False, *args, **kwargs):
+    """
+    pretrained (bool): a recommended kwargs for all entrypoints
+    args & kwargs are arguments for the function
+    """
+    from torchvision.models.resnet import resnet50
+    model = resnet50(*args, **kwargs)
+    checkpoint = 'https://download.pytorch.org/models/resnet50-19c8e357.pth'
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(checkpoint, progress=False))
+    return model
+
