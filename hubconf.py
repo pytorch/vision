8a6f0a03fa8cdc4625667aac2989730f61a64989
Ailing Zhang <ailzhang@fb.com>
2018-10-26 10:17:00 -0700

    address comment

--- hubconf.py
+++ hubconf.py
@@ -4,16 +4,16 @@ This is an example hubconf.py for pytorch/vision repo
 ## Users can get this published model by calling:
 hub_model = hub.load(
     'pytorch/vision:master', # repo_owner/repo_name:branch
-    'wrapper1', # entrypoint
+    'resnet18', # entrypoint
     1234, # args for callable [not applicable to resnet]
     pretrained=True) # kwargs for callable
 
 ## Protocol on repo owner side
 1. The "published" models should be at least in a branch/tag. It can't be a random commit.
 2. Repo owner should have the following field defined in hubconf.py
-  2.1 Function/entrypoint with function signature def wrapper1(pretrained=False, *args, **kwargs):
+  2.1 Function/entrypoint with function signature "def resnet18(pretrained=False, *args, **kwargs):"
   2.2 Pretrained allows users to load pretrained weights from repo owner.
-  2.3 Args and kwargs are passed to the callable resnet18,
+  2.3 Args and kwargs are passed to the callable _resnet18,
   2.4 Docstring of function works as a help message, explaining what does the model do and what's
       the allowed arguments.
   2.5 Dependencies is a list optionally provided by repo owner, to specify what packages are required
@@ -37,28 +37,28 @@ import torch.utils.model_zoo as model_zoo
 dependencies = ['torch', 'math']
 
 
-def wrapper1(pretrained=False, *args, **kwargs):
+def resnet18(pretrained=False, *args, **kwargs):
     """
     Resnet18 model
     pretrained (bool): a recommended kwargs for all entrypoints
     args & kwargs are arguments for the function
     """
-    from torchvision.models.resnet import resnet18
-    model = resnet18(*args, **kwargs)
+    from torchvision.models.resnet import resnet18 as _resnet18
+    model = _resnet18(*args, **kwargs)
     checkpoint = 'https://download.pytorch.org/models/resnet18-5c106cde.pth'
     if pretrained:
         model.load_state_dict(model_zoo.load_url(checkpoint, progress=False))
     return model
 
 
-def wrapper2(pretrained=False, *args, **kwargs):
+def resnet50(pretrained=False, *args, **kwargs):
     """
     Resnet50 model
     pretrained (bool): a recommended kwargs for all entrypoints
     args & kwargs are arguments for the function
     """
-    from torchvision.models.resnet import resnet50
-    model = resnet50(*args, **kwargs)
+    from torchvision.models.resnet import resnet50 as  _resnet50
+    model = _resnet50(*args, **kwargs)
     checkpoint = 'https://download.pytorch.org/models/resnet50-19c8e357.pth'
     if pretrained:
         model.load_state_dict(model_zoo.load_url(checkpoint, progress=False))
