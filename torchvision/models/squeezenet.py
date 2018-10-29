21d7b7684c90e82a6f333cea3d4c215e697b2b21
AdanMora <ajmorafallas@gmail.com>
2018-10-29 01:22:08 -0600

    AdaptiveAvgpool2d integrated
    The Average Pool Layer on SqueezeNet and ResNet was hardcoded, this was changed, now any the input size is accepted.

--- torchvision/models/squeezenet.py
+++ torchvision/models/squeezenet.py
@@ -82,7 +82,7 @@ class SqueezeNet(nn.Module):
             nn.Dropout(p=0.5),
             final_conv,
             nn.ReLU(inplace=True),
-            nn.AvgPool2d(13, stride=1)
+            nn.AdaptiveAvgPool2d((1,1))
         )
 
         for m in self.modules():
