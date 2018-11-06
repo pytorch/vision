4cbd1fe9e3081fb6a5a8832bc85db19fa17cab7e
erikgaas <erikgaas@gmail.com>
2018-11-05 18:47:48 -0800

    lint

--- torchvision/models/resnet.py
+++ torchvision/models/resnet.py
@@ -20,9 +20,11 @@ def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
 
+
 def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
 
 class BasicBlock(nn.Module):
     expansion = 1
