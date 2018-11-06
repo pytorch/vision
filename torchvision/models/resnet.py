df02cd6480211f9c111f29e35284a103610909cf
erikgaas <erikgaas@gmail.com>
2018-11-05 18:06:17 -0800

    fix stride

--- torchvision/models/resnet.py
+++ torchvision/models/resnet.py
@@ -22,7 +22,7 @@ def conv3x3(in_planes, out_planes, stride=1):
 
 def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1, bias=False)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 class BasicBlock(nn.Module):
     expansion = 1
