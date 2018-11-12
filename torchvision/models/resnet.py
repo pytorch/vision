5b44d7160b48e20b5496658731f2fadaf80b7381
Amir Arsalan Soltani <Amir-Arsalan@users.noreply.github.com>
2018-11-12 12:08:07 -0500

    Update resnet.py

--- torchvision/models/resnet.py
+++ torchvision/models/resnet.py
@@ -99,8 +99,8 @@ class Bottleneck(nn.Module):
 class ResNet(nn.Module):
 
     def __init__(self, block, layers, num_classes=1000):
-        self.inplanes = 64
         super(ResNet, self).__init__()
+        self.inplanes = 64
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
                                bias=False)
         self.bn1 = nn.BatchNorm2d(64)
