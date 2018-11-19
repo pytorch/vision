0524545cada91a5c2d40cf55e626b7bed78e1782
Amir Arsalan Soltani <Amir-Arsalan@users.noreply.github.com>
2018-11-19 07:43:49 -0500

    Update alexnet.py

--- torchvision/models/alexnet.py
+++ torchvision/models/alexnet.py
@@ -41,7 +41,7 @@ class AlexNet(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), 256 * 6 * 6)
+        x = x.view(x.size(0), x.size(1) * x.size(2) * x.size(3)) #x.size(1), x.size(2) and x.size(3) represent 256, 6 and 6 respectively
         x = self.classifier(x)
         return x
 
