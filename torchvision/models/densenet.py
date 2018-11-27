c9914e00384a8ef735f0ae1e1ead2216b7b086d2
quinwu <wukuan1995@gmail.com>
2018-11-27 14:43:28 +0800

    Changing to AdaptiveAvgPool2d on DenseNet

--- torchvision/models/densenet.py
+++ torchvision/models/densenet.py
@@ -112,7 +112,7 @@ class DenseNet(nn.Module):
     def forward(self, x):
         features = self.features(x)
         out = F.relu(features, inplace=True)
-        out = F.adaptive_avg_pool2d(out, (1,1)).view(features.size(0),-1)
+        out = F.adaptive_avg_pool2d(out, (1,1)).view(features.size(0), -1)
         out = self.classifier(out)
         return out
 
