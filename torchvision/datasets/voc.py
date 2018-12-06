15fc44a421857d1b3975bbed6919bae44d6bb6e7
Benjamin Pinaya <bpg_92@hotmail.com>
2018-12-06 12:19:26 +0100

    Removed getBB, added parse_voc_xml, variable naming change.

--- torchvision/datasets/voc.py
+++ torchvision/datasets/voc.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import tarfile
+import collections
 import torch.utils.data as data
 if sys.version_info[0] == 2:
     import xml.etree.cElementTree as ET
@@ -86,38 +87,32 @@ class VOCSegmentation(data.Dataset):
         self.transform = transform
         self.target_transform = target_transform
         self.image_set = image_set
-        _base_dir = DATASET_YEAR_DICT[year]['base_dir']
-        _voc_root = os.path.join(self.root, _base_dir)
-        _image_dir = os.path.join(_voc_root, 'JPEGImages')
-        _mask_dir = os.path.join(_voc_root, 'SegmentationClass')
+        base_dir = DATASET_YEAR_DICT[year]['base_dir']
+        voc_root = os.path.join(self.root, base_dir)
+        image_dir = os.path.join(voc_root, 'JPEGImages')
+        mask_dir = os.path.join(voc_root, 'SegmentationClass')
 
         if download:
             download_extract(self.url, self.root, self.filename, self.md5)
 
-        if not os.path.isdir(_voc_root):
+        if not os.path.isdir(voc_root):
             raise RuntimeError('Dataset not found or corrupted.' +
                                ' You can use download=True to download it')
 
-        _splits_dir = os.path.join(_voc_root, 'ImageSets/Segmentation')
+        splits_dir = os.path.join(voc_root, 'ImageSets/Segmentation')
 
-        _split_f = os.path.join(_splits_dir, image_set.rstrip('\n') + '.txt')
+        split_f = os.path.join(splits_dir, image_set.rstrip('\n') + '.txt')
 
-        if not os.path.exists(_split_f):
+        if not os.path.exists(split_f):
             raise ValueError(
                 'Wrong image_set entered! Please use image_set="train" '
                 'or image_set="trainval" or image_set="val"')
 
-        self.images = []
-        self.masks = []
-        with open(os.path.join(_split_f), "r") as lines:
-            for line in lines:
-                _image = os.path.join(_image_dir, line.rstrip('\n') + ".jpg")
-                _mask = os.path.join(_mask_dir, line.rstrip('\n') + ".png")
-                assert os.path.isfile(_image)
-                assert os.path.isfile(_mask)
-                self.images.append(_image)
-                self.masks.append(_mask)
+        with open(os.path.join(split_f), "r") as f:
+            file_names = [x.strip() for x in f.readlines()]
 
+        self.images = [os.path.join(image_dir, x + ".jpg") for x in file_names]
+        self.masks = [os.path.join(mask_dir, x + ".png") for x in file_names]
         assert (len(self.images) == len(self.masks))
 
     def __getitem__(self, index):
@@ -128,16 +123,16 @@ class VOCSegmentation(data.Dataset):
         Returns:
             tuple: (image, target) where target is the image segmentation.
         """
-        _img = Image.open(self.images[index]).convert('RGB')
-        _target = Image.open(self.masks[index])
+        img = Image.open(self.images[index]).convert('RGB')
+        target = Image.open(self.masks[index])
 
         if self.transform is not None:
-            _img = self.transform(_img)
+            img = self.transform(img)
 
         if self.target_transform is not None:
-            _target = self.target_transform(_target)
+            target = self.target_transform(target)
 
-        return _img, _target
+        return img, target
 
     def __len__(self):
         return len(self.images)
@@ -155,10 +150,9 @@ class VOCDetection(data.Dataset):
             downloaded again.
         class_to_ind (dict, optional): dictionary lookup of classnames -> indexes
             (default: alphabetic indexing of VOC's 20 classes).
-        keep_difficult (boolean, optional): keep difficult instances or not.
         transform (callable, optional): A function/transform that  takes in an PIL image
             and returns a transformed version. E.g, ``transforms.RandomCrop``
-        target_transform (callable, optional): A function/transform that takes in the
+        target_transform (callable, required): A function/transform that takes in the
             target and transforms it.
     """
 
@@ -168,7 +162,6 @@ class VOCDetection(data.Dataset):
                  image_set='train',
                  download=False,
                  class_to_ind=None,
-                 keep_difficult=False,
                  transform=None,
                  target_transform=None):
         self.root = root
@@ -181,41 +174,33 @@ class VOCDetection(data.Dataset):
         self.image_set = image_set
         self.class_to_ind = class_to_ind or dict(
             zip(VOC_CLASSES, range(len(VOC_CLASSES))))
-        self.keep_difficult = keep_difficult
-        _base_dir = DATASET_YEAR_DICT[year]['base_dir']
-        _voc_root = os.path.join(self.root, _base_dir)
-        _image_dir = os.path.join(_voc_root, 'JPEGImages')
-        _annotation_dir = os.path.join(_voc_root, 'Annotations')
+        base_dir = DATASET_YEAR_DICT[year]['base_dir']
+        voc_root = os.path.join(self.root, base_dir)
+        image_dir = os.path.join(voc_root, 'JPEGImages')
+        annotation_dir = os.path.join(voc_root, 'Annotations')
 
         if download:
             download_extract(self.url, self.root, self.filename, self.md5)
 
-        if not os.path.isdir(_voc_root):
+        if not os.path.isdir(voc_root):
             raise RuntimeError('Dataset not found or corrupted.' +
                                ' You can use download=True to download it')
 
-        _splits_dir = os.path.join(_voc_root, 'ImageSets/Main')
+        splits_dir = os.path.join(voc_root, 'ImageSets/Main')
 
-        _split_f = os.path.join(_splits_dir, image_set.rstrip('\n') + '.txt')
+        split_f = os.path.join(splits_dir, image_set.rstrip('\n') + '.txt')
 
-        if not os.path.exists(_split_f):
+        if not os.path.exists(split_f):
             raise ValueError(
                 'Wrong image_set entered! Please use image_set="train" '
                 'or image_set="trainval" or image_set="val" or a valid'
                 'image_set from the VOC ImageSets/Main folder.')
 
-        self.images = []
-        self.annotations = []
-        with open(os.path.join(_split_f), "r") as lines:
-            for line in lines:
-                _image = os.path.join(_image_dir, line.rstrip('\n') + ".jpg")
-                _annotation = os.path.join(_annotation_dir,
-                                           line.rstrip('\n') + ".xml")
-                assert os.path.isfile(_image)
-                assert os.path.isfile(_annotation)
-                self.images.append(_image)
-                self.annotations.append(_annotation)
+        with open(os.path.join(split_f), "r") as f:
+            file_names = [x.strip() for x in f.readlines()]
 
+        self.images = [os.path.join(image_dir, x + ".jpg") for x in file_names]
+        self.annotations = [os.path.join(annotation_dir, x + ".xml") for x in file_names]
         assert (len(self.images) == len(self.annotations))
 
     def __getitem__(self, index):
@@ -224,44 +209,41 @@ class VOCDetection(data.Dataset):
             index (int): Index
 
         Returns:
-            tuple: (image, target) where target is a list of bounding boxes of
-                relative coordinates like``[[xmin, ymin, xmax, ymax, ind], [...], ...]``.
+            tuple: (image, target) where target is a dictionary of the XML tree.
         """
-        _img = Image.open(self.images[index]).convert('RGB')
-        _target = self._get_bboxes(ET.parse(self.annotations[index]).getroot())
+        img = Image.open(self.images[index]).convert('RGB')
+        target = self.parse_voc_xml(
+            ET.parse(self.annotations[index]).getroot())
 
         if self.transform is not None:
-            _img = self.transform(_img)
+            img = self.transform(img)
 
         if self.target_transform is not None:
-            _target = self.target_transform(_target)
+            target = self.target_transform(target)
 
-        return _img, _target
+        return img, target
 
     def __len__(self):
         return len(self.images)
 
-    def _get_bboxes(self, target):
-        res = []
-        for obj in target.iter('object'):
-            difficult = int(obj.find('difficult').text) == 1
-            if not self.keep_difficult and difficult:
-                continue
-            name = obj.find('name').text.lower().strip()
-            bbox = obj.find('bndbox')
-            width = int(target.find('size').find('width').text)
-            height = int(target.find('size').find('height').text)
-            bndbox = []
-            for i, cur_bb in enumerate(bbox):
-                bb_sz = int(cur_bb.text) - 1
-                # relative coordinates
-                bb_sz = bb_sz / width if i % 2 == 0 else bb_sz / height
-                bndbox.append(bb_sz)
-
-            label_ind = self.class_to_ind[name]
-            bndbox.append(label_ind)
-            res.append(bndbox)  # [xmin, ymin, xmax, ymax, ind]
-        return res
+    def parse_voc_xml(self, node):
+        voc_dict = {}
+        children = list(node)
+        if children:
+            def_dic = collections.defaultdict(list)
+            for dc in map(self.parse_voc_xml, children):
+                for ind, v in dc.items():
+                    def_dic[ind].append(v)
+            voc_dict = {
+                node.tag:
+                {ind: v[0] if len(v) == 1 else v
+                 for ind, v in def_dic.items()}
+            }
+        if node.text:
+            text = node.text.strip()
+            if not children:
+                voc_dict[node.tag] = text
+        return voc_dict
 
 
 def download_extract(url, root, filename, md5):
