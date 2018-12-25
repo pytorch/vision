8ce007047f2d988c52e31c33039c78580a10cf05
hysts <hysts@users.noreply.github.com>
2018-12-25 22:45:49 +0900

    Add KMNIST dataset (#687)
    * Add KMNIST
    
    * Add documentation
    
    * Add KMNIST to module's __all__ list

--- torchvision/datasets/__init__.py
+++ torchvision/datasets/__init__.py
@@ -3,7 +3,7 @@ from .folder import ImageFolder, DatasetFolder
 from .coco import CocoCaptions, CocoDetection
 from .cifar import CIFAR10, CIFAR100
 from .stl10 import STL10
-from .mnist import MNIST, EMNIST, FashionMNIST
+from .mnist import MNIST, EMNIST, FashionMNIST, KMNIST
 from .svhn import SVHN
 from .phototour import PhotoTour
 from .fakedata import FakeData
@@ -18,6 +18,6 @@ __all__ = ('LSUN', 'LSUNClass',
            'ImageFolder', 'DatasetFolder', 'FakeData',
            'CocoCaptions', 'CocoDetection',
            'CIFAR10', 'CIFAR100', 'EMNIST', 'FashionMNIST',
-           'MNIST', 'STL10', 'SVHN', 'PhotoTour', 'SEMEION',
+           'MNIST', 'KMNIST', 'STL10', 'SVHN', 'PhotoTour', 'SEMEION',
            'Omniglot', 'SBU', 'Flickr8k', 'Flickr30k',
            'VOCSegmentation', 'VOCDetection', 'Cityscapes')
