813576b473817ca10c39a2f3a869873708089aa0
ekka <ekagra.ranjan@gmail.com>
2019-02-14 18:17:02 +0530

    Updated inceptionV3 to accept different sized images (Adaptive avg pool) (#744)
    * Updated inceptionV3 to accept different sized images (Adaptive avg pool)
    
    The update allows inceptionV3 to process images larger or smaller than prescribed image size (299x299) using adaptive average pooling. Will be useful while finetuning or testing on different resolution images.
    
    * Update inception.py

--- torchvision/models/inception.py
+++ torchvision/models/inception.py
@@ -118,7 +118,8 @@ class Inception3(nn.Module):
         # 8 x 8 x 2048
         x = self.Mixed_7c(x)
         # 8 x 8 x 2048
-        x = F.avg_pool2d(x, kernel_size=8)
+        # Adaptive average pooling
+        x = F.adaptive_avg_pool2d(x, (1, 1))
         # 1 x 1 x 2048
         x = F.dropout(x, training=self.training)
         # 1 x 1 x 2048
@@ -310,6 +311,9 @@ class InceptionAux(nn.Module):
         x = self.conv0(x)
         # 5 x 5 x 128
         x = self.conv1(x)
+        # 1 x 1 x 768
+        # Adaptive average pooling
+        x = F.adaptive_avg_pool2d(x, (1, 1))
         # 1 x 1 x 768
         x = x.view(x.size(0), -1)
         # 768
