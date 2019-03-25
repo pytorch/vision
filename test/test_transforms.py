04c70ca03f88c737e3987b673c2b34deff0431b7
ekka <ekagra.ranjan@gmail.com>
2019-03-25 18:06:55 +0530

    Restore test_linear_transformation()

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -952,6 +952,28 @@ class Tester(unittest.TestCase):
         # Checking if ColorJitter can be printed as string
         color_jitter.__repr__()
 
+    def test_linear_transformation(self):
+        x = torch.randn(250, 10, 10, 3)
+        flat_x = x.view(x.size(0), x.size(1) * x.size(2) * x.size(3))
+        # compute principal components
+        sigma = torch.mm(flat_x.t(), flat_x) / flat_x.size(0)
+        u, s, _ = np.linalg.svd(sigma.numpy())
+        zca_epsilon = 1e-10  # avoid division by 0
+        d = torch.Tensor(np.diag(1. / np.sqrt(s + zca_epsilon)))
+        u = torch.Tensor(u)
+        principal_components = torch.mm(torch.mm(u, d), u.t())
+        # initialize whitening matrix
+        whitening = transforms.LinearTransformation(principal_components)
+        # pass first vector
+        xwhite = whitening(x[0].view(10, 10, 3))
+        # estimate covariance
+        xwhite = xwhite.view(1, 300).numpy()
+        cov = np.dot(xwhite, xwhite.T) / x.size(0)
+        assert np.allclose(cov, np.identity(1), rtol=1e-3)
+
+        # Checking if LinearTransformation can be printed as string
+        whitening.__repr__()
+
     def test_affine_transformation(self):
         num_samples = 1000
         x = torch.randn(num_samples, 3, 10, 10)
