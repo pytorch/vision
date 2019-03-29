b66ad34519dfea94b598bbc24784b52aa606a8eb
Michael Kösel <michaelkoesel@gmx.de>
2019-03-29 12:26:32 +0100

    don't use legacy code

--- torchvision/models/googlenet.py
+++ torchvision/models/googlenet.py
@@ -76,9 +76,10 @@ class GoogLeNet(nn.Module):
             if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
                 import scipy.stats as stats
                 X = stats.truncnorm(-2, 2, scale=0.01)
-                values = torch.Tensor(X.rvs(m.weight.numel()))
+                values = torch.as_tensor(X.rvs(m.weight.numel()), dtype=m.weight.dtype)
                 values = values.view(m.weight.size())
-                m.weight.data.copy_(values)
+                with torch.no_grad():
+                    m.weight.copy_(values)
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
