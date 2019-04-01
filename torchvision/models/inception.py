e2a0f20ae463dad52e2b77c0e4f6929d48b14b1c
Sepehr Sameni <Sepehr.Sameni@gmail.com>
2019-04-01 11:31:09 +0430

    add aux_logits support to inception
    it is related to pytorch/pytorch#18668

--- torchvision/models/inception.py
+++ torchvision/models/inception.py
@@ -29,8 +29,17 @@ def inception_v3(pretrained=False, **kwargs):
     if pretrained:
         if 'transform_input' not in kwargs:
             kwargs['transform_input'] = True
+        if 'aux_logits' in kwargs:
+            original_aux_logits = kwargs['aux_logits']
+            kwargs['aux_logits'] = True
+        else:
+            original_aux_logits = True
         model = Inception3(**kwargs)
         model.load_state_dict(model_zoo.load_url(model_urls['inception_v3_google']))
+        if not original_aux_logits:
+            kwargs['aux_logits'] = original_aux_logits
+            model.aux_logits = original_aux_logits
+            del model.AuxLogits
         return model
 
     return Inception3(**kwargs)
