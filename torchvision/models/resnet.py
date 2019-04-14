adaed2ac92db5403e6218d77be8b44bbdff0ae7e
Ross Wightman <rwightman@gmail.com>
2019-04-14 00:53:20 -0700

    Fix Python 2.x integer div issue

--- torchvision/models/resnet.py
+++ torchvision/models/resnet.py
@@ -72,7 +72,7 @@ class Bottleneck(nn.Module):
         super(Bottleneck, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
-        width = int(planes * (base_width / 64)) * groups
+        width = int(planes * (base_width / 64.)) * groups
         # Both self.conv2 and self.downsample layers downsample the input when stride != 1
         self.conv1 = conv1x1(inplanes, width)
         self.bn1 = norm_layer(width)
