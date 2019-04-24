d79f37326f30c8b4cd8336275c2d9e70a70088c5
Dhananjay <cs17btech11014@iith.ac.in>
2019-04-24 22:29:24 +0530

    added is_valid_file option

--- torchvision/datasets/folder.py
+++ torchvision/datasets/folder.py
@@ -32,20 +32,32 @@ def is_image_file(filename):
     return has_file_allowed_extension(filename, IMG_EXTENSIONS)
 
 
-def make_dataset(dir, class_to_idx, extensions):
+def make_dataset(dir, class_to_idx, extensions, is_valid_file=None):
     images = []
     dir = os.path.expanduser(dir)
+    if extensions == None and is_valid_file == None:
+        raise ValueError("Both extensions and is_valid_file cannot be None")
+    if extensions != None and is_valid_file != None:
+        raise ValueError("One of the extensions and is_valid_file should be None")
+
     for target in sorted(class_to_idx.keys()):
         d = os.path.join(dir, target)
         if not os.path.isdir(d):
             continue
-
-        for root, _, fnames in sorted(os.walk(d)):
-            for fname in sorted(fnames):
-                if has_file_allowed_extension(fname, extensions):
+        if(extensions != None):
+            for root, _, fnames in sorted(os.walk(d)):
+                for fname in sorted(fnames):
+                    if has_file_allowed_extension(fname, extensions):
+                        path = os.path.join(root, fname)
+                        item = (path, class_to_idx[target])
+                        images.append(item)
+        else:
+            for root, _, fnames in sorted(os.walk(d)):
+                for fname in sorted(fnames):
                     path = os.path.join(root, fname)
-                    item = (path, class_to_idx[target])
-                    images.append(item)
+                    if is_valid_file(path):
+                        item = (path, class_to_idx[target])
+                        images.append(item)
 
     return images
 
@@ -65,11 +77,15 @@ class DatasetFolder(VisionDataset):
         root (string): Root directory path.
         loader (callable): A function to load a sample given its path.
         extensions (tuple[string]): A list of allowed extensions.
+            both extensions and is_valid_file should not be passed.
         transform (callable, optional): A function/transform that takes in
             a sample and returns a transformed version.
             E.g, ``transforms.RandomCrop`` for images.
         target_transform (callable, optional): A function/transform that takes
             in the target and transforms it.
+        is_valid_file (callable, optional): A function that takes path of a Image file
+            and check if the file is a valid_file (used to check of corrupt files).
+            both extensions and is_valid_file should not be passed.
 
      Attributes:
         classes (list): List of the class names.
@@ -78,12 +94,12 @@ class DatasetFolder(VisionDataset):
         targets (list): The class_index value for each image in the dataset
     """
 
-    def __init__(self, root, loader, extensions, transform=None, target_transform=None):
+    def __init__(self, root, loader, extensions=None, transform=None, target_transform=None, is_valid_file=None):
         super(DatasetFolder, self).__init__(root)
         self.transform = transform
         self.target_transform = target_transform
         classes, class_to_idx = self._find_classes(self.root)
-        samples = make_dataset(self.root, class_to_idx, extensions)
+        samples = make_dataset(self.root, class_to_idx, extensions, is_valid_file)
         if len(samples) == 0:
             raise (RuntimeError("Found 0 files in subfolders of: " + self.root + "\n"
                                 "Supported extensions are: " + ",".join(extensions)))
@@ -184,6 +200,8 @@ class ImageFolder(DatasetFolder):
         target_transform (callable, optional): A function/transform that takes in the
             target and transforms it.
         loader (callable, optional): A function to load an image given its path.
+        is_valid_file (callable, optional): A function that takes path of a Image file
+            and check if the file is a valid_file (used to check of corrupt files)
 
      Attributes:
         classes (list): List of the class names.
@@ -192,8 +210,13 @@ class ImageFolder(DatasetFolder):
     """
 
     def __init__(self, root, transform=None, target_transform=None,
-                 loader=default_loader):
-        super(ImageFolder, self).__init__(root, loader, IMG_EXTENSIONS,
+                 loader=default_loader, is_valid_file=None):
+        if(is_valid_file==None):
+            super(ImageFolder, self).__init__(root, loader, IMG_EXTENSIONS,
+                                          transform=transform,
+                                          target_transform=target_transform)
+        else:
+            super(ImageFolder, self).__init__(root, loader, None, is_valid_file
                                           transform=transform,
                                           target_transform=target_transform)
         self.imgs = self.samples
