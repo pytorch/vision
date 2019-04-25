3b6a8a7587460a578133773edd5f72adb8dbe46b
Philip Meier <gamesnmore@online.de>
2019-04-25 13:42:40 +0200

    stylistic changes

--- torchvision/datasets/utils.py
+++ torchvision/datasets/utils.py
@@ -17,7 +17,7 @@ def gen_bar_updater():
     return bar_update
 
 
-def calc_md5(fpath, chunk_size=1024*1024):
+def calculate_md5(fpath, chunk_size=1024*1024):
     md5 = hashlib.md5()
     with open(fpath, 'rb') as f:
         for chunk in iter(lambda: f.read(chunk_size), b''):
@@ -26,17 +26,16 @@ def calc_md5(fpath, chunk_size=1024*1024):
 
 
 def check_md5(fpath, md5, **kwargs):
-    return md5 == calc_md5(fpath, **kwargs)
+    return md5 == calculate_md5(fpath, **kwargs)
 
 
 def check_integrity(fpath, md5=None):
     if not os.path.isfile(fpath):
         return False
+    if md5 is None:
+        return True
     else:
-        if md5 is None:
-            return True
-        else:
-            return check_md5(fpath, md5)
+        return check_md5(fpath, md5)
 
 
 def makedir_exist_ok(dirpath):
