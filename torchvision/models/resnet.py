ce18507b9a41b21aa49bf99c23ac287ebd536ff0
bddppq <baidingding7@gmail.com>
2019-05-07 02:23:41 -0700

    Change usage of "view" to "reshape" in resnet to enable running with mkldnn (#890)

--- torchvision/models/resnet.py
+++ torchvision/models/resnet.py
@@ -198,7 +198,7 @@ class ResNet(nn.Module):
         x = self.layer4(x)
 
         x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
+        x = x.reshape(x.size(0), -1)
         x = self.fc(x)
 
         return x
