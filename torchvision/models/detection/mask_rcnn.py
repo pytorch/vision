2d54f702c4881166184b78fc8f07aaca2db84313
Francisco Massa <fvsmassa@gmail.com>
2019-05-14 02:25:48 -0700

    Zero initialize bias for mask heads

--- torchvision/models/detection/mask_rcnn.py
+++ torchvision/models/detection/mask_rcnn.py
@@ -300,6 +300,8 @@ class MaskRCNNHeads(nn.Sequential):
         for name, param in self.named_parameters():
             if "weight" in name:
                 nn.init.kaiming_normal_(param, mode="fan_out", nonlinearity="relu")
+            elif "bias" in name:
+                nn.init.constant_(param, 0)
 
 
 class MaskRCNNC4Predictor(nn.Sequential):
@@ -313,6 +315,8 @@ class MaskRCNNC4Predictor(nn.Sequential):
         for name, param in self.named_parameters():
             if "weight" in name:
                 nn.init.kaiming_normal_(param, mode="fan_out", nonlinearity="relu")
+            elif "bias" in name:
+                nn.init.constant_(param, 0)
 
 
 class KeypointRCNNHeads(nn.Sequential):
