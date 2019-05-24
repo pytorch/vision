3e5e03d4f27d8120181074c875193c1d5bd27bdf
Koen van de Sande <koen@tibed.net>
2019-05-24 12:16:47 +0200

    Add test for zip lookup class

--- test/test_datasets_utils.py
+++ test/test_datasets_utils.py
@@ -3,6 +3,7 @@ import shutil
 import tempfile
 import torchvision.datasets.utils as utils
 import unittest
+import zipfile
 
 TEST_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                          'assets', 'grace_hopper_517x606.jpg')
@@ -40,6 +41,31 @@ class Tester(unittest.TestCase):
         utils.download_url(url, temp_dir)
         assert not len(os.listdir(temp_dir)) == 0, 'The downloaded root directory is empty after download.'
         shutil.rmtree(temp_dir)
+        
+    def test_ziplookup(self):
+        temp_dir = tempfile.mkdtemp()
+        temp_filename = os.path.join(temp_dir, "ziplookup.zip")
+        try:
+            z = zipfile.ZipFile(temp_filename, "w", zipfile.ZIP_STORED, allowZip64=True)
+            z.write(TEST_FILE, "hopper.jpg")
+            z.write(TEST_FILE)
+            z.write(TEST_FILE, "hopper79.jpg")
+            z.write(TEST_FILE, "somepath/hopper.jpg")
+            z.close()
+            
+            lookup = utils.ForkSafeZipLookup(temp_filename)
+            f = lookup["hopper.jpg"]
+            assert f.name.endswith(".jpg")
+            f = lookup["somepath/hopper.jpg"]
+            assert f.name.endswith(".jpg")
+            try:
+                f = lookup["does_not_exist.jpg"]
+                assert False, "Should not return something for non-existant file"
+            except KeyError:
+                pass
+            del lookup
+        finally:
+            shutil.rmtree(temp_dir)
 
 
 if __name__ == '__main__':
