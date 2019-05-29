c59f0474a68c53ae567e548d5beeee9820050044
Francisco Massa <fvsmassa@gmail.com>
2019-05-29 14:50:13 +0200

    [WIP] Add tests for datasets (#966)
    * WIP
    
    * WIP: minor improvements
    
    * Add tests
    
    * Fix typo
    
    * Use download_and_extract on caltech, cifar and omniglot
    
    * Add a print message during extraction
    
    * Remove EMNIST from test

---
+++ test/test_datasets.py
@@ -0,0 +1,38 @@
+import PIL
+import shutil
+import tempfile
+import unittest
+
+import torchvision
+
+
+class Tester(unittest.TestCase):
+
+    def test_mnist(self):
+        tmp_dir = tempfile.mkdtemp()
+        dataset = torchvision.datasets.MNIST(tmp_dir, download=True)
+        self.assertEqual(len(dataset), 60000)
+        img, target = dataset[0]
+        self.assertTrue(isinstance(img, PIL.Image.Image))
+        self.assertTrue(isinstance(target, int))
+        shutil.rmtree(tmp_dir)
+
+    def test_kmnist(self):
+        tmp_dir = tempfile.mkdtemp()
+        dataset = torchvision.datasets.KMNIST(tmp_dir, download=True)
+        img, target = dataset[0]
+        self.assertTrue(isinstance(img, PIL.Image.Image))
+        self.assertTrue(isinstance(target, int))
+        shutil.rmtree(tmp_dir)
+
+    def test_fashionmnist(self):
+        tmp_dir = tempfile.mkdtemp()
+        dataset = torchvision.datasets.FashionMNIST(tmp_dir, download=True)
+        img, target = dataset[0]
+        self.assertTrue(isinstance(img, PIL.Image.Image))
+        self.assertTrue(isinstance(target, int))
+        shutil.rmtree(tmp_dir)
+
+
+if __name__ == '__main__':
+    unittest.main()
