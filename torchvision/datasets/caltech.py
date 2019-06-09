pytorch/vision

commit 7716aba57e6e12a544c42136b274508955526163

[WIP] Add test for ImageNet  (#976)
* added fake data

* fixed fake data

* renamed extract and download methods and added functionality

* added raw fake data

* refactored imagenet and added test

* flake8

* added fake devkit and mocked download_url

* reversed uncommenting

* added mock to CI

* fixed tests for imagefolder

* flake8

torchvision/datasets/caltech.py
@@ -4,7 +4,7 @@ import os
 import os.path
 
 from .vision import VisionDataset
-from .utils import download_and_extract, makedir_exist_ok
+from .utils import download_and_extract_archive, makedir_exist_ok
 
 
 class Caltech101(VisionDataset):
@@ -113,12 +113,12 @@ class Caltech101(VisionDataset):
             print('Files already downloaded and verified')
             return
 
-        download_and_extract(
+        download_and_extract_archive(
             "http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz",
             self.root,
             "101_ObjectCategories.tar.gz",
             "b224c7392d521a49829488ab0f1120d9")
-        download_and_extract(
+        download_and_extract_archive(
             "http://www.vision.caltech.edu/Image_Datasets/Caltech101/Annotations.tar",
             self.root,
             "101_Annotations.tar",
@@ -201,7 +201,7 @@ class Caltech256(VisionDataset):
             print('Files already downloaded and verified')
             return
 
-        download_and_extract(
+        download_and_extract_archive(
             "http://www.vision.caltech.edu/Image_Datasets/Caltech256/256_ObjectCategories.tar",
             self.root,
             "256_ObjectCategories.tar",

torchvision/datasets/cifar.py
@@ -11,7 +11,7 @@ else:
     import pickle
 
 from .vision import VisionDataset
-from .utils import check_integrity, download_and_extract
+from .utils import check_integrity, download_and_extract_archive
 
 
 class CIFAR10(VisionDataset):
@@ -147,7 +147,7 @@ class CIFAR10(VisionDataset):
         if self._check_integrity():
             print('Files already downloaded and verified')
             return
-        download_and_extract(self.url, self.root, self.filename, self.tgz_md5)
+        download_and_extract_archive(self.url, self.root, self.filename, self.tgz_md5)
 
     def extra_repr(self):
         return "Split: {}".format("Train" if self.train is True else "Test")

torchvision/datasets/omniglot.py
@@ -3,7 +3,7 @@ from PIL import Image
 from os.path import join
 import os
 from .vision import VisionDataset
-from .utils import download_and_extract, check_integrity, list_dir, list_files
+from .utils import download_and_extract_archive, check_integrity, list_dir, list_files
 
 
 class Omniglot(VisionDataset):
@@ -88,7 +88,7 @@ class Omniglot(VisionDataset):
         filename = self._get_target_folder()
         zip_filename = filename + '.zip'
         url = self.download_url_prefix + '/' + zip_filename
-        download_and_extract(url, self.root, zip_filename, self.zips_md5[filename])
+        download_and_extract_archive(url, self.root, zip_filename, self.zips_md5[filename])
 
     def _get_target_folder(self):
         return 'images_background' if self.background else 'images_evaluation'

torchvision/datasets/stl10.py
@@ -5,7 +5,7 @@ import os.path
 import numpy as np
 
 from .vision import VisionDataset
-from .utils import check_integrity, download_and_extract
+from .utils import check_integrity, download_and_extract_archive
 
 
 class STL10(VisionDataset):
@@ -152,7 +152,7 @@ class STL10(VisionDataset):
         if self._check_integrity():
             print('Files already downloaded and verified')
             return
-        download_and_extract(self.url, self.root, self.filename, self.tgz_md5)
+        download_and_extract_archive(self.url, self.root, self.filename, self.tgz_md5)
 
     def extra_repr(self):
         return "Split: {split}".format(**self.__dict__)

.travis.yml
@@ -34,6 +34,7 @@ before_install:
     fi
   - pip install future
   - pip install pytest pytest-cov codecov
+  - pip install mock
 
 
 install:

commit 19ae7ac43d923cb71be02bd60a27b2cec860d11d

Fix calls to 'download_and_extract_archive()'
Changes made in 7716aba57 broke calls to this method.

torchvision/datasets/caltech.py
@@ -116,13 +116,13 @@ class Caltech101(VisionDataset):
         download_and_extract_archive(
             "http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz",
             self.root,
-            "101_ObjectCategories.tar.gz",
-            "b224c7392d521a49829488ab0f1120d9")
+            filename="101_ObjectCategories.tar.gz",
+            md5="b224c7392d521a49829488ab0f1120d9")
         download_and_extract_archive(
             "http://www.vision.caltech.edu/Image_Datasets/Caltech101/Annotations.tar",
             self.root,
-            "101_Annotations.tar",
-            "6f83eeb1f24d99cab4eb377263132c91")
+            filename="101_Annotations.tar",
+            md5="6f83eeb1f24d99cab4eb377263132c91")
 
     def extra_repr(self):
         return "Target type: {target_type}".format(**self.__dict__)
@@ -204,5 +204,5 @@ class Caltech256(VisionDataset):
         download_and_extract_archive(
             "http://www.vision.caltech.edu/Image_Datasets/Caltech256/256_ObjectCategories.tar",
             self.root,
-            "256_ObjectCategories.tar",
-            "67b4f42ca05d46448c6bb8ecd2220f6d")
+            filename="256_ObjectCategories.tar",
+            md5="67b4f42ca05d46448c6bb8ecd2220f6d")

torchvision/datasets/cifar.py
@@ -147,7 +147,7 @@ class CIFAR10(VisionDataset):
         if self._check_integrity():
             print('Files already downloaded and verified')
             return
-        download_and_extract_archive(self.url, self.root, self.filename, self.tgz_md5)
+        download_and_extract_archive(self.url, self.root, filename=self.filename, md5=self.tgz_md5)
 
     def extra_repr(self):
         return "Split: {}".format("Train" if self.train is True else "Test")

torchvision/datasets/omniglot.py
@@ -88,7 +88,7 @@ class Omniglot(VisionDataset):
         filename = self._get_target_folder()
         zip_filename = filename + '.zip'
         url = self.download_url_prefix + '/' + zip_filename
-        download_and_extract_archive(url, self.root, zip_filename, self.zips_md5[filename])
+        download_and_extract_archive(url, self.root, filename=zip_filename, md5=self.zips_md5[filename])
 
     def _get_target_folder(self):
         return 'images_background' if self.background else 'images_evaluation'

torchvision/datasets/stl10.py
@@ -152,7 +152,7 @@ class STL10(VisionDataset):
         if self._check_integrity():
             print('Files already downloaded and verified')
             return
-        download_and_extract_archive(self.url, self.root, self.filename, self.tgz_md5)
+        download_and_extract_archive(self.url, self.root, filename=self.filename, md5=self.tgz_md5)
 
     def extra_repr(self):
         return "Split: {split}".format(**self.__dict__)