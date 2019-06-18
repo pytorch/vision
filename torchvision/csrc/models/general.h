a82e602f9f8c2377940d1714bf764f4994ba377e
Phil Noonan <philip.noonan@outlook.com>
2019-06-18 11:55:57 +0100

    added macro to allow for exported symbols on windows

--- torchvision/csrc/models/general.h
+++ torchvision/csrc/models/general.h
@@ -1,16 +1,10 @@
 #ifndef VISION_GENERAL_H
 #define VISION_GENERAL_H
 
-namespace vision {
-namespace models {
-
 #ifdef _WIN32
 #define VISION_API __declspec(dllexport)
 #else
 #define VISION_API
 #endif
-
-} // namespace models
-} // namespace vision
 
 #endif // VISION_GENERAL_H
