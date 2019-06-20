2830ade613505fd5a9af19976d9870810314018f
Geovanni Zhang <850734033@qq.com>
2019-06-20 19:39:56 +0800

    fix:wrong random matrix

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -429,11 +429,11 @@ class Tester(unittest.TestCase):
         trans = transforms.ToTensor()
 
         with self.assertRaises(TypeError):
-            trans(np.array([1, height, width]).tolist())
+            trans(np.random.rand(1, height, width).tolist())
 
         with self.assertRaises(ValueError):
-            trans(np.array([height]))
-            trans(np.array([1, 1, height, width]))
+            trans(np.random.rand(height))
+            trans(np.random.rand(1, 1, height, width))
 
         for channels in test_channels:
             input_data = torch.ByteTensor(channels, height, width).random_(0, 255).float().div_(255)
