2b6da28c15b7b5ac6fc00b11a28908b5342a99a6
Sergey Zagoruyko <zagoruyko2@gmail.com>
2019-06-26 12:15:33 +0300

    Add pretrained Wide ResNet (#912)
    * add wide resnet
    
    * add docstring for wide resnet
    
    * update WRN-50-2 model
    
    * add docs
    
    * extend WRN docstring
    
    * use pytorch storage for WRN
    
    * fix rebase
    
    * fix typo in docs

--- hubconf.py
+++ hubconf.py
@@ -5,7 +5,7 @@ from torchvision.models.alexnet import alexnet
 from torchvision.models.densenet import densenet121, densenet169, densenet201, densenet161
 from torchvision.models.inception import inception_v3
 from torchvision.models.resnet import resnet18, resnet34, resnet50, resnet101, resnet152,\
-    resnext50_32x4d, resnext101_32x8d
+    resnext50_32x4d, resnext101_32x8d, wide_resnet50_2, wide_resnet101_2
 from torchvision.models.squeezenet import squeezenet1_0, squeezenet1_1
 from torchvision.models.vgg import vgg11, vgg13, vgg16, vgg19, vgg11_bn, vgg13_bn, vgg16_bn, vgg19_bn
 from torchvision.models.segmentation import fcn_resnet101, deeplabv3_resnet101
