e996c3670f6300b2a1064e791f8c311a05bbea20
1e100 <38598618+1e100@users.noreply.github.com>
2019-06-29 22:53:00 -0700

    Restore initialization

--- torchvision/models/mnasnet.py
+++ torchvision/models/mnasnet.py
@@ -132,6 +132,21 @@ class MNASNet(torch.nn.Module):
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+"""
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode="fan_out",
                                         nonlinearity="relu")
                 if m.bias is not None:
@@ -142,6 +157,7 @@ class MNASNet(torch.nn.Module):
             elif isinstance(m, nn.Linear):
                 nn.init.normal_(m.weight, 0.01)
                 nn.init.zeros_(m.bias)
+"""
 
 
 def _load_pretrained(model_name, model, progress):
