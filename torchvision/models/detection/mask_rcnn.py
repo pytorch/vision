a18c9df532e4c3ab1aaad25991fbd0b3b21655d9
buoyancy99 <buoyuan99@gmail.com>
2019-07-12 00:40:23 -0700

    Update max size to ensure divisible by 32 after rescaling

--- torchvision/models/detection/mask_rcnn.py
+++ torchvision/models/detection/mask_rcnn.py
@@ -150,7 +150,7 @@ class MaskRCNN(FasterRCNN):
     """
     def __init__(self, backbone, num_classes=None,
                  # transform parameters
-                 min_size=800, max_size=1333,
+                 min_size=800, max_size=1312,
                  image_mean=None, image_std=None,
                  # RPN parameters
                  rpn_anchor_generator=None, rpn_head=None,
