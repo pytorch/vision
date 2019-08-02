037ef8703446c9d5ebade99e14fda43791c00f6d
Daksh Jotwani <dakshjotwani@gmail.com>
2019-08-02 15:59:08 +0530

    Add landmark csv support

--- torchvision/datasets/vggface2.py
+++ torchvision/datasets/vggface2.py
@@ -4,6 +4,31 @@ import torchvision.transforms.functional as F
 from .folder import ImageFolder
 
 
+def read_bbox_csv(root, csv_path):
+    bb_data = {}
+    with open(csv_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        for path, x, y, w, h in reader:
+            path = os.path.join(root, path) + '.jpg'
+            bb_data[path] = (int(x), int(y), int(w), int(h))
+
+    return bb_data
+
+
+def read_landmark_csv(root, csv_path):
+    landmark_data = {}
+    with open(csv_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        for row in reader:
+            path = os.path.join(root, row[0]) + '.jpg'
+            landmarks = tuple(float(x) for x in row[1:])
+            landmark_data[path] = landmarks
+
+    return landmark_data
+
+
 class VGGFace2(ImageFolder):
     '''`VGGFace2: A large scale image dataset for face recognition
         <http://www.robots.ox.ac.uk/~vgg/data/vgg_face2/index.html>`_ Dataset.
@@ -21,13 +46,15 @@ class VGGFace2(ImageFolder):
             and returns a transformed version. E.g, ``transforms.ToTensor``.
         target_transform (callable, optional): A function/transform that takes in the target and transforms it.
         bbox_crop (boolean, optional): Crops bounding box from image as target.
-        bbox_csv (string, optional): path to downloaded bb landmarks. Required
+        bbox_csv (string, optional): path to downloaded bounding box csv. Required
             if ``bbox`` is in target_type or bb_target_crop is True.
-
+        landmark_csv (string, optional): path to downloaded landmarks csv. Required
+            if ``landmark`` is in target_type.
     '''
 
     def __init__(self, root, target_type='id', transform=None,
-                 target_transform=None, bbox_crop=False, bbox_csv=None):
+                 target_transform=None, bbox_crop=False, bbox_csv=None,
+                 landmark_csv=None):
         super(VGGFace2, self).__init__(root, transform=transform,
                                        target_transform=target_transform)
 
@@ -37,30 +64,25 @@ class VGGFace2(ImageFolder):
             self.target_type = [target_type]
 
         self.bbox_crop = bbox_crop
-        self.get_bbox = self.bbox_crop or 'bbox' in self.target_type
 
-        if self.get_bbox:
+        if self.bbox_crop or 'bbox' in self.target_type:
             if bbox_csv is None:
                 raise ValueError("bbox_csv cannot be None if 'bbox' "
                                  "in target_type or bbox_crop=True")
+            self.bb_data = read_bbox_csv(self.root, bbox_csv)
 
-            self.bb_data = {}
-            with open(bbox_csv, newline='') as csvfile:
-                reader = csv.reader(csvfile)
-                next(reader)
-                for path, x, y, w, h in reader:
-                    path = os.path.join(self.root, path) + '.jpg'
-                    self.bb_data[path] = (int(x), int(y), int(w), int(h))
+        if 'landmark' in target_type:
+            if landmark_csv is None:
+                raise ValueError("bbox_csv cannot be None if 'landmark' "
+                                 "in target_type")
+            self.landmark_data = read_landmark_csv(self.root, landmark_csv)
 
     def __getitem__(self, index):
         path, label = self.samples[index]
         sample = self.loader(path)
 
-        if self.get_bbox:
-            bbox = self.bb_data[path]
-
         if self.bbox_crop:
-            x, y, w, h = bbox
+            x, y, w, h = self.bb_data[path]
             sample = F.crop(sample, x, y, h, w)
 
         target = []
@@ -68,7 +90,9 @@ class VGGFace2(ImageFolder):
             if t == 'id':
                 target.append(label)
             elif t == 'bbox':
-                target.extend(bbox)
+                target.append(self.bb_data[path])
+            elif t == 'landmark':
+                target.append(self.landmark_data[path])
 
         if self.transform is not None:
             sample = self.transform(sample)
