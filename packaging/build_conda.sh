237f9a84a1eb6501cd6578d2d0171bc2240f49f1
Edward Z. Yang <ezyang@fb.com>
2019-08-08 12:28:32 -0700

    Refactor version suffix so conda packages don't get suffixes.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- packaging/build_conda.sh
+++ packaging/build_conda.sh
@@ -4,6 +4,7 @@ set -ex
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "$script_dir/pkg_helpers.bash"
 
+export BUILD_TYPE=conda
 setup_env 0.5.0
 export SOURCE_ROOT_DIR="$PWD"
 setup_conda_pytorch_constraint
