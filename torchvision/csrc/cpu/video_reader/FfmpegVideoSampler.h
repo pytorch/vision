6f7431089032669fa9061147822d848d4ed38ce1
zyan3 <zyan3@devgpu002.prn1.facebook.com>
2019-08-29 22:12:58 -0700

    [torchvision video reader]inception commit

---
+++ torchvision/csrc/cpu/video_reader/FfmpegVideoSampler.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "FfmpegSampler.h"
+
+/**
+ * Class transcode video frames from one format into another
+ */
+
+class FfmpegVideoSampler : public FfmpegSampler {
+ public:
+  explicit FfmpegVideoSampler(
+      const VideoFormat& in,
+      const VideoFormat& out,
+      int swsFlags = SWS_AREA);
+  ~FfmpegVideoSampler() override;
+
+  int init() override;
+
+  int32_t getImageBytes() const;
+  // returns number of bytes of the sampled data
+  std::unique_ptr<DecodedFrame> sample(const AVFrame* frame) override;
+
+  const VideoFormat& getInFormat() const {
+    return inFormat_;
+  }
+
+  const VideoFormat& getOutFormat() const {
+    return outFormat_;
+  }
+
+ private:
+  VideoFormat inFormat_;
+  VideoFormat outFormat_;
+  int swsFlags_;
+  SwsContext* scaleContext_{nullptr};
+};
