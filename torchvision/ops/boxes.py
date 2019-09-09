78f169b5e1b96871936e45ca310788f3b3e4bb78
Lara Haidar <haidar.lara@gmail.com>
2019-09-09 02:01:31 -0700

    Register Torchvision Ops as Cutom Ops (#1267)
    * Register torchvision ops
    
    * install ORT only with python 3
    
    * remane lib + address other comments
    
    * fix lint
    
    * fix lib copy
    
    * find file with pattern instead of suffix
    
    * use relative path
    
    * revert rename and use imp to find lib
    
    * fix typo

--- torchvision/ops/boxes.py
+++ torchvision/ops/boxes.py
@@ -1,5 +1,5 @@
 import torch
-from torchvision.extension import _lazy_import
+import torchvision.ops._custom_ops
 
 
 def nms(boxes, scores, iou_threshold):
@@ -29,8 +29,7 @@ def nms(boxes, scores, iou_threshold):
         of the elements that have been kept
         by NMS, sorted in decreasing order of scores
     """
-    _C = _lazy_import()
-    return _C.nms(boxes, scores, iou_threshold)
+    return torch.ops.torchvision.nms(boxes, scores, iou_threshold)
 
 
 def batched_nms(boxes, scores, idxs, iou_threshold):
