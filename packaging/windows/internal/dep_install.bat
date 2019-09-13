90d8cbe784c187e00aee96b0f597874f7103507b
peter <peter_jiachen@163.com>
2019-09-13 16:33:18 +0800

    Test MS agent

---
+++ packaging/windows/internal/dep_install.bat
@@ -0,0 +1,11 @@
+@echo off
+
+REM curl -k https://www.7-zip.org/a/7z1805-x64.exe -O
+REM if errorlevel 1 exit /b 1
+
+REM start /wait 7z1805-x64.exe /S
+REM if errorlevel 1 exit /b 1
+
+REM set "PATH=%ProgramFiles%\7-Zip;%PATH%"
+
+choco install -y curl 7zip cmake
--- packaging/windows/internal/vs_install.bat
+++ packaging/windows/internal/vs_install.bat
@@ -1,23 +1,32 @@
 @echo off
 
-set VS_DOWNLOAD_LINK=https://aka.ms/vs/15/release/vs_buildtools.exe
-IF "%VS_LATEST%" == "1" (
-   set VS_INSTALL_ARGS= --nocache --norestart --quiet --wait --add Microsoft.VisualStudio.Workload.VCTools
-   set VSDEVCMD_ARGS=
-) ELSE (
-   set VS_INSTALL_ARGS=--nocache --quiet --wait --add Microsoft.VisualStudio.Workload.VCTools ^
-                                                --add Microsoft.VisualStudio.Component.VC.Tools.14.11 ^
-                                                --add Microsoft.Component.MSBuild ^
-                                                --add Microsoft.VisualStudio.Component.Roslyn.Compiler ^
-                                                --add Microsoft.VisualStudio.Component.TextTemplating ^
-                                                --add Microsoft.VisualStudio.Component.VC.CoreIde ^
-                                                --add Microsoft.VisualStudio.Component.VC.Redist.14.Latest ^
-                                                --add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Core ^
-                                                --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 ^
-                                                --add Microsoft.VisualStudio.Component.VC.Tools.14.11 ^
-                                                --add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Win81
-   set VSDEVCMD_ARGS=-vcvars_ver=14.11
-)
+REM set VS_DOWNLOAD_LINK=https://aka.ms/vs/15/release/vs_buildtools.exe
+REM IF "%VS_LATEST%" == "1" (
+REM    set VS_INSTALL_ARGS= --nocache --norestart --quiet --wait --add Microsoft.VisualStudio.Workload.VCTools
+REM    set VSDEVCMD_ARGS=
+REM ) ELSE (
+REM    set VS_INSTALL_ARGS=--nocache --quiet --wait --add Microsoft.VisualStudio.Workload.VCTools ^
+REM                                                 --add Microsoft.VisualStudio.Component.VC.Tools.14.11 ^
+REM                                                 --add Microsoft.Component.MSBuild ^
+REM                                                 --add Microsoft.VisualStudio.Component.Roslyn.Compiler ^
+REM                                                 --add Microsoft.VisualStudio.Component.TextTemplating ^
+REM                                                 --add Microsoft.VisualStudio.Component.VC.CoreIde ^
+REM                                                 --add Microsoft.VisualStudio.Component.VC.Redist.14.Latest ^
+REM                                                 --add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Core ^
+REM                                                 --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 ^
+REM                                                 --add Microsoft.VisualStudio.Component.VC.Tools.14.11 ^
+REM                                                 --add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Win81
+REM    set VSDEVCMD_ARGS=-vcvars_ver=14.11
+REM )
+
+set VS_DOWNLOAD_LINK=https://aka.ms/vs/15/release/ca16a813d/vs_buildtools.exe
+set VS_INSTALL_ARGS=--nocache --quiet --wait --add Microsoft.VisualStudio.Workload.VCTools ^
+                    --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 ^
+                    --add Microsoft.VisualStudio.Component.VC.DiagnosticTools ^
+                    --add Microsoft.VisualStudio.Component.Windows10SDK.16299.Desktop ^
+                    --add Microsoft.VisualStudio.Component.VC.CMake.Project ^
+                    --add Microsoft.VisualStudio.Component.VC.ATL ^
+                    --add Microsoft.VisualStudio.Component.VC.140
 
 curl -k -L %VS_DOWNLOAD_LINK% --output vs_installer.exe
 if errorlevel 1 exit /b 1
