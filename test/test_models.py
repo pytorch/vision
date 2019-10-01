19e848d86cd052ca5a59a346b1950d78582cf03f
eellison <eellison@fb.com>
2019-10-01 10:10:53 -0700

    add support for video resnet models, restructure script test to just ignore RCNN models

--- test/test_models.py
+++ test/test_models.py
@@ -26,25 +26,17 @@ def get_available_video_models():
     return [k for k, v in models.video.__dict__.items() if callable(v) and k[0].lower() == k[0] and k[0] != "_"]
 
 
-torchub_models = [
-    "deeplabv3_resnet101",
-    "mobilenet_v2",
-    "resnext50_32x4d",
-    "fcn_resnet101",
-    "googlenet",
-    "densenet121",
-    "resnet18",
-    "alexnet",
-    "shufflenet_v2_x1_0",
-    "squeezenet1_0",
-    "vgg11",
-    "inception_v3",
+# rcnn not yet supported
+unsupported_script_models = [
+    'fasterrcnn_resnet50_fpn',
+    'maskrcnn_resnet50_fpn',
+    'keypointrcnn_resnet50_fpn'
 ]
 
 
 class Tester(unittest.TestCase):
     def check_script(self, model, name):
-        if name not in torchub_models:
+        if name in unsupported_script_models:
             return
         scriptable = True
         msg = ""
