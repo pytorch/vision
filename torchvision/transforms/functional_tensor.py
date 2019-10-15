9664ec87f84e3db2974486016b9fe4a2c39f9e0d
surgan12 <cs17btech11038@iith.ac.in>
2019-10-15 19:36:58 +0530

    vflip and hflip tensor

---
+++ torchvision/transforms/functional_tensor.py
@@ -0,0 +1,32 @@
+import torch
+import torchvision.transforms.functional as F
+
+def vflip(img_tensor):
+    """Vertically flip the given the Image Tensor.
+
+    Args:
+        img_tensor (Tensor): Image Tensor to be flipped.
+
+    Returns:
+        Tensor:  Vertically flipped image Tensor.
+    """
+    if not F._is_tensor_image(img_tensor):
+    	raise TypeError('tensor is not a torch image.')
+    
+    return img_tensor.flip(1)
+
+
+def hflip(img_tensor):
+    """Horizontally flip the given the Image Tensor.
+
+    Args:
+        img_tensor (Tensor): Image Tensor to be flipped.
+
+    Returns:
+        Tensor:  Horizontally flipped image Tensor.
+    """
+
+    if not F._is_tensor_image(img_tensor):
+    	raise TypeError('tensor is not a torch image.')
+    
+    return img_tensor.flip(2)
