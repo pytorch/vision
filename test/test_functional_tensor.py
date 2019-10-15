444a69f84df7d08bba07d87b19b15e684fee0aae
surgan12 <cs17btech11038@iith.ac.in>
2019-10-15 20:08:53 +0530

    changes made

--- test/test_functional_tensor.py
+++ test/test_functional_tensor.py
@@ -2,23 +2,22 @@ import torchvision.transforms.functional_tensor as F_t
 import unittest
 import torch
 
+
 class Tester(unittest.TestCase):
 
-	def test_vflip(self):
-		img_tensor = torch.randn(3,16,16)
-		vflipped_img = F_t.vflip(img_tensor)
-		vflipped_img_again = F_t.vflip(vflipped_img)
+    def test_vflip(self):
+        img_tensor = torch.randn(3,16,16)
+        vflipped_img = F_t.vflip(img_tensor)
+        vflipped_img_again = F_t.vflip(vflipped_img)
+        self.assertEqual(vflipped_img.shape, img_tensor.shape)
+        self.assertTrue(torch.equal(img_tensor, vflipped_img_again))
 
-		assert vflipped_img.shape == img_tensor.shape
-		assert torch.equal(img_tensor, vflipped_img_again)
-
-	def test_hflip(self):
-		img_tensor = torch.randn(3,16,16)
-		hflipped_img = F_t.hflip(img_tensor)
-		hflipped_img_again = F_t.hflip(hflipped_img)
-
-		assert hflipped_img.shape == img_tensor.shape
-		assert torch.equal(img_tensor, hflipped_img_again)
+    def test_hflip(self):
+        img_tensor = torch.randn(3,16,16)
+        hflipped_img = F_t.hflip(img_tensor)
+        hflipped_img_again = F_t.hflip(hflipped_img)
+        self.assertEqual(hflipped_img.shape, img_tensor.shape)
+        self.assertTrue(torch.equal(img_tensor, hflipped_img_again))
 
 if __name__ == '__main__':
     unittest.main()
--- torchvision/transforms/functional_tensor.py
+++ torchvision/transforms/functional_tensor.py
@@ -1,32 +1,33 @@
 import torch
 import torchvision.transforms.functional as F
 
+
 def vflip(img_tensor):
     """Vertically flip the given the Image Tensor.
 
     Args:
-        img_tensor (Tensor): Image Tensor to be flipped.
+        img_tensor (Tensor): Image Tensor to be flipped in the form CXHXW.
 
     Returns:
         Tensor:  Vertically flipped image Tensor.
     """
     if not F._is_tensor_image(img_tensor):
-    	raise TypeError('tensor is not a torch image.')
-    
-    return img_tensor.flip(1)
+        raise TypeError('tensor is not a torch image.')
+
+    return img_tensor.flip(-2)
 
 
 def hflip(img_tensor):
     """Horizontally flip the given the Image Tensor.
 
     Args:
-        img_tensor (Tensor): Image Tensor to be flipped.
+        img_tensor (Tensor): Image Tensor to be flipped in the form CXHXW.
 
     Returns:
         Tensor:  Horizontally flipped image Tensor.
     """
 
     if not F._is_tensor_image(img_tensor):
-    	raise TypeError('tensor is not a torch image.')
-    
-    return img_tensor.flip(2)
+        raise TypeError('tensor is not a torch image.')
+
+    return img_tensor.flip(-1)
