5838bd0cbd61e7911c1b8810079283590ea39bc8
Raghu Krishnamoorthi <raghuramank@gmail.com>
2019-10-15 18:21:43 -0700

    Restore relu settings to default in resnet.py Summary:
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- torchvision/models/resnet.py
+++ torchvision/models/resnet.py
@@ -141,7 +141,7 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
                                bias=False)
         self.bn1 = norm_layer(self.inplanes)
-        self.relu = nn.ReLU(inplace=False)
+        self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
