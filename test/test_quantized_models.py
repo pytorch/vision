c12133b0803e9a3e3b2169fa6a1d236fb552910b
Raghu Krishnamoorthi <raghuramank@gmail.com>
2019-10-17 17:15:58 -0700

    Update tests to follow similar structure to test_models.py, allowing for modular testing
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- test/test_quantized_models.py
+++ test/test_quantized_models.py
@@ -2,45 +2,93 @@ import torch
 import unittest
 import torchvision
 
+from common_utils import TestCase, map_nested_tensor_object
+from collections import OrderedDict
+from itertools import product
+import torch
+import numpy as np
+from torchvision import models
+import unittest
+import traceback
+import random
 
-class Tester(unittest.TestCase):
-    def test_model_quantizable(self):
-        image = torch.rand(1, 3, 224, 224, dtype=torch.float32)
-        model_arch_list = ['mobilenet_v2', 'resnet18', 'resnet34', 'resnet50', 'resnext101_32x8d'
-                          ]
+
+def set_rng_seed(seed):
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+
+quantizable_models = ['mobilenet_v2',
+                      'resnet18',
+                      'resnet34',
+                      'resnet50',
+                      'resnext101_32x8d',
+]
+
+
+
+scriptable_quantizable_models = ['mobilenet_v2',
+                      'resnet18',
+                      'resnet34',
+                      'resnet50',
+                      'resnext101_32x8d',
+]
+
+
+
+class ModelTester(TestCase):
+    def check_script(self, model, name):
+        if name not in scriptable_quantizable_models:
+            return
+        scriptable = True
+        msg = ""
+        try:
+            torch.jit.script(model)
+        except Exception as e:
+            tb = traceback.format_exc()
+            scriptable = False
+            msg = str(e) + str(tb)
+        self.assertTrue(scriptable, msg)
+
+    def _test_classification_model(self, name, input_shape):
+        # passing num_class equal to a number other than 1000 helps in making the test
+        # more enforcing in nature
+
         for eval in [True, False]:
-            for arch in model_arch_list:
-                model = torchvision.models.quantization.__dict__[arch](pretrained=False)
-                if eval:
-                    model.eval()
-                    model.qconfig = torch.quantization.default_qconfig
-                else:
-                    model.train()
-                    model.qconfig = torch.quantization.default_qat_qconfig
+            model = torchvision.models.quantization.__dict__[name](pretrained=False)
+            if eval:
+                model.eval()
+                model.qconfig = torch.quantization.get_default_qconfig('fbgemm')
+            else:
+                model.train()
+                model.qconfig = torch.quantization.default_qat_qconfig
 
-                model.fuse_model()
-                if eval:
-                    torch.quantization.prepare(model, inplace=True)
-                else:
-                    torch.quantization.prepare_qat(model, inplace=True)
-                    model.eval()
-                torch.quantization.convert(model, inplace=True)
-                # Ensure that quantized model runs successfully
-                model(image)
-
-    def test_model_scriptability(self):
-        model_arch_list = ['mobilenet_v2','resnet18', 'resnet34', 'resnet50', 'resnext101_32x8d',
-                          ]
-        for arch in model_arch_list:
-            model = torchvision.models.quantization.__dict__[arch](pretrained=False)
-            model.eval()
-            model.qconfig = torch.quantization.default_qconfig
 
             model.fuse_model()
-            torch.quantization.prepare(model, inplace=True)
+            if eval:
+                torch.quantization.prepare(model, inplace=True)
+            else:
+                torch.quantization.prepare_qat(model, inplace=True)
+                model.eval()
             torch.quantization.convert(model, inplace=True)
-            # Ensure that quantized model is scriptable
-            torch.jit.script(model)
+            # Ensure that quantized model runs successfully
+            x = torch.rand(input_shape)
+            out = model(x)
+
+        self.check_script(model, name)
+
+
+for model_name in quantizable_models:
+    # for-loop bodies don't define scopes, so we have to save the variables
+    # we want to close over in some way
+    def do_test(self, model_name=model_name):
+        input_shape = (1, 3, 224, 224)
+        if model_name in ['inception_v3']:
+            input_shape = (1, 3, 299, 299)
+        self._test_classification_model(model_name, input_shape)
+
+    setattr(ModelTester, "test_" + model_name, do_test)
 
 
 if __name__ == '__main__':
