d194082c067d8635370e6fce7345aa03cbb32e86
ekka <ekagra.ranjan@gmail.com>
2019-10-19 01:49:01 +0530

    Make crop scriptable  (#1379)
    * Make crop torchscriptable
    
    Relevant #1375
    
    * Invert x and y axis
    
    * fix lint
    
    * Add crop test
    
    * revert deletion of space in functional
    
    * add import random
    
    * add dimension in doc
    
    * add import
    
    * fix flake8
    
    * change to self.assert*
    
    * convert to uint8
    
    * assertTrue
    
    * lint

--- test/test_functional_tensor.py
+++ test/test_functional_tensor.py
@@ -1,6 +1,9 @@
-import torchvision.transforms.functional_tensor as F_t
-import unittest
 import torch
+import torchvision.transforms as transforms
+import torchvision.transforms.functional_tensor as F_t
+import torchvision.transforms.functional as F
+import unittest
+import random
 
 
 class Tester(unittest.TestCase):
@@ -18,6 +21,20 @@ class Tester(unittest.TestCase):
         hflipped_img_again = F_t.hflip(hflipped_img)
         self.assertEqual(hflipped_img.shape, img_tensor.shape)
         self.assertTrue(torch.equal(img_tensor, hflipped_img_again))
+
+    def test_crop(self):
+        img_tensor = torch.randint(0, 255, (3, 16, 16), dtype=torch.uint8)
+        top = random.randint(0, 15)
+        left = random.randint(0, 15)
+        height = random.randint(1, 16 - top)
+        width = random.randint(1, 16 - left)
+        img_cropped = F_t.crop(img_tensor, top, left, height, width)
+        img_PIL = transforms.ToPILImage()(img_tensor)
+        img_PIL_cropped = F.crop(img_PIL, top, left, height, width)
+        img_cropped_GT = transforms.ToTensor()(img_PIL_cropped)
+
+        self.assertTrue(torch.equal(img_cropped, (img_cropped_GT * 255).to(torch.uint8)),
+                        "functional_tensor crop not working")
 
 
 if __name__ == '__main__':
