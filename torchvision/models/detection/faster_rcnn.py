6083ba3931bac04e1a788943b5f89ba966ea23ae
invisprints <invisprints@outlook.com>
2019-11-05 14:02:20 +0800

    add fasterrcnn_resnet101_fpn

--- torchvision/models/detection/faster_rcnn.py
+++ torchvision/models/detection/faster_rcnn.py
@@ -17,7 +17,7 @@ from .backbone_utils import resnet_fpn_backbone
 
 
 __all__ = [
-    "FasterRCNN", "fasterrcnn_resnet50_fpn",
+    "FasterRCNN", "fasterrcnn_resnet50_fpn", "fasterrcnn_resnet101_fpn"
 ]
 
 
@@ -333,6 +333,56 @@ def fasterrcnn_resnet50_fpn(pretrained=False, progress=True,
     model = FasterRCNN(backbone, num_classes, **kwargs)
     if pretrained:
         state_dict = load_state_dict_from_url(model_urls['fasterrcnn_resnet50_fpn_coco'],
+                                              progress=progress)
+        model.load_state_dict(state_dict)
+    return model
+
+def fasterrcnn_resnet101_fpn(pretrained=False, progress=False,
+                            num_classes=91, pretrained_backbone=True, **kwargs):
+    """
+    Constructs a Faster R-CNN model with a ResNet-101-FPN backbone.
+
+    The input to the model is expected to be a list of tensors, each of shape ``[C, H, W]``, one for each
+    image, and should be in ``0-1`` range. Different images can have different sizes.
+
+    The behavior of the model changes depending if it is in training or evaluation mode.
+
+    During training, the model expects both the input tensors, as well as a targets (list of dictionary),
+    containing:
+        - boxes (``FloatTensor[N, 4]``): the ground-truth boxes in ``[x1, y1, x2, y2]`` format, with values
+          between ``0`` and ``H`` and ``0`` and ``W``
+        - labels (``Int64Tensor[N]``): the class label for each ground-truth box
+
+    The model returns a ``Dict[Tensor]`` during training, containing the classification and regression
+    losses for both the RPN and the R-CNN.
+
+    During inference, the model requires only the input tensors, and returns the post-processed
+    predictions as a ``List[Dict[Tensor]]``, one for each input image. The fields of the ``Dict`` are as
+    follows:
+        - boxes (``FloatTensor[N, 4]``): the predicted boxes in ``[x1, y1, x2, y2]`` format, with values between
+          ``0`` and ``H`` and ``0`` and ``W``
+        - labels (``Int64Tensor[N]``): the predicted labels for each image
+        - scores (``Tensor[N]``): the scores or each prediction
+
+    Example::
+
+        >>> model = torchvision.models.detection.fasterrcnn_resnet101_fpn(pretrained=False)
+        >>> model.eval()
+        >>> x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+        >>> predictions = model(x)
+
+    Arguments:
+        pretrained (bool): If True, returns a model pre-trained on COCO train2017(currently don't have pre-trained models)
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    if pretrained:
+        raise Exception("resnet101 cannot use pre-trained models") 
+        # no need to download the backbone if pretrained is set
+        pretrained_backbone = False
+    backbone = resnet_fpn_backbone('resnet101', pretrained_backbone)
+    model = FasterRCNN(backbone, num_classes, **kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls['fasterrcnn_resnet101_fpn_coco'],
                                               progress=progress)
         model.load_state_dict(state_dict)
     return model
