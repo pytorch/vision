6a4571a4262fc593c63d5bdbc05ba41f93dd6d7b
Francisco Massa <fvsmassa@gmail.com>
2019-11-30 16:33:19 +0100

    Test

--- test/common_utils.py
+++ test/common_utils.py
@@ -29,6 +29,7 @@ def get_tmp_dir(src=None, **kwargs):
 
 ACCEPT = os.getenv('EXPECTTEST_ACCEPT')
 TEST_WITH_SLOW = os.getenv('PYTORCH_TEST_WITH_SLOW', '0') == '1'
+# hello
 # TEST_WITH_SLOW = True  # TODO: Delete this line once there is a PYTORCH_TEST_WITH_SLOW aware CI job
 
 
