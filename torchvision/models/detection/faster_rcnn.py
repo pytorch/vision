88d326eb5ac2679ea0134cc74602f00b562f7cb3
Francisco Massa <fvsmassa@gmail.com>
2020-01-02 18:48:40 +0100

    V3

--- torchvision/models/detection/faster_rcnn.py
+++ torchvision/models/detection/faster_rcnn.py
@@ -319,15 +319,16 @@ def fasterrcnn_resnet50_fpn(pretrained=False, progress=True,
 
         >>> model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
         >>> # For training
-        >>> images, boxes, labels = torch.rand(4, 3, 600, 1200), torch.rand(4, 11, 4), torch.rand(4, 11)
+        >>> images, boxes = torch.rand(4, 3, 600, 1200), torch.rand(4, 11, 4)
+        >>> labels = torch.randint(1, 91, (4, 11))
         >>> images = list(image for image in images)
         >>> targets = []
         >>> for i in range(len(images)):
         >>>     d = {}
         >>>     d['boxes'] = boxes[i]
-        >>>     d['labels'] = labels[i].type(torch.int64)
+        >>>     d['labels'] = labels[i]
         >>>     targets.append(d)
-        >>> output = model(images,targets)
+        >>> output = model(images, targets)
         >>> # For inference
         >>> model.eval()
         >>> x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
