e6d36b3355275a66b276db819639d7b96ef68159
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos057.prn3.facebook.com>
2020-01-03 04:32:01 -0800

    2020-01-03 nightly release (07cbb46aba8569f0fac95667d57421391e6d36e9)

--- torchvision/models/detection/faster_rcnn.py
+++ torchvision/models/detection/faster_rcnn.py
@@ -318,6 +318,18 @@ def fasterrcnn_resnet50_fpn(pretrained=False, progress=True,
     Example::
 
         >>> model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+        >>> # For training
+        >>> images, boxes = torch.rand(4, 3, 600, 1200), torch.rand(4, 11, 4)
+        >>> labels = torch.randint(1, 91, (4, 11))
+        >>> images = list(image for image in images)
+        >>> targets = []
+        >>> for i in range(len(images)):
+        >>>     d = {}
+        >>>     d['boxes'] = boxes[i]
+        >>>     d['labels'] = labels[i]
+        >>>     targets.append(d)
+        >>> output = model(images, targets)
+        >>> # For inference
         >>> model.eval()
         >>> x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
         >>> predictions = model(x)
