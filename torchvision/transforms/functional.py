4c0dceb4e554285e07ab9f64014bf84be9970ccb
Philip Meier <github.pmeier@posteo.de>
2020-01-21 22:51:28 +0100

    remove unrelated change

--- torchvision/transforms/functional.py
+++ torchvision/transforms/functional.py
@@ -85,8 +85,14 @@ def to_tensor(pic):
         img = 255 * torch.from_numpy(np.array(pic, np.uint8, copy=False))
     else:
         img = torch.ByteTensor(torch.ByteStorage.from_buffer(pic.tobytes()))
-
-    img = img.view(pic.size[1], pic.size[0], len(pic.getbands()))
+    # PIL image mode: L, LA, P, I, F, RGB, YCbCr, RGBA, CMYK
+    if pic.mode == 'YCbCr':
+        nchannel = 3
+    elif pic.mode == 'I;16':
+        nchannel = 1
+    else:
+        nchannel = len(pic.mode)
+    img = img.view(pic.size[1], pic.size[0], nchannel)
     # put it from HWC to CHW format
     # yikes, this transpose takes 80% of the loading time/CPU
     img = img.transpose(0, 1).transpose(0, 2).contiguous()
