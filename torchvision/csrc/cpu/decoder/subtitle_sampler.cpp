28b7f8ae219ede3cef93100251bfff75a9a53644
Francisco Massa <fvsmassa@gmail.com>
2020-01-27 21:09:03 +0100

    Base decoder for video. (#1747) (#1793)
    Summary:
    Pull Request resolved: https://github.com/pytorch/vision/pull/1747
    
    Pull Request resolved: https://github.com/pytorch/vision/pull/1746
    
    Added the implementation of ffmpeg based decoder with functionality that can be used in VUE and TorchVision.
    
    Reviewed By: fmassa
    
    Differential Revision: D19358914
    
    fbshipit-source-id: abb672f89bfaca6351dda2354f0d35cf8e47fa0f
    
    Co-authored-by: Yuri Putivsky <yuri@fb.com>

---
+++ torchvision/csrc/cpu/decoder/subtitle_sampler.cpp
@@ -0,0 +1,46 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include "subtitle_sampler.h"
+#include "util.h"
+
+namespace ffmpeg {
+
+SubtitleSampler::~SubtitleSampler() {
+  cleanUp();
+}
+
+void SubtitleSampler::shutdown() {
+  cleanUp();
+}
+
+bool SubtitleSampler::init(const SamplerParameters& params) {
+  cleanUp();
+  // set formats
+  params_ = params;
+  return true;
+}
+
+int SubtitleSampler::getSamplesBytes(AVSubtitle* sub) const {
+  return Util::size(*sub);
+}
+
+int SubtitleSampler::sample(AVSubtitle* sub, ByteStorage* out) {
+  if (!sub) {
+    return 0; // flush
+  }
+
+  return Util::serialize(*sub, out);
+}
+
+int SubtitleSampler::sample(const ByteStorage* in, ByteStorage* out) {
+  if (in) {
+    // Get a writable copy
+    *out = *in;
+    return out->length();
+  }
+  return 0;
+}
+
+void SubtitleSampler::cleanUp() {}
+
+} // namespace ffmpeg
