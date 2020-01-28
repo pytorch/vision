31081f365b4f0309a05f61b97e1562f253a31a4c
joaoflf <fernandesj@protonmail.com>
2020-01-28 15:22:39 +0000

    Change object check

--- torchvision/datasets/voc.py
+++ torchvision/datasets/voc.py
@@ -219,7 +219,7 @@ class VOCDetection(VisionDataset):
                 for ind, v in dc.items():
                     def_dic[ind].append(v)
             if node.tag == 'annotation':
-                if isinstance(def_dic['object'], object):
+                if isinstance(def_dic['object'], list) == False:
                     def_dic['object'] = [def_dic['object']]
             voc_dict = {
                 node.tag:
