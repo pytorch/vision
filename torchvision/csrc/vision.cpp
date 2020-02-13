36b0a8fb050c817dcecc4ac426e40d79914c6c6b
Ryad ZENINE <r.zenine@gmail.com>
2020-02-13 14:40:17 +0100

    Add PNG reading capabilities

--- torchvision/csrc/vision.cpp
+++ torchvision/csrc/vision.cpp
@@ -14,6 +14,9 @@
 #include "ROIAlign.h"
 #include "ROIPool.h"
 #include "empty_tensor_op.h"
+#ifdef __linux__
+#include "image.h"
+#endif
 #include "nms.h"
 
 // If we are in a Windows environment, we need to define
@@ -52,4 +55,7 @@ static auto registry =
         .op("torchvision::ps_roi_align", &ps_roi_align)
         .op("torchvision::ps_roi_pool", &ps_roi_pool)
         .op("torchvision::deform_conv2d", &deform_conv2d)
+#ifdef __linux__
+        .op("torchvision::decode_png", &decodePNG)
+#endif
         .op("torchvision::_cuda_version", &_cuda_version);
