e65a44a611ea37d3058371d23a0e503ce85ded1f
Ryad ZENINE <r.zenine@gmail.com>
2020-03-12 20:49:32 +0100

    fix python path for env

--- packaging/build_conda.sh
+++ packaging/build_conda.sh
@@ -16,11 +16,7 @@ setup_junit_results_folder
 # just to see if it works before cleanup
 if [[ "$(uname)" == Linux ]] ; then
     conda create -n build_env python="$PYTHON_VERSION" anaconda -yq
-    activate build_env
-    conda env list
-    conda env list |grep build_env
-    conda env list |grep build_env|sed 's/build_env//'|sed 's/ //g'
-    "$(conda env list |grep build_env|sed 's/build_env//'|sed 's/ //g')/python" -m pip install -q onnxruntime
+    "$(conda info --base)/envs/build_env/bin/python" -m pip -q install onnxruntime
 fi
 
 conda build $CONDA_CHANNEL_FLAGS -c defaults -c conda-forge --no-anaconda-upload --python "$PYTHON_VERSION" packaging/torchvision
