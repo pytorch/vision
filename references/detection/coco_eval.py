8b461b93177b37a91d5dce8a6a91db5ea6d7480f
Philip Meier <github.pmeier@posteo.de>
2020-03-26 15:41:14 +0100

    remove six from references

--- references/detection/coco_eval.py
+++ references/detection/coco_eval.py
@@ -5,7 +5,6 @@ import numpy as np
 import copy
 import time
 import torch
-import torch._six
 
 from pycocotools.cocoeval import COCOeval
 from pycocotools.coco import COCO
@@ -246,7 +245,7 @@ def loadRes(self, resFile):
 
     # print('Loading and preparing results...')
     # tic = time.time()
-    if isinstance(resFile, torch._six.string_classes):
+    if isinstance(resFile, str):
         anns = json.load(open(resFile))
     elif type(resFile) == np.ndarray:
         anns = self.loadNumpyAnnotations(resFile)
