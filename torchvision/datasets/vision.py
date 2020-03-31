f6c6dbcbcc9bc3741c7ba57c169640916e3e3e69
Philip Meier <github.pmeier@posteo.de>
2020-03-31 07:24:13 +0200

    revert str to torch._six._string_classes

--- torchvision/datasets/vision.py
+++ torchvision/datasets/vision.py
@@ -7,7 +7,7 @@ class VisionDataset(data.Dataset):
     _repr_indent = 4
 
     def __init__(self, root, transforms=None, transform=None, target_transform=None):
-        if isinstance(root, str):
+        if isinstance(root, torch._six.string_classes):
             root = os.path.expanduser(root)
         self.root = root
 
