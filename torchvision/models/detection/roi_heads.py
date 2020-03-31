986d2423c3370e332cc0f042cc1c4daf0dd49add
Negin Raoof <neginmr@utexas.edu>
2020-03-31 12:12:15 -0700

    ONNX export for variable input sizes (#1840)
    * fixes and tests for variable input size
    
    * transform test fix
    
    * Fix comment
    
    * Dynamic shape for keypoint_rcnn
    
    * Update test_onnx.py
    
    * Update rpn.py
    
    * Fix for split on RPN
    
    * Fixes for feedbacks
    
    * flake8
    
    * topk fix
    
    * Fix build
    
    * branch on tracing
    
    * fix for scalar tensor
    
    * Fixes for script type annotations
    
    * Update rpn.py
    
    * clean up
    
    * clean up
    
    * Update rpn.py
    
    * Updated for feedback
    
    * Fix for comments
    
    * revert to use tensor
    
    * Added test for box clip
    
    * Fixes for feedback
    
    * Fix for feedback
    
    * ORT version revert
    
    * Update ort
    
    * Update .travis.yml
    
    * Update test_onnx.py
    
    * Update test_onnx.py
    
    * Tensor sizes
    
    * Fix for dynamic split
    
    * Try disable tests
    
    * pytest verbose
    
    * revert one test
    
    * enable tests
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update test_onnx.py
    
    * Update .travis.yml
    
    * Passing device
    
    * Fixes for test
    
    * Fix for boxes datatype
    
    * clean up
    
    Co-authored-by: Francisco Massa <fvsmassa@gmail.com>

--- torchvision/models/detection/roi_heads.py
+++ torchvision/models/detection/roi_heads.py
@@ -678,20 +678,13 @@ class RoIHeads(torch.nn.Module):
         device = class_logits.device
         num_classes = class_logits.shape[-1]
 
-        boxes_per_image = [len(boxes_in_image) for boxes_in_image in proposals]
+        boxes_per_image = [boxes_in_image.shape[0] for boxes_in_image in proposals]
         pred_boxes = self.box_coder.decode(box_regression, proposals)
 
         pred_scores = F.softmax(class_logits, -1)
 
-        # split boxes and scores per image
-        if len(boxes_per_image) == 1:
-            # TODO : remove this when ONNX support dynamic split sizes
-            # and just assign to pred_boxes instead of pred_boxes_list
-            pred_boxes_list = [pred_boxes]
-            pred_scores_list = [pred_scores]
-        else:
-            pred_boxes_list = pred_boxes.split(boxes_per_image, 0)
-            pred_scores_list = pred_scores.split(boxes_per_image, 0)
+        pred_boxes_list = pred_boxes.split(boxes_per_image, 0)
+        pred_scores_list = pred_scores.split(boxes_per_image, 0)
 
         all_boxes = []
         all_scores = []
