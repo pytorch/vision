bbdf5965a1c7f9c6a7cb5ec896e21bdc9255fe26
peter <peterghost86@gmail.com>
2020-04-03 16:40:46 +0800

    Use cmd

---
+++ packaging/windows/internal/build_wheels.bat
@@ -0,0 +1,11 @@
+if "%VC_YEAR%" == "2017" powershell packaging/windows/internal/vs_install.ps1
+if errorlevel 1 exit /b 1
+
+call packaging/windows/internal/cuda_install.bat
+if errorlevel 1 exit /b 1
+
+call packaging/windows/internal/nightly_defaults.bat Conda
+if errorlevel 1 exit /b 1
+
+call packaging/windows/build_vision.bat %CUDA_VERSION% %TORCHVISION_BUILD_VERSION% %TORCHVISION_BUILD_NUMBER%
+if errorlevel 1 exit /b 1
