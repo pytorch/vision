Fix for scale_factor computation

test/test_onnx.py
@@ -321,10 +321,10 @@ class ONNXExporterTester(unittest.TestCase):
 
     def get_test_images(self):
         image_url = "http://farm3.staticflickr.com/2469/3915380994_2e611b1779_z.jpg"
-        image = self.get_image_from_url(url=image_url, size=(200, 300))
+        image = self.get_image_from_url(url=image_url, size=(100, 320))
 
         image_url2 = "https://pytorch.org/tutorials/_static/img/tv_tutorial/tv_image05.png"
-        image2 = self.get_image_from_url(url=image_url2, size=(250, 200))
+        image2 = self.get_image_from_url(url=image_url2, size=(250, 380))
 
         images = [image]
         test_images = [image2]
@@ -375,7 +375,6 @@ class ONNXExporterTester(unittest.TestCase):
 
         assert torch.all(out2.eq(out_trace2))
 
-    @unittest.skip("Disable test until export of interpolate script module to ONNX is fixed")
     def test_mask_rcnn(self):
         images, test_images = self.get_test_images()
 
@@ -416,7 +415,6 @@ class ONNXExporterTester(unittest.TestCase):
         assert torch.all(out2[0].eq(out_trace2[0]))
         assert torch.all(out2[1].eq(out_trace2[1]))
 
-    @unittest.skip("Disable test until export of interpolate script module to ONNX is fixed")
     def test_keypoint_rcnn(self):
         class KeyPointRCNN(torch.nn.Module):
             def __init__(self):

torchvision/models/detection/transform.py
@@ -10,6 +10,21 @@ from .image_list import ImageList
 from .roi_heads import paste_masks_in_image
 
 
+@torch.jit.unused
+def compute_scale_factor(image, self_min_size, self_max_size):
+        # type: (Tensor, float, float) -> Tensor
+        from torch.onnx import operators
+        im_shape = operators.shape_as_tensor(image)[-2:]
+        min_size = torch.min(im_shape).to(dtype=torch.float32)
+        max_size = torch.max(im_shape).to(dtype=torch.float32)
+
+        scale_factor = self_min_size / min_size
+        if max_size * scale_factor > self_max_size:
+            scale_factor = self_max_size / max_size
+
+        return scale_factor
+
+
 class GeneralizedRCNNTransform(nn.Module):
     """
     Performs input / target transformation before feeding the data to a GeneralizedRCNN
@@ -76,17 +91,13 @@ class GeneralizedRCNNTransform(nn.Module):
     def resize(self, image, target):
         # type: (Tensor, Optional[Dict[str, Tensor]])
         h, w = image.shape[-2:]
-        im_shape = torch.tensor(image.shape[-2:])
-        min_size = float(torch.min(im_shape))
-        max_size = float(torch.max(im_shape))
         if self.training:
             size = float(self.torch_choice(self.min_size))
         else:
             # FIXME assume for now that testing uses the largest scale
             size = float(self.min_size[-1])
-        scale_factor = size / min_size
-        if max_size * scale_factor > self.max_size:
-            scale_factor = self.max_size / max_size
+
+        scale_factor = compute_scale_factor(image, size, float(self.max_size))
         image = torch.nn.functional.interpolate(
             image[None], scale_factor=scale_factor, mode='bilinear',
             align_corners=False)[0]