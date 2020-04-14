39891cace4feb48038239ffee5505e55a9003f3a
Francisco Massa <fvsmassa@gmail.com>
2020-04-14 18:54:40 +0200

    Master version bump 0.6 -> 0.7 (#2102)

--- packaging/build_wheel.sh
+++ packaging/build_wheel.sh
@@ -5,7 +5,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "$script_dir/pkg_helpers.bash"
 
 export BUILD_TYPE=wheel
-setup_env 0.6.0
+setup_env 0.7.0
 setup_wheel_python
 pip_install numpy pyyaml future ninja
 setup_pip_pytorch_version
