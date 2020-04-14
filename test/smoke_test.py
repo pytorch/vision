6761b82995e83c509f4716f054d745835de2db89
Brian Johnson <brianjo@fb.com>
2020-04-14 13:44:03 -0700

    root commit

--- test/smoke_test.py
+++
@@ -1,4 +0,0 @@
-import torch
-import torchvision
-import torchvision.datasets as dset
-import torchvision.transforms
--- torchvision/csrc/vision.h
+++
@@ -1,6 +0,0 @@
-#ifndef VISION_H
-#define VISION_H
-
-#include <torchvision/models/models.h>
-
-#endif // VISION_H
