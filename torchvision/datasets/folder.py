d6d70a9a882413bef145ba5dd156d585009951ce
dwSun <tonghuren@icloud.com>
2020-04-16 13:28:48 +0800

    improve documentation of DatasetFolder and ImageFolder

--- torchvision/datasets/folder.py
+++ torchvision/datasets/folder.py
@@ -81,7 +81,7 @@ class DatasetFolder(VisionDataset):
             both extensions and is_valid_file should not be passed.
 
      Attributes:
-        classes (list): List of the class names.
+        classes (list): List of the class names sorted alphabetically.
         class_to_idx (dict): Dict with items (class_name, class_index).
         samples (list): List of (sample path, class_index) tuples
         targets (list): The class_index value for each image in the dataset
@@ -193,7 +193,7 @@ class ImageFolder(DatasetFolder):
             and check if the file is a valid file (used to check of corrupt files)
 
      Attributes:
-        classes (list): List of the class names.
+        classes (list): List of the class names sorted alphabetically.
         class_to_idx (dict): Dict with items (class_name, class_index).
         imgs (list): List of (image path, class_index) tuples
     """
