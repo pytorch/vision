d466a90dad2bbf3d31290ee7ec255d4101b3634b
F-G Fernandez <fgfm03@hotmail.fr>
2020-05-11 14:44:13 +0200

    test: Fixed roi pooling tensor unittest

--- test/test_ops.py
+++ test/test_ops.py
@@ -552,7 +552,7 @@ class BoxConversionTester(unittest.TestCase):
     @staticmethod
     def _get_box_sequences():
         # Define here the argument type of `boxes` supported by region pooling operations
-        box_tensor = torch.tensor([[0, 0, 0, 100, 100], [[1, 0, 0, 100, 100]]], dtype=torch.float)
+        box_tensor = torch.tensor([[0, 0, 0, 100, 100], [1, 0, 0, 100, 100]], dtype=torch.float)
         box_list = [torch.tensor([[0, 0, 100, 100]], dtype=torch.float),
                     torch.tensor([[0, 0, 100, 100]], dtype=torch.float)]
         box_tuple = tuple(box_list)
