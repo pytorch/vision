df8d7767d0f47f7e6869b9d2f92a902c5cb6e03d
peterjc123 <peterghost86@gmail.com>
2020-05-30 02:57:26 +0800

    Reuse common logic and migrate CUDA >= 10.1 jobs to VS 2019 for Windo… (#2264)

--- packaging/windows/internal/build_wheels.bat
+++ packaging/windows/internal/build_wheels.bat
@@ -1,5 +1,5 @@
 if "%VC_YEAR%" == "2017" set VSDEVCMD_ARGS=-vcvars_ver=14.11
-if "%VC_YEAR%" == "2017" powershell packaging/windows/internal/vs_install.ps1
+if "%VC_YEAR%" == "2017" powershell packaging/windows/internal/vs2017_install.ps1
 if errorlevel 1 exit /b 1
 
 call packaging/windows/internal/cuda_install.bat
