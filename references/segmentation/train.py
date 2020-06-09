43e1ffcb32590265a47428922bd1131df7bb15df
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-06-08 17:45:11 -0700

    results

--- references/segmentation/train.py
+++ references/segmentation/train.py
@@ -58,43 +58,29 @@ def criterion(inputs, target):
 
 
 def evaluate(model, data_loader, device, num_classes):
-    total_eval_time = time.time()
-    t = time.time()
     model.eval()
-    print("model.eval(): ", time.time() - t)
     confmat = utils.ConfusionMatrix(num_classes)
     metric_logger = utils.MetricLogger(delimiter="  ")
     header = 'Test:'
     with torch.no_grad():
         i = 0
-        proc_images_t = time.time()
         for image, target in metric_logger.log_every(data_loader, 100, header):
-            #print("\n\nBATCH #", i)
             to_device_t = time.time()
             image, target = image.to(device), target.to(device)
-            #print("to_device_t", time.time() - to_device_t)
             
             model_t = time.time()
             output = model(image)
-            #print("Model(image): ", time.time() - model_t)
 
             output = output['out']
 
             cmp_upd = time.time()
             for a, b in zip(target, output):
                 confmat.update(a.flatten(), b.argmax(0).flatten())
-            #print("COMP UPD: ", time.time() - cmp_upd)
 
             i += 1
-            if i == 20:
+            if i == 100:
                 break
-        print("PROC IMAGES TIME: ", time.time() - proc_images_t)
 
-        reduce_from_all_processes_t = time.time()
-        confmat.reduce_from_all_processes()
-        print("REDUCE IMAGES: ", time.time()- reduce_from_all_processes_t)
-
-    print("TOTAL EVAL TIME: ", time.time() - total_eval_time)
     return confmat
 
 
@@ -123,7 +109,7 @@ def train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, devi
 def main(args):
     total_t = time.time()
     pr = cProfile.Profile()
-    pr.enable()
+    #pr.enable()
     
     if args.output_dir:
         utils.mkdir(args.output_dir)
@@ -133,7 +119,6 @@ def main(args):
 
     device = torch.device(args.device)
 
-    load_data_t = time.time()
     dataset, num_classes = get_dataset(args.dataset, "train", get_transform(train=True))
     dataset_test, _ = get_dataset(args.dataset, "val", get_transform(train=False))
 
@@ -154,9 +139,6 @@ def main(args):
         sampler=test_sampler, num_workers=args.workers,
         collate_fn=utils.collate_fn)
 
-    print("\n LOADING DATA: ", time.time() - load_data_t)
-    
-    pre_eval_t = time.time()
     model = torchvision.models.segmentation.__dict__[args.model](num_classes=num_classes,
                                                                  aux_loss=args.aux_loss,
                                                                  pretrained=args.pretrained)
@@ -173,21 +155,21 @@ def main(args):
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
         model_without_ddp = model.module
 
-    print("PREEVAL: ", time.time() - pre_eval_t)
-   
     if args.test_only:
-        eval_t = time.time()
-        confmat = evaluate(model, data_loader_test, device=device, num_classes=num_classes)
-        print(confmat)
-        print("\n\nEVAL: ", time.time() - eval_t)
-        print("GLOBAL TIME :", time.time() - total_t)
-        pr.disable()
-        print("CPROF STATS: ")
-        s = io.StringIO()
-        sortby = SortKey.CUMULATIVE
-        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-        ps.print_stats()
-        print(s.getvalue())
+        #eval_t = time.time()
+        with torch.autograd.profiler.profile() as prof:
+            confmat = evaluate(model, data_loader_test, device=device, num_classes=num_classes)
+        print(prof.key_averages(group_by_input_shape=True).table(sort_by="self_cpu_time_total"))
+        #print(confmat)
+        #print("\n\nEVAL: ", time.time() - eval_t)
+        #print("GLOBAL TIME :", time.time() - total_t)
+        #pr.disable()
+        #print("CPROF STATS: ")
+        #s = io.StringIO()
+        #sortby = SortKey.CUMULATIVE
+        #ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        #ps.print_stats()
+        #print(s.getvalue())
         return
     
     params_to_optimize = [
