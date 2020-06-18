2a6ff9fa38bcebb3d9ae2ed6edf28e020e3a38ea
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-06-18 18:55:16 -0500

    Minor error correction

--- setup.py
+++ setup.py
@@ -281,7 +281,7 @@ def get_extensions():
         ext_modules.append(extension(
             'torchvision.image',
             image_src,
-            include_dirs=include_dirs + image_path + image_include,
+            include_dirs=include_dirs + [image_path] + image_include,
             library_dirs=library_dirs + image_library,
             define_macros=image_macros,
             extra_compile_args=extra_compile_args
