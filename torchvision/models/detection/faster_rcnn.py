b0179c322dd1e86ac5db3b7b89c9d46b789ae104
Urwa Muaz <43106180+muaz-urwa@users.noreply.github.com>
2020-06-30 16:28:17 +0500

    optional focal loss

--- torchvision/models/detection/faster_rcnn.py
+++ torchvision/models/detection/faster_rcnn.py
@@ -156,7 +156,8 @@ class FasterRCNN(GeneralizedRCNN):
                  box_score_thresh=0.05, box_nms_thresh=0.5, box_detections_per_img=100,
                  box_fg_iou_thresh=0.5, box_bg_iou_thresh=0.5,
                  box_batch_size_per_image=512, box_positive_fraction=0.25,
-                 bbox_reg_weights=None):
+                 bbox_reg_weights=None,
+                 focal_loss = False, focal_loss_gamma = 2):
 
         if not hasattr(backbone, "out_channels"):
             raise ValueError(
@@ -222,7 +223,8 @@ class FasterRCNN(GeneralizedRCNN):
             box_fg_iou_thresh, box_bg_iou_thresh,
             box_batch_size_per_image, box_positive_fraction,
             bbox_reg_weights,
-            box_score_thresh, box_nms_thresh, box_detections_per_img)
+            box_score_thresh, box_nms_thresh, box_detections_per_img,
+            focal_loss, focal_loss_gamma)
 
         if image_mean is None:
             image_mean = [0.485, 0.456, 0.406]
