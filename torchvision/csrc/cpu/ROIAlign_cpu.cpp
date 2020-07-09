3ea19695be67ea33055c0f6f3746ee2ed5e83a63
Francisco Massa <fvsmassa@gmail.com>
2020-07-09 12:50:01 -0700

    Fix wrong clamping in RoIAlign with aligned=True (#2438) (#2445)
    Summary:
    * Fix wrong clamping in RoIAlign with aligned=True
    
    * Fix silly mistake
    
    * Bugfix pointed out during code-review
    
    Pull Request resolved: https://github.com/pytorch/vision/pull/2445
    
    Reviewed By: zhangguanheng66
    
    Differential Revision: D22458789
    
    Pulled By: fmassa
    
    fbshipit-source-id: cbe4d7df64b56b2c0b44c21c3fb155d40c74e057

--- torchvision/csrc/cpu/ROIAlign_cpu.cpp
+++ torchvision/csrc/cpu/ROIAlign_cpu.cpp
@@ -141,9 +141,13 @@ void ROIAlignForward(
     T roi_end_w = offset_rois[3] * spatial_scale - offset;
     T roi_end_h = offset_rois[4] * spatial_scale - offset;
 
-    // Force malformed ROIs to be 1x1
-    T roi_width = std::max(roi_end_w - roi_start_w, (T)1.);
-    T roi_height = std::max(roi_end_h - roi_start_h, (T)1.);
+    T roi_width = roi_end_w - roi_start_w;
+    T roi_height = roi_end_h - roi_start_h;
+    if (!aligned) {
+      // Force malformed ROIs to be 1x1
+      roi_width = std::max(roi_width, (T)1.);
+      roi_height = std::max(roi_height, (T)1.);
+    }
 
     T bin_size_h = static_cast<T>(roi_height) / static_cast<T>(pooled_height);
     T bin_size_w = static_cast<T>(roi_width) / static_cast<T>(pooled_width);
@@ -309,9 +313,13 @@ void ROIAlignBackward(
     T roi_end_w = offset_rois[3] * spatial_scale - offset;
     T roi_end_h = offset_rois[4] * spatial_scale - offset;
 
-    // Force malformed ROIs to be 1x1
-    T roi_width = std::max(roi_end_w - roi_start_w, (T)1.);
-    T roi_height = std::max(roi_end_h - roi_start_h, (T)1.);
+    T roi_width = roi_end_w - roi_start_w;
+    T roi_height = roi_end_h - roi_start_h;
+    if (!aligned) {
+      // Force malformed ROIs to be 1x1
+      roi_width = std::max(roi_width, (T)1.);
+      roi_height = std::max(roi_height, (T)1.);
+    }
 
     T bin_size_h = static_cast<T>(roi_height) / static_cast<T>(pooled_height);
     T bin_size_w = static_cast<T>(roi_width) / static_cast<T>(pooled_width);
