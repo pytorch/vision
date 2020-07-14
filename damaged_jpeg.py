4aa61806b408fba84845071db2bb1548316e2a41
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-07-14 14:58:05 -0500

    Remove unused test file

--- damaged_jpeg.py
+++
@@ -1,9 +0,0 @@
-
-import os.path as osp
-import torchvision.io.image as image
-
-HERE = osp.basename(osp.abspath(__file__))
-TEST_ASSETS = osp.join(HERE, 'test', 'assets')
-D_JPEG = osp.join(TEST_ASSETS, 'damaged_jpeg')
-
-image.read_jpeg(osp.join(D_JPEG, 'corrupt34_2.jpg'))
