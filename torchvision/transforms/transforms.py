7fc47eaa29163d0b6a9d6cdb5363396bb1853989
Areiser <areiser@hm.edu>
2020-08-03 14:30:57 +0200

    Clarify scaling of ToTensor transform for target masks in the documentation (#2507)
    * Clarify scaling of ToTensor transform for target masks in the documentation
    
    * Add link to the segmentation reference
    
    Co-authored-by: vfdev <vfdev.5@gmail.com>
    
    Co-authored-by: vfdev <vfdev.5@gmail.com>

--- torchvision/transforms/transforms.py
+++ torchvision/transforms/transforms.py
@@ -72,6 +72,12 @@ class ToTensor(object):
     or if the numpy.ndarray has dtype = np.uint8
 
     In the other cases, tensors are returned without scaling.
+
+    .. note::
+        Because the input image is scaled to [0.0, 1.0], this transformation should not be used when
+        transforming target image masks. See the `references`_ for implementing the transforms for image masks.
+
+    .. _references: https://github.com/pytorch/vision/tree/master/references/segmentation
     """
 
     def __call__(self, pic):
