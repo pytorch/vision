5f6823d34e48fbae439634da1c35623ca05370eb
ChiangYintso <392711804@qq.com>
2020-08-04 13:55:40 +0800

    format code

--- torchvision/models/detection/generalized_rcnn.py
+++ torchvision/models/detection/generalized_rcnn.py
@@ -78,8 +78,7 @@ class GeneralizedRCNN(nn.Module):
                 if isinstance(boxes, torch.Tensor):
                     if len(boxes.shape) != 2 or boxes.shape[-1] != 4:
                         raise ValueError("Expected target boxes to be a tensor"
-                                         "of shape [N, 4], got {:}.".format(
-                                          boxes.shape))
+                                         "of shape [N, 4], got {:}.".format(boxes.shape))
                 else:
                     raise ValueError("Expected target boxes to be of type "
                                      "Tensor, got {:}.".format(type(boxes)))
