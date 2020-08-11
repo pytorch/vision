3f7415acb79e128805b673a6b97e9c45bbd08908
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-08-11 13:54:18 -0500

    Add CMake build pipeline

---
+++ packaging/build_cmake.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+. "$script_dir/pkg_helpers.bash"
+
+export BUILD_TYPE=conda
+setup_env 0.8.0
+export SOURCE_ROOT_DIR="$PWD"
+setup_conda_pytorch_constraint
+setup_conda_cudatoolkit_constraint
+setup_visual_studio_constraint
+setup_junit_results_folder
+
+mkdir cpp_build
+cd cpp_build
+cmake ..
