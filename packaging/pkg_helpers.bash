e2b97ec273df8d3b0ff2c278b36dd983d077e1d0
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-08-19 16:33:47 -0500

    Use cURL on Windows

--- packaging/pkg_helpers.bash
+++ packaging/pkg_helpers.bash
@@ -309,8 +309,7 @@ download_copy_ffmpeg() {
   mkdir ffmpeg_tmp
   cd ffmpeg_tmp
   if [[ "$OSTYPE" == "msys" ]]; then
-    conda install -yq wget
-    wget -q https://anaconda.org/pytorch/ffmpeg/4.3/download/win-64/ffmpeg-4.3-ha925a31_0.tar.bz2
+    curl -L -q https://anaconda.org/pytorch/ffmpeg/4.3/download/win-64/ffmpeg-4.3-ha925a31_0.tar.bz2 --output ffmpeg-4.3-ha925a31_0.tar.bz2
     tar -xjvf ffmpeg-4.3-ha925a31_0.tar.bz2
     cp Library/bin/*.dll ../torchvision
   else
