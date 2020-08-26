c124bb16491efb3d139daa4fd9ddf76827cc14a9
Bruno Korbar <bjuncek@gmail.com>
2020-08-26 04:34:51 -0500

    API build next

--- torchvision/csrc/cpu/video/register.cpp
+++ torchvision/csrc/cpu/video/register.cpp
@@ -9,7 +9,10 @@ static auto registerVideo =
     torch::class_<Video>("torchvision", "Video")
         .def(torch::init<std::string, std::string, bool>())
         .def("get_current_stream", &Video::getCurrentStream)
-        .def("get_FPS", &Video::getFPS);
+        .def("duration", &Video::getDuration)
+        .def("fps", &Video::getFPS)
+        .def("seek", &Video::Seek)
+        .def("next", &Video::Next);
 
 } //namespace
 #endif
