1a32bc5de02d45a7041799506dd1fd39fdfeb951
neginraoof <neginmr@utexas.edu>
2020-08-26 16:10:54 -0700

    flake8

--- torchvision/ops/_register_onnx_ops.py
+++ torchvision/ops/_register_onnx_ops.py
@@ -28,7 +28,7 @@ def _register_custom_op():
             warnings.warn("ONNX export of ROIAlign with aligned=True does not match PyTorch when using malformed boxes,"
                           " ONNX forces ROIs to be 1x1 or larger.")
             scale = torch.tensor(0.5 / spatial_scale).to(dtype=torch.float)
-            rois = g.op("Sub", rois,  scale)
+            rois = g.op("Sub", rois, scale)
         return g.op('RoiAlign', input, rois, batch_indices, spatial_scale_f=spatial_scale,
                     output_height_i=pooled_height, output_width_i=pooled_width, sampling_ratio_i=sampling_ratio)
 
