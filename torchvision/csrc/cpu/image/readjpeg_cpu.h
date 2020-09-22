9083d6ef90ba27c9a77032987dd67616e6572b9e
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-09-22 00:29:45 -0500

    Use C10_EXPORT

--- torchvision/csrc/cpu/image/readjpeg_cpu.h
+++ torchvision/csrc/cpu/image/readjpeg_cpu.h
@@ -2,4 +2,4 @@
 
 #include <torch/torch.h>
 
-C10_API torch::Tensor decodeJPEG(const torch::Tensor& data);
+C10_EXPORT torch::Tensor decodeJPEG(const torch::Tensor& data);
--- torchvision/csrc/cpu/image/writejpeg_cpu.h
+++ torchvision/csrc/cpu/image/writejpeg_cpu.h
@@ -2,8 +2,8 @@
 
 #include <torch/torch.h>
 
-C10_API torch::Tensor encodeJPEG(const torch::Tensor& data, int64_t quality);
-C10_API void writeJPEG(
+C10_EXPORT torch::Tensor encodeJPEG(const torch::Tensor& data, int64_t quality);
+C10_EXPORT void writeJPEG(
     const torch::Tensor& data,
     std::string filename,
     int64_t quality);
