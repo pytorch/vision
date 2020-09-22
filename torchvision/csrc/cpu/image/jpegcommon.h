c58178695ff02a9d15af439deedb7049b4244530
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-09-21 21:35:47 -0500

    Update common header

--- torchvision/csrc/cpu/image/jpegcommon.h
+++ torchvision/csrc/cpu/image/jpegcommon.h
@@ -5,8 +5,8 @@
 #include <setjmp.h>
 #include <string>
 
-const static JOCTET EOI_BUFFER[1] = {JPEG_EOI};
-const static size_t JPEG_BUF_SIZE = 16384;
+extern const JOCTET EOI_BUFFER[1] = {JPEG_EOI};
+extern const size_t JPEG_BUF_SIZE = 16384;
 
 struct torch_jpeg_error_mgr {
   struct jpeg_error_mgr pub; /* "public" fields */
@@ -15,18 +15,4 @@ struct torch_jpeg_error_mgr {
 };
 
 typedef struct torch_jpeg_error_mgr* torch_jpeg_error_ptr;
-
-void torch_jpeg_error_exit(j_common_ptr cinfo) {
-  /* cinfo->err really points to a torch_jpeg_error_mgr struct, so coerce
-   * pointer */
-  torch_jpeg_error_ptr myerr = (torch_jpeg_error_ptr)cinfo->err;
-
-  /* Always display the message. */
-  /* We could postpone this until after returning, if we chose. */
-  // (*cinfo->err->output_message)(cinfo);
-  /* Create the message */
-  (*(cinfo->err->format_message))(cinfo, myerr->jpegLastErrorMsg);
-
-  /* Return control to the setjmp point */
-  longjmp(myerr->setjmp_buffer, 1);
-}
+extern void torch_jpeg_error_exit(j_common_ptr cinfo);
