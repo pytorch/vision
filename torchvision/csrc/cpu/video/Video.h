5d7a0df7dcdd0a596f9d2e9fa8da265a31a2d8d3
Bruno Korbar <bjuncek@gmail.com>
2020-09-23 06:13:37 -0500

    clangangangang

--- torchvision/csrc/cpu/video/Video.h
+++ torchvision/csrc/cpu/video/Video.h
@@ -3,11 +3,10 @@
 #ifndef VIDEO_H_
 #define VIDEO_H_
 
-
+#include <map>
+#include <regex>
 #include <string>
 #include <vector>
-#include <regex>
-#include <map>
 
 #include <ATen/ATen.h>
 #include <Python.h>
@@ -15,53 +14,54 @@
 #include <torch/script.h>
 
 
-
 #include <exception>
-#include "sync_decoder.h"
-#include "memory_buffer.h"
 #include "defs.h"
-#include "util.h"
+#include "memory_buffer.h"
+#include "sync_decoder.h"
 
 using namespace ffmpeg;
 
-
 struct Video : torch::CustomClassHolder {
-    
-    
-    
-    std::tuple<std::string, long> current_stream; // streaam type, id
-    // global video metadata
-    std::map<std::string, std::vector<double>> streamFPS;
-    std::map<std::string, std::vector<double>> streamDuration;
-    public:
-        Video(std::string videoPath, std::string stream, bool isReadFile);
-        std::tuple<std::string, int64_t> getCurrentStream() const;
-        std::vector<double> getDuration(std::string stream="") const;
-        std::vector<double> getFPS(std::string stream="") const;
-        void Seek(double ts, bool any_frame);
-        std::tuple<torch::Tensor, double> Next(std::string stream);
+  std::tuple<std::string, long> current_stream; // streaam type, id
+  // global video metadata
+  std::map<std::string, std::vector<double>> streamFPS;
+  std::map<std::string, std::vector<double>> streamDuration;
 
-    private:
-        bool video_any_frame=false; // add this to input parameters?
-        bool succeeded=false; // decoder init flag
-        // seekTS and doSeek act as a flag - if it's not set, next function simply
-        // retruns the next frame. If it's set, we look at the global seek
-        // time in comination with any_frame settings
-        double seekTS=-1; 
-        bool doSeek=false;
+ public:
+  Video(std::string videoPath, std::string stream, bool isReadFile);
+  std::tuple<std::string, int64_t> getCurrentStream() const;
+  std::vector<double> getDuration(std::string stream = "") const;
+  std::vector<double> getFPS(std::string stream = "") const;
+  void Seek(double ts, bool any_frame);
+  std::tuple<torch::Tensor, double> Next(std::string stream);
 
-        void _getDecoderParams(double videoStartS, int64_t getPtsOnly, std::string stream, long stream_id, bool all_streams, double seekFrameMarginUs); // this needs to be improved
-        bool _setCurrentStream();
-        std::map<std::string, std::vector<double>> streamTimeBase; // not used
+ private:
+  bool video_any_frame = false; // add this to input parameters?
+  bool succeeded = false; // decoder init flag
+  // seekTS and doSeek act as a flag - if it's not set, next function simply
+  // retruns the next frame. If it's set, we look at the global seek
+  // time in comination with any_frame settings
+  double seekTS = -1;
+  bool doSeek = false;
 
-        DecoderInCallback callback = nullptr;;
-        std::vector<DecoderMetadata> metadata;
-                
-    protected:
-        SyncDecoder decoder;
-        DecoderParameters params;
-    
+  void _getDecoderParams(
+      double videoStartS,
+      int64_t getPtsOnly,
+      std::string stream,
+      long stream_id,
+      bool all_streams,
+      double seekFrameMarginUs); // this needs to be improved
+  bool _setCurrentStream();
+  std::map<std::string, std::vector<double>> streamTimeBase; // not used
+
+  DecoderInCallback callback = nullptr;
+  ;
+  std::vector<DecoderMetadata> metadata;
+
+ protected:
+  SyncDecoder decoder;
+  DecoderParameters params;
+
 }; // struct Video
 
-
-#endif  // VIDEO_H_
+#endif // VIDEO_H_
