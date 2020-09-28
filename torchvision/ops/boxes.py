d616e894f8d1024bf4d8bd583122e45e92a5d265
Aditya Oke <okeaditya315@gmail.com>
2020-09-28 22:02:47 +0530

    Adds docstrings

--- torchvision/ops/boxes.py
+++ torchvision/ops/boxes.py
@@ -136,7 +136,8 @@ def clip_boxes_to_image(boxes: Tensor, size: Tuple[int, int]) -> Tensor:
 def box_cxcywh_to_xyxy(boxes: Tensor) -> Tensor:
     """
     Converts bounding boxes from (cx, cy, w, h) format to (x1, y1, x2, y2) format.
-
+    (cx, cy) refers to center of bounding box
+    (w, h) are width and height of bounding box
     Arguments:
         boxes (Tensor[N, 4]): boxes in (cx, cy, w, h) format which will be converted.
 
@@ -158,7 +159,8 @@ def box_cxcywh_to_xyxy(boxes: Tensor) -> Tensor:
 def box_xyxy_to_cxcywh(boxes: Tensor) -> Tensor:
     """
     Converts bounding boxes from (x1, y1, x2, y2) format to (cx, cy, w, h) format.
-
+    (x1, y1) refer to top left of bounding box
+    (x2, y2) refer to bottom right of bounding box
     Arguments:
         boxes (Tensor[N, 4]): boxes in (x1, y1, x2, y2) format which will be converted.
 
@@ -179,7 +181,8 @@ def box_xyxy_to_cxcywh(boxes: Tensor) -> Tensor:
 def box_xywh_to_xyxy(boxes: Tensor) -> Tensor:
     """
     Converts bounding boxes from (x, y, w, h) format to (x1, y1, x2, y2) format.
-
+    (x, y) refers to top left of bouding box.
+    (w, h) refers to width and height of box.
     Arguments:
         boxes (Tensor[N, 4]): boxes in (x, y, w, h) which will be converted.
 
@@ -194,7 +197,8 @@ def box_xywh_to_xyxy(boxes: Tensor) -> Tensor:
 def box_xyxy_to_xywh(boxes: Tensor) -> Tensor:
     """
     Converts bounding boxes from (x1, y1, x2, y2) format to (x, y, w, h) format.
-
+    (x1, y1) refer to top left of bounding box
+    (x2, y2) refer to bottom right of bounding box
     Arguments:
         boxes (Tensor[N, 4]): boxes in (x1, y1, x2, y2) which will be converted.
 
