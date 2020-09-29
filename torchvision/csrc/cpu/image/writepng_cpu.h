3065fd2fd96311231c18750f26828a6887222bdf
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-09-28 19:31:12 -0500

    Style issues correction

--- torchvision/csrc/cpu/image/writepng_cpu.h
+++ torchvision/csrc/cpu/image/writepng_cpu.h
@@ -2,7 +2,9 @@
 
 #include <torch/torch.h>
 
-C10_EXPORT torch::Tensor encodePNG(const torch::Tensor& data, int64_t compression_level);
+C10_EXPORT torch::Tensor encodePNG(
+    const torch::Tensor& data,
+    int64_t compression_level);
 C10_EXPORT void writePNG(
     const torch::Tensor& data,
     std::string filename,
