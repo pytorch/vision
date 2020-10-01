2425f4526f67cfd282db3f6f341bd7b4682702d5
Aditya Oke <okeaditya315@gmail.com>
2020-10-01 15:53:03 +0530

    fixes documentation

--- torchvision/ops/boxes.py
+++ torchvision/ops/boxes.py
@@ -137,11 +137,19 @@ def clip_boxes_to_image(boxes: Tensor, size: Tuple[int, int]) -> Tensor:
 def box_convert(boxes: Tensor, in_fmt: str, out_fmt: str) -> Tensor:
     """
     Converts boxes from given in_fmt to out_fmt.
+    Supported in_fmt and out_fmt are:
+
+    'xyxy': boxes are represented via corners, x1, y1 being top left and x2, y2 being bottom right.
+
+    'xywh' : boxes are represented via corner, width and height, x1, y2 being top left, w, h being width and height.
+
+    'cxcywh' : boxes are represented via centre, width and height, cx, cy being center of box, w, h
+    being width and height.
 
     Arguments:
         boxes (Tensor[N, 4]): boxes which will be converted.
-        in_fmt (str): Input format of given boxes.
-        out_fmt (str): Output format of given boxes.
+        in_fmt (str): Input format of given boxes. Supported formats are ['xyxy', 'xywh', 'cxcywh'].
+        out_fmt (str): Output format of given boxes. Supported formats are ['xyxy', 'xywh', 'cxcywh']
 
     Returns:
         boxes (Tensor[N, 4]): Boxes into converted format.
@@ -151,35 +159,34 @@ def box_convert(boxes: Tensor, in_fmt: str, out_fmt: str) -> Tensor:
     assert out_fmt in allowed_fmts
 
     if in_fmt == out_fmt:
-        # return boxes.clone()  # to ensure always returning a copy
         boxes_converted = boxes.clone()
         return boxes_converted
 
+    if in_fmt != 'xyxy' and out_fmt != 'xyxy':
+        if in_fmt == "xywh":
+            boxes_xyxy = _box_xywh_to_xyxy(boxes)
+            if out_fmt == "cxcywh":
+                boxes_converted = _box_xyxy_to_cxcywh(boxes_xyxy)
+
+        elif in_fmt == "cxcywh":
+            boxes_xyxy = _box_cxcywh_to_xyxy(boxes)
+            if out_fmt == "xywh":
+                boxes_converted = _box_xyxy_to_xywh(boxes_xyxy)
+
+        # convert one to xyxy and change either in_fmt or out_fmt to xyxy
     else:
-        if in_fmt != 'xyxy' and out_fmt != 'xyxy':
+        if in_fmt == "xyxy":
+            if out_fmt == "xywh":
+                boxes_converted = _box_xyxy_to_xywh(boxes)
+            elif out_fmt == "cxcywh":
+                boxes_converted = _box_xyxy_to_cxcywh(boxes)
+        elif out_fmt == "xyxy":
             if in_fmt == "xywh":
-                boxes_xyxy = _box_xywh_to_xyxy(boxes)
-                if out_fmt == "cxcywh":
-                    boxes_converted = _box_xyxy_to_cxcywh(boxes_xyxy)
-
+                boxes_converted = _box_xywh_to_xyxy(boxes)
             elif in_fmt == "cxcywh":
-                boxes_xyxy = _box_cxcywh_to_xyxy(boxes)
-                if out_fmt == "xywh":
-                    boxes_converted = _box_xyxy_to_xywh(boxes_xyxy)
+                boxes_converted = _box_cxcywh_to_xyxy(boxes)
 
-            # convert one to xyxy and change either in_fmt or out_fmt to xyxy
-        else:
-            if in_fmt == "xyxy":
-                if out_fmt == "xywh":
-                    boxes_converted = _box_xyxy_to_xywh(boxes)
-                elif out_fmt == "cxcywh":
-                    boxes_converted = _box_xyxy_to_cxcywh(boxes)
-            elif out_fmt == "xyxy":
-                if in_fmt == "xywh":
-                    boxes_converted = _box_xywh_to_xyxy(boxes)
-                elif in_fmt == "cxcywh":
-                    boxes_converted = _box_cxcywh_to_xyxy(boxes)
-        return boxes_converted
+    return boxes_converted
 
 
 def box_area(boxes: Tensor) -> Tensor:
