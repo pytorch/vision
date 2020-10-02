b9f7dd6b22abf3bbc7755096ffd4c386ba9bb635
Francisco Massa <fvsmassa@gmail.com>
2020-10-02 18:49:26 +0200

    More tests

--- .circleci/unittest/linux/scripts/setup_env.sh
+++ .circleci/unittest/linux/scripts/setup_env.sh
@@ -41,4 +41,5 @@ conda env update --file "${this_dir}/environment.yml" --prune
 
 conda list
 #conda install av=6.2.0 -c conda-forge -y
-#conda list
+conda install ffmpeg=4.2.2 -c conda-forge -y
+conda list
