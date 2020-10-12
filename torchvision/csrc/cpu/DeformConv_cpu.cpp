b12aa4b561492a419fd46f924691f4551a22e197
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos077.prn3.facebook.com>
2020-10-12 04:30:34 -0700

    2020-10-12 nightly release (610c9d2a063bf0695d4f1c3cc396f0c1f2c3a4af)

--- torchvision/csrc/cpu/DeformConv_cpu.cpp
+++ torchvision/csrc/cpu/DeformConv_cpu.cpp
@@ -326,6 +326,9 @@ at::Tensor DeformConv2d_forward_cpu(
       out_w);
 
   auto out = at::zeros({batch_sz, out_channels, out_h, out_w}, input.options());
+  if (batch_sz == 0) {
+    return out;
+  }
 
   // Separate batches into blocks
   out = out.view({batch_sz / n_parallel_imgs,
@@ -713,6 +716,9 @@ static std::tuple<at::Tensor, at::Tensor> deform_conv2d_backward_input_cpu(
 
   auto grad_input = at::zeros_like(input);
   auto grad_offset = at::zeros_like(offset);
+  if (batch_sz == 0) {
+    return std::make_tuple(grad_input, grad_offset);
+  }
   auto columns = at::empty(
       {n_in_channels * weight_w * weight_h, n_parallel_imgs * out_h * out_w},
       input.options());
@@ -839,6 +845,9 @@ static at::Tensor deform_conv2d_backward_parameters_cpu(
   long out_w = grad_out.size(3);
 
   auto grad_weight = at::zeros_like(weight);
+  if (batch_sz == 0) {
+    return grad_weight;
+  }
 
   at::Tensor grad_out_buf = grad_out
                                 .reshape({batch_sz / n_parallel_imgs,
