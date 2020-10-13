690b8729f242a71c352f917fdc7c3794da2417ab
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-10-12 19:05:30 -0500

    Test relocation in Linux

--- packaging/wheel/relocate.py
+++ packaging/wheel/relocate.py
@@ -16,10 +16,19 @@ import os.path as osp
 from base64 import urlsafe_b64encode
 
 # Third party imports
-import toml
 from auditwheel.lddtree import lddtree
 from wheel.bdist_wheel import get_abi_tag
 
+
+WHITELIST = {
+    'libgcc_s.so.1', 'libstdc++.so.6', 'libm.so.6',
+    'libdl.so.2', 'librt.so.1', 'libc.so.6',
+    'libnsl.so.1', 'libutil.so.1', 'libpthread.so.0',
+    'libresolv.so.2', 'libX11.so.6', 'libXext.so.6',
+    'libXrender.so.1', 'libICE.so.6', 'libSM.so.6',
+    'libGL.so.1', 'libgobject-2.0.so.0', 'libgthread-2.0.so.0',
+    'libglib-2.0.so.0', 'ld-linux-x86-64.so.2', 'ld-2.17.so'
+}
 
 HERE = osp.dirname(osp.abspath(__file__))
 PACKAGE_ROOT = osp.dirname(osp.dirname(HERE))
@@ -101,6 +110,120 @@ def patch_new_path(library_path, new_dir):
     return osp.join(new_dir, new_name)
 
 
+def relocate_library(patchelf, output_dir, output_library, binary):
+    print('Relocating {0}'.format(binary))
+    binary_path = osp.join(output_library, binary)
+
+    ld_tree = lddtree(binary_path)
+    tree_libs = ld_tree['libs']
+
+    binary_queue = [(n, binary) for n in ld_tree['needed']]
+    binary_paths = {binary: binary_path}
+    binary_dependencies = {}
+
+    while binary_queue != []:
+        library, parent = binary_queue.pop(0)
+        library_info = tree_libs[library]
+        print(library)
+
+        if library_info['path'] is None:
+            print('Omitting {0}'.format(library))
+            continue
+
+        if library in WHITELIST:
+            # Omit glibc/gcc/system libraries
+            print('Omitting {0}'.format(library))
+            continue
+
+        parent_dependencies = binary_dependencies.get(parent, [])
+        parent_dependencies.append(library)
+        binary_dependencies[parent] = parent_dependencies
+
+        if library in binary_paths:
+            continue
+
+        binary_paths[library] = library_info['path']
+        binary_queue += [(n, library) for n in library_info['needed']]
+
+    print('Copying dependencies to wheel directory')
+    new_libraries_path = osp.join(output_dir, 'torchvision.libs')
+    os.makedirs(new_libraries_path)
+
+    new_names = {binary: binary_path}
+
+    for library in binary_paths:
+        if library != binary:
+            library_path = binary_paths[library]
+            new_library_path = patch_new_path(library_path, new_libraries_path)
+            print('{0} -> {1}'.format(library, new_library_path))
+            shutil.copyfile(library_path, new_library_path)
+            new_names[library] = new_library_path
+
+    print('Updating dependency names by new files')
+    for library in binary_paths:
+        if library != binary:
+            if library not in binary_dependencies:
+                continue
+            library_dependencies = binary_dependencies[library]
+            new_library_name = new_names[library]
+            for dep in library_dependencies:
+                new_dep = osp.basename(new_names[dep])
+                print('{0}: {1} -> {2}'.format(library, dep, new_dep))
+                subprocess.check_output(
+                    [
+                        patchelf,
+                        '--replace-needed',
+                        dep,
+                        new_dep,
+                        new_library_name
+                    ],
+                    cwd=new_libraries_path)
+
+            print('Updating library rpath')
+            subprocess.check_output(
+                [
+                    patchelf,
+                    '--set-rpath',
+                    "$ORIGIN",
+                    new_library_name
+                ],
+                cwd=new_libraries_path)
+
+            subprocess.check_output(
+                [
+                    patchelf,
+                    '--print-rpath',
+                    new_library_name
+                ],
+                cwd=new_libraries_path)
+
+    print("Update main library dependencies")
+    library_dependencies = binary_dependencies[binary]
+    for dep in library_dependencies:
+        new_dep = osp.basename(new_names[dep])
+        print('{0}: {1} -> {2}'.format(binary, dep, new_dep))
+        subprocess.check_output(
+            [
+                patchelf,
+                '--replace-needed',
+                dep,
+                new_dep,
+                binary
+            ],
+            cwd=output_library)
+
+    print('Update main library rpath')
+    subprocess.check_output(
+        [
+            patchelf,
+            '--set-rpath',
+            "$ORIGIN:$ORIGIN/../torchvision.libs",
+            binary_path
+        ],
+        cwd=output_library
+    )
+
+
 def patch_linux():
     # Get patchelf location
     patchelf = find_program('patchelf')
@@ -120,123 +243,23 @@ def patch_linux():
 
     image_binary = 'image.so'
     video_binary = 'video_reader.so'
+    torchvision_binaries = [image_binary, video_binary]
     for wheel in wheels:
         print('Unzipping wheel...')
-        print('{0}'.format(osp.basename(wheel)))
+        wheel_file = osp.basename(wheel)
+        wheel_dir = osp.dirname(wheel)
+        print('{0}'.format(wheel_file))
+        wheel_name, _ = osp.splitext(wheel_file)
         unzip_file(wheel, output_dir)
 
         print('Finding ELF dependencies...')
-
         output_library = osp.join(output_dir, 'torchvision')
-        image_binary_path = osp.join(output_library, image_binary)
-
-        ld_tree = lddtree(image_binary_path)
-        tree_libs = ld_tree['libs']
-
-        binary_queue = [(n, image_binary) for n in ld_tree['needed']]
-        binary_paths = {image_binary: image_binary_path}
-        binary_dependencies = {}
-
-        while binary_queue != []:
-            library, parent = binary_queue.pop(0)
-            library_info = tree_libs[library]
-            print(library)
-            print(library_info)
-            if (library_info['path'].startswith('/lib')):
-                # Omit glibc/gcc/system libraries
-                continue
-
-            parent_dependencies = binary_dependencies.get(parent, [])
-            parent_dependencies.append(library)
-            binary_dependencies[parent] = parent_dependencies
-
-            if library in binary_paths:
-                continue
-
-            binary_paths[library] = library_info['path']
-            binary_queue += [(n, library) for n in library_info['needed']]
-
-        print('Copying dependencies to wheel directory')
-        new_libraries_path = osp.join(output_dir, 'duckling.libs')
-        os.makedirs(new_libraries_path)
-        new_names = {main_binary: binary_path}
-
-        for library in binary_paths:
-            if library != main_binary:
-                library_path = binary_paths[library]
-                new_library_path = patch_new_path(library_path, new_libraries_path)
-                print('{0} -> {1}'.format(library, new_library_path))
-                shutil.copyfile(library_path, new_library_path)
-                new_names[library] = new_library_path
-
-        print('Updating dependency names by new files')
-        for library in binary_paths:
-            if library != main_binary:
-                if library not in binary_dependencies:
-                    continue
-                library_dependencies = binary_dependencies[library]
-                new_library_name = new_names[library]
-                for dep in library_dependencies:
-                    new_dep = osp.basename(new_names[dep])
-                    print('{0}: {1} -> {2}'.format(library, dep, new_dep))
-                    subprocess.check_output(
-                        [
-                            patchelf,
-                            '--replace-needed',
-                            dep,
-                            new_dep,
-                            new_library_name
-                        ],
-                        cwd=new_libraries_path)
-
-                print('Updating library rpath')
-                subprocess.check_output(
-                    [
-                        patchelf,
-                        '--set-rpath',
-                        "$ORIGIN",
-                        new_library_name
-                    ],
-                    cwd=new_libraries_path)
-
-                subprocess.check_output(
-                    [
-                        patchelf,
-                        '--print-rpath',
-                        new_library_name
-                    ],
-                    cwd=new_libraries_path)
-
-        print("Update main library dependencies")
-        library_dependencies = binary_dependencies[main_binary]
-        for dep in library_dependencies:
-            new_dep = osp.basename(new_names[dep])
-            print('{0}: {1} -> {2}'.format(main_binary, dep, new_dep))
-            subprocess.check_output(
-                [
-                    patchelf,
-                    '--replace-needed',
-                    dep,
-                    new_dep,
-                    main_binary
-                ],
-                cwd=output_library)
-
-        print('Update main library rpath')
-        subprocess.check_output(
-            [
-                patchelf,
-                '--set-rpath',
-                "$ORIGIN:$ORIGIN/../duckling.libs",
-                binary_path
-            ],
-            cwd=output_library
-        )
+        for binary in torchvision_binaries:
+            if osp.exists(osp.join(output_library, binary)):
+                relocate_library(patchelf, output_dir, output_library, binary)
 
         print('Update RECORD file in wheel')
-        dist_info = osp.join(
-            output_dir, 'pyduckling_native-{0}.dist-info'.format(version))
-        record_file = osp.join(dist_info, 'RECORD')
+        record_file = glob.glob(osp.join(output_dir, '*.dist-info'))[0]
 
         with open(record_file, 'w') as f:
             for root, _, files in os.walk(output_dir):
@@ -250,9 +273,9 @@ def patch_linux():
                         f.write('{0},{1},{2}\n'.format(rel_file, digest, size))
 
         print('Compressing wheel')
-        shutil.make_archive(dist, 'zip', output_dir)
-        os.remove(dist)
-        shutil.move('{0}.zip'.format(dist), dist)
+        shutil.make_archive(wheel_name, 'zip', output_dir)
+        os.remove(wheel)
+        shutil.move('{0}.zip'.format(osp.join(wheel_dir, wheel_name)), wheel)
         shutil.rmtree(output_dir)
 
 
