dcf6b915d476adfb1ad26768d34c10d98ae36bb9
Bruno Korbar <korbar@vggdev9.vggdev.cluster>
2020-10-15 18:06:56 +0100

    Clearing up some documentation as a result of a feedback

--- torchvision/io/__init__.py
+++ torchvision/io/__init__.py
@@ -91,11 +91,15 @@ class VideoReader:
         self._c = torch.classes.torchvision.Video(path, stream)
 
     def __next__(self):
-        """Decodes and returns the next frame of the current stream
+        """Decodes and returns the next frame of the current stream.
+        At the moment, frames are encoded as a dict with mandatory
+        data and pts fields, where data is a tensor, and pts is a
+        presentation timestamp of the frame expressed in seconds
+        as a float.
 
         Returns:
             (dict): a dictionary with fields ``data`` and ``pts``
-            containing decoded frame and corresponding timestamp
+                containing decoded frame and corresponding timestamp
 
         """
         frame, pts = self._c.next()
