0086e0ea71ea439967acf96a7e88e337ddb6f51b
Bruno Korbar <korbar@vggdev9.vggdev.cluster>
2020-10-19 21:28:08 +0100

    Addressing Victor's comments.

--- torchvision/io/__init__.py
+++ torchvision/io/__init__.py
@@ -91,7 +91,12 @@ class VideoReader:
 
     def __init__(self, path, stream="video"):
         if not _has_video_opt():
-            raise RuntimeError("Not compiled with video_reader support")
+            raise RuntimeError(
+                "Not compiled with video_reader support, "
+                + "to enable video_reader support, please install "
+                + "ffmpeg (version 4.2 is currently supported) and"
+                + "build torchvision from source."
+            )
         self._c = torch.classes.torchvision.Video(path, stream)
 
     def __next__(self):
