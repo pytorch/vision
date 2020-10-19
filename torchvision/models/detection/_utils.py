2fa1bfdac5cb2e368d32a71c5cd9fbd012ba47f9
Alex Ter-Sarkisov <ater1980@gmail.com>
2020-10-19 19:49:48 +0100

    Update _utils.py
    fixed style

--- torchvision/models/detection/_utils.py
+++ torchvision/models/detection/_utils.py
@@ -350,6 +350,7 @@ def smooth_l1_loss(input, target, beta: float = 1. / 9, size_average: bool = Tru
         return loss.mean()
     return loss.sum()
 
+
 ########################   AVERAGE PRECISION COMPUTATION ########################
 # adapted from Matterport Mask R-CNN implementation                             #
 # https://github.com/matterport/Mask_RCNN                                       #
@@ -373,10 +374,8 @@ def compute_overlaps_masks(masks1, masks2):
     # intersections and union: transpose predictions, the overlap matrix is num_predxnum_gts
     intersections = masks1.float().matmul(masks2.t().float())
     # +1: divide by 0
-    overlaps = intersections / (union-intersections)
+    overlaps = intersections / (union - intersections)
     return overlaps
-
-
 
 
 # compute average precision for the  specified IoU threshold
@@ -388,7 +387,7 @@ def compute_matches(gt_boxes, gt_class_ids, gt_masks,
     pred_boxes = pred_boxes[indices]
     pred_class_ids = pred_class_ids[indices]
     pred_scores = pred_scores[indices]
-    pred_masks = pred_masks[indices,...]
+    pred_masks = pred_masks[indices, ...]
     # Compute IoU overlaps [pred_masks, gt_masks]
     overlaps = compute_overlaps_masks(pred_masks, gt_masks)
     # separate predictions for each gt object (a total of gt_masks splits
@@ -402,46 +401,39 @@ def compute_matches(gt_boxes, gt_class_ids, gt_masks,
     for _i, splits in enumerate(split_overlaps):
         # ground truth class
         gt_class = gt_class_ids[_i]
-        if (splits>iou_threshold).any():
-           # get best predictions, their indices inthe IoU tensor and their classes
-           global_best_preds_inds = torch.nonzero(splits[0]>iou_threshold).view(-1)
-           pred_classes = pred_class_ids[global_best_preds_inds]
-           best_preds = splits[0][splits[0]>iou_threshold]
-           #  sort them locally-nothing else,
-           local_best_preds_sorted = best_preds.argsort().flip(dims=(0,))
-           # loop through each prediction's index, sorted in the descending order
-           for p in local_best_preds_sorted:
-               if pred_classes[p]==gt_class:
-                  # Hit?
-                  match_count +=1
-                  pred_match[global_best_preds_inds[p]] = _i
-                  gt_match[_i] = global_best_preds_inds[p]
-                  # important: if the prediction is True Positive, finish the loop
-                  break
-
+        if (splits > iou_threshold).any():
+            # get best predictions, their indices inthe IoU tensor and their classes
+            global_best_preds_inds = torch.nonzero(splits[0] > iou_threshold).view(-1)
+            pred_classes = pred_class_ids[global_best_preds_inds]
+            best_preds = splits[0][splits[0] > iou_threshold]
+            #  sort them locally-nothing else,
+            local_best_preds_sorted = best_preds.argsort().flip(dims=(0,))
+            # loop through each prediction's index, sorted in the descending order
+            for p in local_best_preds_sorted:
+                if pred_classes[p] == gt_class:
+                    # Hit?
+                    match_count += 1
+                    pred_match[global_best_preds_inds[p]] = _i
+                    gt_match[_i] = global_best_preds_inds[p]
+                    # important: if the prediction is True Positive, finish the loop
+                    break
 
     return gt_match, pred_match, overlaps
-
-
 
 
 # AP for a single IoU threshold and 1 image
 def compute_ap(gt_boxes, gt_class_ids, gt_masks,
                pred_boxes, pred_class_ids, pred_scores, pred_masks,
                iou_threshold=0.5):
-
-
     # Get matches and overlaps
     gt_match, pred_match, overlaps = compute_matches(
         gt_boxes, gt_class_ids, gt_masks,
         pred_boxes, pred_class_ids, pred_scores, pred_masks,
         iou_threshold)
 
-
     # Compute precision and recall at each prediction box step
-    precisions = (pred_match>-1).cumsum(dim=0).float().div(torch.arange(pred_match.numel()).float()+1)
-    recalls = (pred_match>-1).cumsum(dim=0).float().div(gt_match.numel())
-
+    precisions = (pred_match > -1).cumsum(dim=0).float().div(torch.arange(pred_match.numel()).float() + 1)
+    recalls = (pred_match > -1).cumsum(dim=0).float().div(gt_match.numel())
 
     # Pad with start and end values to simplify the math
     precisions = torch.cat([torch.tensor([0]).float(), precisions, torch.tensor([0]).float()])
@@ -452,7 +444,7 @@ def compute_ap(gt_boxes, gt_class_ids, gt_masks,
     for i in range(len(precisions) - 2, -1, -1):
         precisions[i] = torch.max(precisions[i], precisions[i + 1])
     # Compute mean AP over recall range
-    indices = torch.nonzero(recalls[:-1] !=recalls[1:]).squeeze_(1)+1
+    indices = torch.nonzero(recalls[:-1] != recalls[1:]).squeeze_(1) + 1
     mAP = torch.sum((recalls[indices] - recalls[indices - 1]) *
-                 precisions[indices])
+                    precisions[indices])
     return mAP, precisions, recalls, overlaps
