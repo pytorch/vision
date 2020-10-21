ee8567473745392a51aea23faa4e7b38103a9586
frgfm <fgfm03@hotmail.fr>
2020-10-21 22:42:06 +0200

    fix: Fixed annotation typing

--- torchvision/models/vgg.py
+++ torchvision/models/vgg.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from .utils import load_state_dict_from_url
 from typing import Union
-from torch.jit.annotations import List
+from torch.jit.annotations import List, Dict
 
 
 __all__ = [
@@ -71,19 +71,19 @@ def make_layers(cfg: List[Union[str, int]], batch_norm: bool = False) -> nn.Sequ
     layers = []
     in_channels = 3
     for v in cfg:
-        if v == 'M':
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-        else:
-            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+        if isinstance(v, int):
+            conv2d = nn.Conv2d(in_channels, int(v), kernel_size=3, padding=1)
             if batch_norm:
                 layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
             else:
                 layers += [conv2d, nn.ReLU(inplace=True)]
             in_channels = v
+        else:
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
     return nn.Sequential(*layers)
 
 
-cfgs = {
+cfgs: Dict[str, List[Union[str, int]]] = {
     'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
     'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
     'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
