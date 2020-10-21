624270fd804a0a5e013626e0a22c69ced24b43b9
frgfm <fgfm03@hotmail.fr>
2020-10-21 13:16:58 +0200

    docs: Fixed docstring of RandomResizedCrop

--- torchvision/transforms/transforms.py
+++ torchvision/transforms/transforms.py
@@ -739,7 +739,7 @@ class RandomResizedCrop(torch.nn.Module):
             int instead of sequence like (h, w), a square output size ``(size, size)`` is
             made. If provided a tuple or list of length 1, it will be interpreted as (size[0], size[0]).
         scale (tuple of float): range of size of the origin size cropped
-        ratio (tuple of float): range of aspect ratio of the origin aspect ratio cropped.
+        ratio (tuple of float): range of the crop's aspect ratio before resizing.
         interpolation (int): Desired interpolation enum defined by `filters`_.
             Default is ``PIL.Image.BILINEAR``. If input is Tensor, only ``PIL.Image.NEAREST``, ``PIL.Image.BILINEAR``
             and ``PIL.Image.BICUBIC`` are supported.
