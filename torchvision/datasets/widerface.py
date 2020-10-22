e4ee45ff3b7e1659d85ccc5ac14bfa46931c1dd7
Joshua Bradley <jgbradley1@gmail.com>
2020-10-22 01:55:45 -0400

    improve parsing of annotation files

--- torchvision/datasets/widerface.py
+++ torchvision/datasets/widerface.py
@@ -10,11 +10,11 @@ import string
 import gzip
 import lzma
 from typing import Any, Callable, Dict, IO, List, Optional, Tuple, Union
-from .utils import download_and_extract_archive
+from .utils import download_file_from_google_drive, download_and_extract_archive, check_integrity
 
 
 class WIDERFace(VisionDataset):
-    """`WIDERFace <http://yann.lecun.com/exdb/mnist/>`_ Dataset.
+    """`WIDERFace <http://shuoyang1213.me/WIDERFACE/>`_ Dataset.
 
     WIDER FACE dataset is a face detection benchmark dataset, of which images are 
     selected from the publicly available WIDER dataset. We choose 32,203 images and 
@@ -33,8 +33,8 @@ class WIDERFace(VisionDataset):
 	    Year = {2016}}
 
     Args:
-        root (string): Root directory of dataset where ``MNIST/processed/training.pt``
-            and  ``MNIST/processed/test.pt`` exist.
+        root (string): Root directory of dataset where ``widerface/WIDER_train.zip widerface/WIDER_val.zip``
+            and  ``widerface/WIDER_test.zip widerface/wider_face_split.zip`` exist.
         split (string): One of {'train', 'valid', 'test', 'all'}.
             The specified dataset is selected.
             Defaults to ``train``.
@@ -56,12 +56,16 @@ class WIDERFace(VisionDataset):
             downloaded again.
     """
 
+    base_folder = "widerface"
     file_list = [
-        # Download URL                                                                  MD5 Hash                            Filename
-        # ("https://drive.google.com/uc?export=download&id=0B6eKvaijfFUDQUUwd21EckhUbWs", "3fedf70df600953d25982bcd13d91ba2", "WIDER_train.zip"),
-        ("https://drive.google.com/uc?export=download&id=0B6eKvaijfFUDd3dIRmpvSk8tLUk", "dfa7d7e790efa35df3788964cf0bbaea", "WIDER_val.zip"),
-        # ("https://drive.google.com/uc?export=download&id=0B6eKvaijfFUDbW4tdGpaYjgzZkU", "e5d8f4248ed24c334bbd12f49c29dd40", "WIDER_test.zip")
+        # File ID                        MD5 Hash                            Filename
+        ("0B6eKvaijfFUDQUUwd21EckhUbWs", "3fedf70df600953d25982bcd13d91ba2", "WIDER_train.zip"),
+        ("0B6eKvaijfFUDd3dIRmpvSk8tLUk", "dfa7d7e790efa35df3788964cf0bbaea", "WIDER_val.zip"),
+        ("0B6eKvaijfFUDbW4tdGpaYjgzZkU", "e5d8f4248ed24c334bbd12f49c29dd40", "WIDER_test.zip")
     ]
+    annotations_file = ("http://mmlab.ie.cuhk.edu.hk/projects/WIDERFace/support/bbx_annotation/wider_face_split.zip",
+                        "0e3767bcf0e326556d407bf5bff5d27c",
+                        "wider_face_split.zip")
 
     def __init__(
             self,
@@ -74,26 +78,83 @@ class WIDERFace(VisionDataset):
     ) -> None:
         super(WIDERFace, self).__init__(root, transform=transform,
                                     target_transform=target_transform)
-        print("root dir: " + root)
-        print(self.root)
+        print("root dir: " + self.root)
+        self.imgs_path = []
+        self.words = []
         self.split = split
 
         if isinstance(target_type, list):
             self.target_type = target_type
         else:
             self.target_type = [target_type]
-
         if not self.target_type and self.target_transform is not None:
             raise RuntimeError('target_transform is specified but target_type is empty')
 
-        # if download:
-        #     self.download()
-
-        # if not self._check_exists():
-        #     raise RuntimeError('Dataset not found.' +
-        #                        ' You can use download=True to download it')
+        if download:
+            self.download()
         
-        print("done downloading wider face")
+        if not self._check_integrity():
+            raise RuntimeError("Dataset not found or corrupted. " +
+                               "You can use download=True to download it")
+
+        print("Finished initializing WIDERFace")
+
+        ann_file = os.path.expanduser(os.path.join(self.root, self.base_folder, "wider_face_split", "wider_face_train_bbx_gt.txt"))
+        print("ann_file: " + ann_file)
+        f = open(ann_file, "r")
+        lines = f.readlines()
+
+        isFile = True
+        isNumBoxes, isBoxAnnotation = False, False
+        num_boxes = 0
+        box_counter = 0
+        labels = []
+        for line in lines:
+            line = line.rstrip()
+            if isFile:
+                # print(line)
+                self.imgs_path.append(line)
+                isFile = False
+                isNumBoxes = True
+            elif isNumBoxes:
+                num_boxes = int(line)
+                isNumBoxes = False
+                isBoxAnnotation = True
+            elif isBoxAnnotation:
+                box_counter += 1
+                # line = line.split(" ")
+                # line = [int(x) for x in line]
+                # labels.append(line)
+                if box_counter == num_boxes:
+                    isBoxAnnotation = False
+                    isFile = True
+                    # print("read {} bounding boxes".format(box_counter))
+                    # self.words.append(labels.copy())
+                    box_counter = 0
+                    # labels.clear()
+            else:
+                print("ERROR parsing annotations file")
+
+        # isFirst = True
+        # labels = []
+        # for line in lines:
+        #     line = line.rstrip()
+        #     if line.startswith("#"):
+        #         if isFirst is True:
+        #             isFirst = False
+        #         else:
+        #             labels_copy = labels.copy()
+        #             self.words.append(labels_copy)
+        #             labels.clear()
+        #         path = line[2:]
+        #         path = ann_file.replace("label.txt","images/") + path
+        #         self.imgs_path.append(path)
+        #     else:
+        #         line = line.split(" ")
+        #         label = [float(x) for x in line]
+        #         labels.append(label)
+        # self.words.append(labels)
+
 
         # if self.train:
         #     data_file = self.training_file
@@ -109,59 +170,60 @@ class WIDERFace(VisionDataset):
         Returns:
             tuple: (image, target) where target is index of the target class.
         """
-        img, target = self.data[index], int(self.targets[index])
+        # img, target = self.data[index], int(self.targets[index])
 
-        # doing this so that it is consistent with all other datasets
-        # to return a PIL Image
-        img = Image.fromarray(img.numpy(), mode='L')
+        # # doing this so that it is consistent with all other datasets
+        # # to return a PIL Image
+        # img = Image.fromarray(img.numpy(), mode='L')
 
-        if self.transform is not None:
-            img = self.transform(img)
+        # if self.transform is not None:
+        #     img = self.transform(img)
 
-        if self.target_transform is not None:
-            target = self.target_transform(target)
+        # if self.target_transform is not None:
+        #     target = self.target_transform(target)
 
-        return img, target
+        # return img, target
+        return 0, 1
 
     def __len__(self) -> int:
-        return len(self.data)
-    
+        return len(self.imgs_path)
+
+    # TODO - checking integrity of the annotations_file is not working
+    def _check_integrity(self) -> bool:
+        all_files = self.file_list.copy()
+        all_files.append(self.annotations_file)
+
+        for (_, md5, filename) in all_files:
+            fpath = os.path.join(self.root, self.base_folder, filename)
+            _, ext = os.path.splitext(filename)
+            # Allow original archive to be deleted (zip and 7z)
+            # Only need the extracted images
+            print("filename: " + fpath)
+            if ext not in [".zip", ".7z"] and not check_integrity(fpath, md5):
+                return False
+
+        # Should check a hash of the images
+        return os.path.isdir(os.path.join(self.root, self.base_folder, "WIDER_train"))
+
     def download(self) -> None:
-        for (file_url, md5, filename) in self.file_list:
-            download_and_extract_archive(url=file_url, download_root=self.root, filename=filename, md5=md5)
-            # download_file_from_google_drive(file_id, os.path.join(self.root, self.base_folder), filename, md5)
+        import zipfile
 
-        # with zipfile.ZipFile(os.path.join(self.root, self.base_folder, "img_align_celeba.zip"), "r") as f:
-        #     f.extractall(os.path.join(self.root, self.base_folder))
-    
-    # def download_mnist(self) -> None:
-    #     """Download the MNIST data if it doesn't exist in processed_folder already."""
+        # if self._check_integrity():
+        #     print('Files already downloaded and verified')
+        #     return
 
-    #     if self._check_exists():
-    #         return
+        # download data
+        for (file_id, md5, filename) in self.file_list:
+            download_file_from_google_drive(file_id, os.path.join(self.root, self.base_folder), filename, md5)
 
-    #     os.makedirs(self.raw_folder, exist_ok=True)
-    #     os.makedirs(self.processed_folder, exist_ok=True)
+        # extract data
+        for (file_id, md5, filename) in self.file_list:
+            with zipfile.ZipFile(os.path.join(self.root, self.base_folder, filename), "r") as f:
+                f.extractall(os.path.join(self.root, self.base_folder))
 
-    #     # download files
-    #     for url, md5 in self.resources:
-    #         filename = url.rpartition('/')[2]
-    #         download_and_extract_archive(url, download_root=self.raw_folder, filename=filename, md5=md5)
-
-    #     # process and save as torch files
-    #     print('Processing...')
-
-    #     training_set = (
-    #         read_image_file(os.path.join(self.raw_folder, 'train-images-idx3-ubyte')),
-    #         read_label_file(os.path.join(self.raw_folder, 'train-labels-idx1-ubyte'))
-    #     )
-    #     test_set = (
-    #         read_image_file(os.path.join(self.raw_folder, 't10k-images-idx3-ubyte')),
-    #         read_label_file(os.path.join(self.raw_folder, 't10k-labels-idx1-ubyte'))
-    #     )
-    #     with open(os.path.join(self.processed_folder, self.training_file), 'wb') as f:
-    #         torch.save(training_set, f)
-    #     with open(os.path.join(self.processed_folder, self.test_file), 'wb') as f:
-    #         torch.save(test_set, f)
-
-    #     print('Done!')
+        # download and extract annotations files
+        download_and_extract_archive(url=self.annotations_file[0],
+                                     download_root=os.path.join(self.root, self.base_folder),
+                                     extract_root=os.path.join(self.root, self.base_folder),
+                                     filename=self.annotations_file[2],
+                                     md5=self.annotations_file[1])
