2fb21bdc01ddc0e0f3c84ccd11e446b97f85aef0
frgfm <fgfm03@hotmail.fr>
2020-10-22 12:05:16 +0200

    refactor: Removed un-necessary import

--- torchvision/models/resnet.py
+++ torchvision/models/resnet.py
@@ -2,8 +2,7 @@ import torch
 from torch import Tensor
 import torch.nn as nn
 from .utils import load_state_dict_from_url
-from typing import Type, Any, Callable, Union
-from torch.jit.annotations import List, Optional
+from typing import Type, Any, Callable, Union, List, Optional
 
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
