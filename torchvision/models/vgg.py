52be09b2e4ad004bdc854477ff5953fd007a4f91
frgfm <fgfm03@hotmail.fr>
2020-10-23 11:38:00 +0200

    refactor: Refactored typing to minize changes

--- torchvision/models/vgg.py
+++ torchvision/models/vgg.py
@@ -70,15 +70,16 @@ def make_layers(cfg: List[Union[str, int]], batch_norm: bool = False) -> nn.Sequ
     layers = []
     in_channels = 3
     for v in cfg:
-        if isinstance(v, int):
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            v = int(v)
             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
             if batch_norm:
                 layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
             else:
                 layers += [conv2d, nn.ReLU(inplace=True)]
             in_channels = v
-        else:
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
     return nn.Sequential(*layers)
 
 
