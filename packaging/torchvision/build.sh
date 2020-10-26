60aa67a805b3fbd2941817ff9433948a77d91f7e
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-10-26 18:40:22 -0500

    Try to install pyelftools solely

--- packaging/torchvision/build.sh
+++ packaging/torchvision/build.sh
@@ -5,9 +5,6 @@ if [[ $(uname) == "Linux" ]]; then
     pushd pyelftools-0.26
     python setup.py install --single-version-externally-managed --record files.txt
     popd
-    pushd auditwheel-3.2.0
-    python setup.py install --single-version-externally-managed --record files.txt
-    popd
     popd
 fi
 
@@ -16,9 +13,6 @@ python setup.py install --single-version-externally-managed --record=record.txt
 if [[ $(uname) == "Linux" ]]; then
     pushd ext_deps
     pushd pyelftools-0.26
-    cat files.txt | xargs rm -rf
-    popd
-    pushd auditwheel-3.2.0
     cat files.txt | xargs rm -rf
     popd
     popd
