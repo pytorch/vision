87b0736f4e89b5a4ec8d67e5906340f70adb2346
Vasileios Vryniotis <vvryniotis@fb.com>
2020-10-27 15:11:38 +0000

    Clean up and refactor ROIAlign implementation: - Remove primitive const declaration from method names. - Passing as const ref instead of value where possible. - Remove unnecessary headers.

--- torchvision/csrc/cpu/vision_cpu.h
+++ torchvision/csrc/cpu/vision_cpu.h
@@ -24,24 +24,24 @@ VISION_API at::Tensor ROIPool_backward_cpu(
 VISION_API at::Tensor ROIAlign_forward_cpu(
     const at::Tensor& input,
     const at::Tensor& rois,
-    const double spatial_scale,
-    const int64_t pooled_height,
-    const int64_t pooled_width,
-    const int64_t sampling_ratio,
-    const bool aligned);
+    double spatial_scale,
+    int64_t pooled_height,
+    int64_t pooled_width,
+    int64_t sampling_ratio,
+    bool aligned);
 
 VISION_API at::Tensor ROIAlign_backward_cpu(
     const at::Tensor& grad,
     const at::Tensor& rois,
-    const double spatial_scale,
-    const int64_t pooled_height,
-    const int64_t pooled_width,
-    const int64_t batch_size,
-    const int64_t channels,
-    const int64_t height,
-    const int64_t width,
-    const int64_t sampling_ratio,
-    const bool aligned);
+    double spatial_scale,
+    int64_t pooled_height,
+    int64_t pooled_width,
+    int64_t batch_size,
+    int64_t channels,
+    int64_t height,
+    int64_t width,
+    int64_t sampling_ratio,
+    bool aligned);
 
 VISION_API std::tuple<at::Tensor, at::Tensor> PSROIPool_forward_cpu(
     const at::Tensor& input,
