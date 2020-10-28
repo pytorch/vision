47d0c90687e2516622dadcae2b39ab27f07f84a4
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-10-27 23:53:05 -0500

    Use machomachomangler on unittest

--- .circleci/unittest/windows/scripts/run_test.sh
+++ .circleci/unittest/windows/scripts/run_test.sh
@@ -5,5 +5,6 @@ set -e
 eval "$(./conda/Scripts/conda.exe 'shell.bash' 'hook')"
 conda activate ./env
 
+conda remove -y ffmpeg
 python -m torch.utils.collect_env
 pytest --cov=torchvision --junitxml=test-results/junit.xml -v --durations 20 test --ignore=test/test_datasets_download.py
