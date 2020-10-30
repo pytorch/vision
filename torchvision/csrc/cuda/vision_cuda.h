b06e43d60ddc51e090c387c49ee24289c81ba4a7
Vasilis Vryniotis <datumbox@users.noreply.github.com>
2020-10-30 11:28:33 +0000

    PSROIAlign + Dispatcher + Autocast + Code Cleanup (#2928)
    * Fixing types.
    
    * Dispatcher + Autocast.
    
    * + Autograd.
    
    * Clean up and refactor PSROIAlign implementation:
    - Remove primitive const declaration from method names.
    - Using references when possible.
    - Sync naming of internal methods with other ops.
    
    * Restoring names of internal methods to avoid conflicts.
    
    * Restore include headers.

--- torchvision/csrc/cuda/vision_cuda.h
+++ torchvision/csrc/cuda/vision_cuda.h
@@ -65,23 +65,23 @@ VISION_API at::Tensor PSROIPool_backward_cuda(
 VISION_API std::tuple<at::Tensor, at::Tensor> PSROIAlign_forward_cuda(
     const at::Tensor& input,
     const at::Tensor& rois,
-    const float spatial_scale,
-    const int pooled_height,
-    const int pooled_width,
-    const int sampling_ratio);
+    double spatial_scale,
+    int64_t pooled_height,
+    int64_t pooled_width,
+    int64_t sampling_ratio);
 
 VISION_API at::Tensor PSROIAlign_backward_cuda(
     const at::Tensor& grad,
     const at::Tensor& rois,
-    const at::Tensor& mapping_channel,
-    const float spatial_scale,
-    const int pooled_height,
-    const int pooled_width,
-    const int sampling_ratio,
-    const int batch_size,
-    const int channels,
-    const int height,
-    const int width);
+    const at::Tensor& channel_mapping,
+    double spatial_scale,
+    int64_t pooled_height,
+    int64_t pooled_width,
+    int64_t sampling_ratio,
+    int64_t batch_size,
+    int64_t channels,
+    int64_t height,
+    int64_t width);
 
 VISION_API at::Tensor nms_cuda(
     const at::Tensor& dets,
