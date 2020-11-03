249b7893d0f5f5f2929add5e04a802e2f9266ff6
peterjc123 <peterghost86@gmail.com>
2020-11-03 11:15:19 +0800

    More fixes

--- torchvision/csrc/cpu/image/read_write_file_cpu.cpp
+++ torchvision/csrc/cpu/image/read_write_file_cpu.cpp
@@ -20,8 +20,17 @@ torch::Tensor read_file(VISION_STRING filename) {
   struct VISION_STAT stat_buf;
   int rc = VISION_STAT(filename.c_str(), &stat_buf);
   // errno is a variable defined in errno.h
+#if defined(_WIN32) && defined(UNICODE)
+  size_t size = wcslen(filename) + 1;
+  std::unique_ptr<char> filename_ascii_raw(new char[size]);
+  std::wcstombs(filename_ascii_raw.get(), filename, size);
+  std::string filename_ascii(filename_ascii_raw.get());
+#else
+  std::string& filename_ascii = filename;  
+#endif
+
   TORCH_CHECK(
-      rc == 0, "[Errno ", errno, "] ", strerror(errno), ": '", filename, "'");
+      rc == 0, "[Errno ", errno, "] ", strerror(errno), ": '", filename_ascii, "'");
 
   int64_t size = stat_buf.st_size;
 
@@ -30,17 +39,17 @@ torch::Tensor read_file(VISION_STRING filename) {
 #ifdef _WIN32
   // Expected error for Unicode variant here
   auto data =
-      torch::from_file(filename, /*shared=*/false, /*size=*/size, torch::kU8)
+      torch::from_file(filename_ascii, /*shared=*/false, /*size=*/size, torch::kU8)
           .clone();
 #else
   auto data =
-      torch::from_file(filename, /*shared=*/false, /*size=*/size, torch::kU8);
+      torch::from_file(filename_ascii, /*shared=*/false, /*size=*/size, torch::kU8);
 #endif
 
   return data;
 }
 
-void write_file(std::string filename, torch::Tensor& data) {
+void write_file(VISION_STRING filename, torch::Tensor& data) {
   // Check that the input tensor is on CPU
   TORCH_CHECK(data.device() == torch::kCPU, "Input tensor should be on CPU");
 
@@ -52,7 +61,11 @@ void write_file(std::string filename, torch::Tensor& data) {
 
   auto fileBytes = data.data_ptr<uint8_t>();
   auto fileCStr = filename.c_str();
+#if defined(_WIN32) && defined(UNICODE)
+  FILE* outfile = _wfopen(fileCStr, L"wb");
+#else
   FILE* outfile = fopen(fileCStr, "wb");
+#endif
 
   TORCH_CHECK(outfile != nullptr, "Error opening output file");
 
