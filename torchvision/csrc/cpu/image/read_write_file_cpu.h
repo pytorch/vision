628f24ae86f6915f9464e1dfc34bc3878853d024
peterjc123 <peterghost86@gmail.com>
2020-11-03 11:19:38 +0800

    Move string definitions to header

--- torchvision/csrc/cpu/image/read_write_file_cpu.h
+++ torchvision/csrc/cpu/image/read_write_file_cpu.h
@@ -4,6 +4,16 @@
 #include <sys/stat.h>
 #include <torch/torch.h>
 
-C10_EXPORT torch::Tensor read_file(std::string filename);
+#ifdef _WIN32
+#ifdef UNICODE
+#define VISION_STRING std::wstring
+#else
+#define VISION_STRING std::string
+#endif
+#else
+#define VISION_STRING std::string
+#endif
 
-C10_EXPORT void write_file(std::string filename, torch::Tensor& data);
+C10_EXPORT torch::Tensor read_file(VISION_STRING filename);
+
+C10_EXPORT void write_file(VISION_STRING filename, torch::Tensor& data);
