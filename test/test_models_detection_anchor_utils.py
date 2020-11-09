ec203aa654b3b2f97c585d3da760417a54cd7f8a
Aditya Oke <okeaditya315@gmail.com>
2020-11-10 00:19:43 +0530

    tries fixing

--- test/test_models_detection_anchor_utils.py
+++ test/test_models_detection_anchor_utils.py
@@ -4,8 +4,8 @@ from torchvision.models.detection.anchor_utils import AnchorGenerator
 
 class Tester(unittest.TestCase):
     def test_incorrect_anchors(self):
-        incorrect_sizes = (128, 258, 512)
-        incorrect_aspects = (0.5, 1.0, 2.0)
+        incorrect_sizes = ((128, 258, 512), (128, 32))
+        incorrect_aspects = ((0.5), (1.0), (2.0, 3.0))
         self.assertRaises(ValueError, AnchorGenerator, incorrect_sizes, incorrect_aspects)
 
 
