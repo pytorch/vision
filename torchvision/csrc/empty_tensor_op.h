4fcf11d8006db4f0e603d131d1c462c00bc6fead
Vasilis Vryniotis <datumbox@users.noreply.github.com>
2020-11-16 15:02:18 +0000

    Removing python headers and Win32 Python2 inits in C++ (#3011)
    * Removing python headers and code for Python2 compatibility.
    
    * Restoring python3 inits.

--- torchvision/csrc/empty_tensor_op.h
+++ torchvision/csrc/empty_tensor_op.h
@@ -2,8 +2,6 @@
 
 // All pure C++ headers for the C++ frontend.
 #include <torch/all.h>
-// Python bindings for the C++ frontend (includes Python.h).
-#include <torch/python.h>
 
 class NewEmptyTensorOp : public torch::autograd::Function<NewEmptyTensorOp> {
  public:
