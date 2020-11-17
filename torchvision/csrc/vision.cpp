2bf961f5d9adcd54c18829a151cbf4e92067469a
Brian Hirsh <hirsheybar@fb.com>
2020-11-16 17:06:26 -0800

    Revert D24714803: make duplicate def() calls an error in the dispatcher. Updating all fb operators to use the new dispatcher registration API
    Differential Revision:
    D24714803
    
    Original commit changeset: c809aad8a698
    
    fbshipit-source-id: fb2ada65f9fc00d965708d202bd9d050f13ef467

--- torchvision/csrc/vision.cpp
+++ torchvision/csrc/vision.cpp
@@ -39,14 +39,14 @@ int64_t _cuda_version() {
 #endif
 }
 
-TORCH_LIBRARY_FRAGMENT(torchvision, m) {
-    m.def("nms", nms);
-    m.def("roi_align(Tensor input, Tensor rois, float spatial_scale, int pooled_height, int pooled_width, int sampling_ratio, bool aligned) -> Tensor",
-        &roi_align);
-    m.def("roi_pool", roi_pool);
-    m.def("_new_empty_tensor_op", new_empty_tensor);
-    m.def("ps_roi_align", ps_roi_align);
-    m.def("ps_roi_pool", ps_roi_pool);
-    m.def("deform_conv2d", deform_conv2d);
-    m.def("_cuda_version", _cuda_version);
-}
+static auto registry =
+    torch::RegisterOperators()
+        .op("torchvision::nms", &nms)
+        .op("torchvision::roi_align(Tensor input, Tensor rois, float spatial_scale, int pooled_height, int pooled_width, int sampling_ratio, bool aligned) -> Tensor",
+            &roi_align)
+        .op("torchvision::roi_pool", &roi_pool)
+        .op("torchvision::_new_empty_tensor_op", &new_empty_tensor)
+        .op("torchvision::ps_roi_align", &ps_roi_align)
+        .op("torchvision::ps_roi_pool", &ps_roi_pool)
+        .op("torchvision::deform_conv2d", &deform_conv2d)
+        .op("torchvision::_cuda_version", &_cuda_version);
