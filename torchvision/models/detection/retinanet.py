388b1e96f8086f4a849b26a5040bcf7a0988c609
Vasilis Vryniotis <vvryniotis@fb.com>
2020-11-17 12:22:21 +0000

    Enable support for images without annotations

--- torchvision/models/detection/retinanet.py
+++ torchvision/models/detection/retinanet.py
@@ -108,10 +108,9 @@ class RetinaNetClassificationHead(nn.Module):
             foreground_idxs_per_image = matched_idxs_per_image >= 0
             num_foreground = foreground_idxs_per_image.sum()
             # no matched_idxs means there were no annotations in this image
-            # TODO: enable support for images without annotations that works on distributed
-            if False:  # matched_idxs_per_image.numel() == 0:
+            if matched_idxs_per_image.numel() == 0:
                 gt_classes_target = torch.zeros_like(cls_logits_per_image)
-                valid_idxs_per_image = torch.arange(cls_logits_per_image.shape[0])
+                valid_idxs_per_image = torch.arange(cls_logits_per_image.shape[0], device=cls_logits_per_image.device)
             else:
                 # create the target classification
                 gt_classes_target = torch.zeros_like(cls_logits_per_image)
@@ -192,27 +191,29 @@ class RetinaNetRegressionHead(nn.Module):
         for targets_per_image, bbox_regression_per_image, anchors_per_image, matched_idxs_per_image in \
                 zip(targets, bbox_regression, anchors, matched_idxs):
             # no matched_idxs means there were no annotations in this image
-            # TODO enable support for images without annotations with distributed support
-            # if matched_idxs_per_image.numel() == 0:
-            #     continue
+            if matched_idxs_per_image.numel() == 0:
+                device = targets_per_image['boxes'].device
+                bbox_regression_per_image = torch.zeros_like(targets_per_image['boxes'], device=device)
+                target_regression = torch.zeros_like(targets_per_image['boxes'], device=device)
+                num_foreground = torch.tensor(0, dtype=torch.int64, device=device)
+            else:
+                # get the targets corresponding GT for each proposal
+                # NB: need to clamp the indices because we can have a single
+                # GT in the image, and matched_idxs can be -2, which goes
+                # out of bounds
+                matched_gt_boxes_per_image = targets_per_image['boxes'][matched_idxs_per_image.clamp(min=0)]
 
-            # get the targets corresponding GT for each proposal
-            # NB: need to clamp the indices because we can have a single
-            # GT in the image, and matched_idxs can be -2, which goes
-            # out of bounds
-            matched_gt_boxes_per_image = targets_per_image['boxes'][matched_idxs_per_image.clamp(min=0)]
+                # determine only the foreground indices, ignore the rest
+                foreground_idxs_per_image = matched_idxs_per_image >= 0
+                num_foreground = foreground_idxs_per_image.sum()
 
-            # determine only the foreground indices, ignore the rest
-            foreground_idxs_per_image = matched_idxs_per_image >= 0
-            num_foreground = foreground_idxs_per_image.sum()
+                # select only the foreground boxes
+                matched_gt_boxes_per_image = matched_gt_boxes_per_image[foreground_idxs_per_image, :]
+                bbox_regression_per_image = bbox_regression_per_image[foreground_idxs_per_image, :]
+                anchors_per_image = anchors_per_image[foreground_idxs_per_image, :]
 
-            # select only the foreground boxes
-            matched_gt_boxes_per_image = matched_gt_boxes_per_image[foreground_idxs_per_image, :]
-            bbox_regression_per_image = bbox_regression_per_image[foreground_idxs_per_image, :]
-            anchors_per_image = anchors_per_image[foreground_idxs_per_image, :]
-
-            # compute the regression targets
-            target_regression = self.box_coder.encode_single(matched_gt_boxes_per_image, anchors_per_image)
+                # compute the regression targets
+                target_regression = self.box_coder.encode_single(matched_gt_boxes_per_image, anchors_per_image)
 
             # compute the loss
             losses.append(torch.nn.functional.l1_loss(
