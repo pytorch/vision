897b19819cd3bfb46d64d8da52a246aee5ce34b9
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos980.prn1.facebook.com>
2020-11-20 04:30:38 -0800

    2020-11-20 nightly release (a51c49e4fc135f0690beca44e7a109b9726aed98)

--- torchvision/csrc/cpu/image/readjpeg_cpu.h
+++ torchvision/csrc/cpu/image/readjpeg_cpu.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <torch/torch.h>
+#include "image_read_mode.h"
 
 C10_EXPORT torch::Tensor decodeJPEG(
     const torch::Tensor& data,
-    int64_t channels = 0);
+    ImageReadMode mode = IMAGE_READ_MODE_UNCHANGED);
