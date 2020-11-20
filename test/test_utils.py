1568b54b69cd274f1a02fc740054c9310dd77dd3
Vasilis Vryniotis <vvryniotis@fb.com>
2020-11-20 18:19:36 +0000

    Replace random to white to reduce size and change font on tests.

--- test/test_utils.py
+++ test/test_utils.py
@@ -8,7 +8,7 @@ from io import BytesIO
 import torchvision.transforms.functional as F
 from torchvision.io.image import read_image
 from PIL import Image
-from common_utils import set_rng_seed
+
 
 class Tester(unittest.TestCase):
 
@@ -81,13 +81,12 @@ class Tester(unittest.TestCase):
                             'Pixel Image not stored in file object')
 
     def test_draw_boxes(self):
-        set_rng_seed(0)
-        img = torch.randint(0, 255, (3, 100, 100), dtype=torch.uint8)
+        img = torch.full((3, 100, 100), 255, dtype=torch.uint8)
         boxes = torch.tensor([[0, 0, 20, 20], [0, 0, 0, 0],
                              [10, 15, 30, 35], [23, 35, 93, 95]], dtype=torch.float)
         labels = ["a", "b", "c", "d"]
         colors = ["green", "#FF00FF", (0, 255, 0), "red"]
-        result = utils.draw_bounding_boxes(img, boxes, labels=labels, colors=colors, font="Arial.ttf", font_size=10)
+        result = utils.draw_bounding_boxes(img, boxes, labels=labels, colors=colors)
 
         path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "assets", "fakedata", "draw_boxes_util.png")
         if not os.path.exists(path):
--- torchvision/utils.py
+++ torchvision/utils.py
@@ -183,7 +183,7 @@ def draw_bounding_boxes(
         draw.rectangle(bbox, width=width, outline=color)
 
         if labels is not None:
-            txt_font = None if font is None else ImageFont.truetype(font=font, size=font_size)
+            txt_font = ImageFont.load_default() if font is None else ImageFont.truetype(font=font, size=font_size)
             draw.text((bbox[0], bbox[1]), labels[i], fill=color, font=txt_font)
 
     return torch.from_numpy(np.array(img_to_draw)).permute(2, 0, 1)
