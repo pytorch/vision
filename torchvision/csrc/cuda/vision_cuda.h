f206a8bfe10feea60c82233d2e82027d7524a606
Vasilis Vryniotis <vvryniotis@fb.com>
2020-12-02 00:36:27 +0000

    Syncing variable names between the cpp files and their header files.

--- torchvision/csrc/cuda/vision_cuda.h
+++ torchvision/csrc/cuda/vision_cuda.h
@@ -7,18 +7,18 @@
 VISION_API std::tuple<at::Tensor, at::Tensor> roi_pool_forward_cuda(
     const at::Tensor& input,
     const at::Tensor& rois,
-    const double spatial_scale,
-    const int64_t pooled_height,
-    const int64_t pooled_width);
+    double spatial_scale,
+    int64_t pooled_height,
+    int64_t pooled_width);
 
 VISION_API at::Tensor roi_pool_backward_cuda(
     const at::Tensor& grad,
     const at::Tensor& rois,
     const at::Tensor& argmax,
-    const double spatial_scale,
-    const int64_t pooled_height,
-    const int64_t pooled_width,
-    const int64_t batch_size,
-    const int64_t channels,
-    const int64_t height,
-    const int64_t width);
+    double spatial_scale,
+    int64_t pooled_height,
+    int64_t pooled_width,
+    int64_t batch_size,
+    int64_t channels,
+    int64_t height,
+    int64_t width);
