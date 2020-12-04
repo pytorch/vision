5afde0e7d57968d8f8b48bd44df577750f89f75c
Eli Uriegas <1700823+seemethere@users.noreply.github.com>
2020-12-03 17:52:19 -0800

    .circleci: Add Python 3.9, CUDA 11.2 to CI
    (cherry picked from commit 2f40a483d73018ae6e1488a484c5927f2b309969)
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- test/common_utils.py
+++ test/common_utils.py
@@ -18,6 +18,10 @@ from collections import OrderedDict
 import numpy as np
 from PIL import Image
 
+IS_PY39 = sys.version_info.major == 3 and sys.version_info.minor == 9
+PY39_SEGFAULT_SKIP_MSG = "Segmentation fault with Python 3.9, see https://github.com/pytorch/vision/issues/3367"
+PY39_SKIP = unittest.skipIf(IS_PY39, PY39_SEGFAULT_SKIP_MSG)
+
 
 @contextlib.contextmanager
 def get_tmp_dir(src=None, **kwargs):
