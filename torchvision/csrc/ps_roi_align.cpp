b75e88c777e7c830c9cfc9a31b8c0bba08e6703e
Vasilis Vryniotis <vvryniotis@fb.com>
2020-12-07 17:04:14 +0000

    Moving ps_roi_align op registration.

--- torchvision/csrc/ps_roi_align.cpp
+++ torchvision/csrc/ps_roi_align.cpp
@@ -1,4 +1,3 @@
-#include "ps_roi_align.h"
 #include <torch/extension.h>
 
 #if defined(WITH_CUDA) || defined(WITH_HIP)
@@ -43,6 +42,10 @@ std::tuple<at::Tensor, at::Tensor> ps_roi_align_autocast(
       std::get<0>(result).to(input.scalar_type()),
       std::get<1>(result).to(input.scalar_type()));
 }
+
+TORCH_LIBRARY_IMPL(torchvision, Autocast, m) {
+  m.impl("ps_roi_align", ps_roi_align_autocast);
+}
 #endif
 
 at::Tensor _ps_roi_align_backward(
@@ -73,6 +76,13 @@ at::Tensor _ps_roi_align_backward(
       channels,
       height,
       width);
+}
+
+TORCH_LIBRARY_FRAGMENT(torchvision, m) {
+  m.def(
+    "ps_roi_align(Tensor input, Tensor rois, float spatial_scale, int pooled_height, int pooled_width, int sampling_ratio) -> (Tensor, Tensor)");
+  m.def(
+    "_ps_roi_align_backward(Tensor grad, Tensor rois, Tensor channel_mapping, float spatial_scale, int pooled_height, int pooled_width, int sampling_ratio, int batch_size, int channels, int height, int width) -> Tensor");
 }
 
 namespace {
@@ -220,6 +230,11 @@ at::Tensor ps_roi_align_backward_autograd(
       channels,
       height,
       width)[0];
+}
+
+TORCH_LIBRARY_IMPL(torchvision, Autograd, m) {
+  m.impl("ps_roi_align", ps_roi_align_autograd);
+  m.impl("_ps_roi_align_backward", ps_roi_align_backward_autograd);
 }
 
 } // namespace ops
--- torchvision/csrc/vision.cpp
+++ torchvision/csrc/vision.cpp
@@ -10,7 +10,6 @@
 #include <hip/hip_runtime.h>
 #endif
 
-#include "ps_roi_align.h"
 #include "ps_roi_pool.h"
 #include "roi_align.h"
 #include "roi_pool.h"
@@ -38,10 +37,6 @@ using namespace vision::ops;
 
 TORCH_LIBRARY(torchvision, m) {
   m.def(
-      "ps_roi_align(Tensor input, Tensor rois, float spatial_scale, int pooled_height, int pooled_width, int sampling_ratio) -> (Tensor, Tensor)");
-  m.def(
-      "_ps_roi_align_backward(Tensor grad, Tensor rois, Tensor channel_mapping, float spatial_scale, int pooled_height, int pooled_width, int sampling_ratio, int batch_size, int channels, int height, int width) -> Tensor");
-  m.def(
       "ps_roi_pool(Tensor input, Tensor rois, float spatial_scale, int pooled_height, int pooled_width) -> (Tensor, Tensor)");
   m.def(
       "_ps_roi_pool_backward(Tensor grad, Tensor rois, Tensor channel_mapping, float spatial_scale, int pooled_height, int pooled_width, int batch_size, int channels, int height, int width) -> Tensor");
@@ -57,8 +52,6 @@ TORCH_LIBRARY(torchvision, m) {
 }
 
 TORCH_LIBRARY_IMPL(torchvision, CPU, m) {
-  m.impl("ps_roi_align", ps_roi_align_forward_cpu);
-  m.impl("_ps_roi_align_backward", ps_roi_align_backward_cpu);
   m.impl("ps_roi_pool", ps_roi_pool_forward_cpu);
   m.impl("_ps_roi_pool_backward", ps_roi_pool_backward_cpu);
   m.impl("roi_align", roi_align_forward_cpu);
@@ -70,8 +63,6 @@ TORCH_LIBRARY_IMPL(torchvision, CPU, m) {
 // TODO: Place this in a hypothetical separate torchvision_cuda library
 #if defined(WITH_CUDA) || defined(WITH_HIP)
 TORCH_LIBRARY_IMPL(torchvision, CUDA, m) {
-  m.impl("ps_roi_align", ps_roi_align_forward_cuda);
-  m.impl("_ps_roi_align_backward", ps_roi_align_backward_cuda);
   m.impl("ps_roi_pool", ps_roi_pool_forward_cuda);
   m.impl("_ps_roi_pool_backward", ps_roi_pool_backward_cuda);
   m.impl("roi_align", roi_align_forward_cuda);
@@ -84,7 +75,6 @@ TORCH_LIBRARY_IMPL(torchvision, CUDA, m) {
 // Autocast only needs to wrap forward pass ops.
 #if defined(WITH_CUDA) || defined(WITH_HIP)
 TORCH_LIBRARY_IMPL(torchvision, Autocast, m) {
-  m.impl("ps_roi_align", ps_roi_align_autocast);
   m.impl("ps_roi_pool", ps_roi_pool_autocast);
   m.impl("roi_align", roi_align_autocast);
   m.impl("roi_pool", roi_pool_autocast);
@@ -92,8 +82,6 @@ TORCH_LIBRARY_IMPL(torchvision, Autocast, m) {
 #endif
 
 TORCH_LIBRARY_IMPL(torchvision, Autograd, m) {
-  m.impl("ps_roi_align", ps_roi_align_autograd);
-  m.impl("_ps_roi_align_backward", ps_roi_align_backward_autograd);
   m.impl("ps_roi_pool", ps_roi_pool_autograd);
   m.impl("_ps_roi_pool_backward", ps_roi_pool_backward_autograd);
   m.impl("roi_align", roi_align_autograd);
