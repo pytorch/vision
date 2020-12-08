24c214f2859ef0449811027ff3bce5461b6b203c
Vasilis Vryniotis <vvryniotis@fb.com>
2020-12-08 19:10:42 +0000

    Removed all backward methods from header files.

--- torchvision/csrc/ps_roi_pool.h
+++ torchvision/csrc/ps_roi_pool.h
@@ -6,26 +6,12 @@
 namespace vision {
 namespace ops {
 
-// C++ Forward
 VISION_API std::tuple<at::Tensor, at::Tensor> ps_roi_pool(
     const at::Tensor& input,
     const at::Tensor& rois,
     double spatial_scale,
     int64_t pooled_height,
     int64_t pooled_width);
-
-// C++ Backward
-VISION_API at::Tensor _ps_roi_pool_backward(
-    const at::Tensor& grad,
-    const at::Tensor& rois,
-    const at::Tensor& channel_mapping,
-    double spatial_scale,
-    int64_t pooled_height,
-    int64_t pooled_width,
-    int64_t batch_size,
-    int64_t channels,
-    int64_t height,
-    int64_t width);
 
 } // namespace ops
 } // namespace vision
