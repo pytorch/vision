db2adb2452d6aaf26b17025d8960dc4e14f5433f
Vasilis Vryniotis <vvryniotis@fb.com>
2020-12-10 20:50:11 +0000

    Move autograd implementations on separate files.

--- torchvision/csrc/ops/ps_roi_pool.h
+++ torchvision/csrc/ops/ps_roi_pool.h
@@ -13,5 +13,21 @@ VISION_API std::tuple<at::Tensor, at::Tensor> ps_roi_pool(
     int64_t pooled_height,
     int64_t pooled_width);
 
+namespace detail {
+
+VISION_API at::Tensor _ps_roi_pool_backward(
+    const at::Tensor& grad,
+    const at::Tensor& rois,
+    const at::Tensor& channel_mapping,
+    double spatial_scale,
+    int64_t pooled_height,
+    int64_t pooled_width,
+    int64_t batch_size,
+    int64_t channels,
+    int64_t height,
+    int64_t width);
+
+} // namespace detail
+
 } // namespace ops
 } // namespace vision
