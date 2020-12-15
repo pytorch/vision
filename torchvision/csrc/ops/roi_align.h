32e7abf723d22493076d9fc4d31149af3ee25707
Francisco Massa <fmassa@fb.com>
2020-12-14 16:50:53 -0800

    Move autograd implementations on separate files. (#3154)
    Reviewed By: datumbox
    
    Differential Revision: D25531038
    
    fbshipit-source-id: 481434d15d0709417b3c36ff2b13e10a7994dff2

--- torchvision/csrc/ops/roi_align.h
+++ torchvision/csrc/ops/roi_align.h
@@ -15,5 +15,22 @@ VISION_API at::Tensor roi_align(
     int64_t sampling_ratio,
     bool aligned);
 
+namespace detail {
+
+VISION_API at::Tensor _roi_align_backward(
+    const at::Tensor& grad,
+    const at::Tensor& rois,
+    double spatial_scale,
+    int64_t pooled_height,
+    int64_t pooled_width,
+    int64_t batch_size,
+    int64_t channels,
+    int64_t height,
+    int64_t width,
+    int64_t sampling_ratio,
+    bool aligned);
+
+} // namespace detail
+
 } // namespace ops
 } // namespace vision
