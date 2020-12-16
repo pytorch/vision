834b185be63fd8241d5827148bffa204a84642d8
Vasilis Vryniotis <vvryniotis@fb.com>
2020-12-16 12:40:40 +0000

    Adding first conv layers.

--- torchvision/models/mobilenetv3.py
+++ torchvision/models/mobilenetv3.py
@@ -4,7 +4,7 @@ from torch.nn import functional as F
 from typing import Callable, List, Optional
 
 
-def _make_divisible(v: float, divisor: int = 8, min_value: Optional[int] = None) -> int:
+def _make_divisible(v: float, divisor: int, min_value: Optional[int] = None) -> int:
     """
     This function is taken from the original tf repo.
     It ensures that all layers have a channel number that is divisible by 8
@@ -58,7 +58,7 @@ class SqueezeExcitation(nn.Module):
 
     def __init__(self, input_channels: int, squeeze_factor: int = 4):
         super().__init__()
-        squeeze_channels = _make_divisible(input_channels // squeeze_factor)
+        squeeze_channels = _make_divisible(input_channels // squeeze_factor, 8)
         self.fc1 = nn.Conv2d(input_channels, squeeze_channels, 1)
         self.fc2 = nn.Conv2d(squeeze_channels, input_channels, 1)
 
@@ -74,10 +74,10 @@ class SqueezeExcitation(nn.Module):
 class InvertedResidualConfig:
     def __init__(self, input_channels: int, kernel: int, expanded_channels: int, output_channels: int, use_se: bool,
                  activation: str, stride: int, width_mult: float):
-        self.input_channels = _make_divisible(input_channels * width_mult)
+        self.input_channels = _make_divisible(input_channels * width_mult, 8)
         self.kernel = kernel
-        self.expanded_channels = _make_divisible(expanded_channels * width_mult)
-        self.output_channels = _make_divisible(output_channels * width_mult)
+        self.expanded_channels = _make_divisible(expanded_channels * width_mult, 8)
+        self.output_channels = _make_divisible(output_channels * width_mult, 8)
         self.use_se = use_se
         self.use_hs = activation == "HS"
         self.stride = stride
@@ -91,7 +91,7 @@ class InvertedResidual(nn.Module):
 
         self.use_res_connect = cnf.stride == 1 and cnf.input_channels == cnf.output_channels
 
-        layers: List[nn.Module] = []
+        layers = []
         # expand
         if cnf.expanded_channels != cnf.input_channels:
             layers.extend([
@@ -132,20 +132,22 @@ class MobileNetV3(nn.Module):
             inverted_residual_setting: List[InvertedResidualConfig],
             last_channel: int,
             num_classes: int = 1000,
-            blocks: Optional[List[Callable[..., nn.Module]]] = None,
+            block: Optional[Callable[..., nn.Module]] = None,
             norm_layer: Optional[Callable[..., nn.Module]] = None
     ) -> None:
         super().__init__()
 
-        if blocks is None:
-            blocks = [SqueezeExcitation, InvertedResidual]
-        se_layer, bottleneck_layer = blocks
+        if block is None:
+            block = InvertedResidual
 
         if norm_layer is None:
             norm_layer = partial(nn.BatchNorm2d, eps=0.001, momentum=0.01)
 
-        layers: List[nn.Module] = [
-
+        firstconv_output_channels = inverted_residual_setting[0].input_channels
+        layers = [
+            nn.Conv2d(3, firstconv_output_channels, 3, stride=2, padding=1, bias=False),
+            norm_layer(firstconv_output_channels),
+            HardSwish(inplace=True),
         ]
 
         pass
@@ -188,6 +190,7 @@ def mobilenetv3(mode: str = "large", width_mult: float = 1.0):
             bneck_conf(96, 5, 576, 96, True, "HS", 1),
         ]
         last_channel = 1024
-    last_channel = _make_divisible(last_channel * width_mult)
+
+    last_channel = _make_divisible(last_channel * width_mult, 8)
 
     return MobileNetV3(inverted_residual_setting, last_channel)
