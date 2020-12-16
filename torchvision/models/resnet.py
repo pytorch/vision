722e06a30a3a3a5cf5fa09e003b607cbc87af832
Hannah_Haensen <hannah.schieber@hotmail.com>
2020-12-16 18:02:13 +0100

    Update resnet.py

--- torchvision/models/resnet.py
+++ torchvision/models/resnet.py
@@ -265,7 +265,7 @@ def _resnet(
     layers: List[int],
     pretrained: bool,
     progress: bool,
-    multi_grid: int = 1,
+    multi_grid: int,
     **kwargs: Any
 ) -> ResNet:
     model = ResNet(block=block, layers=layers, multi_grid=multi_grid, **kwargs)
@@ -285,7 +285,8 @@ def resnet18(pretrained: bool = False, progress: bool = True, multi_grid: int = 
         progress (bool): If True, displays a progress bar of the download to stderr
         multi_grid (Any): if argument is given, the 4th layer of resnet will be multi_grid
     """
-    return _resnet('resnet18', BasicBlock, [2, 2, 2, 2], pretrained, progress, multi_grid,
+    return _resnet('resnet18', BasicBlock, [2, 2, 2, 2],
+                   pretrained, progress, multi_grid,
                    **kwargs)
 
 
@@ -298,7 +299,8 @@ def resnet34(pretrained: bool = False, progress: bool = True, multi_grid: int = 
         progress (bool): If True, displays a progress bar of the download to stderr
         multi_grid (Any): if argument is given, the 4th layer of resnet will be multi_grid
     """
-    return _resnet('resnet34', BasicBlock, [3, 4, 6, 3], pretrained, progress, multi_grid,
+    return _resnet('resnet34', BasicBlock, [3, 4, 6, 3],
+                   pretrained, progress, multi_grid,
                    **kwargs)
 
 
@@ -311,7 +313,8 @@ def resnet50(pretrained: bool = False, progress: bool = True, multi_grid: int = 
         progress (bool): If True, displays a progress bar of the download to stderr
         multi_grid (Any): if argument is given, the 4th layer of resnet will be multi_grid
     """
-    return _resnet('resnet50', Bottleneck, [3, 4, 6, 3], pretrained, progress, multi_grid,
+    return _resnet('resnet50', Bottleneck, [3, 4, 6, 3],
+                   pretrained, progress, multi_grid,
                    **kwargs)
 
 
@@ -324,7 +327,8 @@ def resnet101(pretrained: bool = False, progress: bool = True, multi_grid: int =
         progress (bool): If True, displays a progress bar of the download to stderr
         multi_grid (Any): if argument is given, the 4th layer of resnet will be multi_grid
     """
-    return _resnet('resnet101', Bottleneck, [3, 4, 23, 3], pretrained, progress, multi_grid,
+    return _resnet('resnet101', Bottleneck, [3, 4, 23, 3],
+                   pretrained, progress, multi_grid,
                    **kwargs)
 
 
@@ -337,7 +341,8 @@ def resnet152(pretrained: bool = False, progress: bool = True, multi_grid: int =
         progress (bool): If True, displays a progress bar of the download to stderr
         multi_grid (Any): if argument is given, the 4th layer of resnet will be multi_grid
     """
-    return _resnet('resnet152', Bottleneck, [3, 8, 36, 3], pretrained, progress, multi_grid,
+    return _resnet('resnet152', Bottleneck, [3, 8, 36, 3],
+                   pretrained, progress, multi_grid,
                    **kwargs)
 
 
@@ -352,8 +357,9 @@ def resnext50_32x4d(pretrained: bool = False, progress: bool = True, multi_grid:
     """
     kwargs['groups'] = 32
     kwargs['width_per_group'] = 4
-    return _resnet('resnext50_32x4d', Bottleneck, [3, 4, 6, 3], multi_grid,
-                   pretrained, progress, **kwargs)
+    return _resnet('resnext50_32x4d', Bottleneck, [3, 4, 6, 3],
+                   pretrained, progress, multi_grid,
+                   **kwargs)
 
 
 def resnext101_32x8d(pretrained: bool = False, progress: bool = True, multi_grid: int = 1, **kwargs: Any) -> ResNet:
@@ -367,8 +373,9 @@ def resnext101_32x8d(pretrained: bool = False, progress: bool = True, multi_grid
     """
     kwargs['groups'] = 32
     kwargs['width_per_group'] = 8
-    return _resnet('resnext101_32x8d', Bottleneck, [3, 4, 23, 3], multi_grid,
-                   pretrained, progress, **kwargs)
+    return _resnet('resnext101_32x8d', Bottleneck, [3, 4, 23, 3],
+                   pretrained, progress, multi_grid,
+                   **kwargs)
 
 
 def wide_resnet50_2(pretrained: bool = False, progress: bool = True, multi_grid: int = 1, **kwargs: Any) -> ResNet:
@@ -386,8 +393,9 @@ def wide_resnet50_2(pretrained: bool = False, progress: bool = True, multi_grid:
         multi_grid (Any): if argument is given, the 4th layer of resnet will be multi_grid
     """
     kwargs['width_per_group'] = 64 * 2
-    return _resnet('wide_resnet50_2', Bottleneck, [3, 4, 6, 3], multi_grid,
-                   pretrained, progress, **kwargs)
+    return _resnet('wide_resnet50_2', Bottleneck, [3, 4, 6, 3],
+                   pretrained, progress, multi_grid,
+                   **kwargs)
 
 
 def wide_resnet101_2(pretrained: bool = False, progress: bool = True, multi_grid: int = 1, **kwargs: Any) -> ResNet:
@@ -405,5 +413,6 @@ def wide_resnet101_2(pretrained: bool = False, progress: bool = True, multi_grid
         multi_grid (Any): if argument is given, the 4th layer of resnet will be multi_grid
     """
     kwargs['width_per_group'] = 64 * 2
-    return _resnet('wide_resnet101_2', Bottleneck, [3, 4, 23, 3], pretrained, progress, multi_grid,
+    return _resnet('wide_resnet101_2', Bottleneck, [3, 4, 23, 3],
+                   pretrained, progress, multi_grid,
                    **kwargs)
