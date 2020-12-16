2ebe8baf701d292d0763a3f236ea4c46b8ddf114
Vasilis Vryniotis <vvryniotis@fb.com>
2020-12-16 13:00:59 +0000

    Adding mobilenet.py for BC.

--- torchvision/models/quantization/mobilenetv2.py
+++ torchvision/models/quantization/mobilenetv2.py
@@ -1,6 +1,6 @@
 from torch import nn
 from torchvision.models.utils import load_state_dict_from_url
-from torchvision.models.mobilenet import InvertedResidual, ConvBNReLU, MobileNetV2, model_urls
+from torchvision.models.mobilenetv2 import InvertedResidual, ConvBNReLU, MobileNetV2, model_urls
 from torch.quantization import QuantStub, DeQuantStub, fuse_modules
 from .utils import _replace_relu, quantize_model
 
