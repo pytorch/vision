d8c596d3e7c36c65c8dd1652b4e58a0d48a45f6e
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos201.prn3.facebook.com>
2021-01-15 04:30:53 -0800

    2021-01-15 nightly release (3b19d6fc0f47280f947af5cebb83827d0ce93f7d)

--- torchvision/csrc/ops/autograd/ps_roi_pool_kernel.cpp
+++ torchvision/csrc/ops/autograd/ps_roi_pool_kernel.cpp
@@ -53,11 +53,12 @@ class PSROIPoolFunction : public torch::autograd::Function<PSROIPoolFunction> {
         input_shape[2],
         input_shape[3]);
 
-    return {grad_in,
-            torch::autograd::Variable(),
-            torch::autograd::Variable(),
-            torch::autograd::Variable(),
-            torch::autograd::Variable()};
+    return {
+        grad_in,
+        torch::autograd::Variable(),
+        torch::autograd::Variable(),
+        torch::autograd::Variable(),
+        torch::autograd::Variable()};
   }
 };
 
