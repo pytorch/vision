4dc7135e9470b26ff79d8c5b6f624c66ef38aa27
Alessio Falai <falai.alessio@gmail.com>
2021-01-19 23:14:44 +0100

    add float check in rcnn normalize

--- torchvision/models/detection/transform.py
+++ torchvision/models/detection/transform.py
@@ -117,6 +117,11 @@ class GeneralizedRCNNTransform(nn.Module):
         return image_list, targets
 
     def normalize(self, image):
+        if not image.is_floating_point():
+            raise TypeError(
+                f"Expected input images to be of floating type (in range [0, 1]), "
+                f"but found type {image.dtype} instead"
+            )
         dtype, device = image.dtype, image.device
         mean = torch.as_tensor(self.image_mean, dtype=dtype, device=device)
         std = torch.as_tensor(self.image_std, dtype=dtype, device=device)
