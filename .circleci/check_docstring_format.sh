6d33c6ca9416e708781d26ed6a0f19f921af1f2d
Nicolas Hug <nicolashug@fb.com>
2021-01-22 14:45:31 +0000

    removed CI check

--- .circleci/check_docstring_format.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-match=`git grep -A 1 Parameters | grep "\-\-\-"`
-if [ ! -z "$match" ]
-then
-  echo "Bad docstring format. Please use the Args: syntax to specify arguments"
-  exit 1
-fi
