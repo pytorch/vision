984fbaa0abd7cc8abf593e49ff782d6df98e58c7
Francisco Massa <fvsmassa@gmail.com>
2021-01-22 12:21:56 +0100

    Use smaller version of mobilenet model

--- android/test_app/make_assets.py
+++ android/test_app/make_assets.py
@@ -3,14 +3,14 @@ import torchvision
 
 print(torch.__version__)
 
-model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True, box_score_thresh=0.7, rpn_post_nms_top_n_test=100, rpn_score_thresh=0.4, rpn_pre_nms_top_n_test=150)
+model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True, box_score_thresh=0.7, rpn_post_nms_top_n_test=100, rpn_score_thresh=0.4, rpn_pre_nms_top_n_test=150)
 
 model.eval()
 script_model = torch.jit.script(model)
 torch.jit.save(script_model, "app/src/main/assets/frcnn_resnet50_fpn.pt")
 
 # model_300_500 = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True, min_size=300, max_size=500)
-model_300_500 = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True, box_score_thresh=0.7, rpn_post_nms_top_n_test=100, rpn_score_thresh=0.4, rpn_pre_nms_top_n_test=150)
+model_300_500 = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True, box_score_thresh=0.7, rpn_post_nms_top_n_test=100, rpn_score_thresh=0.4, rpn_pre_nms_top_n_test=150)
 model_300_500.eval()
 script_model_300_500 = torch.jit.script(model_300_500)
 torch.jit.save(script_model_300_500, "app/src/main/assets/frcnn_resnet50_fpn_300_500.pt")
