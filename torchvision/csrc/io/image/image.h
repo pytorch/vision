1076410f4dc63333b98d83d1d4f6b59d298517b9
Vasilis Vryniotis <vvryniotis@fb.com>
2021-02-02 10:04:26 -0800

    Add namespace on image C++ codebase (#3312)
    Summary:
    * Moving jpegcommon inside cpu implementation
    
    * Adding namespaces on image and moving private methods to anonymous.
    
    * Fixing headers.
    
    * Renaming public image methods to match the ones on python.
    
    * Refactoring to remove the double ifs in common_jpeg.h
    
    Reviewed By: fmassa
    
    Differential Revision: D26197738
    
    fbshipit-source-id: a9d3ca2d8a248504fbe4bdfda94b6cbdda2270f1

--- torchvision/csrc/io/image/image.h
+++ torchvision/csrc/io/image/image.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "cpu/read_image_impl.h"
-#include "cpu/read_write_file_impl.h"
-#include "cpu/readjpeg_impl.h"
-#include "cpu/readpng_impl.h"
-#include "cpu/writejpeg_impl.h"
-#include "cpu/writepng_impl.h"
+#include "cpu/decode_image.h"
+#include "cpu/decode_jpeg.h"
+#include "cpu/decode_png.h"
+#include "cpu/encode_jpeg.h"
+#include "cpu/encode_png.h"
+#include "cpu/read_write_file.h"
