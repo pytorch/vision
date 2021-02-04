8c4646e380ea57705becd6eb15d08c2a2031e937
Nicolas Hug <nicolashug@fb.com>
2021-02-04 06:30:04 -0800

    remove normalize from save_image (#3324)
    Reviewed By: datumbox
    
    Differential Revision: D26226617
    
    fbshipit-source-id: 874d01718da096645ad61307a071f036470c1f07

--- torchvision/utils.py
+++ torchvision/utils.py
@@ -115,7 +115,6 @@ def make_grid(
 def save_image(
     tensor: Union[torch.Tensor, List[torch.Tensor]],
     fp: Union[Text, pathlib.Path, BinaryIO],
-    normalize: bool = False,
     format: Optional[str] = None,
     **kwargs
 ) -> None:
