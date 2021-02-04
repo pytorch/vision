37c1aa60efb7da9c7188db3d6eefdfaba9b28259
Ivan Kobzarev <ivankobzarev@fb.com>
2021-02-04 11:42:38 -0800

    [android] assets mobilenetv3

--- android/test_app/make_assets.py
+++ android/test_app/make_assets.py
@@ -1,16 +1,17 @@
 import torch
 import torchvision
+from torch.utils.mobile_optimizer import optimize_for_mobile
 
 print(torch.__version__)
 
-model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True, box_score_thresh=0.7, rpn_post_nms_top_n_test=100, rpn_score_thresh=0.4, rpn_pre_nms_top_n_test=150)
+model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(
+    pretrained=True,
+    box_score_thresh=0.7,
+    rpn_post_nms_top_n_test=100,
+    rpn_score_thresh=0.4,
+    rpn_pre_nms_top_n_test=150)
 
 model.eval()
 script_model = torch.jit.script(model)
-torch.jit.save(script_model, "app/src/main/assets/frcnn_resnet50_fpn.pt")
-
-# model_300_500 = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True, min_size=300, max_size=500)
-model_300_500 = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True, box_score_thresh=0.7, rpn_post_nms_top_n_test=100, rpn_score_thresh=0.4, rpn_pre_nms_top_n_test=150)
-model_300_500.eval()
-script_model_300_500 = torch.jit.script(model_300_500)
-torch.jit.save(script_model_300_500, "app/src/main/assets/frcnn_resnet50_fpn_300_500.pt")
+opt_script_model = optimize_for_mobile(script_model)
+opt_script_model.save("app/src/main/assets/frcnn_mnetv3.pt")
