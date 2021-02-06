d554c7594147ae0a2643f71e94d0b541a3d05778
Aditya Oke <okeaditya315@gmail.com>
2021-02-06 21:42:43 +0530

    adds alpha and docstring

--- torchvision/utils.py
+++ torchvision/utils.py
@@ -216,6 +216,7 @@ def draw_bounding_boxes(
 def draw_segmentation_masks(
     image: torch.Tensor,
     masks: torch.Tensor,
+    alpha: Optional[int] = 0.2,
     colors: Optional[List[Union[str, Tuple[int, int, int]]]] = None,
 ) -> torch.Tensor:
 
@@ -226,7 +227,7 @@ def draw_segmentation_masks(
     Args:
         image (Tensor): Tensor of shape (C x H x W)
         masks (Tensor): Tensor of shape (num_classes, H, W). Each containing probability of predicted class.
-        labels (List[str]): List containing the labels of masks.
+        alpha (int): Integer denoting factor of transpaerency of masks.
         colors (List[Union[str, Tuple[int, int, int]]]): List containing the colors of masks. The colors can
             be represented as `str` or `Tuple[int, int, int]`.
     """
@@ -247,7 +248,7 @@ def draw_segmentation_masks(
         palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
         colors_t = torch.as_tensor([i for i in range(num_classes)])[:, None] * palette
         color_arr = (colors_t % 255).numpy().astype("uint8")
-
+        color_arr[1:, 3] = 255
     else:
         color_list = []
         for color in colors:
@@ -263,8 +264,9 @@ def draw_segmentation_masks(
         color_arr = np.array(color_list).astype("uint8")
 
     img_to_draw.putpalette(color_arr)
-    img_to_draw = torch.from_numpy(np.array(img_to_draw))
 
-    # Project the drawn image to orignal one
-    image[: 1] = img_to_draw
-    return image
+    img_to_draw = torch.from_numpy(np.array(img_to_draw.convert('RGBA')))
+    img_to_draw = img_to_draw.permute((2, 1, 0))
+
+    return (torch.cat([image, torch.full(image.shape[1:], 255).unsqueeze(0)]).float()
+            * alpha + img_to_draw.float() * (1.0 - alpha)).to(dtype=torch.uint8)
