0eed9fec828f5fb0e0f1a58237f1514e74fa96e4
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared13440.02.ftw1.facebook.com>
2021-02-10 04:30:14 -0800

    2021-02-10 nightly release (03fec9c73c28823e9ddb76ad46a7a448783e5f21)

--- torchvision/models/quantization/mobilenet.py
+++ torchvision/models/quantization/mobilenet.py
@@ -1,4 +1,4 @@
 from .mobilenetv2 import QuantizableMobileNetV2, mobilenet_v2, __all__ as mv2_all
-from .mobilenetv3 import QuantizableMobileNetV3, mobilenet_v3_large, mobilenet_v3_small, __all__ as mv3_all
+from .mobilenetv3 import QuantizableMobileNetV3, mobilenet_v3_large, __all__ as mv3_all
 
 __all__ = mv2_all + mv3_all
