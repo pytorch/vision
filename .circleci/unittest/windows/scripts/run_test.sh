15d60c6989d3e2d3fa8f86cc2c220e501ae00f20
Philip Meier <github.pmeier@posteo.de>
2021-02-24 08:52:19 +0100

    disable parallelism for GPU tests

--- .circleci/unittest/windows/scripts/run_test.sh
+++ .circleci/unittest/windows/scripts/run_test.sh
@@ -6,5 +6,18 @@ eval "$(./conda/Scripts/conda.exe 'shell.bash' 'hook')"
 conda activate ./env
 
 export PYTORCH_TEST_WITH_SLOW='1'
+if [ "${CU_VERSION:-}" == cpu ] ; then
+    NUMPROCESSES="auto"
+else
+    NUMPROCESSES="1"
+fi
+
 python -m torch.utils.collect_env
-pytest -n=auto --cov=torchvision --junitxml=test-results/junit.xml -v --durations 20 test --ignore=test/test_datasets_download.py
+pytest \
+    --numprocesses=$NUMPROCESSES \
+    --cov=torchvision \
+    --junitxml=test-results/junit.xml \
+    --verbose \
+    --durations 20 \
+    --ignore=test/test_datasets_download.py \
+    test
