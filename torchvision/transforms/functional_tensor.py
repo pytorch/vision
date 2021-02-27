c48b460d6518a67df4ec2d53846e7bc0d97ec775
Sanket Thakur <sanketkumar1996@gmail.com>
2021-02-27 11:45:59 +0100

    fixed inconsistent casting

--- torchvision/transforms/functional_tensor.py
+++ torchvision/transforms/functional_tensor.py
@@ -227,7 +227,7 @@ def adjust_gamma(img: Tensor, gamma: float, gain: float = 1) -> Tensor:
     result = (gain * result ** gamma).clamp(0, 1)
 
     if result.dtype != dtype:
-        result = convert_image_dtype(result, dtype)
+        result = result.to(dtype)
     return result
 
 
