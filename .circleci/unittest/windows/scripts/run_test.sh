290fe5e78f7754494a6c5085838e6c79bb971c78
Philip Meier <github.pmeier@posteo.de>
2021-03-01 14:25:26 +0100

    Run CI unittests in parallel (#3445)
    * enable parallel tests
    
    * disable parallelism for GPU tests
    
    * [test] limit maximum processes on linux
    
    * [debug] limit max processes even further
    
    * [test] use subprocesses over threads
    
    * [test] limit intra-op threads
    
    * only limit intra op threads for CPU tests
    
    * [poc] use low timeout for showcasing
    
    * [poc] fix syntax
    
    * set timeout to 5 minutes
    
    * fix timeout on windows
    
    Co-authored-by: Francisco Massa <fvsmassa@gmail.com>

--- .circleci/unittest/windows/scripts/run_test.sh
+++ .circleci/unittest/windows/scripts/run_test.sh
@@ -6,5 +6,20 @@ eval "$(./conda/Scripts/conda.exe 'shell.bash' 'hook')"
 conda activate ./env
 
 export PYTORCH_TEST_WITH_SLOW='1'
+if [ "${CU_VERSION:-}" == cpu ] ; then
+    NUMPROCESSES="auto"
+    export OMP_NUM_THREADS="1"
+else
+    NUMPROCESSES="1"
+fi
+
 python -m torch.utils.collect_env
-pytest --cov=torchvision --junitxml=test-results/junit.xml -v --durations 20 test --ignore=test/test_datasets_download.py
+pytest \
+    --numprocesses=$NUMPROCESSES \
+    --timeout=300 \
+    --cov=torchvision \
+    --junitxml=test-results/junit.xml \
+    --verbose \
+    --durations 20 \
+    --ignore=test/test_datasets_download.py \
+    test
