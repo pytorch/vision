c10d91027a72694da2f45f00361167438a2aac1b
mattip <matti.picus@gmail.com>
2021-03-04 19:49:14 +0200

    add 0.9 docs

---
+++ 0.9/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: 'master',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
