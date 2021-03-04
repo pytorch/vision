7848e89127c6b205d491b41272aeff8a683a00be
Nicolas Hug <nicolashug@fb.com>
2021-03-04 10:47:51 +0000

    Actually remove file

--- torchvision/transforms/functional_pil.py
+++ torchvision/transforms/functional_pil.py
@@ -203,36 +203,14 @@ def crop(img: Image.Image, top: int, left: int, height: int, width: int) -> Imag
     return img.crop((left, top, left + width, top + height))
 
 
-# def resize(img, size, interpolation=Image.BILINEAR):
 @torch.jit.unused
 def resize(img, size, interpolation='bilinear'):
     from .functional_tensor import resize as ft_resize
-    from . import convert
+    from .functional import to_tensor, to_pil_image
 
-    t = convert.to_tensor(img)
+    t = to_tensor(img)
     resized_t = ft_resize(t, size, interpolation.value)
-    return convert.to_pil_image(resized_t)
-    # if not _is_pil_image(img):
-    #     raise TypeError('img should be PIL Image. Got {}'.format(type(img)))
-    # if not (isinstance(size, int) or (isinstance(size, Sequence) and len(size) in (1, 2))):
-    #     raise TypeError('Got inappropriate size arg: {}'.format(size))
-
-    # if isinstance(size, int) or len(size) == 1:
-    #     if isinstance(size, Sequence):
-    #         size = size[0]
-    #     w, h = img.size
-    #     if (w <= h and w == size) or (h <= w and h == size):
-    #         return img
-    #     if w < h:
-    #         ow = size
-    #         oh = int(size * h / w)
-    #         return img.resize((ow, oh), interpolation)
-    #     else:
-    #         oh = size
-    #         ow = int(size * w / h)
-    #         return img.resize((ow, oh), interpolation)
-    # else:
-    #     return img.resize(size[::-1], interpolation)
+    return to_pil_image(resized_t)
 
 
 @torch.jit.unused
