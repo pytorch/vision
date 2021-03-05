6ae585736292a79cc4dd186335ecf2373a6fb835
ain-soph <ain-soph@live.com>
2021-03-05 01:45:34 -0500

    Implement ZipFolder

--- torchvision/datasets/folder.py
+++ torchvision/datasets/folder.py
@@ -1,9 +1,11 @@
 from .vision import VisionDataset
+from torchvision import get_image_backend
 
 from PIL import Image
-
+import io
 import os
 import os.path
+import zipfile
 from typing import Any, Callable, cast, Dict, List, Optional, Tuple
 
 
@@ -208,7 +210,6 @@ def accimage_loader(path: str) -> Any:
 
 
 def default_loader(path: str) -> Any:
-    from torchvision import get_image_backend
     if get_image_backend() == 'accimage':
         return accimage_loader(path)
     else:
@@ -255,3 +256,84 @@ class ImageFolder(DatasetFolder):
                                           target_transform=target_transform,
                                           is_valid_file=is_valid_file)
         self.imgs = self.samples
+
+class ZipFolder(DatasetFolder):
+    def __init__(self, root: str, transform: Optional[Callable] = None, target_transform: Optional[Callable] = None,
+                 is_valid_file: Optional[Callable[[str], bool]] = None, memory: bool = True) -> None:
+        if not root.endswith('.zip'):
+            raise TypeError("Need ZIP file for data source: ", root)
+        if memory:
+            with open(root, 'rb') as z:
+                data = z.read()
+            self.root_zip = zipfile.ZipFile(io.BytesIO(data), 'r')
+        else:
+            self.root_zip = zipfile.ZipFile(root, 'r')
+        super().__init__(root, self.zip_loader, IMG_EXTENSIONS if is_valid_file is None else None,
+                         transform=transform, target_transform=target_transform, is_valid_file=is_valid_file)
+        self.imgs = self.samples
+
+    @staticmethod
+    def initialize_from_folder(root: str, zip_path: str = None):
+        root = os.path.normpath(root)
+        folder_dir, folder_base = os.path.split(root)
+        if zip_path is None:
+            zip_path = os.path.join(folder_dir, f'{folder_base}_store.zip')
+        with zipfile.ZipFile(zip_path, mode='w', compression=zipfile.ZIP_STORED) as zf:
+            for walk_root, walk_dirs, walk_files in os.walk(root):
+                zip_root = walk_root.removeprefix(folder_dir)
+                for _file in walk_files:
+                    org_path = os.path.join(walk_root, _file)
+                    zip_path = os.path.join(zip_root, _file)
+                    zf.write(org_path, zip_path)
+
+    def make_dataset(
+        self,
+        directory: str,
+        class_to_idx: dict[str, int],
+        extensions: Optional[tuple[str, ...]] = None,
+        is_valid_file: Optional[Callable[[str], bool]] = None,
+    ) -> list[tuple[str, int]]:
+        instances = []
+        both_none = extensions is None and is_valid_file is None
+        both_something = extensions is not None and is_valid_file is not None
+        if both_none or both_something:
+            raise ValueError("Both extensions and is_valid_file cannot be None or not None at the same time")
+        if extensions is not None:
+            def is_valid_file(x: str) -> bool:
+                return has_file_allowed_extension(x, cast(tuple[str, ...], extensions))
+        is_valid_file = cast(Callable[[str], bool], is_valid_file)
+        for filepath in self.root_zip.namelist():
+            if is_valid_file(filepath):
+                target_class = os.path.basename(os.path.dirname(filepath))
+                instances.append((filepath, class_to_idx[target_class]))
+        return instances
+
+    def zip_loader(self, path: str) -> Image.Image:
+        f = io.BytesIO(self.root_zip.read(path))
+        if get_image_backend() == 'accimage':
+            try:
+                import accimage  # type: ignore
+                return accimage.Image(f)
+            except IOError:
+                pass   # fall through to PIL
+        return Image.open(f).convert('RGB')
+
+    def _find_classes(self, *args, **kwargs):
+        """
+        Finds the class folders in a dataset.
+        Args:
+            dir (string): Root directory path.
+        Returns:
+            tuple: (classes, class_to_idx) where classes are relative to (dir), and class_to_idx is a dictionary.
+        Ensures:
+            No class is a subdirectory of another.
+        """
+        classes = set()
+        for filepath in self.root_zip.namelist():
+            root, target_class = os.path.split(os.path.dirname(filepath))
+            if root:
+                classes.add(target_class)
+        classes = list(classes)
+        classes.sort()
+        class_to_idx = {classes[i]: i for i in range(len(classes))}
+        return classes, class_to_idx
