ff22a3a6fc8983d810426c01552c2e9f71b5b361
Nicolas Hug <nicolashug@fb.com>
2021-03-05 20:10:46 +0000

    flake8

--- torchvision/ops/boxes.py
+++ torchvision/ops/boxes.py
@@ -64,7 +64,7 @@ def batched_nms(
     # Benchmarks that drove the following thresholds are at
     # https://github.com/pytorch/vision/issues/1311#issuecomment-781329339
     # Ideally for GPU we'd use a higher threshold
-    if boxes.numel()  > 4_000 and not torchvision._is_tracing():
+    if boxes.numel() > 4_000 and not torchvision._is_tracing():
         return _batched_nms_vanilla(boxes, scores, idxs, iou_threshold)
     else:
         return _batched_nms_coordinate_trick(boxes, scores, idxs, iou_threshold)
