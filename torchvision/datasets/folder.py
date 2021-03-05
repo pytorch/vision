711d0f658d1f7c1f7a205c242f8cfc5b92fc8af2
ain-soph <ain-soph@live.com>
2021-03-05 02:14:09 -0500

    minimize modification

--- torchvision/datasets/folder.py
+++ torchvision/datasets/folder.py
@@ -1,5 +1,4 @@
 from .vision import VisionDataset
-from torchvision import get_image_backend
 
 from PIL import Image
 import io
@@ -210,6 +209,7 @@ def accimage_loader(path: str) -> Any:
 
 
 def default_loader(path: str) -> Any:
+    from torchvision import get_image_backend
     if get_image_backend() == 'accimage':
         return accimage_loader(path)
     else:
