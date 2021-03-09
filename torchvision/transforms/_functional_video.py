ccd93e58c9e84ed0be8c0b04efb630e64cdf5c9d
Nikhil Kumar <64120577+nikhilkmr300@users.noreply.github.com>
2021-03-10 01:38:03 +0530

    Fixed typo on line 12

--- torchvision/transforms/_functional_video.py
+++ torchvision/transforms/_functional_video.py
@@ -9,7 +9,7 @@ warnings.warn(
 
 def _is_tensor_video_clip(clip):
     if not torch.is_tensor(clip):
-        raise TypeError("clip should be Tesnor. Got %s" % type(clip))
+        raise TypeError("clip should be Tensor. Got %s" % type(clip))
 
     if not clip.ndimension() == 4:
         raise ValueError("clip should be 4D. Got %dD" % clip.dim())
