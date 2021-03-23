395c0531b43aeb783a662835eb630f8007a74351
Francisco Massa <fvsmassa@gmail.com>
2021-03-23 17:03:49 +0100

    Use FX to have a more robust intermediate feature extraction

--- test/test_backbone_utils.py
+++ test/test_backbone_utils.py
@@ -1,8 +1,10 @@
 import unittest
 
-
 import torch
+import torchvision
 from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+
+from torchvision.models._utils import IntermediateLayerGetter, IntermediateLayerGetter2
 
 
 class ResnetFPNBackboneTester(unittest.TestCase):
@@ -23,3 +25,46 @@ class ResnetFPNBackboneTester(unittest.TestCase):
         resnet50_fpn = resnet_fpn_backbone(backbone_name='resnet50', pretrained=False)
         y = resnet50_fpn(x)
         self.assertEqual(list(y.keys()), ['0', '1', '2', '3', 'pool'])
+
+
+class IntermediateLayerGetterTester(unittest.TestCase):
+    def test_old_new_match(self):
+        model = torchvision.models.resnet18(pretrained=False)
+
+        return_layers = {'layer2': '5', 'layer4': 'pool'}
+
+        old_model = IntermediateLayerGetter2(model, return_layers).eval()
+        new_model = IntermediateLayerGetter(model, return_layers).eval()
+
+        # check that we have same parameters
+        for (n1, p1), (n2, p2) in zip(old_model.named_parameters(), new_model.named_parameters()):
+            self.assertEqual(n1, n2)
+            self.assertTrue(p1.equal(p2))
+
+        # and state_dict matches
+        for (n1, p1), (n2, p2) in zip(old_model.state_dict().items(), new_model.state_dict().items()):
+            self.assertEqual(n1, n2)
+            self.assertTrue(p1.equal(p2))
+
+        # check that we actually compute the same thing
+        x = torch.rand(2, 3, 224, 224)
+        old_out = old_model(x)
+        new_out = new_model(x)
+        self.assertEqual(old_out.keys(), new_out.keys())
+        for k in old_out.keys():
+            o1 = old_out[k]
+            o2 = new_out[k]
+            self.assertTrue(o1.equal(o2))
+
+        # check torchscriptability
+        script_new_model = torch.jit.script(new_model)
+        new_out_script = script_new_model(x)
+        self.assertEqual(old_out.keys(), new_out_script.keys())
+        for k in old_out.keys():
+            o1 = old_out[k]
+            o2 = new_out_script[k]
+            self.assertTrue(o1.equal(o2))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- torchvision/models/_utils.py
+++ torchvision/models/_utils.py
@@ -1,11 +1,12 @@
 from collections import OrderedDict
 
 import torch
+import torch.fx
 from torch import nn
-from typing import Dict
+from typing import Dict, Any, Callable, Tuple, Optional
 
 
-class IntermediateLayerGetter(nn.ModuleDict):
+class IntermediateLayerGetter2(nn.ModuleDict):
     """
     Module wrapper that returns intermediate layers from a model
 
@@ -54,7 +55,7 @@ class IntermediateLayerGetter(nn.ModuleDict):
             if not return_layers:
                 break
 
-        super(IntermediateLayerGetter, self).__init__(layers)
+        super().__init__(layers)
         self.return_layers = orig_return_layers
 
     def forward(self, x):
@@ -65,3 +66,99 @@ class IntermediateLayerGetter(nn.ModuleDict):
                 out_name = self.return_layers[name]
                 out[out_name] = x
         return out
+
+
+# taken from https://github.com/pytorch/examples/blob/master/fx/module_tracer.py
+# with slight modifications
+class ModulePathTracer(torch.fx.Tracer):
+    """
+    ModulePathTracer is an FX tracer that--for each operation--also records
+    the qualified name of the Module from which the operation originated.
+    """
+
+    # The current qualified name of the Module being traced. The top-level
+    # module is signified by empty string. This is updated when entering
+    # call_module and restored when exiting call_module
+    current_module_qualified_name : str = ''
+    # A map from FX Node to the qualname of the Module from which it
+    # originated. This is recorded by `create_proxy` when recording an
+    # operation
+    node_to_originating_module : Dict[torch.fx.Node, str] = {}
+
+    def call_module(self, m: torch.nn.Module, forward: Callable[..., Any],
+                    args : Tuple[Any, ...], kwargs : Dict[str, Any]) -> Any:
+        """
+        Override of Tracer.call_module (see
+        https://pytorch.org/docs/stable/fx.html#torch.fx.Tracer.call_module).
+        This override:
+        1) Stores away the qualified name of the caller for restoration later
+        2) Installs the qualified name of the caller in `current_module_qualified_name`
+           for retrieval by `create_proxy`
+        3) Delegates into the normal Tracer.call_module method
+        4) Restores the caller's qualified name into current_module_qualified_name
+        """
+        old_qualname = self.current_module_qualified_name
+        try:
+            module_qualified_name = self.path_of_module(m)
+            self.current_module_qualified_name = module_qualified_name
+            if not self.is_leaf_module(m, module_qualified_name):
+                out = forward(*args, **kwargs)
+                self.node_to_originating_module[out.node] = module_qualified_name
+                return out
+            return self.create_proxy('call_module', module_qualified_name, args, kwargs)
+        finally:
+            self.current_module_qualified_name = old_qualname
+
+    def create_proxy(self, kind: str, target: torch.fx.node.Target, args: Tuple[Any, ...],
+                     kwargs: Dict[str, Any], name: Optional[str] = None, type_expr: Optional[Any] = None):
+        """
+        Override of `Tracer.create_proxy`. This override intercepts the recording
+        of every operation and stores away the current traced module's qualified
+        name in `node_to_originating_module`
+        """
+        proxy = super().create_proxy(kind, target, args, kwargs, name, type_expr)
+        self.node_to_originating_module[proxy.node] = self.current_module_qualified_name
+        return proxy
+
+
+def IntermediateLayerGetter(model: nn.Module, return_layers: Dict[str, str]) -> nn.Module:
+    return_layers = {str(k): str(v) for k, v in return_layers.items()}
+
+    # Instantiate our ModulePathTracer and use that to trace the model
+    tracer = ModulePathTracer()
+    graph = tracer.trace(model)
+
+    name = model.__class__.__name__ if isinstance(model, nn.Module) else model.__name__
+    m = torch.fx.GraphModule(tracer.root, graph, name)
+
+    # Get output node
+    orig_output_node: Optional[torch.fx.Node] = None
+    for n in reversed(m.graph.nodes):
+        if n.op == "output":
+            orig_output_node = n
+            break
+    assert orig_output_node
+    # and remove it
+    m.graph.erase_node(orig_output_node)
+
+    # find output nodes corresponding to return_layers
+    nodes = [n for n in m.graph.nodes]
+    output_node = OrderedDict()
+    for n in nodes:
+        module_qualname = tracer.node_to_originating_module.get(n)
+        if module_qualname in return_layers:
+            output_node[return_layers[module_qualname]] = n
+
+    # TODO raise error if some of return layers don't exist
+    # TODO have duplicate nodes but full coverage for module names
+
+    # and add them in the end of the graph
+    with m.graph.inserting_after(nodes[-1]):
+        m.graph.output(output_node)
+
+    m.graph.eliminate_dead_code()
+    m.recompile()
+
+    # remove unused modules / parameters
+    m = torch.fx.GraphModule(m, m.graph)
+    return m
