41a49274db4fea1817d5493a1205915df488b393
Nicolas Hug <nicolashug@fb.com>
2021-03-24 18:09:15 +0000

    Compute areas only once

--- torchvision/csrc/ops/quantized/cpu/qnms_kernel.cpp
+++ torchvision/csrc/ops/quantized/cpu/qnms_kernel.cpp
@@ -13,7 +13,6 @@ at::Tensor qnms_kernel_impl(
     const at::Tensor& scores,
     double iou_threshold) {
 
-
   TORCH_CHECK(!dets.is_cuda(), "dets must be a CPU tensor");
   TORCH_CHECK(!scores.is_cuda(), "scores must be a CPU tensor");
   TORCH_CHECK(
@@ -28,14 +27,13 @@ at::Tensor qnms_kernel_impl(
   auto x2_t = dets.select(1, 2).contiguous();
   auto y2_t = dets.select(1, 3).contiguous();
 
-  // TODO: compute areas here, to avoid duplicated computation in the most inner loop
-  // at::Tensor areas_t = (x2_t - x1_t) * (y2_t - y1_t);
-
   auto order_t = std::get<1>(scores.sort(0, /* descending=*/true));
   auto ndets = dets.size(0);
 
   at::Tensor suppressed_t = at::zeros({ndets}, dets.options().dtype(at::kByte));
   at::Tensor keep_t = at::zeros({ndets}, dets.options().dtype(at::kLong));
+
+  at::Tensor areas_t = at::zeros({ndets}, dets.options().dtype(at::kFloat));
 
   auto suppressed = suppressed_t.data_ptr<uint8_t>();
   auto keep = keep_t.data_ptr<int64_t>();
@@ -44,9 +42,15 @@ at::Tensor qnms_kernel_impl(
   auto y1 = y1_t.data_ptr<scalar_t>();
   auto x2 = x2_t.data_ptr<scalar_t>();
   auto y2 = y2_t.data_ptr<scalar_t>();
+  auto areas = areas_t.data_ptr<float>();
+  auto areas_a = areas_t.accessor<float, 1>();
 
   const auto dets_scale = dets.q_scale();
   const auto dets_zero_point = dets.q_zero_point();
+
+  for (int64_t i = 0; i < ndets; i++) {
+    areas_a[i] = dets_scale**2 * (x2[i].val_ - x1[i].val_) * (y2[i].val_ - y1[i].val_);
+  }
 
   int64_t num_to_keep = 0;
 
@@ -59,9 +63,7 @@ at::Tensor qnms_kernel_impl(
     auto iy1 = at::native::dequantize_val(dets_scale, dets_zero_point, y1[i]);
     auto ix2 = at::native::dequantize_val(dets_scale, dets_zero_point, x2[i]);
     auto iy2 = at::native::dequantize_val(dets_scale, dets_zero_point, y2[i]);
-    auto iw = ix2 - ix1;
-    auto ih = iy2 - iy1;
-    auto iarea = iw * ih;
+    auto iarea = areas[i];
 
     for (int64_t _j = _i + 1; _j < ndets; _j++) {
       auto j = order[_j];
@@ -71,10 +73,6 @@ at::Tensor qnms_kernel_impl(
       auto jy1 = at::native::dequantize_val(dets_scale, dets_zero_point, y1[j]);
       auto jx2 = at::native::dequantize_val(dets_scale, dets_zero_point, x2[j]);
       auto jy2 = at::native::dequantize_val(dets_scale, dets_zero_point, y2[j]);
-      auto jw = jx2 - jx1;
-      auto jh = jy2 - jy1;
-      auto jarea = jw * jh;
-
       auto xx1 = std::max(ix1, jx1);
       auto yy1 = std::max(iy1, jy1);
       auto xx2 = std::min(ix2, jx2);
@@ -83,7 +81,7 @@ at::Tensor qnms_kernel_impl(
       auto w = std::max(0.f, xx2 - xx1);
       auto h = std::max(0.f, yy2 - yy1);
       auto inter = w * h;
-      auto ovr = inter / (iarea + jarea - inter);
+      auto ovr = inter / (iarea + areas[j] - inter);
       if (ovr > iou_threshold)
         suppressed[j] = 1;
     }
