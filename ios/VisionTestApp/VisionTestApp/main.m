3feb5caa0b5d4a94179f10a684952295bda527e8
Yuchen Huang <hyc@fb.com>
2021-04-01 13:06:57 -0700

    [iOS] added VisionTestApp for testing vision ops
    [ghstack-poisoned]

---
+++ ios/VisionTestApp/VisionTestApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VisionTestApp
+//
+//  Created by Yuchen Huang on 3/31/21.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  NSString * appDelegateClassName;
+  @autoreleasepool {
+      // Setup code that might create autoreleased objects goes here.
+      appDelegateClassName = NSStringFromClass([AppDelegate class]);
+  }
+  return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
---
+++ ios/VisionTestApp/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex -o pipefail
+
+rm -rf ./install
+rm ./VisionTestApp/*.pt
