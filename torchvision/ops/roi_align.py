93d9bbf2519ee13d16fba331053c051ef213854a
Nicolas Hug <nicolashug@fb.com>
2021-04-08 09:57:42 +0100

    Fixed confusion  about feature maps according to comment

--- torchvision/ops/roi_align.py
+++ torchvision/ops/roi_align.py
@@ -20,12 +20,13 @@ def roi_align(
     Performs Region of Interest (RoI) Align operator with average pooling, as described in Mask R-CNN.
 
     Args:
-        input (Tensor[N, C, H, W]): The input tensor, i.e. a batch with ``N`` feature maps
+        input (Tensor[N, C, H, W]): The input tensor, i.e. a batch with ``N`` elements. Each element
+            contains ``C`` feature maps of dimensions ``H x W``.
         boxes (Tensor[K, 5] or List[Tensor[L, 4]]): the box coordinates in (x1, y1, x2, y2)
             format where the regions will be taken from.
             The coordinate must satisfy ``0 <= x1 < x2`` and ``0 <= y1 < y2``.
             If a single Tensor is passed, then the first column should
-            contain the index of the corresponding feature map in the batch, i.e. a number in ``[0, N - 1]``.
+            contain the index of the corresponding element in the batch, i.e. a number in ``[0, N - 1]``.
             If a list of Tensors is passed, then each Tensor will correspond to the boxes for an element i
             in the batch.
         output_size (int or Tuple[int, int]): the size of the output (in bins or pixels) after the pooling
