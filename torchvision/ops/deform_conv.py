3926c905b74af680b181fb06cca8ab7d871813af
Nicolas Hug <nicolashug@fb.com>
2021-04-14 16:27:03 +0100

    put back error on warnings for sphinx (#3671)

--- torchvision/ops/deform_conv.py
+++ torchvision/ops/deform_conv.py
@@ -29,23 +29,20 @@ def deform_conv2d(
 
     Args:
         input (Tensor[batch_size, in_channels, in_height, in_width]): input tensor
-        offset (Tensor[batch_size, 2 * offset_groups * kernel_height * kernel_width,
-            out_height, out_width]): offsets to be applied for each position in the
-            convolution kernel.
-        weight (Tensor[out_channels, in_channels // groups, kernel_height, kernel_width]):
-            convolution weights, split into groups of size (in_channels // groups)
+        offset (Tensor[batch_size, 2 * offset_groups * kernel_height * kernel_width, out_height, out_width]):
+            offsets to be applied for each position in the convolution kernel.
+        weight (Tensor[out_channels, in_channels // groups, kernel_height, kernel_width]): convolution weights,
+            split into groups of size (in_channels // groups)
         bias (Tensor[out_channels]): optional bias of shape (out_channels,). Default: None
         stride (int or Tuple[int, int]): distance between convolution centers. Default: 1
         padding (int or Tuple[int, int]): height/width of padding of zeroes around
             each image. Default: 0
         dilation (int or Tuple[int, int]): the spacing between kernel elements. Default: 1
-        mask (Tensor[batch_size, offset_groups * kernel_height * kernel_width,
-            out_height, out_width]): masks to be applied for each position in the
-            convolution kernel. Default: None
+        mask (Tensor[batch_size, offset_groups * kernel_height * kernel_width, out_height, out_width]):
+            masks to be applied for each position in the convolution kernel. Default: None
 
     Returns:
         Tensor[batch_sz, out_channels, out_h, out_w]: result of convolution
-
 
     Examples::
         >>> input = torch.rand(4, 3, 10, 10)
