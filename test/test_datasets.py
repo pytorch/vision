29c0d1e18f7f1c1a044e6bbd661ad26acae7e27e
Prabhat Roy <prabhatroy@fb.com>
2021-04-19 17:53:05 +0100

    Removed unused import

--- test/test_datasets.py
+++ test/test_datasets.py
@@ -9,11 +9,7 @@ from torch._utils_internal import get_file_path_2
 import torchvision
 from torchvision.datasets import utils
 from common_utils import get_tmp_dir
-from fakedata_generation import (
-    make_tar,
-    places365_root,
-    widerface_root,
-)
+from fakedata_generation import places365_root
 import xml.etree.ElementTree as ET
 from urllib.request import Request, urlopen
 import itertools
