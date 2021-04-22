222ddd4f4dad5634f5df4a3f0e31b051d86d1ecd
Aditya Oke <okeaditya315@gmail.com>
2021-04-22 21:55:58 +0530

    consistent code

--- gallery/plot_transforms.py
+++ gallery/plot_transforms.py
@@ -24,7 +24,7 @@ def plot(img, title: str = "", with_orig: bool = True, **kwargs):
         plt.axis('off')
 
     if with_orig:
-        _plot(orig_img, "Original image")
+        _plot(orig_img, "Original Image")
     _plot(img, title, **kwargs)
 
 
@@ -35,7 +35,7 @@ def plot(img, title: str = "", with_orig: bool = True, **kwargs):
 # (see also :func:`~torchvision.transforms.functional.pad`)
 # fills image borders with some pixel values.
 padded_img = T.Pad(padding=30)(orig_img)
-plot(padded_img, "Padded image")
+plot(padded_img, "Padded Image")
 
 ####################################
 # Resize
@@ -44,7 +44,7 @@ plot(padded_img, "Padded image")
 # (see also :func:`~torchvision.transforms.functional.resize`)
 # resizes an image.
 resized_img = T.Resize(size=30)(orig_img)
-plot(resized_img, "Resized image")
+plot(resized_img, "Resized Image")
 
 ####################################
 # CenterCrop
@@ -75,7 +75,7 @@ plot(img5, "Center Image", with_orig=False)
 # The :class:`~torchvision.transforms.ColorJitter` transform
 # randomly changes the brightness, saturation, and other properties of an image.
 jitted_img = T.ColorJitter(brightness=.5, hue=.3)(orig_img)
-plot(jitted_img, "Jitted image")
+plot(jitted_img, "Jitted Image")
 
 ####################################
 # Grayscale
@@ -84,7 +84,7 @@ plot(jitted_img, "Jitted image")
 # (see also :func:`~torchvision.transforms.functional.to_grayscale`)
 # converts an image to grayscale
 gray_img = T.Grayscale()(orig_img)
-plot(gray_img, "Grayscale image", cmap='gray')
+plot(gray_img, "Grayscale Image", cmap='gray')
 
 ####################################
 # RandomPerspective
@@ -93,7 +93,7 @@ plot(gray_img, "Grayscale image", cmap='gray')
 # (see also :func:`~torchvision.transforms.functional.perspective`)
 # performs random perspective transform on an image.
 perspectived_img = T.RandomPerspective(distortion_scale=0.6, p=1.0)(orig_img)
-plot(perspectived_img, "Perspective transformed image")
+plot(perspectived_img, "Perspective transformed Image")
 
 ####################################
 # RandomRotation
@@ -102,7 +102,7 @@ plot(perspectived_img, "Perspective transformed image")
 # (see also :func:`~torchvision.transforms.functional.rotate`)
 # rotates an image with random angle.
 rotated_img = T.RandomRotation(degrees=(30, 70))(orig_img)
-plot(rotated_img, "Rotated image")
+plot(rotated_img, "Rotated Image")
 
 ####################################
 # RandomAffine
@@ -111,7 +111,7 @@ plot(rotated_img, "Rotated image")
 # (see also :func:`~torchvision.transforms.functional.affine`)
 # performs random affine transform on an image.
 affined_img = T.RandomAffine(degrees=(30, 70), translate=(0.1, 0.3), scale=(0.5, 0.75))(orig_img)
-plot(affined_img, "Affine transformed image")
+plot(affined_img, "Affine transformed Image")
 
 ####################################
 # RandomCrop
@@ -120,7 +120,7 @@ plot(affined_img, "Affine transformed image")
 # (see also :func:`~torchvision.transforms.functional.crop`)
 # crops an image at a random location.
 crop_img = T.RandomCrop(size=(128, 128))(orig_img)
-plot(crop_img, "Random crop")
+plot(crop_img, "Random cropped Image")
 
 ####################################
 # RandomResizedCrop
@@ -130,7 +130,7 @@ plot(crop_img, "Random crop")
 # crops an image at a random location, and then resizes the crop to a given
 # size.
 resized_crop_img = T.RandomResizedCrop(size=(32, 32))(orig_img)
-plot(resized_crop_img, "Random resized crop")
+plot(resized_crop_img, "Random resized cropped Image")
 
 ####################################
 # RandomHorizontalFlip
@@ -143,7 +143,7 @@ plot(resized_crop_img, "Random resized crop")
 #   Since the transform is applied randomly, the two images below may actually be
 #   the same.
 random_hflip_img = T.RandomHorizontalFlip(p=0.5)(orig_img)
-plot(random_hflip_img, "Random horizontal flip")
+plot(random_hflip_img, "Random horizontal flipped Image")
 
 ####################################
 # RandomVerticalFlip
@@ -156,7 +156,7 @@ plot(random_hflip_img, "Random horizontal flip")
 #   Since the transform is applied randomly, the two images below may actually be
 #   the same.
 random_vflip_img = T.RandomVerticalFlip(p=0.5)(orig_img)
-plot(random_vflip_img, "Random vertical flip")
+plot(random_vflip_img, "Random vertical flipped Image")
 
 ####################################
 # RandomApply
@@ -168,7 +168,7 @@ plot(random_vflip_img, "Random vertical flip")
 #   Since the transform is applied randomly, the two images below may actually be
 #   the same.
 random_apply_img = T.RandomApply(transforms=[T.RandomCrop(size=(64, 64))], p=0.5)(orig_img)
-plot(random_apply_img, "Random Apply transform")
+plot(random_apply_img, "Random Apply transformed Image")
 
 ####################################
 # GaussianBlur
@@ -177,4 +177,4 @@ plot(random_apply_img, "Random Apply transform")
 # (see also :func:`~torchvision.transforms.functional.gaussian_blur`)
 # performs gaussianblur transform on an image.
 gaus_blur_img = T.GaussianBlur(kernel_size=(5, 9), sigma=(0.4, 3.0))(orig_img)
-plot(gaus_blur_img, "Gaussian Blur of image")
+plot(gaus_blur_img, "Gaussian Blurred Image")
