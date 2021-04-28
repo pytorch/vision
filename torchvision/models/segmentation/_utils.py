bf01d6d40ab5df5bdfab2340244e8ef6b08cb389
edxsx <65843134+edxsx@users.noreply.github.com>
2021-04-28 00:05:14 -0400

    Update _utils.py
    Split the backbone and the classifier onto 2 GPUs where device=['device0', 'device1']

--- torchvision/models/segmentation/_utils.py
+++ torchvision/models/segmentation/_utils.py
@@ -9,9 +9,9 @@ class _SimpleSegmentationModel(nn.Module):
 
     def __init__(self, backbone, classifier, aux_classifier=None):
         super(_SimpleSegmentationModel, self).__init__()
-        self.backbone = backbone
-        self.classifier = classifier
-        self.aux_classifier = aux_classifier
+        self.backbone = backbone.to(device[0])
+        self.classifier = classifier.to(device[1])
+        self.aux_classifier = aux_classifier.to(device[1])
 
     def forward(self, x):
         input_shape = x.shape[-2:]
@@ -20,13 +20,13 @@ class _SimpleSegmentationModel(nn.Module):
 
         result = OrderedDict()
         x = features["out"]
-        x = self.classifier(x)
+        x = self.classifier(x.to(device[1]))
         x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
         result["out"] = x
 
         if self.aux_classifier is not None:
             x = features["aux"]
-            x = self.aux_classifier(x)
+            x = self.aux_classifier(x.to(device[1]))
             x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
             result["aux"] = x
 
