b7b81b17b9d4dff3dcbe102730590c58df2d75ef
Bruno Korbar <bjuncek@gmail.com>
2021-05-05 12:01:48 -0500

    special case if there are no default arguments

--- test/datasets_utils.py
+++ test/datasets_utils.py
@@ -637,7 +637,11 @@ class VideoDatasetTestCase(DatasetTestCase):
 
     def _set_default_frames_per_clip(self, inject_fake_data):
         argspec = inspect.getfullargspec(self.DATASET_CLASS.__init__)
-        args_without_default = argspec.args[1:-len(argspec.defaults)]
+        # edge case if dataset doesn't have default parameters
+        if argspec.defaults:
+            args_without_default = argspec.args[1:-len(argspec.defaults)]
+        else:
+            args_without_default = argspec.args[1:]
         frames_per_clip_last = args_without_default[-1] == "frames_per_clip"
 
         @functools.wraps(inject_fake_data)
