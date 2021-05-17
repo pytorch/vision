c88e9b214233e33c794e28a96c547025000c3927
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared29093.08.ftw1.facebook.com>
2021-05-17 04:31:55 -0700

    2021-05-17 nightly release (32bccc53cd442e59904a001d269acda2c4357f34)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -137,6 +137,11 @@ html_logo = '_static/img/pytorch-logo-dark.svg'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# TODO: remove this once https://github.com/pytorch/pytorch_sphinx_theme/issues/125 is fixed
+html_css_files = [
+    'css/custom_torchvision.css',
+]
+
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
