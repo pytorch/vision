bc72847403b070a695f40c88b72c334c878d68f5
Aditya Oke <okeaditya315@gmail.com>
2021-06-03 23:42:01 +0530

    undo latex

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -146,6 +146,24 @@ htmlhelp_basename = 'PyTorchdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
+
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
