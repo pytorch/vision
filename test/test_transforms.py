06511f2406844fc7c901bb3a41e9ea4b6d917a61
Dbhasin@1 <drishti_b@me.iitr.ac.in>
2021-06-03 18:52:24 +0530

    port test_pad in test_transforms to pytest

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -120,10 +120,10 @@ class Tester(unittest.TestCase):
                     input_center_tl[index] = (input_image_size[index] - crop_size[index]) // 2
 
             output_center = output_pil[
-                                      :,
-                                      crop_center_tl[0]:crop_center_tl[0] + center_size[0],
-                                      crop_center_tl[1]:crop_center_tl[1] + center_size[1]
-                                      ]
+                            :,
+                            crop_center_tl[0]:crop_center_tl[0] + center_size[0],
+                            crop_center_tl[1]:crop_center_tl[1] + center_size[1]
+                            ]
 
             img_center = img[
                          :,
