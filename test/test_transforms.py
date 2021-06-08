32f65450443783f1452477f15688ff68d9c3bd9b
Nicolas Hug <nicolashug@fb.com>
2021-06-08 09:57:55 +0100

    put back test_center_crop

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -1729,6 +1729,44 @@ def test_random_crop():
         t(img)
 
 
+def test_center_crop():
+    height = random.randint(10, 32) * 2
+    width = random.randint(10, 32) * 2
+    oheight = random.randint(5, (height - 2) / 2) * 2
+    owidth = random.randint(5, (width - 2) / 2) * 2
+
+    img = torch.ones(3, height, width)
+    oh1 = (height - oheight) // 2
+    ow1 = (width - owidth) // 2
+    imgnarrow = img[:, oh1:oh1 + oheight, ow1:ow1 + owidth]
+    imgnarrow.fill_(0)
+    result = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.CenterCrop((oheight, owidth)),
+        transforms.ToTensor(),
+    ])(img)
+    assert result.sum() == 0
+    oheight += 1
+    owidth += 1
+    result = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.CenterCrop((oheight, owidth)),
+        transforms.ToTensor(),
+    ])(img)
+    sum1 = result.sum()
+    assert sum1 > 1
+    oheight += 1
+    owidth += 1
+    result = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.CenterCrop((oheight, owidth)),
+        transforms.ToTensor(),
+    ])(img)
+    sum2 = result.sum()
+    assert sum2 > 0
+    assert sum2 > sum1
+
+
 @pytest.mark.parametrize('odd_image_size', (True, False))
 @pytest.mark.parametrize('delta', (1, 3, 5))
 @pytest.mark.parametrize('delta_width', (-2, -1, 0, 1, 2))
