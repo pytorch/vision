92bfaa173aecc0489cdd8703d0f15729e0153105
Nicolas Hug <nicolashug@fb.com>
2021-06-09 17:27:00 +0100

    trailing whitespace

--- test/conftest.py
+++ test/conftest.py
@@ -44,7 +44,7 @@ def pytest_collection_modifyitems(items):
             # Here we're not in fbcode, so we can safely collect and skip tests.
             if not needs_cuda and torch.cuda.is_available():
                 # Similar to what happens in RE workers: we don't need the CircleCI GPU machines
-                # to run the CPU-only tests. 
+                # to run the CPU-only tests.
                 item.add_marker(pytest.mark.skip(reason=CIRCLECI_GPU_NO_CUDA_MSG))
 
         out_items.append(item)
