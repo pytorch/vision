00b9a2ce713709119e4cc299e5cfbfe5435bab12
Anirudh Dagar <anirudhdagar6@gmail.com>
2021-06-09 16:42:12 +0530

    remove if else since now we only test on list sizes for scripted

--- test/test_transforms_tensor.py
+++ test/test_transforms_tensor.py
@@ -676,11 +676,6 @@ def test_resize_scripted(dt, size, max_size, interpolation, device):
     if max_size is not None and isinstance(size, Sequence) and len(size) != 1:
         pytest.xfail()
     else:
-        if isinstance(size, int):
-            script_size = [size, ]
-        else:
-            script_size = size
-
         transform = T.Resize(size=script_size, interpolation=interpolation, max_size=max_size)
         s_transform = torch.jit.script(transform)
         _test_transform_vs_scripted(transform, s_transform, tensor)
