2b02dfe847db30efeb3536540b77d5a4870bd1de
Philip Meier <github.pmeier@posteo.de>
2021-06-09 11:36:13 +0200

    [skip CI] cleanup PR labeling workflow (#4019)

--- .github/process_commit.py
+++ .github/process_commit.py
@@ -1,10 +1,15 @@
-import json
+"""
+This script finds all responsible users for labeling a PR by a commit SHA. It is used by the workflow in
+'.github/workflows/pr-labels.yml'. If there exists no PR associated with the commit or the PR is properly labeled,
+this script is a no-op.
+"""
+
 import sys
-from typing import Any, Dict, Iterable, Optional, Set, Tuple
+from typing import Any, Collection, Optional, Set, Tuple
 
 import requests
 
-# If the PR has any of these labels, we mark it as properly labeled.
+# If the PR has any of these labels, we accept it as properly labeled.
 REQUIRED_LABELS = {
     "new feature",
     "bug",
@@ -33,41 +38,27 @@ REQUIRED_LABELS = {
 }
 
 
-def main(commit_hash: str) -> Dict[str, Any]:
+def find_responsible_users(commit_hash: str) -> Collection:
     pr_number = get_pr_number(commit_hash)
-    if pr_number is None:
-        return _to_json(has_associated_pr=False)
+    if not pr_number:
+        return ""
 
     merger, labels = get_pr_merger_and_labels(pr_number)
     is_properly_labeled = bool(REQUIRED_LABELS.intersection(labels))
     if is_properly_labeled:
-        return _to_json(has_associated_pr=True, is_properly_labeled=True)
+        return ""
 
-    users = {merger, *get_pr_reviewers(pr_number)}
-    return _to_json(has_associated_pr=True, is_properly_labeled=False, users=users)
+    return {merger, *get_pr_reviewers(pr_number)}
 
 
-def _query_torchvision(cmd: str, *, accept) -> Any:
+def query_torchvision(cmd: str, *, accept) -> Any:
     response = requests.get(f"https://api.github.com/repos/pytorch/vision/{cmd}", headers=dict(Accept=accept))
     return response.json()
 
 
-def _to_json(
-    *,
-    has_associated_pr: bool = False,
-    is_properly_labeled: bool = False,
-    users: Iterable[str] = (),
-) -> Dict[str, Any]:
-    return dict(
-        has_associated_pr=has_associated_pr,
-        is_properly_labeled=is_properly_labeled,
-        responsible_users=", ".join(sorted([f"@{user}" for user in users])),
-    )
-
-
 def get_pr_number(commit_hash: str) -> Optional[int]:
     # See https://docs.github.com/en/rest/reference/repos#list-pull-requests-associated-with-a-commit
-    data = _query_torchvision(f"commits/{commit_hash}/pulls", accept="application/vnd.github.groot-preview+json")
+    data = query_torchvision(f"commits/{commit_hash}/pulls", accept="application/vnd.github.groot-preview+json")
     if not data:
         return None
     return data[0]["number"]
@@ -75,7 +66,7 @@ def get_pr_number(commit_hash: str) -> Optional[int]:
 
 def get_pr_merger_and_labels(pr_number: int) -> Tuple[str, Set[str]]:
     # See https://docs.github.com/en/rest/reference/pulls#get-a-pull-request
-    data = _query_torchvision(f"pulls/{pr_number}", accept="application/vnd.github.v3+json")
+    data = query_torchvision(f"pulls/{pr_number}", accept="application/vnd.github.v3+json")
     merger = data["merged_by"]["login"]
     labels = {label["name"] for label in data["labels"]}
     return merger, labels
@@ -83,11 +74,11 @@ def get_pr_merger_and_labels(pr_number: int) -> Tuple[str, Set[str]]:
 
 def get_pr_reviewers(pr_number: int) -> Set[str]:
     # See https://docs.github.com/en/rest/reference/pulls#list-reviews-for-a-pull-request
-    data = _query_torchvision(f"pulls/{pr_number}/reviews", accept="application/vnd.github.v3+json")
+    data = query_torchvision(f"pulls/{pr_number}/reviews", accept="application/vnd.github.v3+json")
     return {review["user"]["login"] for review in data if review["state"] == "APPROVED"}
 
 
 if __name__ == "__main__":
     commit_hash = sys.argv[1]
-    data = main(commit_hash)
-    print(json.dumps(data))
+    users = find_responsible_users(commit_hash)
+    print(", ".join(sorted([f"@{user}" for user in users])))
