1f2646be6801ac7ae82fcccca6f31a1c1c88b3fb
Prabhat Roy <prabhatroy@fb.com>
2021-06-11 19:49:14 +0100

    Removed capture_true argument

--- setup.py
+++ setup.py
@@ -350,7 +350,8 @@ def get_extensions():
     if has_ffmpeg:
         try:
             ffmpeg_version = subprocess.run(
-                'ffmpeg -version | head -n1', shell=True, capture_output=True).stdout.decode('utf-8')
+                'ffmpeg -version | head -n1', shell=True,
+                stdout=subprocess.PIPE).stdout.decode('utf-8')
             ffmpeg_version = ffmpeg_version.split('version')[-1].split()[0]
             if StrictVersion(ffmpeg_version) >= StrictVersion('4.3'):
                 print(f'ffmpeg {ffmpeg_version} not supported yet, please use ffmpeg 4.2.')
