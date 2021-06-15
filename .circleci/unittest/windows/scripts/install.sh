5a298742f9b03b658be211c47425d9262aae58d3
Yi Zhang <zhanyi@microsoft.com>
2021-06-15 00:08:42 -0700

    enablut cuda test on vision
    fix dir
    
    fix lint
    
    only for windows
    
    version transform
    
    fix typo
    
    increase timeout
    
    only support vc 2019
    
    support mutliple version def and remove msbuildextension
    
    retry
    
    retry
    
    vs integration
    
    vs integration
    
    set cuda_home
    
    add logs
    
    add logs
    
    7z with overwrite mode
    
    retry
    
    retry
    
    force install cu11.1
    
    force install cu11.1
    
    force install cu11.1
    
    restore steps
    
    make python setup.py develop works
    
    minor fix
    
    minor change
    
    new step for 102
    
    update step cuda to 11.1
    
    11.1 in step
    
    add logs
    
    minor change
    
    display driver
    
    rm cache
    
    rm cache
    
    reuse install driver step
    
    retry
    
    restore to 10.2
    
    use 11.1 to avoid No CUDA Runtime
    
    add torch.cuda() test after installation
    
    set step version to 11.1
    
    use cuda_install.bat
    
    fix lint
    
    retry
    
    check env change
    
    move step before save cache
    
    force update the envs
    
    merge 3 steps for envs
    
    set env based on cu_version
    
    retry
    
    code clean
    
    minor fix
    
    for cmake gpu
    
    mv gpudrivers
    
    merge
    
    keep conda-force and 11 for cmake gpu
    
    minor fix
    
    use same conda method with unittest
    
    conda-forge is needed
    
    retry 11.1 for cmake
    
    add toolkit root dir
    
    new root env
    
    add more cuda envs
    
    minor fix
    
    rm old cuda dir
    
    rm old cuda env
    
    minor change
    
    update nvtools download link
    
    minor fix
    
    rm temp code
    
    continue clean
    
    minor change
    
    add CUDA_HOME
    
    Update test_backbone_utils.py
    
    put back testing for all tests
    
    try
    
    add package nvjpeg in cu111
    
    retry
    
    retry nvjpeg_11.1
    
    enable all nvjpeg
    
    rm unnecessary change
    
    Update build_cmake.sh
    
    only split remove compatible_cmath
    
    rm compatible_cmath
    
    try install packaging in Pypi
    
    minor try
    
    minor change
    
    tricky for CU_VERSION and CUDA_VERSION

--- .circleci/unittest/windows/scripts/install.sh
+++ .circleci/unittest/windows/scripts/install.sh
@@ -5,28 +5,35 @@ unset PYTORCH_VERSION
 # so no need to set PYTORCH_VERSION.
 # In fact, keeping PYTORCH_VERSION forces us to hardcode PyTorch version in config.
 
-set -e
+set -ex
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 eval "$(./conda/Scripts/conda.exe 'shell.bash' 'hook')"
 conda activate ./env
 
-if [ "${CU_VERSION:-}" == cpu ] ; then
+echo "${CU_VERSION}"
+echo "${CUDA_VERSION}"
+
+if [ "${CU_VERSION:-}" == "cpu" ] ; then
     cudatoolkit="cpuonly"
 else
-    if [[ ${#CU_VERSION} -eq 4 ]]; then
-        CUDA_VERSION="${CU_VERSION:2:1}.${CU_VERSION:3:1}"
-    elif [[ ${#CU_VERSION} -eq 5 ]]; then
+    # CUDA_VERSION derived from CU_VERSION only if CU_VERSION is defined like cu101
+    # Else, CUDA_VERSION will be a strange value
+    if [[ ${#CU_VERSION} -eq 5 ]]; then
         CUDA_VERSION="${CU_VERSION:2:2}.${CU_VERSION:4:1}"
     fi
-    echo "Using CUDA $CUDA_VERSION as determined by CU_VERSION"
+    
+    echo "Using CUDA $CUDA_VERSION as determined by CU_VERSION $CU_VERSION"
+    # unit test workflow logic is a little strange
+    # In unittest_windows_cpu, CU_VERSION is cpu, but CU_VERSION is empty in unittest_windows_gpu.
+    # In fact, the $CUDA_VERSION is defined as enviornment variable in unittest_windows_gpu
     version="$(python -c "print('.'.join(\"${CUDA_VERSION}\".split('.')[:2]))")"
     cudatoolkit="cudatoolkit=${version}"
 fi
 
 printf "Installing PyTorch with %s\n" "${cudatoolkit}"
-conda install -y -c "pytorch-${UPLOAD_CHANNEL}" "pytorch-${UPLOAD_CHANNEL}"::pytorch "${cudatoolkit}" pytest
+conda install -y -c "pytorch-${UPLOAD_CHANNEL}" -c conda-forge "pytorch-${UPLOAD_CHANNEL}"::pytorch "${cudatoolkit}" pytest
 
 if [ $PYTHON_VERSION == "3.6" ]; then
     printf "Installing minimal PILLOW version\n"
@@ -34,5 +41,19 @@ if [ $PYTHON_VERSION == "3.6" ]; then
     pip install pillow>=5.3.0
 fi
 
+# check torch 
+python -c "import torch; print(torch.cuda.is_available())"
+# check cuda driver version
+for path in '/c/Program Files/NVIDIA Corporation/NVSMI/nvidia-smi.exe' /c/Windows/System32/nvidia-smi.exe; do
+    if [[ -x "$path" ]]; then
+        "$path" || echo "true";
+        break
+    fi
+done
+
+# set the cuda env vars
+source "$this_dir/set_cuda_envs.sh"
+
 printf "* Installing torchvision\n"
 "$this_dir/vc_env_helper.bat" python setup.py develop
+
