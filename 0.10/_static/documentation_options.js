ed6d60e7272ded01b7793c419638f91e6993a4fa
mattip <matti.picus@gmail.com>
2021-06-15 16:47:41 +0300

    add v0.10 documentation, adjust versions.html

---
+++ 0.10/_static/documentation_options.js
@@ -0,0 +1,11 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.10.0a0+25b2e69',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true
+};
