208ff257918b81db4f403238aec8e769d268a68d
Nicolas Hug <nicolashug@fb.com>
2021-06-16 09:02:38 +0100

    Add comments

--- torchvision/models/utils.py
+++ torchvision/models/utils.py
@@ -1,3 +1,6 @@
+# Don't remove this file and don't change the imports of load_state_dict_from_url
+# from other files. We need this so that we can swap load_state_dict_from_url with
+# a custom internal version in fbcode.
 try:
     from torch.hub import load_state_dict_from_url
 except ImportError:
