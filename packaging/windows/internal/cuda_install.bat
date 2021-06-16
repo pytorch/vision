8f6b9ab1c2ee6a12b951dc4f3043fa2b6933ecc0
Yi Zhang <zhanyi@microsoft.com>
2021-06-16 09:03:05 -0700

    use 11.1 to avoid No CUDA Runtime

--- packaging/windows/internal/cuda_install.bat
+++ packaging/windows/internal/cuda_install.bat
@@ -204,11 +204,12 @@ xcopy /Y "%SRC_DIR%\temp_build\cudnn\cuda\bin\*.*" "%ProgramFiles%\NVIDIA GPU Co
 xcopy /Y "%SRC_DIR%\temp_build\cudnn\cuda\lib\x64\*.*" "%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v%CUDA_VERSION_STR%\lib\x64"
 xcopy /Y "%SRC_DIR%\temp_build\cudnn\cuda\include\*.*" "%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v%CUDA_VERSION_STR%\include"
 
+
 rem echo Installing GPU driver DLLs
-7z x %SRC_DIR%\temp_build\gpu_driver_dlls.zip -aoa -o"C:\Windows\System32"
+rem 7z x %SRC_DIR%\temp_build\gpu_driver_dlls.zip -aoa -o"C:\Windows\System32"
 
 rem cuda version
-rem C:\Windows\System32\nvidia-smi.exe
+C:\Windows\System32\nvidia-smi.exe
 where nvcc
 nvcc --version
 
