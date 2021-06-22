404f1065f0849fc8d0de508110f0ce84b016228c
Yi Zhang <zhanyi@microsoft.com>
2021-06-22 09:35:46 -0700

    minor change

--- packaging/windows/internal/cuda_install.bat
+++ packaging/windows/internal/cuda_install.bat
@@ -178,8 +178,11 @@ time /t
 popd
 
 echo Installing VS integration...
-xcopy /Y "%SRC_DIR%\temp_build\cuda\CUDAVisualStudioIntegration\extras\visual_studio_integration\MSBuildExtensions\*.*" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Microsoft\VC\v160\BuildCustomizations"
+xcopy /Y "%SRC_DIR%\temp_build\cuda\visual_studio_integration\CUDAVisualStudioIntegration\extras\visual_studio_integration\MSBuildExtensions\*.*" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Microsoft\VC\v160\BuildCustomizations"
 
+rem 11 visual_studio_integration/CUDAVisualStudioIntegration/extras/visual_studio_integration/MSBuildExtensions"
+
+rem 10 CUDAVisualStudioIntegration/extras/visual_studio_integration/MSBuildExtensions
 
 echo Installing NvToolsExt...
 7z x %SRC_DIR%\temp_build\NvToolsExt.7z -o"%SRC_DIR%\temp_build\NvToolsExt"
