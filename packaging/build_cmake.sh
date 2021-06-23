85adf0927e5f73d899cb0b7896f8eb358e2788ca
Yi Zhang <zhanyi@microsoft.com>
2021-06-22 22:17:56 -0700

    install stable pytorch

--- packaging/build_cmake.sh
+++ packaging/build_cmake.sh
@@ -29,7 +29,7 @@ setup_visual_studio_constraint
 setup_junit_results_folder
 
 # conda install -yq pytorch=$PYTORCH_VERSION $CONDA_CUDATOOLKIT_CONSTRAINT $CONDA_CPUONLY_FEATURE  -c "pytorch-${UPLOAD_CHANNEL}"
-conda install -yq -c "pytorch-${UPLOAD_CHANNEL}" -c conda-forge "pytorch-${UPLOAD_CHANNEL}"::pytorch $CONDA_CUDATOOLKIT_CONSTRAINT $CONDA_CPUONLY_FEATURE
+conda install -yq -c pytorch pytorch $CONDA_CUDATOOLKIT_CONSTRAINT $CONDA_CPUONLY_FEATURE
 TORCH_PATH=$(dirname $(python -c "import torch; print(torch.__file__)"))
 
 if [[ "$(uname)" == Darwin || "$OSTYPE" == "msys" ]]; then
