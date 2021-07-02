76ade5716ebc064ac596d8c05e289706fdb95356
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared23466.02.frc2.facebook.com>
2021-07-02 04:33:11 -0700

    2021-07-02 nightly release (ef711591a5db69d36f904ab5c39dec13627a58ad)

--- setup.py
+++ setup.py
@@ -67,7 +67,8 @@ requirements = [
     pytorch_dep,
 ]
 
-pillow_ver = ' >= 5.3.0'
+# Excluding 8.3.0 because of https://github.com/pytorch/vision/issues/4146
+pillow_ver = ' >= 5.3.0, !=8.3.0'
 pillow_req = 'pillow-simd' if get_dist('pillow-simd') is not None else 'pillow'
 requirements.append(pillow_req + pillow_ver)
 
