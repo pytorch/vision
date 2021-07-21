86236bbebb945e2f6c6e3e781418edd35df238dc
Philip Meier <github.pmeier@posteo.de>
2021-07-21 14:26:28 +0200

    simplify

--- test/test_internet.py
+++ test/test_internet.py
@@ -9,60 +9,39 @@ import os
 import pytest
 import warnings
 from urllib.error import URLError
-from urllib.request import Request
+import contextlib
 
 import torchvision.datasets.utils as utils
 from common_utils import get_tmp_dir
 
 
-@pytest.fixture
-def patch_url_redirection(mocker):
+def patch_url_redirection(mocker, redirect_url):
     class Response:
         def __init__(self, url):
             self.url = url
 
-    def factory(*urls):
-        class PatchedOpener:
-            def __init__(self, request_or_url, *args, **kwargs):
-                self._request_or_url = request_or_url
+    @contextlib.contextmanager
+    def patched_opener(*args, **kwargs):
+        yield Response(redirect_url)
 
-            def __enter__(self):
-                url = (
-                    self._request_or_url.full_url
-                    if isinstance(self._request_or_url.full_url, Request)
-                    else self._request_or_url.full_url
-                )
-
-                if url == urls[-1]:
-                    redirect_url = url
-                else:
-                    redirect_url = urls[urls.index(url) + 1]
-
-                return Response(redirect_url)
-
-            def __exit__(self, exc_type, exc_val, exc_tb):
-                pass
-
-        mocker.patch("torchvision.datasets.utils.urllib.request.urlopen", new=PatchedOpener)
-
-    return factory
+    mocker.patch("torchvision.datasets.utils.urllib.request.urlopen", new=patched_opener)
 
 
 class TestDatasetUtils:
-    def test_get_redirect_url(self, patch_url_redirection):
+    def test_get_redirect_url(self, mocker):
         url = "http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz"
-        expected_redirected_url = "https://drive.google.com/file/d/1hbzc_P1FuxMkcabkgn9ZKinBwW683j45/view"
+        expected_redirect_url = "https://drive.google.com/file/d/1hbzc_P1FuxMkcabkgn9ZKinBwW683j45/view"
 
-        patch_url_redirection(url, expected_redirected_url)
+        patch_url_redirection(mocker, expected_redirect_url)
 
         actual = utils._get_redirect_url(url)
-        assert actual == expected_redirected_url
+        assert actual == expected_redirect_url
 
-    def test_get_redirect_url_max_hops_exceeded(self, patch_url_redirection):
+    def test_get_redirect_url_max_hops_exceeded(self, mocker):
         url = "http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz"
-        redirected_url = "https://drive.google.com/file/d/1hbzc_P1FuxMkcabkgn9ZKinBwW683j45/view"
+        redirect_url = "https://drive.google.com/file/d/1hbzc_P1FuxMkcabkgn9ZKinBwW683j45/view"
 
-        patch_url_redirection(url, redirected_url)
+        patch_url_redirection(mocker, redirect_url)
 
         with pytest.raises(RecursionError):
             utils._get_redirect_url(url, max_hops=0)
