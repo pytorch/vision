fca4afb375b7e5a981dda5f2d8272b969c73e9ef
Philip Meier <github.pmeier@posteo.de>
2021-07-22 12:15:32 +0200

    pin delocate version for macos wheel build

--- packaging/build_wheel.sh
+++ packaging/build_wheel.sh
@@ -19,7 +19,7 @@ if [[ "$(uname)" == Darwin || "$OSTYPE" == "msys" ]]; then
     env_path=$(dirname $bin_path)
     if [[ "$(uname)" == Darwin ]]; then
         # Install delocate to relocate the required binaries
-        pip_install delocate
+        pip_install "delocate<0.9"
     else
         cp "$bin_path/Library/bin/libpng16.dll" torchvision
         cp "$bin_path/Library/bin/libjpeg.dll" torchvision
