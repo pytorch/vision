73692759656005631bf0ff8d0ca28b7bdbe580a1
Vincent Moens <vmoens@fb.com>
2021-08-05 10:29:40 +0100

    ffmpeg version 4.2 control

--- setup.py
+++ setup.py
@@ -351,7 +351,8 @@ def get_extensions():
     has_ffmpeg = ffmpeg_exe is not None
     if has_ffmpeg:
         try:
-            ffmpeg_version = re.split("\.|\ ", str(subprocess.check_output(["ffmpeg", "-version"])).split("ffmpeg version ")[1])[:2]
+            ffmpeg_version_str = str(subprocess.check_output(["ffmpeg", "-version"]))
+            ffmpeg_version = re.split(r"ffmpeg\ version\ |\.|\ |-", ffmpeg_version_str)[1:3]
             ffmpeg_version = ".".join(ffmpeg_version)
             if StrictVersion(ffmpeg_version) >= StrictVersion('4.3'):
                 print(f'ffmpeg {ffmpeg_version} not supported yet, please use ffmpeg 4.2.')
