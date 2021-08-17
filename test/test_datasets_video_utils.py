8796cbeb26eaef05a3861f326fc748f542164c6c
Alex Lin <alexconglin@fb.com>
2021-08-17 14:48:13 +0100

    Remove get_list_of_videos under context manager

--- test/test_datasets_video_utils.py
+++ test/test_datasets_video_utils.py
@@ -58,39 +58,39 @@ class TestVideo:
 
     @pytest.mark.skipif(not io.video._av_available(), reason="this test requires av")
     def test_video_clips(self, tmpdir):
-        with get_list_of_videos(tmpdir, num_videos=3) as video_list:
-            video_clips = VideoClips(video_list, 5, 5, num_workers=2)
-            assert video_clips.num_clips() == 1 + 2 + 3
-            for i, (v_idx, c_idx) in enumerate([(0, 0), (1, 0), (1, 1), (2, 0), (2, 1), (2, 2)]):
-                video_idx, clip_idx = video_clips.get_clip_location(i)
-                assert video_idx == v_idx
-                assert clip_idx == c_idx
+        video_list = get_list_of_videos(tmpdir, num_videos=3)
+        video_clips = VideoClips(video_list, 5, 5, num_workers=2)
+        assert video_clips.num_clips() == 1 + 2 + 3
+        for i, (v_idx, c_idx) in enumerate([(0, 0), (1, 0), (1, 1), (2, 0), (2, 1), (2, 2)]):
+            video_idx, clip_idx = video_clips.get_clip_location(i)
+            assert video_idx == v_idx
+            assert clip_idx == c_idx
 
-            video_clips = VideoClips(video_list, 6, 6)
-            assert video_clips.num_clips() == 0 + 1 + 2
-            for i, (v_idx, c_idx) in enumerate([(1, 0), (2, 0), (2, 1)]):
-                video_idx, clip_idx = video_clips.get_clip_location(i)
-                assert video_idx == v_idx
-                assert clip_idx == c_idx
+        video_clips = VideoClips(video_list, 6, 6)
+        assert video_clips.num_clips() == 0 + 1 + 2
+        for i, (v_idx, c_idx) in enumerate([(1, 0), (2, 0), (2, 1)]):
+            video_idx, clip_idx = video_clips.get_clip_location(i)
+            assert video_idx == v_idx
+            assert clip_idx == c_idx
 
-            video_clips = VideoClips(video_list, 6, 1)
-            assert video_clips.num_clips() == 0 + (10 - 6 + 1) + (15 - 6 + 1)
-            for i, v_idx, c_idx in [(0, 1, 0), (4, 1, 4), (5, 2, 0), (6, 2, 1)]:
-                video_idx, clip_idx = video_clips.get_clip_location(i)
-                assert video_idx == v_idx
-                assert clip_idx == c_idx
+        video_clips = VideoClips(video_list, 6, 1)
+        assert video_clips.num_clips() == 0 + (10 - 6 + 1) + (15 - 6 + 1)
+        for i, v_idx, c_idx in [(0, 1, 0), (4, 1, 4), (5, 2, 0), (6, 2, 1)]:
+            video_idx, clip_idx = video_clips.get_clip_location(i)
+            assert video_idx == v_idx
+            assert clip_idx == c_idx
 
     @pytest.mark.skipif(not io.video._av_available(), reason="this test requires av")
     def test_video_clips_custom_fps(self, tmpdir):
-        with get_list_of_videos(tmpdir, num_videos=3, sizes=[12, 12, 12], fps=[3, 4, 6]) as video_list:
-            num_frames = 4
-            for fps in [1, 3, 4, 10]:
-                video_clips = VideoClips(video_list, num_frames, num_frames, fps, num_workers=2)
-                for i in range(video_clips.num_clips()):
-                    video, audio, info, video_idx = video_clips.get_clip(i)
-                    assert video.shape[0] == num_frames
-                    assert info["video_fps"] == fps
-                    # TODO add tests checking that the content is right
+        video_list = get_list_of_videos(tmpdir, num_videos=3, sizes=[12, 12, 12], fps=[3, 4, 6])
+        num_frames = 4
+        for fps in [1, 3, 4, 10]:
+            video_clips = VideoClips(video_list, num_frames, num_frames, fps, num_workers=2)
+            for i in range(video_clips.num_clips()):
+                video, audio, info, video_idx = video_clips.get_clip(i)
+                assert video.shape[0] == num_frames
+                assert info["video_fps"] == fps
+                # TODO add tests checking that the content is right
 
     def test_compute_clips_for_video(self):
         video_pts = torch.arange(30)
