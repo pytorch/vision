3830dd11ae16b0085c7abcd885fc69695a2f63fb
Allen Goodman <allen.goodman@icloud.com>
2021-08-18 13:18:46 -0400

    unit test

--- test/test_masks_to_bounding_boxes.py
+++ test/test_masks_to_bounding_boxes.py
@@ -5,6 +5,8 @@ import numpy
 import pytest
 import torch
 
+import torchvision.ops
+
 ASSETS_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), "assets")
 
 
@@ -28,4 +30,14 @@ def masks() -> torch.Tensor:
 
 
 def test_masks_to_bounding_boxes(masks):
-    pass
+    expected = torch.tensor(
+       [[ 127.,    2.,  165.,   40.],
+        [   4.,  100.,   88.,  184.],
+        [ 168.,  189.,  294.,  300.],
+        [ 556.,  272.,  700.,  416.],
+        [ 800.,  560.,  990.,  725.],
+        [ 294.,  828.,  594., 1092.],
+        [ 756., 1036., 1064., 1491.]]
+    )
+
+    torch.testing.assert_close(torchvision.ops.masks_to_bounding_boxes(masks), expected)
