ef075060c0d2be4c4b28ae0c2a8e87d3842c6519
ABD-01 <myac931@gmail.com>
2021-08-23 09:35:40 +0000

    Updated setup.py to use TorchVersion object for version comparision

--- setup.py
+++ setup.py
@@ -147,9 +147,7 @@ def get_extensions():
     )
 
     is_rocm_pytorch = False
-    TORCH_MAJOR = int(torch.__version__.split('.')[0])
-    TORCH_MINOR = int(torch.__version__.split('.')[1])
-    if TORCH_MAJOR > 1 or (TORCH_MAJOR == 1 and TORCH_MINOR >= 5):
+    if torch.__version__ >= "1.5":
         from torch.utils.cpp_extension import ROCM_HOME
         is_rocm_pytorch = True if ((torch.version.hip is not None) and (ROCM_HOME is not None)) else False
 
