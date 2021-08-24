4bd2b367f5c88f34be47a7ab3e22cd98280e961e
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared20561.02.frc2.facebook.com>
2021-08-24 04:31:49 -0700

    2021-08-24 nightly release (b72129c5338cf3e888b1ce5dbdc0f4cb31a653cf)

--- torchvision/models/segmentation/_utils.py
+++ torchvision/models/segmentation/_utils.py
@@ -1,19 +1,25 @@
 from collections import OrderedDict
+from typing import Optional, Dict
 
-from torch import nn
+from torch import nn, Tensor
 from torch.nn import functional as F
 
 
 class _SimpleSegmentationModel(nn.Module):
     __constants__ = ['aux_classifier']
 
-    def __init__(self, backbone, classifier, aux_classifier=None):
+    def __init__(
+        self,
+        backbone: nn.Module,
+        classifier: nn.Module,
+        aux_classifier: Optional[nn.Module] = None
+    ) -> None:
         super(_SimpleSegmentationModel, self).__init__()
         self.backbone = backbone
         self.classifier = classifier
         self.aux_classifier = aux_classifier
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Dict[str, Tensor]:
         input_shape = x.shape[-2:]
         # contract: features is a dict of tensors
         features = self.backbone(x)
