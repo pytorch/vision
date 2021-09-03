d4efb7d3b31be99faa4c23f8b41d3efe2740748e
Alexander Soare <alexander.soare159@gmail.com>
2021-09-03 14:41:34 +0100

    undo line spacing changes

--- torchvision/models/_utils.py
+++ torchvision/models/_utils.py
@@ -7,20 +7,25 @@ from typing import Dict
 class IntermediateLayerGetter(nn.ModuleDict):
     """
     Module wrapper that returns intermediate layers from a model
+
     It has a strong assumption that the modules have been registered
     into the model in the same order as they are used.
     This means that one should **not** reuse the same nn.Module
     twice in the forward if you want this to work.
+
     Additionally, it is only able to query submodules that are directly
     assigned to the model. So if `model` is passed, `model.feature1` can
     be returned, but not `model.feature1.layer2`.
+
     Args:
         model (nn.Module): model on which we will extract the features
         return_layers (Dict[name, new_name]): a dict containing the names
             of the modules for which the activations will be returned as
             the key of the dict, and the value of the dict is the name
             of the returned activation (which the user can specify).
+
     Examples::
+
         >>> m = torchvision.models.resnet18(pretrained=True)
         >>> # extract layer1 and layer3, giving as names `feat1` and feat2`
         >>> new_m = torchvision.models._utils.IntermediateLayerGetter(m,
