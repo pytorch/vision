9d3f0b1afd78f454f5530dac5bde9a6e8a34c118
Kai Zhang <kaizh@fb.com>
2021-09-09 00:15:55 +0000

    initial code

---
+++ torchvision/models/regnet.py
@@ -0,0 +1,13 @@
+from torch import nn
+
+class RegNetParams:
+    pass
+
+class SqueezeExcitation(nn.Module):
+    pass
+
+class RegNet(nn.Module):
+    pass
+
+def regnet_y_400mf(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> RegNet:
+    pass
