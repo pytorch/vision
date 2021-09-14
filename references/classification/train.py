bce8078b63c272dbcaa22a7ef639be54a2b2d32a
Vasilis Vryniotis <vvryniotis@fb.com>
2021-09-14 11:16:07 +0100

    Update log message.

--- references/classification/train.py
+++ references/classification/train.py
@@ -71,8 +71,8 @@ def evaluate(model, criterion, data_loader, device, print_freq=100, log_suffix='
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
 
-    print(' * Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f}'
-          .format(top1=metric_logger.acc1, top5=metric_logger.acc5))
+    print('{header} Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f}'
+          .format(header=header, top1=metric_logger.acc1, top5=metric_logger.acc5))
     return metric_logger.acc1.global_avg
 
 
