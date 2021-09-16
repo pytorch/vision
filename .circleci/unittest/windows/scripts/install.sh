8c966caa19cbdcabaa85619dcd8a325c794b60b5
Yi Zhang <zhanyi@microsoft.com>
2021-09-16 05:56:13 -0700

    print cuda available

--- .circleci/unittest/windows/scripts/install.sh
+++ .circleci/unittest/windows/scripts/install.sh
@@ -12,7 +12,7 @@ this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 eval "$(./conda/Scripts/conda.exe 'shell.bash' 'hook')"
 conda activate ./env
 
-if [ "${CU_VERSION:-}" == cpu ] ; then
+if [ "${CU_VERSION:-}" == "cpu" ] ; then
     cudatoolkit="cpuonly"
 else
     if [[ ${#CU_VERSION} -eq 4 ]]; then
@@ -27,6 +27,9 @@ fi
 
 printf "Installing PyTorch with %s\n" "${cudatoolkit}"
 conda install -y -c "pytorch-${UPLOAD_CHANNEL}" -c conda-forge "pytorch-${UPLOAD_CHANNEL}"::pytorch "${cudatoolkit}" pytest
+
+export cuda_available=$(python -c "import torch; print(torch.cuda.is_available())")
+echo "cuda available is" $cuda_available
 
 if [ $PYTHON_VERSION == "3.6" ]; then
     printf "Installing minimal PILLOW version\n"
