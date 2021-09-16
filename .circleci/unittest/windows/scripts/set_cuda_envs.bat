ebcd231f158f7248d1093699170abc78b481854e
Yi Zhang <zhanyi@microsoft.com>
2021-09-15 19:49:36 -0700

    enable cuda for unittest

---
+++ .circleci/unittest/windows/scripts/set_cuda_envs.bat
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+version=$CUDA_VERSION
+
+# set cuda envs
+export PATH="/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v${version}/bin:/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v${version}/libnvvp:$PATH"
+export CUDA_PATH_V${version/./_}="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v${version}"
+export CUDA_PATH="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v${version}"
+
+if  [ ! -d "$CUDA_PATH" ]
+then
+    echo "$CUDA_PATH" does not exist
+    exit 1
+fi
+
+if [ ! -f "${CUDA_PATH}\include\nvjpeg.h" ]
+then
+    echo "nvjpeg does not exist"
+    exit 1
+fi
+
+# check cuda driver version
+for path in '/c/Program Files/NVIDIA Corporation/NVSMI/nvidia-smi.exe' /c/Windows/System32/nvidia-smi.exe; do
+    if [[ -x "$path" ]]; then
+        "$path" || echo "true";
+        break
+    fi
+done
+which nvcc
+nvcc --version
+env | grep CUDA
