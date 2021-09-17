346cf93754d0b46780b5a96efc3da28a4b8ecc98
Yi Zhang <zhanyi@microsoft.com>
2021-09-17 04:16:28 -0700

    Revert "Enable building torchvision with ffmpeg regardless of version on Linux (#4413)"
    This reverts commit 9275cc61fb3c26ce15ced0199ad8b7540d48676c.

--- setup.py
+++ setup.py
@@ -360,9 +360,14 @@ def get_extensions():
         has_ffmpeg = False
     if has_ffmpeg:
         try:
-            # This is to check if ffmpeg is installed properly.
-            subprocess.check_output(["ffmpeg", "-version"])
-        except subprocess.CalledProcessError:
+            # this splits on both dots and spaces as the output format differs across versions / platforms
+            ffmpeg_version_str = str(subprocess.check_output(["ffmpeg", "-version"]))
+            ffmpeg_version = re.split(r"ffmpeg version |\.| |-", ffmpeg_version_str)[1:3]
+            ffmpeg_version = ".".join(ffmpeg_version)
+            if StrictVersion(ffmpeg_version) >= StrictVersion('4.3'):
+                print(f'ffmpeg {ffmpeg_version} not supported yet, please use ffmpeg 4.2.')
+                has_ffmpeg = False
+        except (subprocess.CalledProcessError, IndexError, ValueError):
             print('Error fetching ffmpeg version, ignoring ffmpeg.')
             has_ffmpeg = False
 
