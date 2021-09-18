dfd0b2df45883d132a85b6ab88d653beef80f21a
Yi Zhang <zhanyi@microsoft.com>
2021-09-17 21:19:34 -0700

    minor improve

--- .circleci/unittest/windows/scripts/set_cuda_envs.sh
+++ .circleci/unittest/windows/scripts/set_cuda_envs.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -ex
 
 echo CU_VERSION is "${CU_VERSION}"
 echo CUDA_VERSION is "${CUDA_VERSION}"
@@ -14,37 +15,39 @@ else
     fi
 fi
 
-if [[ "$version" == "cpu" ]]; then
-    exit 0
-fi
+# Don't use if [[ "$version" == "cpu" ]]; then exit 0 fi.
+# It would exit the shell. One result is cpu tests would not run if the shell exit.
+# Unless there's an error, Don't exit.
+if [[ "$version" != "cpu" ]]; then
+    # set cuda envs
+    export PATH="/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v${version}/bin:/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v${version}/libnvvp:$PATH"
+    export CUDA_PATH_V${version/./_}="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v${version}"
+    export CUDA_PATH="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v${version}"
 
-
-# set cuda envs
-export PATH="/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v${version}/bin:/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v${version}/libnvvp:$PATH"
-export CUDA_PATH_V${version/./_}="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v${version}"
-export CUDA_PATH="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v${version}"
-
-if  [ ! -d "$CUDA_PATH" ]
-then
-    echo "$CUDA_PATH" does not exist
-    exit 1
-fi
-
-if [ ! -f "${CUDA_PATH}\include\nvjpeg.h" ]
-then
-    echo "nvjpeg does not exist"
-    exit 1
-fi
-
-# check cuda driver version
-for path in '/c/Program Files/NVIDIA Corporation/NVSMI/nvidia-smi.exe' /c/Windows/System32/nvidia-smi.exe; do
-    if [[ -x "$path" ]]; then
-        "$path" || echo "true";
-        break
+    if  [ ! -d "$CUDA_PATH" ]
+    then
+        echo "$CUDA_PATH" does not exist
+        exit 1
     fi
-done
 
-which nvcc
-nvcc --version
-env | grep CUDA
-which nvcc
+    if [ ! -f "${CUDA_PATH}\include\nvjpeg.h" ]
+    then
+        echo "nvjpeg does not exist"
+        exit 1
+    fi
+
+    # check cuda driver version
+    for path in '/c/Program Files/NVIDIA Corporation/NVSMI/nvidia-smi.exe' /c/Windows/System32/nvidia-smi.exe; do
+        if [[ -x "$path" ]]; then
+            "$path" || echo "true";
+            break
+        fi
+    done
+
+    which nvcc
+    nvcc --version
+    env | grep CUDA
+    which nvcc
+fi
+
+
