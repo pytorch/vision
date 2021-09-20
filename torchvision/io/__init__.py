642fa6d2c56a2eb0ba02c31f58f27d2f49c2ae96
frgfm <fgfm03@hotmail.fr>
2021-09-20 12:42:58 +0200

    style: Fixed typing of __iter__

--- torchvision/io/__init__.py
+++ torchvision/io/__init__.py
@@ -127,7 +127,7 @@ class VideoReader:
             raise StopIteration
         return {"data": frame, "pts": pts}
 
-    def __iter__(self) -> Iterator['VideoReader']:
+    def __iter__(self) -> Iterator[Dict[str, Any]]:
         return self
 
     def seek(self, time_s: float) -> 'VideoReader':
