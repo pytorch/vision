e1513ad9e1735e5dc1cd8f0faa3121c8012f3346
Prabhat Roy <prabhatroy@fb.com>
2021-09-22 16:39:57 +0100

    Build with ffmpeg only on Linux

--- setup.py
+++ setup.py
@@ -346,6 +346,21 @@ def get_extensions():
 
     ffmpeg_exe = distutils.spawn.find_executable('ffmpeg')
     has_ffmpeg = ffmpeg_exe is not None
+    # FIXME: Building torchvision with ffmpeg on MacOS or with Python 3.9
+    # FIXME: causes crash. See the following GitHub issues for more details.
+    # FIXME: https://github.com/pytorch/pytorch/issues/65000
+    # FIXME: https://github.com/pytorch/vision/issues/3367
+    if sys.platform != 'linux' or (
+            sys.version_info.major == 3 and sys.version_info.minor == 9):
+        has_ffmpeg = False
+    if has_ffmpeg:
+        try:
+            # This is to check if ffmpeg is installed properly.
+            subprocess.check_output(["ffmpeg", "-version"])
+        except subprocess.CalledProcessError:
+            print('Error fetching ffmpeg version, ignoring ffmpeg.')
+            has_ffmpeg = False
+
     print("FFmpeg found: {}".format(has_ffmpeg))
 
     if has_ffmpeg:
