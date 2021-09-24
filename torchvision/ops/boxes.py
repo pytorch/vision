70dc569e9eeb5c036babe74e18796b9533a38ecd
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared25938.18.frc3.facebook.com>
2021-09-24 04:33:36 -0700

    2021-09-24 nightly release (cdb6fba52f461b276d9b4d0a817b62e69344021c)

--- torchvision/ops/boxes.py
+++ torchvision/ops/boxes.py
@@ -301,24 +301,24 @@ def generalized_box_iou(boxes1: Tensor, boxes2: Tensor) -> Tensor:
 
 def masks_to_boxes(masks: torch.Tensor) -> torch.Tensor:
     """
-    Compute the bounding boxes around the provided masks
+    Compute the bounding boxes around the provided masks.
 
-    Returns a [N, 4] tensor.  Both sets of boxes are expected to be in ``(x1, y1, x2, y2)`` format with
+    Returns a [N, 4] tensor containing bounding boxes. The boxes are in ``(x1, y1, x2, y2)`` format with
     ``0 <= x1 < x2`` and ``0 <= y1 < y2``.
 
     Args:
-        masks (Tensor[N, H, W]): masks to transform where N is the number of
-        masks and (H, W) are the spatial dimensions.
+        masks (Tensor[N, H, W]): masks to transform where N is the number of masks
+            and (H, W) are the spatial dimensions.
 
     Returns:
         Tensor[N, 4]: bounding boxes
     """
     if masks.numel() == 0:
-        return torch.zeros((0, 4))
+        return torch.zeros((0, 4), device=masks.device, dtype=torch.float)
 
     n = masks.shape[0]
 
-    bounding_boxes = torch.zeros((n, 4), device=masks.device, dtype=torch.int)
+    bounding_boxes = torch.zeros((n, 4), device=masks.device, dtype=torch.float)
 
     for index, mask in enumerate(masks):
         y, x = torch.where(masks[index] != 0)
