798f461b8638859c3230a53fceb43548aaf82be0
Aditya Oke <okeaditya315@gmail.com>
2021-09-27 21:14:18 +0530

    Update example

--- gallery/plot_repurposing_annotations.py
+++ gallery/plot_repurposing_annotations.py
@@ -13,6 +13,8 @@ import os
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+
+import torchvision.models.detection
 import torchvision.transforms.functional as F
 
 
@@ -50,10 +52,12 @@ def show(imgs):
 # localization tasks.
 
 ####################################
-# Converting Segmentation Masks to bounding boxes
+# Converting Masks to Bounding Boxes
 # -----------------------------------------------
-# For example, the masks to bounding_boxes operation can be used to transform masks into bounding boxes that can be
+# For example, the :func:`~torchvision.ops.masks_to_boxes` operation can be used to
+# transform masks into bounding boxes that can be
 # used as input to detection models such as FasterRCNN and RetinaNet.
+# We will take images and masks from the PenFudan Dataset.
 
 
 from torchvision.io import read_image
@@ -64,16 +68,41 @@ mask_path = os.path.join(ASSETS_DIRECTORY, "FudanPed00054_mask.png")
 img = read_image(img_path)
 mask = read_image(mask_path)
 mask = F.convert_image_dtype(mask, dtype=torch.float)
+
+#########################
+# Here the masks are represented as a PNG Image, with floating point values.
+# Each pixel is encoded as different colors, with 0 being background
+# Notice that that dimensions of image and mask match.
+
+print(mask.size())
+print(img.size())
+print(mask)
+
+############################
+
+# We get the unique colors, as these would be the object ids.
 obj_ids = torch.unique(mask)
+
+# first id is the background, so remove it.
 obj_ids = obj_ids[1:]
+
+# split the color-encoded mask into a set of boolean masks.
 masks = mask == obj_ids[:, None, None]
 
+########################
+# Now the masks are boolean tensor.
+# The first dimension in this case 3. Signifies number of instance, that is 3 people in the image.
+# The other two dimensions are height and width, which match the dimensions of the image.
+# The boolean tensors represent if the pixel belong to the segmentation mask of the image.
+
+print(masks.size())
+print(masks)
+
 ####################################
-# Let's visualize an image and segmentation masks. We will use the plotting utilities provided by torchvision.utils
-# to visualize. The images are taken from PenFudan Dataset.
+# Let's visualize an image and it's corresponding segmentation masks.
+# We will use the :func:`~torchvision.utils.draw_segmentation_masks` to draw the segmentation masks.
 
-
-from torchvision.utils import draw_segmentation_masks, draw_bounding_boxes
+from torchvision.utils import draw_segmentation_masks
 
 drawn_masks = []
 for mask in masks:
@@ -82,22 +111,86 @@ for mask in masks:
 show(drawn_masks)
 
 ####################################
-# We will use the masks_to_boxes from the torchvision.ops module
-# It returns the boxes in (xmin, ymin, xmax, ymax) format.
+# To convert the boolean masks into bounding boxes.
+# We will use the :func:`~torchvision.ops.masks_to_boxes` from the torchvision.ops module
+# It returns the boxes in ``(xmin, ymin, xmax, ymax)`` format.
 
 boxes = masks_to_boxes(masks)
+print(boxes.size())
+print(boxes)
 
 ####################################
-# These can be visualized very easily with draw_bounding_boxes utility
+# As the shape denotes, there are 3 boxes and in ``(xmin, ymin, xmax, ymax)`` format.
+# These can be visualized very easily with :func:`~torchvision.utils.draw_bounding_boxes` utility
 # provided in torchvision.utils.
 
+from torchvision.utils import draw_bounding_boxes
 drawn_boxes = draw_bounding_boxes(img, boxes, colors="red")
 show(drawn_boxes)
+
+###################################
+# These boxes can now directly be used by detection models in torchvision.
+# Here is demo with a Faster R-CNN model loaded from
+# :func:`~torchvision.models.detection.fasterrcnn_resnet50_fpn`
+
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True, progress=False)
+print(img.size())
+
+img = F.convert_image_dtype(img, torch.float)
+target = {}
+target["boxes"] = boxes
+target["labels"] = labels = torch.ones((masks.size(0),), dtype=torch.int64)
+detection_outputs = model(img.unsqueeze(0), [target])
+
 
 ####################################
 # Converting Segmentation Dataset to Detection Dataset
 # ----------------------------------------------------
 #
 # With this utility it becomes very simple to convert a segmentation dataset to a detection dataset.
-# We will also use box_convert method from torchvision ops which will help in converting boxes to desired format.
+# With this we can now use a segmentation dataset to train a detection model.
 # One can similarly convert panoptic dataset to detection dataset.
+# You can use the below code for Dataset creation in PenFudan Detection Tutorial
+
+class SegmentationToDetectionDataset(torch.utils.data.Dataset):
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
+
+    def __getitem__(self, idx):
+        # load images and masks
+        img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+        mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
+
+        img = read_image(img_path)
+        mask = read_image(mask_path)
+
+        img = F.convert_image_dtype(img, dtype=torch.float)
+        mask = F.convert_image_dtype(mask, dtype=torch.float)
+
+        # We get the unique colors, as these would be the object ids.
+        obj_ids = torch.unique(mask)
+
+        # first id is the background, so remove it.
+        obj_ids = obj_ids[1:]
+
+        # split the color-encoded mask into a set of boolean masks.
+        masks = mask == obj_ids[:, None, None]
+
+        boxes = masks_to_boxes(masks)
+
+        # there is only one class
+        labels = torch.ones((masks.shape[0],), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
