0edf713265c6f11bff8922dd7259152d2030e181
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared9640.09.ftw2.facebook.com>
2021-09-28 04:30:50 -0700

    2021-09-28 nightly release (7ea38b320e5ab5e1abc82fddc90edacfcc89e087)

--- torchvision/transforms/transforms.py
+++ torchvision/transforms/transforms.py
@@ -1098,8 +1098,8 @@ class LinearTransformation(torch.nn.Module):
 class ColorJitter(torch.nn.Module):
     """Randomly change the brightness, contrast, saturation and hue of an image.
     If the image is torch Tensor, it is expected
-    to have [..., 3, H, W] shape, where ... means an arbitrary number of leading dimensions.
-    If img is PIL Image, mode "1", "L", "I", "F" and modes with transparency (alpha channel) are not supported.
+    to have [..., 1 or 3, H, W] shape, where ... means an arbitrary number of leading dimensions.
+    If img is PIL Image, mode "1", "I", "F" and modes with transparency (alpha channel) are not supported.
 
     Args:
         brightness (float or tuple of float (min, max)): How much to jitter brightness.
