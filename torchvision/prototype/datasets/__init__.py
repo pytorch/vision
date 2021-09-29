5b03572cf078537a5007d2b9a6b2b4c5d2880447
Philip Meier <github.pmeier@posteo.de>
2021-09-29 08:26:43 +0200

    add torchdata as prototype dependency

--- torchvision/prototype/datasets/__init__.py
+++ torchvision/prototype/datasets/__init__.py
@@ -1,3 +1,13 @@
+try:
+    import torchdata
+except (ModuleNotFoundError, TypeError) as error:
+    raise ModuleNotFoundError(
+        "`torchvision.prototype.datasets` depends on PyTorch's `torchdata` (https://github.com/pytorch/data). "
+        "You can install it with `pip install git+https://github.com/pytorch/data.git`. "
+        "Note that you cannot install it with `pip install torchdata`, since this is another package."
+    ) from error
+
+
 from ._home import home
 from . import decoder, utils
 
