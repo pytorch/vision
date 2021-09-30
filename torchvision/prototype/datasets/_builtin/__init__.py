8941f7dd42e3ba66cd4e4423da49dbd98d098e46
Philip Meier <github.pmeier@posteo.de>
2021-09-30 15:41:06 +0200

    add prototype datasets for MNIST and variants

--- torchvision/prototype/datasets/_builtin/__init__.py
+++ torchvision/prototype/datasets/_builtin/__init__.py
@@ -1,1 +1,2 @@
 from .caltech import Caltech101, Caltech256
+from .mnist import MNIST, FashionMNIST, KMNIST, EMNIST, QMNIST
--- torchvision/prototype/datasets/utils/_internal.py
+++ torchvision/prototype/datasets/utils/_internal.py
@@ -1,8 +1,17 @@
 import collections.abc
 import difflib
+import enum
+import gzip
 import io
+import itertools
+import lzma
+import os.path
 import pathlib
-from typing import Collection, Sequence, Callable, Union, Any
+from typing import Collection, Sequence, Callable, Any, Iterator, Optional, Tuple, TypeVar, Union
+
+import numpy as np
+import PIL.Image
+from torch.utils.data import IterDataPipe
 
 
 __all__ = [
@@ -10,11 +19,16 @@ __all__ = [
     "sequence_to_str",
     "add_suggestion",
     "create_categories_file",
-    "read_mat"
+    "read_mat",
+    "image_buffer_from_array",
+    "Decompressor",
+    "Slicer",
 ]
 
 # pseudo-infinite until a true infinite buffer is supported by all datapipes
 INFINITE_BUFFER_SIZE = 1_000_000_000
+
+D = TypeVar("D")
 
 
 def sequence_to_str(seq: Sequence, separate_last: str = "") -> str:
@@ -66,3 +80,73 @@ def read_mat(buffer: io.IOBase, **kwargs: Any) -> Any:
         ) from error
 
     return sio.loadmat(buffer, **kwargs)
+
+
+def image_buffer_from_array(array: np.ndarray, *, format: str = "png") -> io.BytesIO:
+    image = PIL.Image.fromarray(array)
+    buffer = io.BytesIO()
+    image.save(buffer, format=format)
+    buffer.seek(0)
+    return buffer
+
+
+class CompressionType(enum.Enum):
+    GZIP = "gzip"
+    LZMA = "lzma"
+
+
+class Decompressor(IterDataPipe[Tuple[str, io.IOBase]]):
+    types = CompressionType
+
+    _DECOMPRESSORS = {
+        types.GZIP: lambda file: gzip.GzipFile(fileobj=file),
+        types.LZMA: lambda file: lzma.LZMAFile(file),
+    }
+
+    def __init__(
+        self,
+        datapipe: IterDataPipe[Tuple[str, io.IOBase]],
+        *,
+        type: Optional[Union[str, CompressionType]] = None,
+    ) -> None:
+        self.datapipe = datapipe
+        if isinstance(type, str):
+            type = self.types(type.upper())
+        self.type = type
+
+    def _detect_compression_type(self, path: str) -> CompressionType:
+        if self.type:
+            return self.type
+
+        # TODO: this needs to be more elaborate
+        ext = os.path.splitext(path)[1]
+        if ext == ".gz":
+            return self.types.GZIP
+        elif ext == ".xz":
+            return self.types.LZMA
+        else:
+            raise RuntimeError("FIXME")
+
+    def __iter__(self) -> Iterator[Tuple[str, io.IOBase]]:
+        for path, file in self.datapipe:
+            type = self._detect_compression_type(path)
+            decompressor = self._DECOMPRESSORS[type]
+            yield path, decompressor(file)
+
+
+class Slicer(IterDataPipe[D]):
+    def __init__(
+        self,
+        datapipe: IterDataPipe[D],
+        *,
+        start: Optional[int] = None,
+        stop: Optional[int] = None,
+        step: Optional[int] = None,
+    ):
+        self.datapipe = datapipe
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+    def __iter__(self) -> Iterator[D]:
+        yield from itertools.islice(self.datapipe, self.start, self.stop, self.step)
