b9da8f3f6108ae39aad5cab9293b6e3ec413f18b
Aditya Oke <okeaditya315@gmail.com>
2021-10-03 01:14:04 +0530

    Remove a small bracket

--- torchvision/utils.py
+++ torchvision/utils.py
@@ -302,4 +302,4 @@ def draw_segmentation_masks(
 
 def _generate_color_palette(num_masks: int):
     palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
-    return [tuple(((i * palette) % 255)) for i in range(num_masks)]
+    return [tuple((i * palette) % 255) for i in range(num_masks)]
