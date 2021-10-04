0dc71f429fc4c4c97368929f1ea88e12907e75e4
Nicolas Hug <nicolashug@fb.com>
2021-10-04 14:02:54 +0100

    got a double import in merge

--- torchvision/ops/poolers.py
+++ torchvision/ops/poolers.py
@@ -6,7 +6,6 @@ from torch import nn, Tensor
 from torchvision.ops.boxes import box_area
 
 from .roi_align import roi_align
-from .roi_align import roi_align
 
 
 # copying result_idx_in_level to a specific index in result[]
