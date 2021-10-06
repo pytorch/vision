b2faf3f73b9bc937abd2d3f96e8c6c1b0fb806e9
Vasilis Vryniotis <vvryniotis@fb.com>
2021-10-06 18:13:32 +0100

    Adding delete method.

--- torchvision/prototype/ops/_utils.py
+++ torchvision/prototype/ops/_utils.py
@@ -4,11 +4,11 @@ import torch
 import warnings
 
 from torch import fx
+from torchvision.models.feature_extraction import LeafModuleAwareTracer
 from typing import Callable, Tuple
 
 
-# TODO: create a util to undo the change
-# TODO: if shouldn't have a _regularized_shotrcut when is has a downsample
+_MODULE_NAME = "_regularized_shotrcut"
 
 
 class RegularizedShortcut(torch.nn.Module):
@@ -29,14 +29,13 @@ def add_regularized_shortcut(
     if not inplace:
         model = copy.deepcopy(model)
 
-    ATTR_NAME = "_regularized_shotrcut"
     tracer = fx.Tracer()
     modifications = {}
     for name, m in model.named_modules():
         if isinstance(m, block_types):
             # Add the Layer directly on submodule prior tracing
             # workaround due to https://github.com/pytorch/pytorch/issues/66197
-            m.add_module(ATTR_NAME, RegularizedShortcut(regularizer_layer))
+            m.add_module(_MODULE_NAME, RegularizedShortcut(regularizer_layer))
 
             graph = tracer.trace(m)
             patterns = {operator.add, torch.add, "add"}
@@ -48,7 +47,7 @@ def add_regularized_shortcut(
                         with graph.inserting_after(node):
                             # Always put the shortcut value first
                             args = node.args if node.args[0] == input else node.args[::-1]
-                            node.replace_all_uses_with(graph.call_module(ATTR_NAME, args))
+                            node.replace_all_uses_with(graph.call_module(_MODULE_NAME, args))
                         graph.erase_node(node)
                         graph.lint()
                         modifications[name] = graph
@@ -70,20 +69,52 @@ def add_regularized_shortcut(
     return model
 
 
+def del_regularized_shortcut(
+        model: torch.nn.Module,
+        inplace: bool = True
+) -> torch.nn.Module:
+    if not inplace:
+        model = copy.deepcopy(model)
+
+    tracer = LeafModuleAwareTracer(leaf_modules=[RegularizedShortcut])
+    graph = tracer.trace(model)
+    for node in graph.nodes:
+        if node.op == "call_module" and node.target.rsplit(".", 1)[-1] == _MODULE_NAME:
+            with graph.inserting_before(node):
+                new_node = graph.call_function(operator.add, node.args)
+                node.replace_all_uses_with(new_node)
+            graph.erase_node(node)
+
+    return fx.GraphModule(model, graph)
+
+
 if __name__ == "__main__":
     from torchvision.models.resnet import resnet18, resnet50, BasicBlock, Bottleneck, load_state_dict_from_url
     from torchvision.ops.stochastic_depth import StochasticDepth
     from functools import partial
 
-    regularizer_layer = partial(StochasticDepth, p=0.1, mode="row")
-    model = resnet50()
-    model = add_regularized_shortcut(model, (BasicBlock, Bottleneck), regularizer_layer)
-    # print(model)
-    out = model(torch.randn((7, 3, 224, 224)))
-    print(out)
+    out = []
+    batch = torch.randn((7, 3, 224, 224))
 
+    print("Before")
+    model = resnet50()
+    out.append(model(batch))
+    fx.symbolic_trace(model).graph.print_tabular()
+
+    print("After addition")
+    regularizer_layer = partial(StochasticDepth, p=0.0, mode="row")
+    model = add_regularized_shortcut(model, (BasicBlock, Bottleneck), regularizer_layer)
+    fx.symbolic_trace(model).graph.print_tabular()
+    # print(model)
+    out.append(model(batch))
     # state_dict = load_state_dict_from_url("https://download.pytorch.org/models/resnet50-0676ba61.pth")
     # model.load_state_dict(state_dict)
 
+    print("After deletion")
+    model = del_regularized_shortcut(model)
     fx.symbolic_trace(model).graph.print_tabular()
+    out.append(model(batch))
+
+    for v in out[1:]:
+        torch.testing.assert_allclose(out[0], v)
 
