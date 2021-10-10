6098b51e1cd3b08b39636b417b593852e09ffc6e
Jirka <jirka.borovec@seznam.cz>
2021-10-10 23:44:07 +0200

    edit setup

--- setup.py
+++ setup.py
@@ -473,6 +473,7 @@ if __name__ == "__main__":
             "scipy": ["scipy"],
         },
         ext_modules=get_extensions(),
+        python_requires='>=3.6',
         cmdclass={
             "build_ext": BuildExtension.with_options(no_python_abi_suffix=True),
             "clean": clean,
