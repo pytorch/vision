f5298f95f27c67bc3b09e2bee1fc1d547324eae0
Bruno Korbar <bjuncek@gmail.com>
2021-10-13 21:21:17 +0100

    add return type

--- torchvision/io/__init__.py
+++ torchvision/io/__init__.py
@@ -130,7 +130,7 @@ class VideoReader:
     def __iter__(self) -> Iterator['VideoReader']:
         return self
 
-    def seek(self, time_s: float, keyframes_only: bool = False):
+    def seek(self, time_s: float, keyframes_only: bool = False) -> 'VideoReader':
         """Seek within current stream.
 
         Args:
