237c749729481822796895cd77b5221a91764029
Nicolas Hug <nicolashug@fb.com>
2021-10-14 18:12:30 +0100

    remove unused lists, and added instructions, applied balck

--- scripts/release_notes/common.py
+++ scripts/release_notes/common.py
@@ -1,69 +1,34 @@
+import json
+import locale
+import os
+import re
+import subprocess
 from collections import namedtuple
 from os.path import expanduser
-import locale
-import subprocess
-import re
+
 import requests
-import os
-import json
-
-categories = [
-    'Uncategorized',
-    'distributed',
-    'mobile',
-    'jit',
-    'visualization',
-    'onnx',
-    'caffe2',
-    'quantization',
-    'amd',
-    'benchmark',
-    'profiler',
-    'dispatcher',
-    'releng',
-    'fx',
-    'code_coverage',
-    'vulkan',
-    'skip',
-    'cpp_frontend',
-    'python_frontend',
-    'complex_frontend',
-    'vmap_frontend',
-    'autograd_frontend',
-    'build_frontend',
-    'memory_format_frontend',
-    'foreach_frontend',
-]
-
-topics = [
-    'bc_breaking',
-    'deprecations',
-    'new_features',
-    'improvements',
-    'bug_fixes',
-    'performance',
-    'docs',
-    'devs',
-    'Untopiced',
-]
 
 
-Features = namedtuple('Features', [
-    'title',
-    'body',
-    'pr_number',
-    'files_changed',
-    'labels',
-])
+Features = namedtuple(
+    "Features",
+    [
+        "title",
+        "body",
+        "pr_number",
+        "files_changed",
+        "labels",
+    ],
+)
 
 
 def dict_to_features(dct):
     return Features(
-        title=dct['title'],
-        body=dct['body'],
-        pr_number=dct['pr_number'],
-        files_changed=dct['files_changed'],
-        labels=dct['labels'])
+        title=dct["title"],
+        body=dct["body"],
+        pr_number=dct["pr_number"],
+        files_changed=dct["files_changed"],
+        labels=dct["labels"],
+    )
 
 
 def features_to_dict(features):
@@ -72,8 +37,7 @@ def features_to_dict(features):
 
 def run(command):
     """Returns (return-code, stdout, stderr)"""
-    p = subprocess.Popen(command, stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE, shell=True)
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, err = p.communicate()
     rc = p.returncode
     enc = locale.getpreferredencoding()
@@ -83,50 +47,52 @@ def run(command):
 
 
 def commit_body(commit_hash):
-    cmd = f'git log -n 1 --pretty=format:%b {commit_hash}'
+    cmd = f"git log -n 1 --pretty=format:%b {commit_hash}"
     ret, out, err = run(cmd)
     return out if ret == 0 else None
 
 
 def commit_title(commit_hash):
-    cmd = f'git log -n 1 --pretty=format:%s {commit_hash}'
+    cmd = f"git log -n 1 --pretty=format:%s {commit_hash}"
     ret, out, err = run(cmd)
     return out if ret == 0 else None
 
 
 def commit_files_changed(commit_hash):
-    cmd = f'git diff-tree --no-commit-id --name-only -r {commit_hash}'
+    cmd = f"git diff-tree --no-commit-id --name-only -r {commit_hash}"
     ret, out, err = run(cmd)
-    return out.split('\n') if ret == 0 else None
+    return out.split("\n") if ret == 0 else None
 
 
 def parse_pr_number(body, commit_hash, title):
-    regex = r'(#[0-9]+)'
+    regex = r"(#[0-9]+)"
     matches = re.findall(regex, title)
     if len(matches) == 0:
-        if 'revert' not in title.lower() and 'updating submodules' not in title.lower():
-            print(f'[{commit_hash}: {title}] Could not parse PR number, ignoring PR')
+        if "revert" not in title.lower() and "updating submodules" not in title.lower():
+            print(f"[{commit_hash}: {title}] Could not parse PR number, ignoring PR")
         return None
     if len(matches) > 1:
-        print(f'[{commit_hash}: {title}] Got two PR numbers, using the last one')
+        print(f"[{commit_hash}: {title}] Got two PR numbers, using the last one")
         return matches[-1][1:]
     return matches[0][1:]
 
 
 def get_ghstack_token():
-    pattern = 'github_oauth = (.*)'
-    with open(expanduser('~/.ghstackrc'), 'r+') as f:
+    pattern = "github_oauth = (.*)"
+    with open(expanduser("~/.ghstackrc"), "r+") as f:
         config = f.read()
     matches = re.findall(pattern, config)
     if len(matches) == 0:
         raise RuntimeError("Can't find a github oauth token")
     return matches[0]
 
+
 token = get_ghstack_token()
 headers = {"Authorization": f"token {token}"}
 
+
 def run_query(query):
-    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    request = requests.post("https://api.github.com/graphql", json={"query": query}, headers=headers)
     if request.status_code == 200:
         return request.json()
     else:
@@ -150,15 +116,16 @@ def gh_labels(pr_number):
     }}
     """
     query = run_query(query)
-    edges = query['data']['repository']['pullRequest']['labels']['edges']
-    return [edge['node']['name'] for edge in edges]
+    edges = query["data"]["repository"]["pullRequest"]["labels"]["edges"]
+    return [edge["node"]["name"] for edge in edges]
 
 
 def get_features(commit_hash, return_dict=False):
     title, body, files_changed = (
         commit_title(commit_hash),
         commit_body(commit_hash),
-        commit_files_changed(commit_hash))
+        commit_files_changed(commit_hash),
+    )
     pr_number = parse_pr_number(body, commit_hash, title)
     labels = []
     if pr_number is not None:
@@ -168,8 +135,9 @@ def get_features(commit_hash, return_dict=False):
         return features_to_dict(result)
     return result
 
+
 class CommitDataCache:
-    def __init__(self, path='results/data.json'):
+    def __init__(self, path="results/data.json"):
         self.path = path
         self.data = {}
         if os.path.exists(path):
@@ -183,44 +151,44 @@ class CommitDataCache:
         return self.data[commit]
 
     def read_from_disk(self):
-        with open(self.path, 'r') as f:
+        with open(self.path, "r") as f:
             data = json.load(f)
-            data = {commit: dict_to_features(dct)
-                    for commit, dct in data.items()}
+            data = {commit: dict_to_features(dct) for commit, dct in data.items()}
         return data
 
     def write_to_disk(self):
         data = {commit: features._asdict() for commit, features in self.data.items()}
-        with open(self.path, 'w') as f:
+        with open(self.path, "w") as f:
             json.dump(data, f)
 
 
 def get_commits_between(base_version, new_version):
-    cmd = f'git merge-base {base_version} {new_version}'
+    cmd = f"git merge-base {base_version} {new_version}"
     rc, merge_base, _ = run(cmd)
     assert rc == 0
 
     # Returns a list of something like
     # b33e38ec47 Allow a higher-precision step type for Vec256::arange (#34555)
-    cmd = f'git log --reverse --oneline {merge_base}..{new_version}'
+    cmd = f"git log --reverse --oneline {merge_base}..{new_version}"
     rc, commits, _ = run(cmd)
     assert rc == 0
 
-    log_lines = commits.split('\n')
-    hashes, titles = zip(*[log_line.split(' ', 1) for log_line in log_lines])
+    log_lines = commits.split("\n")
+    hashes, titles = zip(*[log_line.split(" ", 1) for log_line in log_lines])
     return hashes, titles
 
 
 def convert_to_dataframes(feature_list):
     import pandas as pd
+
     df = pd.DataFrame.from_records(feature_list, columns=Features._fields)
     return df
 
 
 def main(base_version, new_version):
     hashes, titles = get_commits_between(base_version, new_version)
-    
-    cdc = CommitDataCache('data.json')
+
+    cdc = CommitDataCache("data.json")
     for idx, commit in enumerate(hashes):
         if idx % 10 == 0:
             print(f"{idx} / {len(hashes)}")
@@ -230,9 +198,15 @@ def main(base_version, new_version):
 
 
 if __name__ == "__main__":
-    #d = get_features('2ab93592529243862ce8ad5b6acf2628ef8d0dc8')
-    # d = get_features('f5843099d895e72c27ffa9d29cc91dd8df7f3832')
+    # d = get_features('2ab93592529243862ce8ad5b6acf2628ef8d0dc8')
     # print(d)
     # hashes, titles = get_commits_between("tags/v0.9.0", "fc852f3b39fe25dd8bf1dedee8f19ea04aa84c15")
+
+    # Usage: change the tags below accordingly to the current release, then save the json with
+    # cdc.write_to_disk().
+    # Then you can rely on https://nbviewer.org/gist/NicolasHug/248a72aa086fd874936f16eb56240226
+    # to open the json and generate the release notes semi-automatically.
     cdc = main("tags/v0.9.0", "fc852f3b39fe25dd8bf1dedee8f19ea04aa84c15")
-    from IPython import embed; embed()
+    from IPython import embed
+
+    embed()
