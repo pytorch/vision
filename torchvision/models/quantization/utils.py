f273840c7ea5601145a4583a9b380ffef0ae8956
Prabhat Roy <prabhatroy@fb.com>
2021-10-14 14:17:06 -0700

    [fbsync] Use `is` to compare type of objects (#4605)
    Summary:
    
    Reviewed By: fmassa
    
    Differential Revision: D31649969
    
    fbshipit-source-id: 6f37d4e67027b7e700fbc2b4db842757581d24b6
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]users.noreply.github.com>
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]users.noreply.github.com>
    Co-authored-by: Vasilis Vryniotis <datumbox@users.noreply.github.com>

--- torchvision/models/quantization/utils.py
+++ torchvision/models/quantization/utils.py
@@ -9,7 +9,7 @@ def _replace_relu(module: nn.Module) -> None:
         # Checking for explicit type instead of instance
         # as we only want to replace modules of the exact type
         # not inherited classes
-        if type(mod) == nn.ReLU or type(mod) == nn.ReLU6:
+        if type(mod) is nn.ReLU or type(mod) is nn.ReLU6:
             reassign[name] = nn.ReLU(inplace=False)
 
     for key, value in reassign.items():
