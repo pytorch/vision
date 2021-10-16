3bafc3046bb4198eee0f422f6873dfae4b34b833
Joao Gomes <jdsgomes@fb.com>
2021-10-16 10:59:01 +0100

    modifying range of trainable layers

--- test/test_models.py
+++ test/test_models.py
@@ -271,35 +271,35 @@ _model_params = {
 _model_tests_values = {
     "retinanet_resnet50_fpn": {
         "max_trainable": 5,
-        "n_trn_params_per_layer": [46, 65, 78, 88, 89],
+        "n_trn_params_per_layer": [36, 46, 65, 78, 88, 89],
     },
     "keypointrcnn_resnet50_fpn": {
         "max_trainable": 5,
-        "n_trn_params_per_layer": [58, 77, 90, 100, 101],
+        "n_trn_params_per_layer": [48, 58, 77, 90, 100, 101],
     },
     "fasterrcnn_resnet50_fpn": {
         "max_trainable": 5,
-        "n_trn_params_per_layer": [40, 59, 72, 82, 83],
+        "n_trn_params_per_layer": [30, 40, 59, 72, 82, 83],
     },
     "maskrcnn_resnet50_fpn": {
         "max_trainable": 5,
-        "n_trn_params_per_layer": [52, 71, 84, 94, 95],
+        "n_trn_params_per_layer": [42, 52, 71, 84, 94, 95],
     },
     "fasterrcnn_mobilenet_v3_large_fpn": {
         "max_trainable": 6,
-        "n_trn_params_per_layer": [23, 44, 70, 91, 97, 100],
+        "n_trn_params_per_layer": [22, 23, 44, 70, 91, 97, 100],
     },
     "fasterrcnn_mobilenet_v3_large_320_fpn": {
         "max_trainable": 6,
-        "n_trn_params_per_layer": [23, 44, 70, 91, 97, 100],
+        "n_trn_params_per_layer": [22, 23, 44, 70, 91, 97, 100],
     },
     "ssd300_vgg16": {
         "max_trainable": 5,
-        "n_trn_params_per_layer": [51, 57, 63, 67, 71],
+        "n_trn_params_per_layer": [45, 51, 57, 63, 67, 71],
     },
     "ssdlite320_mobilenet_v3_large": {
         "max_trainable": 6,
-        "n_trn_params_per_layer": [99, 138, 200, 239, 257, 266],
+        "n_trn_params_per_layer": [96, 99, 138, 200, 239, 257, 266],
     },
 }
 
@@ -781,7 +781,7 @@ def test_quantized_classification_model(model_name):
 def test_detection_model_trainable_backbone_layers(model_name):
     max_trainable = _model_tests_values[model_name]["max_trainable"]
     n_trainable_params = []
-    for trainable_layers in range(1, max_trainable + 1):
+    for trainable_layers in range(0, max_trainable + 1):
         model = torchvision.models.detection.__dict__[model_name](
             pretrained=False, pretrained_backbone=True, trainable_backbone_layers=trainable_layers
         )
