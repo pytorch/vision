c4c5ac439a1dd4be460e28935f14f000a6792322
Vasilis Vryniotis <vvryniotis@fb.com>
2021-10-18 18:49:59 +0100

    Adding deprecation warning for torchvision.models.segmentation.segmentation.

--- torchvision/models/segmentation/segmentation.py
+++ torchvision/models/segmentation/segmentation.py
@@ -1,3 +1,9 @@
-from .fcn import *
-from .deeplabv3 import *
-from .lraspp import *
+import warnings
+
+from . import *  # noqa: F401, F403
+
+
+warnings.warn(
+    "The 'torchvision.models.segmentation.segmentation' module is deprecated. Please use directly the parent module "
+    "instead."
+)
