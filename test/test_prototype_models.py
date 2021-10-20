df6cd1fecfeb2249b7baa021f13a1f56f09dbb82
Joao Gomes <jdsgomes@fb.com>
2021-10-20 09:59:49 +0100

    adding checks to compare outputs of the prototype vs old models

--- test/test_prototype_models.py
+++ test/test_prototype_models.py
@@ -14,6 +14,7 @@ import torchvision
 from _utils_internal import get_relative_path
 from common_utils import map_nested_tensor_object, freeze_rng_state, set_rng_seed, cpu_and_gpu, needs_cuda
 from torchvision.prototype import models
+from torchvision import models as original_models
 from test_models import _assert_expected, _model_params
 
 
@@ -35,7 +36,7 @@ def get_available_classification_models():
 
 @pytest.mark.parametrize("model_name", get_available_classification_models())
 @pytest.mark.parametrize("dev", cpu_and_gpu())
-@pytest.mark.skipif(os.getenv("PYTORCH_TEST_WITH_PROTOTYPE", "0") == "1", reason="Prototype code tests are disabled")
+@pytest.mark.skipif(os.getenv("PYTORCH_TEST_WITH_PROTOTYPE", "0") == "0", reason="Prototype code tests are disabled")
 def test_classification_model(model_name, dev):
     set_rng_seed(0)
     defaults = {
@@ -59,21 +60,26 @@ def test_classification_model(model_name, dev):
 def test_old_vs_new_classification_factory(model_name, dev):
     set_rng_seed(0)
     defaults = {
-        "num_classes": 50,
+        "num_classes": 1000,
         "pretrained": True,
     }
     input_shape = (1, 3, 224, 224)
     kwargs = {**defaults, **_model_params.get(model_name, {})}
-    model_old = models.__dict__[model_name](**kwargs)
+    model_old = original_models.__dict__[model_name](**kwargs)
     model_old.eval().to(device=dev)
     # RNG always on CPU, to ensure x in cuda tests is bitwise identical to x in cpu tests
     x = torch.rand(input_shape).to(device=dev)
     out_old = model_old(x)
+    # comapre with new model builder parameterized in the old fashion way
+    model_new = models.__dict__[model_name](**kwargs)
+    model_new.eval().to(device=dev)
+    out_new = model_new(x)
+    torch.testing.assert_close(out_new, out_old, rtol=0.0, atol=0.0, check_dtype=False)
+    # compare with new model builder parameterized in the new way 
     defaults.pop("pretrained")
     for weights_val in model_to_default_weights_mapping[model_name]:
         kwargs = {**defaults, **_model_params.get(model_name, {}), "weights": weights_val}
         model_new = models.__dict__[model_name](**kwargs)
         model_new.eval().to(device=dev)
-        # RNG always on CPU, to ensure x in cuda tests is bitwise identical to x in cpu tests
         out_new = model_new(x)
         torch.testing.assert_close(out_new, out_old, rtol=0.0, atol=0.0, check_dtype=False)
