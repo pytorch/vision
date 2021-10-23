d67f493bf54e0a6a38be9a9eafb557882a407040
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared1065.09.ftw2.facebook.com>
2021-10-23 04:32:49 -0700

    2021-10-23 nightly release (b37c8a3ca4c9c626cdac763c6be697231665b0f8)

--- torchvision/prototype/models/__init__.py
+++ torchvision/prototype/models/__init__.py
@@ -3,5 +3,7 @@ from .resnet import *
 from .densenet import *
 from .vgg import *
 from .efficientnet import *
+from .mobilenetv3 import *
+from .mnasnet import *
 from . import detection
 from . import quantization
