ca0f6b1981007c743b6d8768093636cf5f4415fd
Yi Zhang <zhanyi@microsoft.com>
2021-10-25 23:17:15 -0700

    fix as comments

--- packaging/torchvision/checktorch.sh
+++ packaging/torchvision/checktorch.sh
@@ -7,7 +7,7 @@ echo torch.cuda.version is $torch_cuda_version
 echo FORCE_CUDA is "$FORCE_CUDA"
 
 shopt -s nocasematch
-if [ "${USE_CUDA}" == "1" ] ; then
+if [ "${FORCE_CUDA_CUDA}" == "1" ] ; then
     if [ "$torch_cuda_version" == "None" ]; then
         echo "We want to build torch vision with cuda but the installed pytorch isn't with cuda"
         exit 1
