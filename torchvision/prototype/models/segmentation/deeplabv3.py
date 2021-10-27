81e888dd70935cb659ebb7611c90b2fde2db340f
Joao Gomes <jdsgomes@fb.com>
2021-10-27 14:41:06 +0100

    addressing PR comment

--- torchvision/prototype/models/segmentation/deeplabv3.py
+++ torchvision/prototype/models/segmentation/deeplabv3.py
@@ -1,14 +1,13 @@
 import warnings
 from functools import partial
-from typing import Any, Callable, Optional, Type, Dict
-
-from torchvision.prototype.models.resnet import resnet50, resnet101
+from typing import Any, Optional
 
 from ....models.segmentation.deeplabv3 import DeepLabV3, _deeplabv3_mobilenetv3, _deeplabv3_resnet
 from ...transforms.presets import VocEval
 from .._api import Weights, WeightEntry
 from .._meta import _VOC_CATEGORIES
 from ..mobilenetv3 import MobileNetV3LargeWeights, mobilenet_v3_large
+from ..resnet import resnet50, resnet101
 from ..resnet import ResNet50Weights, ResNet101Weights
 
 
@@ -62,43 +61,6 @@ class DeepLabV3MobileNetV3LargeWeights(Weights):
     )
 
 
-def _deeplabv3(
-    weights_class: Type[Weights],
-    model_builder: Callable,
-    weights_backbone_class: Type[Weights],
-    backbone_model_builder: Callable,
-    progress: bool,
-    num_classes: int,
-    backbone_args: Dict[str, Any],
-    weights: Optional[Weights] = None,
-    weights_backbone: Optional[Weights] = None,
-    aux_loss: Optional[bool] = None,
-    **kwargs: Any,
-) -> DeepLabV3:
-    if "pretrained" in kwargs:
-        warnings.warn("The argument pretrained is deprecated, please use weights instead.")
-        weights = weights_class.CocoWithVocLabels_RefV1 if kwargs.pop("pretrained") else None
-
-    weights = weights_class.verify(weights)
-    if "pretrained_backbone" in kwargs:
-        warnings.warn("The argument pretrained_backbone is deprecated, please use weights_backbone instead.")
-        weights_backbone = weights_backbone_class.ImageNet1K_RefV1 if kwargs.pop("pretrained_backbone") else None
-    weights_backbone = weights_backbone_class.verify(weights_backbone)
-
-    if weights is not None:
-        weights_backbone = None
-        aux_loss = True
-        num_classes = len(weights.meta["categories"])
-
-    backbone = backbone_model_builder(weights=weights_backbone, **backbone_args)
-    model = model_builder(backbone, num_classes, aux_loss)
-
-    if weights is not None:
-        model.load_state_dict(weights.state_dict(progress=progress))
-
-    return model
-
-
 def deeplabv3_resnet50(
     weights: Optional[DeepLabV3ResNet50Weights] = None,
     weights_backbone: Optional[ResNet50Weights] = None,
@@ -107,42 +69,60 @@ def deeplabv3_resnet50(
     aux_loss: Optional[bool] = None,
     **kwargs: Any,
 ) -> DeepLabV3:
-    return _deeplabv3(
-        DeepLabV3ResNet50Weights,
-        _deeplabv3_resnet,
-        ResNet50Weights,
-        resnet50,
-        progress,
-        num_classes,
-        {"replace_stride_with_dilation": [False, True, True]},
-        weights,
-        weights_backbone,
-        aux_loss,
-        kwargs=kwargs,
-    )
+    if "pretrained" in kwargs:
+        warnings.warn("The argument pretrained is deprecated, please use weights instead.")
+        weights = DeepLabV3ResNet50Weights.CocoWithVocLabels_RefV1 if kwargs.pop("pretrained") else None
+
+    weights = DeepLabV3ResNet50Weights.verify(weights)
+    if "pretrained_backbone" in kwargs:
+        warnings.warn("The argument pretrained_backbone is deprecated, please use weights_backbone instead.")
+        weights_backbone = ResNet50Weights.ImageNet1K_RefV1 if kwargs.pop("pretrained_backbone") else None
+    weights_backbone = ResNet50Weights.verify(weights_backbone)
+
+    if weights is not None:
+        weights_backbone = None
+        aux_loss = True
+        num_classes = len(weights.meta["categories"])
+
+    backbone = resnet50(weights=weights_backbone, replace_stride_with_dilation=[False, True, True])
+    model = _deeplabv3_resnet(backbone, num_classes, aux_loss)
+
+    if weights is not None:
+        model.load_state_dict(weights.state_dict(progress=progress))
+
+    return model
 
 
 def deeplabv3_resnet101(
-    weights: Optional[DeepLabV3ResNet50Weights] = None,
-    weights_backbone: Optional[ResNet50Weights] = None,
+    weights: Optional[DeepLabV3ResNet101Weights] = None,
+    weights_backbone: Optional[ResNet101Weights] = None,
     progress: bool = True,
     num_classes: int = 21,
     aux_loss: Optional[bool] = None,
     **kwargs: Any,
 ) -> DeepLabV3:
-    return _deeplabv3(
-        DeepLabV3ResNet101Weights,
-        _deeplabv3_resnet,
-        ResNet101Weights,
-        resnet101,
-        progress,
-        num_classes,
-        {"replace_stride_with_dilation": [False, True, True]},
-        weights,
-        weights_backbone,
-        aux_loss,
-        kwargs=kwargs,
-    )
+    if "pretrained" in kwargs:
+        warnings.warn("The argument pretrained is deprecated, please use weights instead.")
+        weights = DeepLabV3ResNet101Weights.CocoWithVocLabels_RefV1 if kwargs.pop("pretrained") else None
+
+    weights = DeepLabV3ResNet101Weights.verify(weights)
+    if "pretrained_backbone" in kwargs:
+        warnings.warn("The argument pretrained_backbone is deprecated, please use weights_backbone instead.")
+        weights_backbone = ResNet101Weights.ImageNet1K_RefV1 if kwargs.pop("pretrained_backbone") else None
+    weights_backbone = ResNet101Weights.verify(weights_backbone)
+
+    if weights is not None:
+        weights_backbone = None
+        aux_loss = True
+        num_classes = len(weights.meta["categories"])
+
+    backbone = resnet101(weights=weights_backbone, replace_stride_with_dilation=[False, True, True])
+    model = _deeplabv3_resnet(backbone, num_classes, aux_loss)
+
+    if weights is not None:
+        model.load_state_dict(weights.state_dict(progress=progress))
+
+    return model
 
 
 def deeplabv3_mobilenet_v3_large(
@@ -153,16 +133,25 @@ def deeplabv3_mobilenet_v3_large(
     aux_loss: Optional[bool] = None,
     **kwargs: Any,
 ) -> DeepLabV3:
-    return _deeplabv3(
-        DeepLabV3MobileNetV3LargeWeights,
-        _deeplabv3_mobilenetv3,
-        MobileNetV3LargeWeights,
-        mobilenet_v3_large,
-        progress,
-        num_classes,
-        {"dilated": True},
-        weights,
-        weights_backbone,
-        aux_loss,
-        kwargs=kwargs,
-    )
+    if "pretrained" in kwargs:
+        warnings.warn("The argument pretrained is deprecated, please use weights instead.")
+        weights = DeepLabV3MobileNetV3LargeWeights.CocoWithVocLabels_RefV1 if kwargs.pop("pretrained") else None
+
+    weights = DeepLabV3MobileNetV3LargeWeights.verify(weights)
+    if "pretrained_backbone" in kwargs:
+        warnings.warn("The argument pretrained_backbone is deprecated, please use weights_backbone instead.")
+        weights_backbone = MobileNetV3LargeWeights.ImageNet1K_RefV1 if kwargs.pop("pretrained_backbone") else None
+    weights_backbone = MobileNetV3LargeWeights.verify(weights_backbone)
+
+    if weights is not None:
+        weights_backbone = None
+        aux_loss = True
+        num_classes = len(weights.meta["categories"])
+
+    backbone = mobilenet_v3_large(weights=weights_backbone, dilated=True)
+    model = _deeplabv3_mobilenetv3(backbone, num_classes, aux_loss)
+
+    if weights is not None:
+        model.load_state_dict(weights.state_dict(progress=progress))
+
+    return model
