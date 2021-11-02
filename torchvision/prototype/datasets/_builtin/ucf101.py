dc205e9c0b0c7c37fb847fb7c1a2ab97348a9f88
Bruno Korbar <bkorbar@quansight.com>
2021-11-02 18:29:52 +0000

    format ucf101 and lint stuff

--- torchvision/prototype/datasets/_builtin/ucf101.py
+++ torchvision/prototype/datasets/_builtin/ucf101.py
@@ -1,10 +1,6 @@
-import abc
-import functools
 import io
-import pathlib
 from pathlib import Path
-import pickle
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union, Iterator
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 from torchvision.prototype.datasets.utils._internal import RarArchiveReader, INFINITE_BUFFER_SIZE
 
@@ -15,11 +11,9 @@ from torch.utils.data import IterDataPipe
 from torch.utils.data.datapipes.iter import (
     Filter,
     Mapper,
-    TarArchiveReader,
     ZipArchiveReader,
     Shuffler,
 )
-from torchdata.datapipes.iter.util.combining import KeyZipperIterDataPipe
 from torchvision.prototype.datasets.decoder import raw
 from torchvision.prototype.datasets.utils import (
     Dataset,
@@ -35,29 +29,17 @@ class ucf101(Dataset):
     """This is a base datapipe that returns a file handler of the video.
     What we want to do is implement either several decoder options or additional
     datapipe extensions to make this work.
-
-    We would want 3 different outcomes:
-        0. Datapipe that returns file handle (decoder=None)
-        1. Datapipe that simply returns keyframes of the video
-        2. Datapipe that returns batched clips
-        3. Datapipe that returns a number of random frames from the video.
-
-    Args:
-        Dataset ([type]): [description]
-
-    Returns:
-        [type]: [description]
     """
     @property
     def info(self) -> DatasetInfo:
         return DatasetInfo(
             "ucf101",
             type=DatasetType.VIDEO,
-            valid_options={'split': ["train", "test"], 'fold': ["1", "2", "3"]}
+            valid_options={'split': ["train", "test"], 'fold': ["1", "2", "3"]},
             # categories=HERE / "ucf101.categories",
-            # homepage="https://www.cs.toronto.edu/~kriz/cifar.html",
+            homepage="https://www.crcv.ucf.edu/data/UCF101.php",
         )
-    
+
     def resources(self, config: DatasetConfig) -> List[OnlineResource]:
         return [
             HttpResource(
@@ -80,8 +62,8 @@ class ucf101(Dataset):
 
         label = annotations_d[1]
         _path, file_handle = file_d
-        return {"path": _path, "file": file_handle, "target":label}
-        
+        return {"path": _path, "file": file_handle, "target": label}
+
     def _filtername(self, data, *, tgt):
         return Path(data[0]).name == tgt
 
@@ -95,16 +77,18 @@ class ucf101(Dataset):
         config: DatasetConfig,
         decoder: Optional[Callable[[io.IOBase], torch.Tensor]],
     ) -> IterDataPipe[Dict[str, Any]]:
-        
+
         annotations = resource_dps[0]
         files = resource_dps[1]
 
         annotations_dp = ZipArchiveReader(annotations)
-        annotations_dp = Filter(annotations_dp, self._filtername, fn_kwargs=dict(tgt=f"{config.split}list0{config.fold}.txt"))
+        annotations_dp = Filter(annotations_dp,
+                                self._filtername,
+                                fn_kwargs=dict(tgt=f"{config.split}list0{config.fold}.txt"))
         annotations_dp = CSVParser(annotations_dp, delimiter=" ")
-        # COMMENT FOR TESTING
-        # annotations_dp = Shuffler(annotations_dp, buffer_size=INFINITE_BUFFER_SIZE)
+        # COMMENT OUT FOR TESTING
+        annotations_dp = Shuffler(annotations_dp, buffer_size=INFINITE_BUFFER_SIZE)
 
         files_dp = RarArchiveReader(files)
-        dp = KeyZipper(annotations_dp, files_dp, self._getname, self._getname)        
+        dp = KeyZipper(annotations_dp, files_dp, self._getname, self._getname)
         return Mapper(dp, self._collate_and_decode, fn_kwargs=dict(decoder=decoder))
