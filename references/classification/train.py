9a34c0c93459e875eff245dc1afb97fa28e28ffd
Vasilis Vryniotis <datumbox@users.noreply.github.com>
2021-11-02 14:35:21 +0000

    Adding multiweight support to Quantized ResNet (#4827)
    * Adding multi-weight support to Quantized ResNet.
    
    * Update references script to support testing quantized models with the new API.
    
    * Handle quantized models correctly in ref script.
    
    * Fixing references for quantization.

--- references/classification/train.py
+++ references/classification/train.py
@@ -153,7 +153,7 @@ def load_data(traindir, valdir, args):
                 crop_size=val_crop_size, resize_size=val_resize_size, interpolation=interpolation
             )
         else:
-            fn = PM.__dict__[args.model]
+            fn = PM.quantization.__dict__[args.model] if hasattr(args, "backend") else PM.__dict__[args.model]
             weights = PM._api.get_weight(fn, args.weights)
             preprocessing = weights.transforms()
 
