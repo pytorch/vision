c1d0fc150c9e1183897facd7a70bc376f4ff08eb
Vasilis Vryniotis <vvryniotis@fb.com>
2021-11-02 11:18:26 +0000

    Adding multi-weight support to Quantized ResNet.

--- test/test_prototype_models.py
+++ test/test_prototype_models.py
@@ -43,6 +43,12 @@ def test_classification_model(model_fn, dev):
     TM.test_classification_model(model_fn, dev)
 
 
+@pytest.mark.parametrize("model_fn", TM.get_models_from_module(models.quantization))
+@pytest.mark.skipif(os.getenv("PYTORCH_TEST_WITH_PROTOTYPE", "0") == "0", reason="Prototype code tests are disabled")
+def test_quantized_classification_model(model_fn):
+    TM.test_quantized_classification_model(model_fn)
+
+
 @pytest.mark.parametrize("model_fn", TM.get_models_from_module(models.segmentation))
 @pytest.mark.parametrize("dev", cpu_and_gpu())
 @pytest.mark.skipif(os.getenv("PYTORCH_TEST_WITH_PROTOTYPE", "0") == "0", reason="Prototype code tests are disabled")
@@ -60,6 +66,7 @@ def test_video_model(model_fn, dev):
 @pytest.mark.parametrize(
     "model_fn, module_name",
     get_models_with_module_names(models)
+    + get_models_with_module_names(models.quantization)
     + get_models_with_module_names(models.segmentation)
     + get_models_with_module_names(models.video),
 )
@@ -68,6 +75,9 @@ def test_video_model(model_fn, dev):
 def test_old_vs_new_factory(model_fn, module_name, dev):
     defaults = {
         "models": {
+            "input_shape": (1, 3, 224, 224),
+        },
+        "quantization": {
             "input_shape": (1, 3, 224, 224),
         },
         "segmentation": {
