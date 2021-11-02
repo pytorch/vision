eaa13dc0dc7c7242bea6f0a7bc9fcd12511a8df6
Vasilis Vryniotis <vvryniotis@fb.com>
2021-11-02 12:54:19 +0000

    Handle quantized models correctly in ref script.

--- test/test_prototype_models.py
+++ test/test_prototype_models.py
@@ -30,10 +30,15 @@ def get_models_with_module_names(module):
     return [(fn, module_name) for fn in TM.get_models_from_module(module)]
 
 
-def test_get_weight():
-    fn = models.resnet50
-    weight_name = "ImageNet1K_RefV2"
-    assert models._api.get_weight(fn, weight_name) == models.ResNet50Weights.ImageNet1K_RefV2
+@pytest.mark.parametrize(
+    "model_fn, weight",
+    [
+        (models.resnet50, models.ResNet50Weights.ImageNet1K_RefV2),
+        (models.quantization.resnet50, models.quantization.QuantizedResNet50Weights.ImageNet1K_FBGEMM_RefV1),
+    ],
+)
+def test_get_weight(model_fn, weight):
+    assert models._api.get_weight(model_fn, weight.name) == weight
 
 
 @pytest.mark.parametrize("model_fn", TM.get_models_from_module(models))
