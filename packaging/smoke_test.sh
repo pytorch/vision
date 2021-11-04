7de68b80d98cfce02e473c18e7d46ea5dc30382d
Yi Zhang <zhanyi@microsoft.com>
2021-11-04 15:02:01 +0800

    add cuda bin path to PATH

--- packaging/smoke_test.sh
+++ packaging/smoke_test.sh
@@ -1,3 +1,4 @@
+export PATH="/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.2/bin:$PATH"
 eval "$(conda shell.bash hook)"
 conda activate "env$PYTHON_VERSION"
 pip install C:/Users/circleci/project/dist/*.whl
