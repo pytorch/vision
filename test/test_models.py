67679ed9e9f385399ac7a676bdc8d7d8e29f37dd
Philip Meier <github.pmeier@posteo.de>
2021-11-05 11:25:04 +0100

    fix indent

--- test/test_models.py
+++ test/test_models.py
@@ -790,7 +790,7 @@ def test_detection_model_trainable_backbone_layers(model_fn):
         for trainable_layers in range(0, max_trainable + 1):
             model = model_fn(pretrained=False, pretrained_backbone=True, trainable_backbone_layers=trainable_layers)
 
-        n_trainable_params.append(len([p for p in model.parameters() if p.requires_grad]))
+            n_trainable_params.append(len([p for p in model.parameters() if p.requires_grad]))
     assert n_trainable_params == _model_tests_values[model_name]["n_trn_params_per_layer"]
 
 
