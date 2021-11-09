12b591583e17a5f7c9d321752c5e1b5a7fa47d90
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2021-11-09 16:12:27 +0530

    KeyZipper updates

--- torchvision/prototype/datasets/_builtin/sintel.py
+++ torchvision/prototype/datasets/_builtin/sintel.py
@@ -1,5 +1,5 @@
 import io
-from typing import Any, Callable, Dict, List, Optional, Tuple, Iterator, Iterable
+from typing import Any, Callable, Dict, List, Optional, Tuple, Iterator, Iterable, TypeVar
 import pathlib
 from functools import partial
 import re
@@ -10,10 +10,12 @@ import numpy as np
 
 from torchdata.datapipes.iter import (
     IterDataPipe,
+    IterableWrapper,
     Demultiplexer,
     Mapper,
     Shuffler,
     Zipper,
+    KeyZipper,
     LineReader,
     ZipArchiveReader,
 )
@@ -33,27 +35,30 @@ from torchvision.prototype.datasets.utils._internal import (
 )
 from torchvision import transforms
 
+T = TypeVar("T")
 
-class FlowDatasetReader(IterDataPipe[torch.Tensor]):
-    def __init__(
-        self,
-        images_datapipe: IterDataPipe[Tuple[Any, io.IOBase]],
-        labels_datapipe: IterDataPipe[Tuple[Any, io.IOBase]]
-    ) -> None:
-        self.images_datapipe = images_datapipe
-        self.labels_datapipe = labels_datapipe
+FILE_NAME_PATTERN = re.compile(r"(frame|image)_(?P<idx>\d+)[.](flo|png)")
 
-    def __iter__(self) -> Iterator[torch.Tensor]:
-        count_images = 0
-        for _, file in self.images_datapipe:
-            yield self._read_image(file)
+try:
+    from itertools import pairwise
+except ImportError:
+    from itertools import tee
 
-        count_labels = 0
-        for _, file in self.labels_datapipe:
-            count_labels += 1
+    def pairwise(iterable: Iterable[T]) -> Iterable[Tuple[T, T]]:
+        a, b = tee(iterable)
+        next(b, None)
+        return zip(a, b)
 
-        print(count_images, count_labels)
-        pass
+class IntCategoryGrouper(IterDataPipe[Tuple[Tuple[str, T], Tuple[str, T]]]):
+    def __init__(self, datapipe: IterDataPipe[Tuple[str, T]]) -> None:
+        self.datapipe = datapipe
+
+    def __iter__(self):
+        for item1, item2 in pairwise(sorted(self.datapipe)):
+            if pathlib.Path(item1[0]).parent != pathlib.Path(item2[0]).parent:
+                continue
+
+            yield item1, item2
 
 
 class SINTEL(Dataset):
@@ -82,7 +87,23 @@ class SINTEL(Dataset):
         *,
         decoder: Optional[Callable[[io.IOBase], torch.Tensor]],
     ) -> Dict[str, Any]:
-        return data
+        # Read images and flo file here
+        # Use decoder for images if available
+        # Return dict
+        flo, images = data
+        img1, img2 = images
+
+        path1, buffer1 = img1
+        path2, buffer2 = img2
+
+        flow_arr = self.read_flo(flo)
+
+        print(buffer1, buffer2)
+        return dict(
+            image1=decoder(buffer1) if decoder else buffer1,
+            image2=decoder(buffer2) if decoder else buffer2,
+            label=flow_arr,
+        )
 
     def _classify_train_test(self, data: Dict[str, Any], *, config: DatasetConfig):
         path = pathlib.Path(data[0])
@@ -104,27 +125,29 @@ class SINTEL(Dataset):
         else:
             return None
 
-    def read_images(self, data: IterDataPipe[Tuple[Any, io.IOBase]]) -> Iterable[torch.Tensor]:
-        count_images = 0
-        for _, file in data:
-            img = Image.open(file)
-            to_tensor = transforms.ToTensor()
-            count_images += 1
-            yield to_tensor(img)
-
-    def read_flo(self, data: IterDataPipe[Tuple[Any, io.IOBase]], config) -> Iterable[np.ndarray]:
+    def read_flo(self, data: IterDataPipe[Tuple[Any, io.IOBase]]) -> Iterable[np.ndarray]:
         count_flo = 0
         for _, file in data:
-            with open(file.name, "rb") as f:
-                magic = np.fromfile(f, np.float32, count=1)
-                if 202021.25 != magic:
-                    raise ValueError("Magic number incorrect. Invalid .flo file")
+            f = file.file_obj
+            magic = np.fromfile(f, np.float32, count=1)
+            if 202021.25 != magic:
+                raise ValueError("Magic number incorrect. Invalid .flo file")
 
-                w = int(np.fromfile(f, np.int32, count=1))
-                h = int(np.fromfile(f, np.int32, count=1))
-                _data = np.fromfile(f, np.float32, count=2 * w * h)
-                count_flo += 1
-                yield _data.reshape(2, h, w)
+            w = int(np.fromfile(f, np.int32, count=1))
+            h = int(np.fromfile(f, np.int32, count=1))
+            _data = np.fromfile(f, np.float32, count=2 * w * h)
+            count_flo += 1
+            yield _data.reshape(2, h, w)
+
+    def flows_key(self, data: Tuple[str, Any]) -> Tuple[str, int]:
+        path = pathlib.Path(data[0])
+        print('path: ', path.name)
+        category = path.parent.name
+        idx = int(FILE_NAME_PATTERN.match(path.name).group("idx"))  # type: ignore[union-attr]
+        return category, idx
+
+    def images_key(self, data: Tuple[Tuple[str, Any], Tuple[str, Any]]) -> Tuple[str, int]:
+        return self.flows_key(data[0])
 
     def _make_datapipe(
         self,
@@ -132,10 +155,11 @@ class SINTEL(Dataset):
         *,
         config: DatasetConfig,
         decoder: Optional[Callable[[io.IOBase], torch.Tensor]]
-    ) -> IterDataPipe[Dict[str, Any]]:
+    ) -> IterDataPipe[Tuple[Dict[str, Any], Dict[str, Any]]]:
         dp = resource_dps[0]
         archive_dp = ZipArchiveReader(dp)
 
+        # Use a Filter
         train_dp, test_dp = Demultiplexer(
             archive_dp,
             2,
@@ -156,10 +180,14 @@ class SINTEL(Dataset):
             drop_none=True,
             buffer_size=INFINITE_BUFFER_SIZE,
         )
-        pass_images_dp = self.read_images(pass_images_dp)
-        flo_dp = self.read_flo(flo_dp, config)
-
-        images_dp = Shuffler(pass_images_dp, buffer_size=INFINITE_BUFFER_SIZE)
         flo_dp = Shuffler(flo_dp, buffer_size=INFINITE_BUFFER_SIZE)
-        zipped_dp = Zipper(images_dp, flo_dp)
+
+        pass_images_dp = IntCategoryGrouper(pass_images_dp)
+        zipped_dp = KeyZipper(
+            flo_dp,
+            pass_images_dp,
+            key_fn=self.flows_key,
+            ref_key_fn=self.images_key,
+        )
+
         return Mapper(zipped_dp, self._collate_and_decode_sample, fn_kwargs=dict(decoder=decoder))
