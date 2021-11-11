535011bf53faa1f7e62503adf2ad353a7b074371
Hu Ye <xiaohuzc@gmail.com>
2021-11-11 14:39:19 +0800

    add SoftTargetCrossEntropy loss

---
+++ references/classification/loss.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SoftTargetCrossEntropy(nn.Module):
+
+    def __init__(self):
+        super(SoftTargetCrossEntropy, self).__init__()
+
+    def forward(self, x: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        loss = torch.sum(-target * F.log_softmax(x, dim=-1), dim=-1)
+        return loss.mean()
