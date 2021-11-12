3dd4891e198de884b46094993aaa2bbc48020942
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared30078.18.frc3.facebook.com>
2021-11-12 04:33:09 -0800

    2021-11-12 nightly release (0fa747eea4569f349fb0dacab44604b7b11c1a74)

--- torchvision/models/detection/ssd.py
+++ torchvision/models/detection/ssd.py
@@ -587,7 +587,8 @@ def ssd300_vgg16(
         num_classes (int): number of output classes of the model (including the background)
         pretrained_backbone (bool): If True, returns a model with backbone pre-trained on Imagenet
         trainable_backbone_layers (int): number of trainable (not frozen) resnet layers starting from final block.
-            Valid values are between 0 and 5, with 5 meaning all backbone layers are trainable.
+            Valid values are between 0 and 5, with 5 meaning all backbone layers are trainable. If ``None`` is
+            passed (the default) this value is set to 4.
     """
     if "size" in kwargs:
         warnings.warn("The size of the model is already fixed; ignoring the argument.")
