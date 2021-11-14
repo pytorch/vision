610f0d72af9faf5d41b9aa905e578930e352bf0b
Hu Ye <xiaohuzc@gmail.com>
2021-11-14 22:53:08 +0800

    support amp training

--- references/detection/train.py
+++ references/detection/train.py
@@ -253,7 +253,7 @@ def main(args):
                 "epoch": epoch,
             }
             if args.amp:
-                scaler.load_state_dict(checkpoint["scaler"])
+                checkpoint['scaler'] = scaler.state_dict()
             utils.save_on_master(checkpoint, os.path.join(args.output_dir, f"model_{epoch}.pth"))
             utils.save_on_master(checkpoint, os.path.join(args.output_dir, "checkpoint.pth"))
 
