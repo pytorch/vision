552f6f1be651c37a3416bb0c7e6f8c7143a27954
Joao Gomes <jdsgomes@fb.com>
2021-11-15 21:26:28 +0000

    removing nested function declarations

--- torchvision/ops/poolers.py
+++ torchvision/ops/poolers.py
@@ -10,6 +10,58 @@ from ..utils import _log_api_usage_once
 from .roi_align import roi_align
 
 
+def _convert_to_roi_format(boxes: List[Tensor]) -> Tensor:
+        concat_boxes = torch.cat(boxes, dim=0)
+        device, dtype = concat_boxes.device, concat_boxes.dtype
+        ids = torch.cat(
+            [
+                torch.full_like(b[:, :1], i, dtype=dtype, layout=torch.strided, device=device)
+                for i, b in enumerate(boxes)
+            ],
+            dim=0,
+        )
+        rois = torch.cat([ids, concat_boxes], dim=1)
+        return rois
+
+
+def _infer_scale(feature: Tensor, original_size: List[int]) -> float:
+        # assumption: the scale is of the form 2 ** (-k), with k integer
+        size = feature.shape[-2:]
+        possible_scales: List[float] = []
+        for s1, s2 in zip(size, original_size):
+            approx_scale = float(s1) / float(s2)
+            scale = 2 ** float(torch.tensor(approx_scale).log2().round())
+            possible_scales.append(scale)
+        assert possible_scales[0] == possible_scales[1]
+        return possible_scales[0]
+
+
+def _setup_scales(
+        features: List[Tensor], image_shapes: List[Tuple[int, int]], canonical_scale: int, canonical_level: int
+    ):
+        assert len(image_shapes) != 0
+        max_x = 0
+        max_y = 0
+        for shape in image_shapes:
+            max_x = max(shape[0], max_x)
+            max_y = max(shape[1], max_y)
+        original_input_shape = (max_x, max_y)
+
+        scales = [_infer_scale(feat, original_input_shape) for feat in features]
+        # get the levels in the feature map by leveraging the fact that the network always
+        # downsamples by a factor of 2 at each level.
+        lvl_min = -torch.log2(torch.tensor(scales[0], dtype=torch.float32)).item()
+        lvl_max = -torch.log2(torch.tensor(scales[-1], dtype=torch.float32)).item()
+        scales = scales
+        map_levels = initLevelMapper(
+            int(lvl_min),
+            int(lvl_max),
+            canonical_scale=canonical_scale,
+            canonical_level=canonical_level,
+        )
+        return scales, map_levels
+
+
 @torch.fx.wrap
 def multiscale_roi_align(
     x: Dict[str, Tensor],
@@ -41,63 +93,14 @@ def multiscale_roi_align(
         result (Tensor)
     """
 
-    def convert_to_roi_format(boxes: List[Tensor]) -> Tensor:
-        concat_boxes = torch.cat(boxes, dim=0)
-        device, dtype = concat_boxes.device, concat_boxes.dtype
-        ids = torch.cat(
-            [
-                torch.full_like(b[:, :1], i, dtype=dtype, layout=torch.strided, device=device)
-                for i, b in enumerate(boxes)
-            ],
-            dim=0,
-        )
-        rois = torch.cat([ids, concat_boxes], dim=1)
-        return rois
-
-    def infer_scale(feature: Tensor, original_size: List[int]) -> float:
-        # assumption: the scale is of the form 2 ** (-k), with k integer
-        size = feature.shape[-2:]
-        possible_scales: List[float] = []
-        for s1, s2 in zip(size, original_size):
-            approx_scale = float(s1) / float(s2)
-            scale = 2 ** float(torch.tensor(approx_scale).log2().round())
-            possible_scales.append(scale)
-        assert possible_scales[0] == possible_scales[1]
-        return possible_scales[0]
-
-    def setup_scales(
-        features: List[Tensor], image_shapes: List[Tuple[int, int]], canonical_scale: int, canonical_level: int
-    ):
-        assert len(image_shapes) != 0
-        max_x = 0
-        max_y = 0
-        for shape in image_shapes:
-            max_x = max(shape[0], max_x)
-            max_y = max(shape[1], max_y)
-        original_input_shape = (max_x, max_y)
-
-        scales = [infer_scale(feat, original_input_shape) for feat in features]
-        # get the levels in the feature map by leveraging the fact that the network always
-        # downsamples by a factor of 2 at each level.
-        lvl_min = -torch.log2(torch.tensor(scales[0], dtype=torch.float32)).item()
-        lvl_max = -torch.log2(torch.tensor(scales[-1], dtype=torch.float32)).item()
-        scales = scales
-        map_levels = initLevelMapper(
-            int(lvl_min),
-            int(lvl_max),
-            canonical_scale=canonical_scale,
-            canonical_level=canonical_level,
-        )
-        return scales, map_levels
-
     x_filtered = []
     for k, v in x.items():
         if k in featmap_names:
             x_filtered.append(v)
     num_levels = len(x_filtered)
-    rois = convert_to_roi_format(boxes)
+    rois = _convert_to_roi_format(boxes)
     if scales is None:
-        scales, mapper = setup_scales(x_filtered, image_shapes, canonical_scale, canonical_level)
+        scales, mapper = _setup_scales(x_filtered, image_shapes, canonical_scale, canonical_level)
 
     assert scales is not None
 
