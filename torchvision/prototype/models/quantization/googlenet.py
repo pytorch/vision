da397899007f0eaf59effb9e812d80f158fd1fd7
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared32909.06.ftw2.facebook.com>
2021-11-17 04:31:13 -0800

    2021-11-17 nightly release (9841a9076e711f73482cb84e106fe5ec40707023)

--- torchvision/prototype/models/quantization/googlenet.py
+++ torchvision/prototype/models/quantization/googlenet.py
@@ -47,7 +47,7 @@ def googlenet(
     **kwargs: Any,
 ) -> QuantizableGoogLeNet:
     if "pretrained" in kwargs:
-        warnings.warn("The argument pretrained is deprecated, please use weights instead.")
+        warnings.warn("The parameter pretrained is deprecated, please use weights instead.")
         if kwargs.pop("pretrained"):
             weights = QuantizedGoogLeNetWeights.ImageNet1K_FBGEMM_TFV1 if quantize else GoogLeNetWeights.ImageNet1K_TFV1
         else:
@@ -62,10 +62,6 @@ def googlenet(
     if weights is not None:
         if "transform_input" not in kwargs:
             kwargs["transform_input"] = True
-        if original_aux_logits:
-            warnings.warn(
-                "auxiliary heads in the pretrained googlenet model are NOT pretrained, so make sure to train them"
-            )
         kwargs["aux_logits"] = True
         kwargs["init_weights"] = False
         kwargs["num_classes"] = len(weights.meta["categories"])
@@ -79,10 +75,14 @@ def googlenet(
         quantize_model(model, backend)
 
     if weights is not None:
-        model.load_state_dict(weights.state_dict(progress=progress))
+        model.load_state_dict(weights.get_state_dict(progress=progress))
         if not original_aux_logits:
             model.aux_logits = False
             model.aux1 = None  # type: ignore[assignment]
             model.aux2 = None  # type: ignore[assignment]
+        else:
+            warnings.warn(
+                "auxiliary heads in the pretrained googlenet model are NOT pretrained, so make sure to train them"
+            )
 
     return model
