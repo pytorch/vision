c610fbd760d345c96729441240c6d00fa5ad8f24
Kai Zhang <kaizh@fb.com>
2021-11-29 23:42:26 +0000

    add api usage log for io

--- torchvision/io/image.py
+++ torchvision/io/image.py
@@ -3,6 +3,7 @@ from enum import Enum
 import torch
 
 from .._internally_replaced_utils import _get_extension_path
+from ..utils import _log_api_usage_once
 
 
 try:
@@ -41,6 +42,7 @@ def read_file(path: str) -> torch.Tensor:
     Returns:
         data (Tensor)
     """
+    _log_api_usage_once("torchvision.io.read_file")
     data = torch.ops.image.read_file(path)
     return data
 
@@ -54,6 +56,7 @@ def write_file(filename: str, data: torch.Tensor) -> None:
         filename (str): the path to the file to be written
         data (Tensor): the contents to be written to the output file
     """
+    _log_api_usage_once("torchvision.io.write_file")
     torch.ops.image.write_file(filename, data)
 
 
@@ -74,6 +77,7 @@ def decode_png(input: torch.Tensor, mode: ImageReadMode = ImageReadMode.UNCHANGE
     Returns:
         output (Tensor[image_channels, image_height, image_width])
     """
+    _log_api_usage_once("torchvision.io.decode_png")
     output = torch.ops.image.decode_png(input, mode.value, False)
     return output
 
@@ -93,6 +97,7 @@ def encode_png(input: torch.Tensor, compression_level: int = 6) -> torch.Tensor:
         Tensor[1]: A one dimensional int8 tensor that contains the raw bytes of the
             PNG file.
     """
+    _log_api_usage_once("torchvision.io.encode_png")
     output = torch.ops.image.encode_png(input, compression_level)
     return output
 
@@ -109,6 +114,7 @@ def write_png(input: torch.Tensor, filename: str, compression_level: int = 6):
         compression_level (int): Compression factor for the resulting file, it must be a number
             between 0 and 9. Default: 6
     """
+    _log_api_usage_once("torchvision.io.write_png")
     output = encode_png(input, compression_level)
     write_file(filename, output)
 
@@ -137,6 +143,7 @@ def decode_jpeg(
     Returns:
         output (Tensor[image_channels, image_height, image_width])
     """
+    _log_api_usage_once("torchvision.io.decode_jpeg")
     device = torch.device(device)
     if device.type == "cuda":
         output = torch.ops.image.decode_jpeg_cuda(input, mode.value, device)
@@ -160,6 +167,7 @@ def encode_jpeg(input: torch.Tensor, quality: int = 75) -> torch.Tensor:
         output (Tensor[1]): A one dimensional int8 tensor that contains the raw bytes of the
             JPEG file.
     """
+    _log_api_usage_once("torchvision.io.encode_jpeg")
     if quality < 1 or quality > 100:
         raise ValueError("Image quality should be a positive number between 1 and 100")
 
@@ -178,6 +186,7 @@ def write_jpeg(input: torch.Tensor, filename: str, quality: int = 75):
         quality (int): Quality of the resulting JPEG file, it must be a number
             between 1 and 100. Default: 75
     """
+    _log_api_usage_once("torchvision.io.write_jpeg")
     output = encode_jpeg(input, quality)
     write_file(filename, output)
 
@@ -201,6 +210,7 @@ def decode_image(input: torch.Tensor, mode: ImageReadMode = ImageReadMode.UNCHAN
     Returns:
         output (Tensor[image_channels, image_height, image_width])
     """
+    _log_api_usage_once("torchvision.io.decode_image")
     output = torch.ops.image.decode_image(input, mode.value)
     return output
 
@@ -221,6 +231,7 @@ def read_image(path: str, mode: ImageReadMode = ImageReadMode.UNCHANGED) -> torc
     Returns:
         output (Tensor[image_channels, image_height, image_width])
     """
+    _log_api_usage_once("torchvision.io.read_image")
     data = read_file(path)
     return decode_image(data, mode)
 
