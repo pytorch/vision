eea34fd5a95e87851e4c3e518bf4ea103746ba5c
atalman <atalman@fb.com>
2021-12-08 14:30:41 -0800

    Try to fix cudatoolkit issue

--- packaging/pkg_helpers.bash
+++ packaging/pkg_helpers.bash
@@ -298,7 +298,7 @@ setup_conda_cudatoolkit_constraint() {
   else
     case "$CU_VERSION" in
       cu115)
-        export CONDA_CUDATOOLKIT_CONSTRAINT="- cudatoolkit >=11.5,<11.6 # [not osx]"
+        export CONDA_CUDATOOLKIT_CONSTRAINT="- cudatoolkit >=11.3,<11.4 # [not osx]"
         ;;
       cu113)
         export CONDA_CUDATOOLKIT_CONSTRAINT="- cudatoolkit >=11.3,<11.4 # [not osx]"
