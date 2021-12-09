a3011870c4275a1650c34ec2f879a0ad12c1dc49
sallysyw <yisallysyw@gmail.com>
2021-12-09 21:37:24 +0000

    Making interpolation mode configurable

--- torchvision/prototype/models/vision_transformer.py
+++ torchvision/prototype/models/vision_transformer.py
@@ -382,7 +382,7 @@ def vit_l_32(*, weights: Optional[ViT_L_32_Weights] = None, progress: bool = Tru
 
 
 def interpolate_embeddings(
-    image_size: int, patch_size: int, model_state: "OrderedDict[str, torch.Tensor]", reset_heads: bool = False
+    image_size: int, patch_size: int, model_state: "OrderedDict[str, torch.Tensor]", interpolation_mode: str = "bicubic", reset_heads: bool = False
 ) -> "OrderedDict[str, torch.Tensor]":
     """This function helps interpolating positional embeddings during checkpoint loading,
     especially when you want to apply a pre-trained model on images with different resolution.
@@ -391,6 +391,7 @@ def interpolate_embeddings(
         image_size (int): Image size of the new model.
         patch_size (int): Patch size of the new model.
         model_state (OrderedDict[str, torch.Tensor]): State dict of the pre-trained model.
+        interpolation_mode (str): The algorithm used for upsampling. Default: bicubic. 
         reset_heads (bool): If true, not copying the state of heads. Default: False.
 
     Returns:
@@ -423,12 +424,12 @@ def interpolate_embeddings(
         pos_embedding_img = pos_embedding_img.reshape(1, hidden_dim, seq_length_1d, seq_length_1d)
         new_seq_length_1d = image_size // patch_size
 
-        # Use bicubic interpolation.
+        # Perform interpolation.
         # (1, hidden_dim, seq_l_1d, seq_l_1d) -> (1, hidden_dim, new_seq_l_1d, new_seq_l_1d)
         new_pos_embedding_img = nn.functional.interpolate(
             pos_embedding_img,
             size=new_seq_length_1d,
-            mode="bicubic",
+            mode=interpolation_mode,
             align_corners=True,
         )
 
