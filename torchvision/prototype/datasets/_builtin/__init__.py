a83850d83f62f5c1471d931e505007008f7e09bc
Philip Meier <github.pmeier@posteo.de>
2021-12-22 12:39:19 +0100

    add prototype dataset

--- torchvision/prototype/datasets/_builtin/__init__.py
+++ torchvision/prototype/datasets/_builtin/__init__.py
@@ -1,6 +1,7 @@
 from .caltech import Caltech101, Caltech256
 from .celeba import CelebA
 from .cifar import Cifar10, Cifar100
+from .clevr import CLEVR
 from .coco import Coco
 from .imagenet import ImageNet
 from .mnist import MNIST, FashionMNIST, KMNIST, EMNIST, QMNIST
