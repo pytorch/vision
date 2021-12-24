d69f820bdc2daa1a1ff4d78677ce80e37ff00bff
Prabhat Roy <prabhatroy@fb.com>
2021-12-24 09:22:27 -0800

    Change struct initialisation style

--- torchvision/csrc/io/decoder/gpu/decoder.cpp
+++ torchvision/csrc/io/decoder/gpu/decoder.cpp
@@ -27,19 +27,19 @@ void Decoder::init(CUcontext context, cudaVideoCodec codec) {
   check_for_cuda_errors(
       cuvidCtxLockCreate(&ctxLock, cuContext), __LINE__, __FILE__);
 
-  CUVIDPARSERPARAMS parserParams = {
-      .CodecType = codec,
-      .ulMaxNumDecodeSurfaces = 1,
-      .ulClockRate = 1000,
-      .ulMaxDisplayDelay = 0u,
-      .pUserData = this,
-      .pfnSequenceCallback = video_sequence_handler,
-      .pfnDecodePicture = picture_decode_handler,
-      .pfnDisplayPicture = picture_display_handler,
-      .pfnGetOperatingPoint = operating_point_handler,
-  };
+  CUVIDPARSERPARAMS parser_params = {};
+  parser_params.CodecType = codec;
+  parser_params.ulMaxNumDecodeSurfaces = 1;
+  parser_params.ulClockRate = 1000;
+  parser_params.ulMaxDisplayDelay = 0u;
+  parser_params.pUserData = this;
+  parser_params.pfnSequenceCallback = video_sequence_handler;
+  parser_params.pfnDecodePicture = picture_decode_handler;
+  parser_params.pfnDisplayPicture = picture_display_handler;
+  parser_params.pfnGetOperatingPoint = operating_point_handler;
+
   check_for_cuda_errors(
-      cuvidCreateVideoParser(&parser, &parserParams), __LINE__, __FILE__);
+      cuvidCreateVideoParser(&parser, &parser_params), __LINE__, __FILE__);
 }
 
 /* Destroy parser object and context lock.
@@ -64,11 +64,11 @@ void Decoder::release() {
 /* Trigger video decoding.
  */
 void Decoder::decode(const uint8_t* data, unsigned long size) {
-  CUVIDSOURCEDATAPACKET pkt = {
-      .flags = CUVID_PKT_TIMESTAMP,
-      .payload_size = size,
-      .payload = data,
-      .timestamp = 0};
+  CUVIDSOURCEDATAPACKET pkt = {};
+  pkt.flags = CUVID_PKT_TIMESTAMP;
+  pkt.payload_size = size;
+  pkt.payload = data;
+  pkt.timestamp = 0;
   if (!data || size == 0) {
     pkt.flags |= CUVID_PKT_ENDOFSTREAM;
   }
@@ -106,13 +106,12 @@ int Decoder::handle_picture_decode(CUVIDPICPARAMS* picParams) {
 /* Process the decoded data and copy it to a cuda memory location.
  */
 int Decoder::handle_picture_display(CUVIDPARSERDISPINFO* dispInfo) {
-  CUVIDPROCPARAMS procParams = {
-      .progressive_frame = dispInfo->progressive_frame,
-      .second_field = dispInfo->repeat_first_field + 1,
-      .top_field_first = dispInfo->top_field_first,
-      .unpaired_field = dispInfo->repeat_first_field < 0,
-      .output_stream = cuvidStream,
-  };
+  CUVIDPROCPARAMS proc_params = {};
+  proc_params.progressive_frame = dispInfo->progressive_frame;
+  proc_params.second_field = dispInfo->repeat_first_field + 1;
+  proc_params.top_field_first = dispInfo->top_field_first;
+  proc_params.unpaired_field = dispInfo->repeat_first_field < 0;
+  proc_params.output_stream = cuvidStream;
 
   CUdeviceptr dpSrcFrame = 0;
   unsigned int nSrcPitch = 0;
@@ -123,7 +122,7 @@ int Decoder::handle_picture_display(CUVIDPARSERDISPINFO* dispInfo) {
           dispInfo->picture_index,
           &dpSrcFrame,
           &nSrcPitch,
-          &procParams),
+          &proc_params),
       __LINE__,
       __FILE__);
 
@@ -184,20 +183,20 @@ int Decoder::handle_picture_display(CUVIDPARSERDISPINFO* dispInfo) {
  * verify if the hardware supports decoding the passed video.
  */
 void Decoder::query_hardware(CUVIDEOFORMAT* videoFormat) {
-  CUVIDDECODECAPS decodeCaps = {
-      .eCodecType = videoFormat->codec,
-      .eChromaFormat = videoFormat->chroma_format,
-      .nBitDepthMinus8 = videoFormat->bit_depth_luma_minus8,
-  };
+  CUVIDDECODECAPS decode_caps = {};
+  decode_caps.eCodecType = videoFormat->codec;
+  decode_caps.eChromaFormat = videoFormat->chroma_format;
+  decode_caps.nBitDepthMinus8 = videoFormat->bit_depth_luma_minus8;
+
   check_for_cuda_errors(cuCtxPushCurrent(cuContext), __LINE__, __FILE__);
-  check_for_cuda_errors(cuvidGetDecoderCaps(&decodeCaps), __LINE__, __FILE__);
+  check_for_cuda_errors(cuvidGetDecoderCaps(&decode_caps), __LINE__, __FILE__);
   check_for_cuda_errors(cuCtxPopCurrent(NULL), __LINE__, __FILE__);
 
-  if (!decodeCaps.bIsSupported) {
+  if (!decode_caps.bIsSupported) {
     TORCH_CHECK(false, "Codec not supported on this GPU");
   }
-  if ((videoFormat->coded_width > decodeCaps.nMaxWidth) ||
-      (videoFormat->coded_height > decodeCaps.nMaxHeight)) {
+  if ((videoFormat->coded_width > decode_caps.nMaxWidth) ||
+      (videoFormat->coded_height > decode_caps.nMaxHeight)) {
     TORCH_CHECK(
         false,
         "Resolution          : ",
@@ -205,33 +204,33 @@ void Decoder::query_hardware(CUVIDEOFORMAT* videoFormat) {
         "x",
         videoFormat->coded_height,
         "\nMax Supported (wxh) : ",
-        decodeCaps.nMaxWidth,
+        decode_caps.nMaxWidth,
         "x",
-        decodeCaps.nMaxHeight,
+        decode_caps.nMaxHeight,
         "\nResolution not supported on this GPU");
   }
   if ((videoFormat->coded_width >> 4) * (videoFormat->coded_height >> 4) >
-      decodeCaps.nMaxMBCount) {
+      decode_caps.nMaxMBCount) {
     TORCH_CHECK(
         false,
         "MBCount             : ",
         (videoFormat->coded_width >> 4) * (videoFormat->coded_height >> 4),
         "\nMax Supported mbcnt : ",
-        decodeCaps.nMaxMBCount,
+        decode_caps.nMaxMBCount,
         "\nMBCount not supported on this GPU");
   }
   // Check if output format supported. If not, check fallback options
-  if (!(decodeCaps.nOutputFormatMask & (1 << videoOutputFormat))) {
-    if (decodeCaps.nOutputFormatMask & (1 << cudaVideoSurfaceFormat_NV12)) {
+  if (!(decode_caps.nOutputFormatMask & (1 << videoOutputFormat))) {
+    if (decode_caps.nOutputFormatMask & (1 << cudaVideoSurfaceFormat_NV12)) {
       videoOutputFormat = cudaVideoSurfaceFormat_NV12;
     } else if (
-        decodeCaps.nOutputFormatMask & (1 << cudaVideoSurfaceFormat_P016)) {
+        decode_caps.nOutputFormatMask & (1 << cudaVideoSurfaceFormat_P016)) {
       videoOutputFormat = cudaVideoSurfaceFormat_P016;
     } else if (
-        decodeCaps.nOutputFormatMask & (1 << cudaVideoSurfaceFormat_YUV444)) {
+        decode_caps.nOutputFormatMask & (1 << cudaVideoSurfaceFormat_YUV444)) {
       videoOutputFormat = cudaVideoSurfaceFormat_YUV444;
     } else if (
-        decodeCaps.nOutputFormatMask &
+        decode_caps.nOutputFormatMask &
         (1 << cudaVideoSurfaceFormat_YUV444_16Bit)) {
       videoOutputFormat = cudaVideoSurfaceFormat_YUV444_16Bit;
     } else {
@@ -283,21 +282,21 @@ int Decoder::handle_video_sequence(CUVIDEOFORMAT* vidFormat) {
     deinterlaceMode = cudaVideoDeinterlaceMode_Weave;
   }
 
-  CUVIDDECODECREATEINFO videoDecodeCreateInfo = {
-      .ulWidth = vidFormat->coded_width,
-      .ulHeight = vidFormat->coded_height,
-      .ulNumDecodeSurfaces = decodeSurface,
-      .CodecType = vidFormat->codec,
-      .ChromaFormat = vidFormat->chroma_format,
-      // With PreferCUVID, JPEG is still decoded by CUDA while video is decoded
-      // by NVDEC hardware
-      .ulCreationFlags = cudaVideoCreate_PreferCUVID,
-      .bitDepthMinus8 = vidFormat->bit_depth_luma_minus8,
-      .OutputFormat = videoOutputFormat,
-      .DeinterlaceMode = deinterlaceMode,
-      .ulNumOutputSurfaces = 2,
-      .vidLock = ctxLock,
-  };
+  CUVIDDECODECREATEINFO video_decode_create_info = {};
+  video_decode_create_info.ulWidth = vidFormat->coded_width;
+  video_decode_create_info.ulHeight = vidFormat->coded_height;
+  video_decode_create_info.ulNumDecodeSurfaces = decodeSurface;
+  video_decode_create_info.CodecType = vidFormat->codec;
+  video_decode_create_info.ChromaFormat = vidFormat->chroma_format;
+  // With PreferCUVID, JPEG is still decoded by CUDA while video is decoded
+  // by NVDEC hardware
+  video_decode_create_info.ulCreationFlags = cudaVideoCreate_PreferCUVID;
+  video_decode_create_info.bitDepthMinus8 = vidFormat->bit_depth_luma_minus8;
+  video_decode_create_info.OutputFormat = videoOutputFormat;
+  video_decode_create_info.DeinterlaceMode = deinterlaceMode;
+  video_decode_create_info.ulNumOutputSurfaces = 2;
+  video_decode_create_info.vidLock = ctxLock;
+
   // AV1 has max width/height of sequence in sequence header
   if (vidFormat->codec == cudaVideoCodec_AV1 &&
       vidFormat->seqhdr_data_length > 0) {
@@ -311,25 +310,27 @@ int Decoder::handle_video_sequence(CUVIDEOFORMAT* vidFormat) {
   if (maxHeight < vidFormat->coded_height) {
     maxHeight = vidFormat->coded_height;
   }
-  videoDecodeCreateInfo.ulMaxWidth = maxWidth;
-  videoDecodeCreateInfo.ulMaxHeight = maxHeight;
+  video_decode_create_info.ulMaxWidth = maxWidth;
+  video_decode_create_info.ulMaxHeight = maxHeight;
   width = vidFormat->display_area.right - vidFormat->display_area.left;
   lumaHeight = vidFormat->display_area.bottom - vidFormat->display_area.top;
-  videoDecodeCreateInfo.ulTargetWidth = vidFormat->coded_width;
-  videoDecodeCreateInfo.ulTargetHeight = vidFormat->coded_height;
+  video_decode_create_info.ulTargetWidth = vidFormat->coded_width;
+  video_decode_create_info.ulTargetHeight = vidFormat->coded_height;
   chromaHeight =
       (int)(ceil(lumaHeight * chroma_height_factor(videoOutputFormat)));
   numChromaPlanes = chroma_plane_count(videoOutputFormat);
-  surfaceHeight = videoDecodeCreateInfo.ulTargetHeight;
-  surfaceWidth = videoDecodeCreateInfo.ulTargetWidth;
-  displayRect.bottom = videoDecodeCreateInfo.display_area.bottom;
-  displayRect.top = videoDecodeCreateInfo.display_area.top;
-  displayRect.left = videoDecodeCreateInfo.display_area.left;
-  displayRect.right = videoDecodeCreateInfo.display_area.right;
+  surfaceHeight = video_decode_create_info.ulTargetHeight;
+  surfaceWidth = video_decode_create_info.ulTargetWidth;
+  displayRect.bottom = video_decode_create_info.display_area.bottom;
+  displayRect.top = video_decode_create_info.display_area.top;
+  displayRect.left = video_decode_create_info.display_area.left;
+  displayRect.right = video_decode_create_info.display_area.right;
 
   check_for_cuda_errors(cuCtxPushCurrent(cuContext), __LINE__, __FILE__);
   check_for_cuda_errors(
-      cuvidCreateDecoder(&decoder, &videoDecodeCreateInfo), __LINE__, __FILE__);
+      cuvidCreateDecoder(&decoder, &video_decode_create_info),
+      __LINE__,
+      __FILE__);
   check_for_cuda_errors(cuCtxPopCurrent(NULL), __LINE__, __FILE__);
   return decodeSurface;
 }
@@ -382,21 +383,20 @@ int Decoder::reconfigure_decoder(CUVIDEOFORMAT* vidFormat) {
   }
   videoFormat.coded_width = vidFormat->coded_width;
   videoFormat.coded_height = vidFormat->coded_height;
-  CUVIDRECONFIGUREDECODERINFO reconfigParams = {
-      .ulWidth = vidFormat->coded_width,
-      .ulHeight = vidFormat->coded_height,
-      .ulTargetWidth = surfaceWidth,
-      .ulTargetHeight = surfaceHeight,
-      .ulNumDecodeSurfaces = decodeSurface,
-  };
-  reconfigParams.display_area.bottom = displayRect.bottom;
-  reconfigParams.display_area.top = displayRect.top;
-  reconfigParams.display_area.left = displayRect.left;
-  reconfigParams.display_area.right = displayRect.right;
+  CUVIDRECONFIGUREDECODERINFO reconfig_params = {};
+  reconfig_params.ulWidth = vidFormat->coded_width;
+  reconfig_params.ulHeight = vidFormat->coded_height;
+  reconfig_params.ulTargetWidth = surfaceWidth;
+  reconfig_params.ulTargetHeight = surfaceHeight;
+  reconfig_params.ulNumDecodeSurfaces = decodeSurface;
+  reconfig_params.display_area.bottom = displayRect.bottom;
+  reconfig_params.display_area.top = displayRect.top;
+  reconfig_params.display_area.left = displayRect.left;
+  reconfig_params.display_area.right = displayRect.right;
 
   check_for_cuda_errors(cuCtxPushCurrent(cuContext), __LINE__, __FILE__);
   check_for_cuda_errors(
-      cuvidReconfigureDecoder(decoder, &reconfigParams), __LINE__, __FILE__);
+      cuvidReconfigureDecoder(decoder, &reconfig_params), __LINE__, __FILE__);
   check_for_cuda_errors(cuCtxPopCurrent(NULL), __LINE__, __FILE__);
 
   return decodeSurface;
