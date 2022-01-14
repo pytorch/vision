6c49ef8551426c16d215257a097266eb56dc1235
Vasilis Vryniotis <vvryniotis@fb.com>
2022-01-14 20:51:43 +0000

    Completed implementation.

--- torchvision/models/convnext.py
+++ torchvision/models/convnext.py
@@ -1,19 +1,36 @@
-import torch
-
 from functools import partial
-from torch import nn, Tensor
 from typing import Any, Callable, List, Optional, Sequence
+
+import torch
+from torch import nn, Tensor
 
 from ..ops.misc import ConvNormActivation
 from ..ops.stochastic_depth import StochasticDepth
 from ..utils import _log_api_usage_once
 
 
+__all__ = [
+    "ConvNeXt",
+    "convnext_tiny",
+]
+
+
+class LayerNorm(nn.LayerNorm):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self.channels_last = kwargs.pop("channels_last", False)
+        super().__init__(*args, **kwargs)
+
+    def forward(self, x):
+        if not self.channels_last:
+            x = x.permute(0, 2, 3, 1)
+        x = super().forward(x)
+        if not self.channels_last:
+            x = x.permute(0, 3, 1, 2)
+        return x
+
+
 class CNBlock(nn.Module):
-    def __init__(self, dim,
-                 stochastic_depth_prob: float,
-                 norm_layer: Callable[..., nn.Module],
-                 layer_scale: Optional[float] = 1e-6):
+    def __init__(self, dim, layer_scale: float, stochastic_depth_prob: float, norm_layer: Callable[..., nn.Module]):
         super().__init__()
         self.block = nn.Sequential(
             ConvNormActivation(
@@ -25,22 +42,16 @@ class CNBlock(nn.Module):
                 activation_layer=None,
                 bias=True,  # TODO: check
             ),
-            ConvNormActivation(
-                dim,
-                4 * dim,
-                kernel_size=1,
-                norm_layer=None,
-                activation_layer=nn.GELU,
-            ),
+            ConvNormActivation(dim, 4 * dim, kernel_size=1, norm_layer=None, activation_layer=nn.GELU, inplace=None),
             ConvNormActivation(
                 4 * dim,
                 dim,
                 kernel_size=1,
                 norm_layer=None,
                 activation_layer=None,
-            )
+            ),
         )
-        self.layer_scale = nn.Parameter(torch.ones(dim) * layer_scale)
+        self.layer_scale = nn.Parameter(torch.ones(dim, 1, 1) * layer_scale)
         self.stochastic_depth = StochasticDepth(stochastic_depth_prob, "row")
 
     def forward(self, input: Tensor) -> Tensor:
@@ -55,7 +66,7 @@ class CNBlockConfig:
     def __init__(
         self,
         input_channels: int,
-        out_channels: int,
+        out_channels: Optional[int],
         num_layers: int,
     ) -> None:
         self.input_channels = input_channels
@@ -69,7 +80,6 @@ class CNBlockConfig:
         s += ", num_layers={num_layers}"
         s += ")"
         return s.format(**self.__dict__)
-
 
 
 class ConvNeXt(nn.Module):
@@ -88,18 +98,82 @@ class ConvNeXt(nn.Module):
 
         if not block_setting:
             raise ValueError("The block_setting should not be empty")
-        elif not (
-            isinstance(block_setting, Sequence)
-            and all([isinstance(s, CNBlockConfig) for s in block_setting])
-        ):
+        elif not (isinstance(block_setting, Sequence) and all([isinstance(s, CNBlockConfig) for s in block_setting])):
             raise TypeError("The block_setting should be List[CNBlockConfig]")
 
         if block is None:
             block = CNBlock
 
         if norm_layer is None:
-            norm_layer = partial(nn.LayerNorm, eps=1e-6)
+            norm_layer = partial(LayerNorm, eps=1e-6)
 
-        layers: List[nn.Module] = [
+        layers: List[nn.Module] = []
 
-        ]
+        firstconv_output_channels = block_setting[0].input_channels
+        layers.append(
+            ConvNormActivation(
+                3,
+                firstconv_output_channels,
+                kernel_size=4,
+                stride=4,
+                padding=0,
+                norm_layer=norm_layer,
+                activation_layer=None,
+                bias=True,
+            )
+        )
+
+        total_stage_blocks = sum(cnf.num_layers for cnf in block_setting)
+        stage_block_id = 0
+        for cnf in block_setting:
+            stage: List[nn.Module] = []
+            for _ in range(cnf.num_layers):
+                # adjust stochastic depth probability based on the depth of the stage block
+                sd_prob = stochastic_depth_prob * float(stage_block_id) / total_stage_blocks
+                stage.append(block(cnf.input_channels, layer_scale, sd_prob, norm_layer))
+                stage_block_id += 1
+            layers.append(nn.Sequential(*stage))
+            if cnf.out_channels is not None:
+                layers.append(
+                    nn.Sequential(
+                        norm_layer(cnf.input_channels),
+                        nn.Conv2d(cnf.input_channels, cnf.out_channels, kernel_size=2, stride=2),
+                    )
+                )
+
+        self.features = nn.Sequential(*layers)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+
+        lastblock = block_setting[-1]
+        lastconv_output_channels = (
+            lastblock.out_channels if lastblock.out_channels is not None else lastblock.input_channels
+        )
+        self.classifier = nn.Sequential(
+            norm_layer(lastconv_output_channels), nn.Flatten(1), nn.Linear(lastconv_output_channels, num_classes)
+        )
+
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d, nn.Linear)):
+                nn.init.trunc_normal_(m.weight, std=0.02)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+
+    def _forward_impl(self, x: Tensor) -> Tensor:
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = self.classifier(x)
+        return x
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self._forward_impl(x)
+
+
+def convnext_tiny(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> ConvNeXt:
+    block_setting = [
+        CNBlockConfig(96, 192, 3),
+        CNBlockConfig(192, 384, 3),
+        CNBlockConfig(384, 768, 9),
+        CNBlockConfig(768, None, 3),
+    ]
+    model = ConvNeXt(block_setting, **kwargs)
+    return model
