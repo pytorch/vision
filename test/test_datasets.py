49468279d9070a5631b6e0198ee562c00ecedb10
Nicolas Hug <contact@nicolas-hug.com>
2022-01-17 16:28:03 +0000

    Add support for PCAM dataset (#5203)
    * Add support for PCAM dataset
    
    * mypy
    
    * Apply suggestions from code review
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>
    
    * Remove classes and class_to_idx attributes
    
    * Use _decompress
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>

--- test/test_datasets.py
+++ test/test_datasets.py
@@ -2577,5 +2577,28 @@ class Flowers102TestCase(datasets_utils.ImageDatasetTestCase):
         return num_images_per_split[config["split"]]
 
 
+class PCAMTestCase(datasets_utils.ImageDatasetTestCase):
+    DATASET_CLASS = datasets.PCAM
+
+    ADDITIONAL_CONFIGS = datasets_utils.combinations_grid(split=("train", "val", "test"))
+    REQUIRED_PACKAGES = ("h5py",)
+
+    def inject_fake_data(self, tmpdir: str, config):
+        base_folder = pathlib.Path(tmpdir) / "pcam"
+        base_folder.mkdir()
+
+        num_images = {"train": 2, "test": 3, "val": 4}[config["split"]]
+
+        images_file = datasets.PCAM._FILES[config["split"]]["images"][0]
+        with datasets_utils.lazy_importer.h5py.File(str(base_folder / images_file), "w") as f:
+            f["x"] = np.random.randint(0, 256, size=(num_images, 10, 10, 3), dtype=np.uint8)
+
+        targets_file = datasets.PCAM._FILES[config["split"]]["targets"][0]
+        with datasets_utils.lazy_importer.h5py.File(str(base_folder / targets_file), "w") as f:
+            f["y"] = np.random.randint(0, 2, size=(num_images, 1, 1, 1), dtype=np.uint8)
+
+        return num_images
+
+
 if __name__ == "__main__":
     unittest.main()
