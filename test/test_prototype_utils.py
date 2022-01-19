c9d82cfb87ef11314e87d22a5bd0f8a5248cb5d9
Philip Meier <github.pmeier@posteo.de>
2022-01-19 20:30:50 +0100

    remove manual ids

--- test/test_prototype_utils.py
+++ test/test_prototype_utils.py
@@ -5,12 +5,12 @@ from torchvision.prototype.utils._internal import sequence_to_str
 @pytest.mark.parametrize(
     ("seq", "separate_last", "expected"),
     [
-        pytest.param([], "", "", id="empty"),
-        pytest.param(["foo"], "", "'foo'", id="single"),
-        pytest.param(["foo", "bar"], "", "'foo', 'bar'", id="double"),
-        pytest.param(["foo", "bar"], "and ", "'foo' and 'bar'", id="double-separate_last"),
-        pytest.param(["foo", "bar", "baz"], "", "'foo', 'bar', 'baz'", id="multi"),
-        pytest.param(["foo", "bar", "baz"], "and ", "'foo', 'bar', and 'baz'", id="multi-separate_last"),
+        ([], "", ""),
+        (["foo"], "", "'foo'"),
+        (["foo", "bar"], "", "'foo', 'bar'"),
+        (["foo", "bar"], "and ", "'foo' and 'bar'"),
+        (["foo", "bar", "baz"], "", "'foo', 'bar', 'baz'"),
+        (["foo", "bar", "baz"], "and ", "'foo', 'bar', and 'baz'"),
     ],
 )
 def test_sequence_to_str(seq, separate_last, expected):
