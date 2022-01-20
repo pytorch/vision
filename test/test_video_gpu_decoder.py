88295fcc4e993ed6236145546499b52374d45ef3
Prabhat Roy <prabhatroy@fb.com>
2022-01-20 12:26:03 -0800

    Refine unit test

--- test/test_video_gpu_decoder.py
+++ test/test_video_gpu_decoder.py
@@ -21,14 +21,6 @@ test_videos = [
     "WUzgd7C1pWA.mp4",
 ]
 
-test_videos_dict = {
-    "v_SoccerJuggling_g23_c01.avi": 8.0,
-    "v_SoccerJuggling_g24_c01.avi": 8.0,
-    "R6llTwEh07w.mp4": 10.0,
-    "SOX5yA1l24A.mp4": 11.0,
-    "WUzgd7C1pWA.mp4": 11.0,
-}
-
 
 @pytest.mark.skipif(_HAS_VIDEO_DECODER is False, reason="Didn't compile with support for gpu decoder")
 class TestVideoGPUDecoder:
@@ -46,19 +38,30 @@ class TestVideoGPUDecoder:
 
     @pytest.mark.skipif(av is None, reason="PyAV unavailable")
     @pytest.mark.parametrize("keyframes", [True, False])
-    def test_seek_reading(self, keyframes):
-        for test_video, time in test_videos_dict.items():
-            full_path = os.path.join(VIDEO_DIR, test_video)
-            decoder = VideoReader(full_path, device="cuda:0")
-            time = time / 2
-            decoder.seek(time, keyframes_only=keyframes)
-            with av.open(full_path) as container:
-                container.seek(int(time * 1000000), any_frame=not keyframes, backward=False)
-                for av_frame in container.decode(container.streams.video[0]):
-                    av_frames = torch.tensor(av_frame.to_rgb(src_colorspace="ITU709").to_ndarray())
-                    vision_frames = next(decoder)["data"]
-                    mean_delta = torch.mean(torch.abs(av_frames.float() - vision_frames.cpu().float()))
-                    assert mean_delta < 0.75
+    @pytest.mark.parametrize(
+        "full_path, duration",
+        [
+            (os.path.join(VIDEO_DIR, x), y)
+            for x, y in [
+                ("v_SoccerJuggling_g23_c01.avi", 8.0),
+                ("v_SoccerJuggling_g24_c01.avi", 8.0),
+                ("R6llTwEh07w.mp4", 10.0),
+                ("SOX5yA1l24A.mp4", 11.0),
+                ("WUzgd7C1pWA.mp4", 11.0),
+            ]
+        ],
+    )
+    def test_seek_reading(self, keyframes, full_path, duration):
+        decoder = VideoReader(full_path, device="cuda:0")
+        time = duration / 2
+        decoder.seek(time, keyframes_only=keyframes)
+        with av.open(full_path) as container:
+            container.seek(int(time * 1000000), any_frame=not keyframes, backward=False)
+            for av_frame in container.decode(container.streams.video[0]):
+                av_frames = torch.tensor(av_frame.to_rgb(src_colorspace="ITU709").to_ndarray())
+                vision_frames = next(decoder)["data"]
+                mean_delta = torch.mean(torch.abs(av_frames.float() - vision_frames.cpu().float()))
+                assert mean_delta < 0.75
 
 
 if __name__ == "__main__":
