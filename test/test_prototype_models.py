7b9845f84287d7ca7ced840f2f4c278592569d69
Yoshitomo Matsubara <yoshitom@uci.edu>
2022-01-22 14:53:09 -0800

    update test

--- test/test_prototype_models.py
+++ test/test_prototype_models.py
@@ -83,7 +83,7 @@ def test_naming_conventions(model_fn):
     weights_enum = _get_model_weights(model_fn)
     print(weights_enum)
     assert weights_enum is not None
-    assert len(weights_enum) == 0 or hasattr(weights_enum, "default")
+    assert len(weights_enum) == 0 or hasattr(weights_enum, "DEFAULT")
 
 
 @pytest.mark.parametrize(
