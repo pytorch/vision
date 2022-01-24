ebc36896bfa2baddfdf40d7fea58677eb16be1b4
Abhijit Deo <72816663+abhi-glitchhg@users.noreply.github.com>
2022-01-24 14:58:57 +0530

    added another test

--- test/test_backbone_utils.py
+++ test/test_backbone_utils.py
@@ -25,6 +25,8 @@ def test_resnet_fpn_backbone(backbone_name):
 
     with pytest.raises(ValueError):
         y = resnet_fpn_backbone(backbone_name=backbone_name, pretrained=False, trainable_layers=6)
+    with pytest.raises(ValueError):
+        y = resnet_fpn_backbone(backbone_name, False, returned_layers=[0, 1, 2, 3])
 
 
 # Needed by TestFxFeatureExtraction.test_leaf_module_and_function
