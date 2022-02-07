3a37185b1f13d54529994c010bbc063a7f397891
Rishav Mukherji <72412977+Amapocho@users.noreply.github.com>
2022-02-07 17:45:40 +0530

    update

--- torchvision/prototype/datasets/_builtin/places365.py
+++ torchvision/prototype/datasets/_builtin/places365.py
@@ -46,35 +46,7 @@ class Places365Standard(Dataset):
          )
         return [train_images, val_images, test_images, file_list]
     
-    def _make_datapipe(
-        self,
-        resource_dps: List[IterDataPipe],
-        *,
-        config: DatasetConfig,
-        decoder: Optional[Callable[[io.IOBase], torch.Tensor]],
-    ) -> IterDataPipe[Dict[str, Any]]:
-        train_images_dp, val_images_dp, test_images_dp,file_list_dp= resource_dps
-
-        #images_dp = Filter(images_dp, self._is_not_background_image)
-        train_images_dp = hint_sharding(train_images_dp)
-        train_images_dp = hint_shuffling(train_images_dp)
-        val_images_dp = hint_sharding(val_images_dp)
-        val_images_dp = hint_shuffling(val_images_dp)
-        test_images_dp = hint_sharding(test_images_dp)
-        test_images_dp = hint_shuffling(test_images_dp)
-        
-        #anns_dp = Filter(anns_dp, self._is_ann)
-
-        dp = IterKeyZipper(
-            train_images_dp,
-            val_images_dp,
-            test_images_dp,
-            #key_fn=self._images_key_fn,
-            #ref_key_fn=self._anns_key_fn,
-            buffer_size=INFINITE_BUFFER_SIZE,
-            keep_key=True,
-        )
-        return resource_dps[0]#Mapper(dp,functools.partial(self._collate_and_decode_sample, decoder=decoder))
+    
 
 class Places365Challenge(Dataset):
     def _make_info(self) -> DatasetInfo:
@@ -101,34 +73,4 @@ class Places365Challenge(Dataset):
             "http://data.csail.mit.edu/places/places365/filelist_places365-challenge.tar",
             sha256="",
          )
-        return [train_images, val_images, test_images, file_list]
-    
-    def _make_datapipe(
-        self,
-        resource_dps: List[IterDataPipe],
-        *,
-        config: DatasetConfig,
-        decoder: Optional[Callable[[io.IOBase], torch.Tensor]],
-    ) -> IterDataPipe[Dict[str, Any]]:
-        train_images_dp, val_images_dp, test_images_dp,file_list_dp= resource_dps
-
-        #images_dp = Filter(images_dp, self._is_not_background_image)
-        train_images_dp = hint_sharding(train_images_dp)
-        train_images_dp = hint_shuffling(train_images_dp)
-        val_images_dp = hint_sharding(val_images_dp)
-        val_images_dp = hint_shuffling(val_images_dp)
-        test_images_dp = hint_sharding(test_images_dp)
-        test_images_dp = hint_shuffling(test_images_dp)
-
-        #anns_dp = Filter(anns_dp, self._is_ann)
-
-        dp = IterKeyZipper(
-            train_images_dp,
-            val_images_dp,
-            test_images_dp,
-            #key_fn=self._images_key_fn,
-            #ref_key_fn=self._anns_key_fn,
-            buffer_size=INFINITE_BUFFER_SIZE,
-            keep_key=True,
-        )
-        return resource_dps[0]#Mapper(dp, functools.partial(self._collate_and_decode_sample, decoder=decoder))
+        return [train_images, val_images, test_images, file_list] 
