adad1df5295556aad2a9e495ce71db980e8e47d1
Philip Meier <github.pmeier@posteo.de>
2022-02-07 11:23:26 +0100

    fix prototype features and functional transforms after review (#5377)
    * fix prototype functional transforms after review
    
    * address features review

--- torchvision/prototype/transforms/__init__.py
+++ torchvision/prototype/transforms/__init__.py
@@ -1,8 +1,4 @@
 from . import functional
 from .functional import InterpolationMode  # usort: skip
 
-from ._transform import Transform
-from ._container import Compose, RandomApply, RandomChoice, RandomOrder  # usort: skip
-from ._geometry import Resize, RandomResize, HorizontalFlip, Crop, CenterCrop, RandomCrop
-from ._misc import Identity, Normalize
 from ._presets import CocoEval, ImageNetEval, VocEval, Kinect400Eval, RaftEval
