60fe673e231595fa0eaea13c7f76bf723c5eb4c7
Joao Gomes <jdsgomes@fb.com>
2022-02-09 10:16:55 +0000

    Clean prototype code and configs

--- torchvision/prototype/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import datasets
-from . import features
-from . import models
-from . import transforms
-from . import utils
--- torchvision/prototype/datasets/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import _internal
-from ._dataset import DatasetType, DatasetConfig, DatasetInfo, Dataset
-from ._query import SampleQuery
-from ._resource import OnlineResource, HttpResource, GDriveResource, ManualDownloadResource, KaggleDownloadResource
