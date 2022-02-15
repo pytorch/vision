4ba0fd0627a0d2cf03cfaf5890d29e9b8d624629
Philip Meier <github.pmeier@posteo.de>
2022-02-15 11:22:09 +0100

    remove legacy classes
    Conflicts:
    	torchvision/prototype/transforms/_augment.py
    	torchvision/prototype/transforms/_auto_augment.py
    	torchvision/prototype/transforms/_geometry.py

--- torchvision/prototype/transforms/_augment.py
+++ torchvision/prototype/transforms/_augment.py
@@ -1,8 +1,9 @@
 import math
+import numbers
+import warnings
 from typing import Any, Dict, Tuple
 
 import torch
-from torchvision import transforms as _transforms
 from torchvision.prototype.transforms import Transform
 
 from . import functional as F
@@ -10,7 +11,6 @@ from .utils import Query
 
 
 class RandomErasing(Transform):
-    _LEGACY_TRANSFORM_CLS = _transforms.RandomErasing
     _DISPATCHER = F.erase
 
     def __init__(
@@ -21,15 +21,29 @@ class RandomErasing(Transform):
         value: float = 0,
     ):
         super().__init__()
-        legacy_transform = self._LEGACY_TRANSFORM_CLS(p=p, scale=scale, ratio=ratio, value=value, inplace=False)
+        if not isinstance(value, (numbers.Number, str, tuple, list)):
+            raise TypeError("Argument value should be either a number or str or a sequence")
+        if isinstance(value, str) and value != "random":
+            raise ValueError("If value is str, it should be 'random'")
+        if not isinstance(scale, (tuple, list)):
+            raise TypeError("Scale should be a sequence")
+        if not isinstance(ratio, (tuple, list)):
+            raise TypeError("Ratio should be a sequence")
+        if (scale[0] > scale[1]) or (ratio[0] > ratio[1]):
+            warnings.warn("Scale and ratio should be of kind (min, max)")
+        if scale[0] < 0 or scale[1] > 1:
+            raise ValueError("Scale should be between 0 and 1")
+        if p < 0 or p > 1:
+            raise ValueError("Random erasing probability should be between 0 and 1")
         # TODO: deprecate p in favor of wrapping the transform in a RandomApply
-        self.p = legacy_transform.p
-        self.scale = legacy_transform.scale
-        self.ratio = legacy_transform.ratio
-        self.value = legacy_transform.value
+        self.p = p
+        self.scale = scale
+        self.ratio = ratio
+        self.value = value
 
     def get_params(self, sample: Any) -> Dict[str, Any]:
-        image = Query(sample).image_for_size_and_channels_extraction()
+        image = Query(sample).image()
+        img_c, (img_h, img_w) = image.num_channels, image.image_size
 
         if isinstance(self.value, (int, float)):
             value = [self.value]
@@ -40,15 +54,41 @@ class RandomErasing(Transform):
         else:
             value = self.value
 
-        if value is not None and not (len(value) in (1, image.shape[-3])):
+        if value is not None and not (len(value) in (1, img_c)):
             raise ValueError(
                 "If value is a sequence, it should have either a single value or "
                 f"{image.shape[-3]} (number of input channels)"
             )
 
-        return dict(
-            zip("ijhwv", self._LEGACY_TRANSFORM_CLS.get_params(image, scale=self.scale, ratio=self.ratio, value=value))
-        )
+        area = img_h * img_w
+
+        log_ratio = torch.log(torch.tensor(self.ratio))
+        for _ in range(10):
+            erase_area = area * torch.empty(1).uniform_(self.scale[0], self.scale[1]).item()
+            aspect_ratio = torch.exp(
+                torch.empty(1).uniform_(
+                    log_ratio[0],  # type: ignore[arg-type]
+                    log_ratio[1],  # type: ignore[arg-type]
+                )
+            ).item()
+
+            h = int(round(math.sqrt(erase_area * aspect_ratio)))
+            w = int(round(math.sqrt(erase_area / aspect_ratio)))
+            if not (h < img_h and w < img_w):
+                continue
+
+            if value is None:
+                v = torch.empty([img_c, h, w], dtype=torch.float32).normal_()
+            else:
+                v = torch.tensor(value)[:, None, None]
+
+            i = torch.randint(0, img_h - h + 1, size=(1,)).item()
+            j = torch.randint(0, img_w - w + 1, size=(1,)).item()
+            break
+        else:
+            i, j, h, w, v = 0, 0, img_h, img_w, image
+
+        return dict(zip("ijhwv", (i, j, h, w, v)))
 
     def _transform(self, input: Any, params: Dict[str, Any]) -> Any:
         if torch.rand(1) >= self.p:
--- torchvision/prototype/transforms/_geometry.py
+++ torchvision/prototype/transforms/_geometry.py
@@ -1,7 +1,10 @@
-from typing import Any, Dict, List, Union, Sequence, Tuple
+import math
+import warnings
+from typing import Any, Dict, List, Union, Sequence, Tuple, cast
 
-from torchvision import transforms as _transforms
+import torch
 from torchvision.prototype.transforms import Transform, InterpolationMode
+from torchvision.transforms.transforms import _setup_size, _interpolation_modes_from_int
 
 from . import functional as F
 from .utils import Query
@@ -45,7 +48,6 @@ class CenterCrop(Transform):
 
 
 class RandomResizedCrop(Transform):
-    _LEGACY_CLS = _transforms.RandomResizedCrop
     _DISPATCHER = F.resized_crop
 
     def __init__(
@@ -56,20 +58,68 @@ class RandomResizedCrop(Transform):
         interpolation: InterpolationMode = InterpolationMode.BILINEAR,
     ) -> None:
         super().__init__()
-        legacy_transform = self._LEGACY_CLS(size=size, scale=scale, ratio=ratio, interpolation=interpolation)
-        self.size = legacy_transform.size
-        self.scale = legacy_transform.scale
-        self.ratio = legacy_transform.ratio
-        self.interpolation = legacy_transform.interpolation
+        self.size = _setup_size(size, error_msg="Please provide only two dimensions (h, w) for size.")
+
+        if not isinstance(scale, Sequence):
+            raise TypeError("Scale should be a sequence")
+        scale = cast(Tuple[float, float], scale)
+        if not isinstance(ratio, Sequence):
+            raise TypeError("Ratio should be a sequence")
+        ratio = cast(Tuple[float, float], ratio)
+        if (scale[0] > scale[1]) or (ratio[0] > ratio[1]):
+            warnings.warn("Scale and ratio should be of kind (min, max)")
+
+        # Backward compatibility with integer value
+        if isinstance(interpolation, int):
+            warnings.warn(
+                "Argument interpolation should be of type InterpolationMode instead of int. "
+                "Please, use InterpolationMode enum."
+            )
+            interpolation = _interpolation_modes_from_int(interpolation)
+
+        self.size = size
+        self.scale = scale
+        self.ratio = ratio
+        self.interpolation = interpolation
 
     def get_params(self, sample: Any) -> Dict[str, Any]:
-        image = Query(sample).image_for_size_extraction()
-        top, left, height, width = self._LEGACY_CLS.get_params(image, scale=list(self.scale), ratio=list(self.ratio))
-        return dict(
-            top=top,
-            left=left,
-            height=height,
-            width=width,
-            size=self.size,
-            interpolation=self.interpolation,
-        )
+        image = Query(sample).image()
+        height, width = image.image_size
+        area = height * width
+
+        log_ratio = torch.log(torch.tensor(self.ratio))
+        for _ in range(10):
+            target_area = area * torch.empty(1).uniform_(self.scale[0], self.scale[1]).item()
+            aspect_ratio = torch.exp(
+                torch.empty(1).uniform_(
+                    log_ratio[0],  # type: ignore[arg-type]
+                    log_ratio[1],  # type: ignore[arg-type]
+                )
+            ).item()
+
+            w = int(round(math.sqrt(target_area * aspect_ratio)))
+            h = int(round(math.sqrt(target_area / aspect_ratio)))
+
+            if 0 < w <= width and 0 < h <= height:
+                i = torch.randint(0, height - h + 1, size=(1,)).item()
+                j = torch.randint(0, width - w + 1, size=(1,)).item()
+                break
+        else:
+            # Fallback to central crop
+            in_ratio = float(width) / float(height)
+            if in_ratio < min(self.ratio):
+                w = width
+                h = int(round(w / min(self.ratio)))
+            elif in_ratio > max(self.ratio):
+                h = height
+                w = int(round(h * max(self.ratio)))
+            else:  # whole image
+                w = width
+                h = height
+            i = (height - h) // 2
+            j = (width - w) // 2
+
+        return dict(top=i, left=j, height=h, width=w)
+
+    def extra_repr(self) -> str:
+        return self._extra_repr_from_attrs("size", "scale", "ratio", "interpolation")
