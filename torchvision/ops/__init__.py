2f7a10dff58b9d2e12a6eb0c0c96ee8cdc67bba2
Hu Ye <xiaohuzc@gmail.com>
2022-02-20 11:18:01 +0800

    add dropblock

--- torchvision/ops/__init__.py
+++ torchvision/ops/__init__.py
@@ -11,6 +11,7 @@ from .boxes import (
 )
 from .boxes import box_convert
 from .deform_conv import deform_conv2d, DeformConv2d
+from .drop_block import drop_block2d, DropBlock2d
 from .feature_pyramid_network import FeaturePyramidNetwork
 from .focal_loss import sigmoid_focal_loss
 from .giou_loss import generalized_box_iou_loss
@@ -21,7 +22,6 @@ from .ps_roi_pool import ps_roi_pool, PSRoIPool
 from .roi_align import roi_align, RoIAlign
 from .roi_pool import roi_pool, RoIPool
 from .stochastic_depth import stochastic_depth, StochasticDepth
-from .drop_block import drop_block2d, DropBlock2d
 
 _register_custom_op()
 
