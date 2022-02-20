09f13965a33f944306b45dde2a2d2d9544a2b9ae
Hu Ye <xiaohuzc@gmail.com>
2022-02-20 11:07:11 +0800

    add dropblock

--- torchvision/ops/__init__.py
+++ torchvision/ops/__init__.py
@@ -21,6 +21,7 @@ from .ps_roi_pool import ps_roi_pool, PSRoIPool
 from .roi_align import roi_align, RoIAlign
 from .roi_pool import roi_pool, RoIPool
 from .stochastic_depth import stochastic_depth, StochasticDepth
+from .drop_block import drop_block2d, DropBlock2d
 
 _register_custom_op()
 
