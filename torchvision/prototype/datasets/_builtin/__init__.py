a6b9ca6c2592ca87c9b4865a19998d4103540f59
Dbhasin1 <drishti_b@me.iitr.c.in>
2022-02-22 17:26:32 +0000

    revert formatting

--- torchvision/prototype/datasets/_builtin/__init__.py
+++ torchvision/prototype/datasets/_builtin/__init__.py
@@ -5,11 +5,10 @@ from .clevr import CLEVR
 from .coco import Coco
 from .cub200 import CUB200
 from .dtd import DTD
-from .eurosat import EuroSAT
 from .fer2013 import FER2013
 from .gtsrb import GTSRB
 from .imagenet import ImageNet
-from .mnist import EMNIST, FashionMNIST, KMNIST, MNIST, QMNIST
+from .mnist import MNIST, FashionMNIST, KMNIST, EMNIST, QMNIST
 from .oxford_iiit_pet import OxfordIITPet
 from .pcam import PCAM
 from .sbd import SBD
