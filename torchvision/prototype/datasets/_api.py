a6af500196de20ad7b1f4abbb7185294399ac28f
Philip Meier <github.pmeier@posteo.de>
2022-02-24 17:37:01 +0100

    revert unrelated change

--- torchvision/prototype/datasets/_api.py
+++ torchvision/prototype/datasets/_api.py
@@ -49,7 +49,7 @@ def find(dct: Dict[str, T], name: str) -> T:
                     "You can use torchvision.datasets.list_datasets() to get a list of all available datasets."
                 ),
             )
-        ) from None
+        ) from error
 
 
 def info(name: str) -> Dict[str, Any]:
