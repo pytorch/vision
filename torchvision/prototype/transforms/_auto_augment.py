e1a356994053be9ed27916e0de31e907bd473070
Philip Meier <github.pmeier@posteo.de>
2022-02-28 11:56:40 +0100

    add prototype AugMix transform

--- torchvision/prototype/transforms/_auto_augment.py
+++ torchvision/prototype/transforms/_auto_augment.py
@@ -1,13 +1,15 @@
+import functools
 import math
-from typing import Any, Dict, Tuple, Optional, Callable, List, cast, TypeVar
+from typing import Any, Dict, Tuple, Optional, Callable, List, cast, TypeVar, Union
 
 import PIL.Image
 import torch
 from torchvision.prototype import features
 from torchvision.prototype.transforms import Transform, InterpolationMode, AutoAugmentPolicy, functional as F
 from torchvision.prototype.utils._internal import apply_recursively
+from torchvision.transforms.functional import pil_to_tensor, to_pil_image
 
-from ._utils import query_image
+from ._utils import query_images
 
 K = TypeVar("K")
 V = TypeVar("V")
@@ -26,27 +28,16 @@ class _AutoAugmentBase(Transform):
         key = keys[int(torch.randint(len(keys), ()))]
         return key, dct[key]
 
-    def _apply_transform(self, sample: Any, transform_id: str, magnitude: float) -> Any:
-        def dispatch(
-            image_tensor_kernel: Callable,
-            image_pil_kernel: Callable,
-            input: Any,
-            *args: Any,
-            **kwargs: Any,
-        ) -> Any:
-            if isinstance(input, (features.BoundingBox, features.SegmentationMask)):
-                raise TypeError(f"{type(input).__name__}'s are not supported by {type(self).__name__}()")
-            elif isinstance(input, features.Image):
-                output = image_tensor_kernel(input, *args, **kwargs)
-                return features.Image.new_like(input, output)
-            elif isinstance(input, torch.Tensor):
-                return image_tensor_kernel(input, *args, **kwargs)
-            elif isinstance(input, PIL.Image.Image):
-                return image_pil_kernel(input, *args, **kwargs)
-            else:
-                return input
+    def _query_image(self, sample: Any) -> Union[PIL.Image.Image, torch.Tensor, features.Image]:
+        images = list(query_images(sample))
+        if len(images) > 1:
+            raise TypeError(
+                f"Auto augment transformations are only properly defined for a single image, but found {len(images)}."
+            )
+        return images[0]
 
-        image = query_image(sample)
+    def _parse_fill(self, sample: Any) -> Optional[List[float]]:
+        image = self._query_image(sample)
         num_channels = F.get_image_num_channels(image)
 
         fill = self.fill
@@ -54,106 +45,137 @@ class _AutoAugmentBase(Transform):
             fill = [float(fill)] * num_channels
         elif fill is not None:
             fill = [float(f) for f in fill]
+        return fill
 
-        interpolation = self.interpolation
+    def _dispatch(
+        self,
+        image_tensor_kernel: Callable,
+        image_pil_kernel: Callable,
+        input: Any,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Any:
+        if isinstance(input, (features.BoundingBox, features.SegmentationMask)):
+            raise TypeError(f"{type(input).__name__}'s are not supported by {type(self).__name__}()")
+        elif isinstance(input, features.Image):
+            output = image_tensor_kernel(input, *args, **kwargs)
+            return features.Image.new_like(input, output)
+        elif isinstance(input, torch.Tensor):
+            return image_tensor_kernel(input, *args, **kwargs)
+        elif isinstance(input, PIL.Image.Image):
+            return image_pil_kernel(input, *args, **kwargs)
+        else:
+            return input
 
-        def transform(input: Any) -> Any:
-            if type(input) in {features.BoundingBox, features.SegmentationMask}:
-                raise TypeError(f"{type(input)} is not supported by {type(self).__name__}()")
-            elif not (type(input) in {features.Image, torch.Tensor} or isinstance(input, PIL.Image.Image)):
-                return input
+    def _apply_transform_to_item(
+        self,
+        item: Any,
+        transform_id: str,
+        magnitude: float,
+        interpolation: InterpolationMode,
+        fill: Optional[List[float]],
+    ) -> Any:
+        if transform_id == "Identity":
+            return item
+        elif transform_id == "ShearX":
+            return self._dispatch(
+                F.affine_image_tensor,
+                F.affine_image_pil,
+                item,
+                angle=0.0,
+                translate=[0, 0],
+                scale=1.0,
+                shear=[math.degrees(magnitude), 0.0],
+                interpolation=interpolation,
+                fill=fill,
+            )
+        elif transform_id == "ShearY":
+            return self._dispatch(
+                F.affine_image_tensor,
+                F.affine_image_pil,
+                item,
+                angle=0.0,
+                translate=[0, 0],
+                scale=1.0,
+                shear=[0.0, math.degrees(magnitude)],
+                interpolation=interpolation,
+                fill=fill,
+            )
+        elif transform_id == "TranslateX":
+            return self._dispatch(
+                F.affine_image_tensor,
+                F.affine_image_pil,
+                item,
+                angle=0.0,
+                translate=[int(magnitude), 0],
+                scale=1.0,
+                shear=[0.0, 0.0],
+                interpolation=interpolation,
+                fill=fill,
+            )
+        elif transform_id == "TranslateY":
+            return self._dispatch(
+                F.affine_image_tensor,
+                F.affine_image_pil,
+                item,
+                angle=0.0,
+                translate=[0, int(magnitude)],
+                scale=1.0,
+                shear=[0.0, 0.0],
+                interpolation=interpolation,
+                fill=fill,
+            )
+        elif transform_id == "Rotate":
+            return self._dispatch(F.rotate_image_tensor, F.rotate_image_pil, item, angle=magnitude)
+        elif transform_id == "Brightness":
+            return self._dispatch(
+                F.adjust_brightness_image_tensor,
+                F.adjust_brightness_image_pil,
+                item,
+                brightness_factor=1.0 + magnitude,
+            )
+        elif transform_id == "Color":
+            return self._dispatch(
+                F.adjust_saturation_image_tensor,
+                F.adjust_saturation_image_pil,
+                item,
+                saturation_factor=1.0 + magnitude,
+            )
+        elif transform_id == "Contrast":
+            return self._dispatch(
+                F.adjust_contrast_image_tensor, F.adjust_contrast_image_pil, item, contrast_factor=1.0 + magnitude
+            )
+        elif transform_id == "Sharpness":
+            return self._dispatch(
+                F.adjust_sharpness_image_tensor,
+                F.adjust_sharpness_image_pil,
+                item,
+                sharpness_factor=1.0 + magnitude,
+            )
+        elif transform_id == "Posterize":
+            return self._dispatch(F.posterize_image_tensor, F.posterize_image_pil, item, bits=int(magnitude))
+        elif transform_id == "Solarize":
+            return self._dispatch(F.solarize_image_tensor, F.solarize_image_pil, item, threshold=magnitude)
+        elif transform_id == "AutoContrast":
+            return self._dispatch(F.autocontrast_image_tensor, F.autocontrast_image_pil, item)
+        elif transform_id == "Equalize":
+            return self._dispatch(F.equalize_image_tensor, F.equalize_image_pil, item)
+        elif transform_id == "Invert":
+            return self._dispatch(F.invert_image_tensor, F.invert_image_pil, item)
+        else:
+            raise ValueError(f"No transform available for {transform_id}")
 
-            if transform_id == "Identity":
-                return input
-            elif transform_id == "ShearX":
-                return dispatch(
-                    F.affine_image_tensor,
-                    F.affine_image_pil,
-                    input,
-                    angle=0.0,
-                    translate=[0, 0],
-                    scale=1.0,
-                    shear=[math.degrees(magnitude), 0.0],
-                    interpolation=interpolation,
-                    fill=fill,
-                )
-            elif transform_id == "ShearY":
-                return dispatch(
-                    F.affine_image_tensor,
-                    F.affine_image_pil,
-                    input,
-                    angle=0.0,
-                    translate=[0, 0],
-                    scale=1.0,
-                    shear=[0.0, math.degrees(magnitude)],
-                    interpolation=interpolation,
-                    fill=fill,
-                )
-            elif transform_id == "TranslateX":
-                return dispatch(
-                    F.affine_image_tensor,
-                    F.affine_image_pil,
-                    input,
-                    angle=0.0,
-                    translate=[int(magnitude), 0],
-                    scale=1.0,
-                    shear=[0.0, 0.0],
-                    interpolation=interpolation,
-                    fill=fill,
-                )
-            elif transform_id == "TranslateY":
-                return dispatch(
-                    F.affine_image_tensor,
-                    F.affine_image_pil,
-                    input,
-                    angle=0.0,
-                    translate=[0, int(magnitude)],
-                    scale=1.0,
-                    shear=[0.0, 0.0],
-                    interpolation=interpolation,
-                    fill=fill,
-                )
-            elif transform_id == "Rotate":
-                return dispatch(F.rotate_image_tensor, F.rotate_image_pil, input, angle=magnitude)
-            elif transform_id == "Brightness":
-                return dispatch(
-                    F.adjust_brightness_image_tensor,
-                    F.adjust_brightness_image_pil,
-                    input,
-                    brightness_factor=1.0 + magnitude,
-                )
-            elif transform_id == "Color":
-                return dispatch(
-                    F.adjust_saturation_image_tensor,
-                    F.adjust_saturation_image_pil,
-                    input,
-                    saturation_factor=1.0 + magnitude,
-                )
-            elif transform_id == "Contrast":
-                return dispatch(
-                    F.adjust_contrast_image_tensor, F.adjust_contrast_image_pil, input, contrast_factor=1.0 + magnitude
-                )
-            elif transform_id == "Sharpness":
-                return dispatch(
-                    F.adjust_sharpness_image_tensor,
-                    F.adjust_sharpness_image_pil,
-                    input,
-                    sharpness_factor=1.0 + magnitude,
-                )
-            elif transform_id == "Posterize":
-                return dispatch(F.posterize_image_tensor, F.posterize_image_pil, input, bits=int(magnitude))
-            elif transform_id == "Solarize":
-                return dispatch(F.solarize_image_tensor, F.solarize_image_pil, input, threshold=magnitude)
-            elif transform_id == "AutoContrast":
-                return dispatch(F.autocontrast_image_tensor, F.autocontrast_image_pil, input)
-            elif transform_id == "Equalize":
-                return dispatch(F.equalize_image_tensor, F.equalize_image_pil, input)
-            elif transform_id == "Invert":
-                return dispatch(F.invert_image_tensor, F.invert_image_pil, input)
-            else:
-                raise ValueError(f"No transform available for {transform_id}")
-
-        return apply_recursively(transform, sample)
+    def _apply_transform_to_sample(self, sample: Any, transform_id: str, magnitude: float) -> Any:
+        return apply_recursively(
+            functools.partial(
+                self._apply_transform_to_item,
+                transform_id=transform_id,
+                magnitude=magnitude,
+                interpolation=self.interpolation,
+                fill=self._parse_fill(sample),
+            ),
+            sample,
+        )
 
 
 class AutoAugment(_AutoAugmentBase):
@@ -277,7 +299,7 @@ class AutoAugment(_AutoAugmentBase):
     def forward(self, *inputs: Any) -> Any:
         sample = inputs if len(inputs) > 1 else inputs[0]
 
-        image = query_image(sample)
+        image = self._query_image(sample)
         image_size = F.get_image_size(image)
 
         policy = self._policies[int(torch.randint(len(self._policies), ()))]
@@ -296,7 +318,7 @@ class AutoAugment(_AutoAugmentBase):
             else:
                 magnitude = 0.0
 
-            sample = self._apply_transform(sample, transform_id, magnitude)
+            sample = self._apply_transform_to_sample(sample, transform_id, magnitude)
 
         return sample
 
@@ -332,8 +354,9 @@ class RandAugment(_AutoAugmentBase):
 
     def forward(self, *inputs: Any) -> Any:
         sample = inputs if len(inputs) > 1 else inputs[0]
+        self._query_image(sample)
 
-        image = query_image(sample)
+        image = self._query_image(sample)
         image_size = F.get_image_size(image)
 
         for _ in range(self.num_ops):
@@ -347,7 +370,7 @@ class RandAugment(_AutoAugmentBase):
             else:
                 magnitude = 0.0
 
-            sample = self._apply_transform(sample, transform_id, magnitude)
+            sample = self._apply_transform_to_sample(sample, transform_id, magnitude)
 
         return sample
 
@@ -382,7 +405,7 @@ class TrivialAugmentWide(_AutoAugmentBase):
     def forward(self, *inputs: Any) -> Any:
         sample = inputs if len(inputs) > 1 else inputs[0]
 
-        image = query_image(sample)
+        image = self._query_image(sample)
         image_size = F.get_image_size(image)
 
         transform_id, (magnitudes_fn, signed) = self._get_random_item(self._AUGMENTATION_SPACE)
@@ -395,4 +418,115 @@ class TrivialAugmentWide(_AutoAugmentBase):
         else:
             magnitude = 0.0
 
-        return self._apply_transform(sample, transform_id, magnitude)
+        return self._apply_transform_to_sample(sample, transform_id, magnitude)
+
+
+class AugMix(_AutoAugmentBase):
+    _PARTIAL_AUGMENTATION_SPACE = {
+        "ShearX": (lambda num_bins, image_size: torch.linspace(0.0, 0.3, num_bins), True),
+        "ShearY": (lambda num_bins, image_size: torch.linspace(0.0, 0.3, num_bins), True),
+        "TranslateX": (lambda num_bins, image_size: torch.linspace(0.0, image_size[0] / 3.0, num_bins), True),
+        "TranslateY": (lambda num_bins, image_size: torch.linspace(0.0, image_size[1] / 3.0, num_bins), True),
+        "Rotate": (lambda num_bins, image_size: torch.linspace(0.0, 30.0, num_bins), True),
+        "Posterize": (
+            lambda num_bins, image_size: cast(torch.Tensor, 4 - (torch.arange(num_bins) / ((num_bins - 1) / 4)))
+            .round()
+            .int(),
+            False,
+        ),
+        "Solarize": (lambda num_bins, image_size: torch.linspace(255.0, 0.0, num_bins), False),
+        "AutoContrast": (lambda num_bins, image_size: None, False),
+        "Equalize": (lambda num_bins, image_size: None, False),
+    }
+    _AUGMENTATION_SPACE: Dict[str, Tuple[Callable[[int, Tuple[int, int]], Optional[torch.Tensor]], bool]] = {
+        **_PARTIAL_AUGMENTATION_SPACE,
+        "Brightness": (lambda num_bins, image_size: torch.linspace(0.0, 0.9, num_bins), True),
+        "Color": (lambda num_bins, image_size: torch.linspace(0.0, 0.9, num_bins), True),
+        "Contrast": (lambda num_bins, image_size: torch.linspace(0.0, 0.9, num_bins), True),
+        "Sharpness": (lambda num_bins, image_size: torch.linspace(0.0, 0.9, num_bins), True),
+    }
+
+    def __init__(
+        self,
+        severity: int = 3,
+        mixture_width: int = 3,
+        chain_depth: int = -1,
+        alpha: float = 1.0,
+        all_ops: bool = True,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(**kwargs)
+        self._PARAMETER_MAX = 10
+        if not (1 <= severity <= self._PARAMETER_MAX):
+            raise ValueError(f"The severity must be between [1, {self._PARAMETER_MAX}]. Got {severity} instead.")
+        self.severity = severity
+        self.mixture_width = mixture_width
+        self.chain_depth = chain_depth
+        self.alpha = alpha
+        self.all_ops = all_ops
+
+    def _sample_dirichlet(self, params: torch.Tensor) -> torch.Tensor:
+        # Must be on a separate method so that we can overwrite it in tests.
+        return torch._sample_dirichlet(params)
+
+    def _apply_augmix(self, input: Any) -> Any:
+        if isinstance(input, (features.BoundingBox, features.SegmentationMask)):
+            raise TypeError(f"{type(input).__name__}'s are not supported by {type(self).__name__}()")
+        elif isinstance(input, torch.Tensor):
+            image = input
+        elif isinstance(input, PIL.Image.Image):
+            image = pil_to_tensor(input)
+        else:
+            return input
+
+        augmentation_space = self._AUGMENTATION_SPACE if self.all_ops else self._PARTIAL_AUGMENTATION_SPACE
+
+        image_size = F.get_image_size(image)
+
+        orig_dims = list(image.shape)
+        batch = image.view([1] * max(4 - image.ndim, 0) + orig_dims)
+        batch_dims = [batch.size(0)] + [1] * (batch.ndim - 1)
+
+        # Sample the beta weights for combining the original and augmented image. To get Beta, we use a Dirichlet
+        # with 2 parameters. The 1st column stores the weights of the original and the 2nd the ones of augmented image.
+        m = self._sample_dirichlet(
+            torch.tensor([self.alpha, self.alpha], device=batch.device).expand(batch_dims[0], -1)
+        )
+
+        # Sample the mixing weights and combine them with the ones sampled from Beta for the augmented images.
+        combined_weights = self._sample_dirichlet(
+            torch.tensor([self.alpha] * self.mixture_width, device=batch.device).expand(batch_dims[0], -1)
+        ) * m[:, 1].view([batch_dims[0], -1])
+
+        mix = m[:, 0].view(batch_dims) * batch
+        for i in range(self.mixture_width):
+            aug = batch
+            depth = self.chain_depth if self.chain_depth > 0 else int(torch.randint(low=1, high=4, size=(1,)).item())
+            for _ in range(depth):
+                transform_id, (magnitudes_fn, signed) = self._get_random_item(augmentation_space)
+
+                magnitudes = magnitudes_fn(self._PARAMETER_MAX, image_size)
+                if magnitudes is not None:
+                    magnitude = float(magnitudes[int(torch.randint(self.severity, ()))])
+                    if signed and torch.rand(()) <= 0.5:
+                        magnitude *= -1
+                else:
+                    magnitude = 0.0
+
+                aug = self._apply_transform_to_item(
+                    image, transform_id, magnitude, interpolation=self.interpolation, fill=self._parse_fill(image)
+                )
+            mix.add_(combined_weights[:, i].view(batch_dims) * aug)
+        mix = mix.view(orig_dims).to(dtype=image.dtype)
+
+        if isinstance(input, features.Image):
+            return features.Image.new_like(input, mix)
+        elif isinstance(input, torch.Tensor):
+            return mix
+        else:  # isinstance(input, PIL.Image.Image):
+            return to_pil_image(mix)
+
+    def forward(self, *inputs: Any) -> Any:
+        sample = inputs if len(inputs) > 1 else inputs[0]
+        self._query_image(sample)
+        return apply_recursively(self._apply_augmix, sample)
--- torchvision/prototype/transforms/_utils.py
+++ torchvision/prototype/transforms/_utils.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional, Union
+from typing import Any, Optional, Union, Iterator
 
 import PIL.Image
 import torch
@@ -6,14 +6,19 @@ from torchvision.prototype import features
 from torchvision.prototype.utils._internal import query_recursively
 
 
+def _extract_image(input: Any) -> Optional[Union[PIL.Image.Image, torch.Tensor, features.Image]]:
+    if type(input) in {torch.Tensor, features.Image} or isinstance(input, PIL.Image.Image):
+        return input
+
+    return None
+
+
+def query_images(sample: Any) -> Iterator[Union[PIL.Image.Image, torch.Tensor, features.Image]]:
+    return query_recursively(_extract_image, sample)
+
+
 def query_image(sample: Any) -> Union[PIL.Image.Image, torch.Tensor, features.Image]:
-    def fn(input: Any) -> Optional[Union[PIL.Image.Image, torch.Tensor, features.Image]]:
-        if type(input) in {torch.Tensor, features.Image} or isinstance(input, PIL.Image.Image):
-            return input
-
-        return None
-
     try:
-        return next(query_recursively(fn, sample))
+        return next(query_images(sample))
     except StopIteration:
         raise TypeError("No image was found in the sample")
