095437aad6cfd25b01fa01961e5774904d738347
Vasilis Vryniotis <datumbox@users.noreply.github.com>
2022-02-28 13:16:04 +0000

    Replace get_image_size/num_channels with get_dimensions (#5487)
    * Replace get_image_size/num_channels with get_image_dims
    
    * Reduce verbosity
    
    * Fix JIT-scriptability
    
    * Refactoring
    
    * More refactoring
    
    * Replace all _FP/_FT direct calls.
    
    * Remove usages of get_image_size and get_image_num_channels from code-base.
    
    * Fix JIT issues
    
    * Adding missing assertion.

--- torchvision/prototype/transforms/_utils.py
+++ torchvision/prototype/transforms/_utils.py
@@ -1,9 +1,11 @@
-from typing import Any, Optional, Union
+from typing import Any, Optional, Tuple, Union
 
 import PIL.Image
 import torch
 from torchvision.prototype import features
 from torchvision.prototype.utils._internal import query_recursively
+
+from .functional._meta import get_dimensions_image_tensor, get_dimensions_image_pil
 
 
 def query_image(sample: Any) -> Union[PIL.Image.Image, torch.Tensor, features.Image]:
@@ -17,3 +19,16 @@ def query_image(sample: Any) -> Union[PIL.Image.Image, torch.Tensor, features.Im
         return next(query_recursively(fn, sample))
     except StopIteration:
         raise TypeError("No image was found in the sample")
+
+
+def get_image_dimensions(image: Union[PIL.Image.Image, torch.Tensor, features.Image]) -> Tuple[int, int, int]:
+    if isinstance(image, features.Image):
+        channels = image.num_channels
+        height, width = image.image_size
+    elif isinstance(image, torch.Tensor):
+        channels, height, width = get_dimensions_image_tensor(image)
+    elif isinstance(image, PIL.Image.Image):
+        channels, height, width = get_dimensions_image_pil(image)
+    else:
+        raise TypeError(f"unable to get image dimensions from object of type {type(image).__name__}")
+    return channels, height, width
