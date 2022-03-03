e836b3d8baf8cbfdd18fcc872631962345a65689
Philip Meier <github.pmeier@posteo.de>
2022-03-03 15:41:20 +0100

    simplify Feature implementation (#5539)
    * simplify Feature implementation
    
    * fix mypy

--- torchvision/prototype/features/_encoded.py
+++ torchvision/prototype/features/_encoded.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 import sys
 from typing import BinaryIO, Tuple, Type, TypeVar, Union, Optional, Any
@@ -13,19 +15,25 @@ D = TypeVar("D", bound="EncodedData")
 
 
 class EncodedData(_Feature):
-    @classmethod
-    def _to_tensor(cls, data: Any, *, dtype: Optional[torch.dtype], device: Optional[torch.device]) -> torch.Tensor:
+    def __new__(
+        cls,
+        data: Any,
+        *,
+        dtype: Optional[torch.dtype] = None,
+        device: Optional[Union[torch.device, str, int]] = None,
+        requires_grad: bool = False,
+    ) -> EncodedData:
         # TODO: warn / bail out if we encounter a tensor with shape other than (N,) or with dtype other than uint8?
-        return super()._to_tensor(data, dtype=dtype, device=device)
+        return super().__new__(cls, data, dtype=dtype, device=device, requires_grad=requires_grad)
 
     @classmethod
-    def from_file(cls: Type[D], file: BinaryIO) -> D:
-        return cls(fromfile(file, dtype=torch.uint8, byte_order=sys.byteorder))
+    def from_file(cls: Type[D], file: BinaryIO, **kwargs: Any) -> D:
+        return cls(fromfile(file, dtype=torch.uint8, byte_order=sys.byteorder), **kwargs)
 
     @classmethod
-    def from_path(cls: Type[D], path: Union[str, os.PathLike]) -> D:
+    def from_path(cls: Type[D], path: Union[str, os.PathLike], **kwargs: Any) -> D:
         with open(path, "rb") as file:
-            return cls.from_file(file)
+            return cls.from_file(file, **kwargs)
 
 
 class EncodedImage(EncodedData):
