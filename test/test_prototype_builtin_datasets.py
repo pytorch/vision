6aee8ba3c830857ce685096668d242121170bde5
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared26501.04.atn3.facebook.com>
2022-03-07 04:30:31 -0800

    2022-03-07 nightly release (350a3e8ebd2e7fd441ae4242d3b63688ec453057)

--- test/test_prototype_builtin_datasets.py
+++ test/test_prototype_builtin_datasets.py
@@ -107,14 +107,7 @@ class TestCommon:
 
         next(iter(dataset.map(transforms.Identity())))
 
-    @parametrize_dataset_mocks(
-        DATASET_MOCKS,
-        marks={
-            "cub200": pytest.mark.xfail(
-                reason="See https://github.com/pytorch/vision/pull/5187#issuecomment-1015479165"
-            )
-        },
-    )
+    @parametrize_dataset_mocks(DATASET_MOCKS)
     def test_traversable(self, test_home, dataset_mock, config):
         dataset_mock.prepare(test_home, config)
 
@@ -122,14 +115,7 @@ class TestCommon:
 
         traverse(dataset)
 
-    @parametrize_dataset_mocks(
-        DATASET_MOCKS,
-        marks={
-            "cub200": pytest.mark.xfail(
-                reason="See https://github.com/pytorch/vision/pull/5187#issuecomment-1015479165"
-            )
-        },
-    )
+    @parametrize_dataset_mocks(DATASET_MOCKS)
     @pytest.mark.parametrize("annotation_dp_type", (Shuffler, ShardingFilter))
     def test_has_annotations(self, test_home, dataset_mock, config, annotation_dp_type):
         def scan(graph):
