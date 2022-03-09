17593a5c90871ce1b72beeb696b501c12b08f067
Vasilis Vryniotis <vvryniotis@fb.com>
2022-03-09 14:12:17 +0000

    Measuring execution times of models.

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -7,4 +7,4 @@ conda activate ./env
 
 export PYTORCH_TEST_WITH_SLOW='1'
 python -m torch.utils.collect_env
-pytest --junitxml=test-results/junit.xml -v --durations 20
+pytest --junitxml=test-results/junit.xml -v --durations 2000 -k "test_classification_model or test_detection_model or test_quantized_classification_model or test_segmentation_model or test_video_model"
