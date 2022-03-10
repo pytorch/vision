536add0fa6f19b73ece39d01ca56f7ac1747bb75
Yassine Alouini <yalouini@idmog.com>
2022-03-10 12:38:52 +0100

    [FEAT] food101 dataset code seems to work now.

--- torchvision/prototype/datasets/_builtin/food101.py
+++ torchvision/prototype/datasets/_builtin/food101.py
@@ -1,16 +1,17 @@
 import enum
 import functools
-import pathlib
 from pathlib import Path
 from typing import Any, Tuple, List, Dict, cast, Optional
 
 import numpy as np
-from torchdata.datapipes.iter import IterDataPipe, Filter, Mapper, CSVParser
+from torchdata.datapipes.iter import IterDataPipe, Filter, Mapper, CSVParser, JsonParser, Demultiplexer
 from torchvision.prototype.datasets.utils import Dataset, DatasetInfo, DatasetConfig, HttpResource, OnlineResource
 from torchvision.prototype.datasets.utils._internal import (
     hint_shuffling,
     hint_sharding,
     path_comparator,
+    getitem,
+    INFINITE_BUFFER_SIZE,
 )
 from torchvision.prototype.features import Label, EncodedImage
 
@@ -18,6 +19,7 @@ from torchvision.prototype.features import Label, EncodedImage
 class Food101Demux(enum.IntEnum):
     METADATA = 0
     IMAGES = 1
+    EXTRA = 2
 
 
 class Food101(Dataset):
@@ -35,22 +37,23 @@ class Food101(Dataset):
 
     _URL = "http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz"
     _SHA256 = "d97d15e438b7f4498f96086a4f7e2fa42a32f2712e87d3295441b2b6314053a4"
-    _META_FILE_NAME = "classes.txt"
+    _CATEGORIES_FILE_NAME = "classes.txt"
 
-    # TODO: Inspired from dtd.py. Is this useful?
     def _classify_archive(self, data: Tuple[str, Any]) -> Optional[int]:
-        path = pathlib.Path(data[0])
-        print(path)
-        if path.name == "images":
+        path = Path(data[0])
+        if path.parents[1].name == "images":
             return Food101Demux.IMAGES
-        else:
+        elif path.parents[0].name == "meta":
             return Food101Demux.METADATA
+        else:
+            # README and licence_agreement files.
+            return Food101Demux.EXTRA
 
     def _make_info(self) -> DatasetInfo:
         return DatasetInfo(
             "food-101",
             homepage="https://data.vision.ee.ethz.ch/cvl/datasets_extra/food-101",
-            # Only train and test splits.
+            # Only train and test splits, no validation.
             valid_options=dict(split=("train", "test")),
         )
 
@@ -65,35 +68,26 @@ class Food101(Dataset):
 
     def _prepare_sample(self, data: Tuple[np.ndarray, int]) -> Dict[str, Any]:
         image_path, image_buffer = data
-        # Extract the category for the image path, it is the folder before the image name.
         category = Path(image_path).parent.name
-        # TODO: Extract the list of all categories. Where to do this?
         return dict(
             image=EncodedImage.from_file(image_buffer),
-            # TODO: Should the categories be prepared as well? Most likely, yes.
-            # For now hardcoded list.
             label=Label.from_category(category, categories=self.categories),
         )
 
-    # TODO: Doesn't seem correct, investigate...
-    def _is_data_file(self, data: Tuple[str, Any], *, split: str) -> bool:
-        # We need to use the train.json and test.json to get the correct
-        # mapping.
-        path = pathlib.Path(data[0])
-        print(path)
-        # Need to compare to the actual list of train and test splits.
-        # return path.name == split
-        return True
+    def _img_in_metadata(self, data: Tuple[str, Any], *, metadata: Dict[str, List[str]]) -> bool:
+        # Check whether or not the file is contained in the list of the splitted metdata.
+        path = Path(data[0])
+        category = Path(path).parent.name
+        img_ids = {e.split("/")[-1] for e in metadata[category]}
+        return path.stem in img_ids
 
-    def _generate_categories(self, root: pathlib.Path) -> List[str]:
+    def _generate_categories(self, root: Path) -> List[str]:
         resources = self.resources(self.default_config)
         dp = resources[0].load(root)
-        dp = Filter(dp, path_comparator("name", self._META_FILE_NAME))
-        # TODO: Probably not the best way to parse, but it works for now. Improve.
+        dp = Filter(dp, path_comparator("name", self._CATEGORIES_FILE_NAME))
+        # TODO: Probably not the best way to parse, but it works for now. Improve later?
         dp = CSVParser(dp, delimiter=" ")
-        categories = []
-        for sample in dp:
-            categories.append(sample[0])
+        categories = [sample[0] for sample in dp]
         return cast(List[str], categories)
 
     def _make_datapipe(
@@ -102,17 +96,14 @@ class Food101(Dataset):
         *,
         config: DatasetConfig,
     ) -> IterDataPipe[Dict[str, Any]]:
-        dp = resource_dps[0]
-
-        # TODO: Could this be useful? Maybe for easily extracting metadata information for the
-        # train and test splits.
-        # metadata_dp, images_dp = Demultiplexer(
-        #     archive_dp, 2, self._classify_archive, drop_none=True, buffer_size=INFINITE_BUFFER_SIZE
-        # )
-
-        # Train/Test filtering based on the meta file.
-        # TODO: Finish this.
-        dp = Filter(dp, functools.partial(self._is_data_file, split=config.split))
-        dp = hint_sharding(dp)
-        dp = hint_shuffling(dp)
-        return Mapper(dp, self._prepare_sample)
+        archive_dp = resource_dps[0]
+        metadata_dp, images_dp, _ = Demultiplexer(
+            archive_dp, 3, self._classify_archive, drop_none=True, buffer_size=INFINITE_BUFFER_SIZE
+        )
+        metadata_dp = Filter(metadata_dp, path_comparator("name", f"{config.split}.json"))
+        metadata_dp = JsonParser(metadata_dp)
+        metadata = next(iter(Mapper(metadata_dp, getitem(1))))
+        images_dp = Filter(images_dp, functools.partial(self._img_in_metadata, metadata=metadata))
+        images_dp = hint_sharding(images_dp)
+        images_dp = hint_shuffling(images_dp)
+        return Mapper(images_dp, self._prepare_sample)
