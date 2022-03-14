81ac57ccb9e22b7b8f8cc35e50e7e02b497db4a7
Joao Gomes <jdsgomes@fb.com>
2022-03-14 15:51:04 +0000

    fix bug

--- torchvision/models/detection/fcos.py
+++ torchvision/models/detection/fcos.py
@@ -571,7 +571,7 @@ class FCOS(nn.Module):
         original_image_sizes: List[Tuple[int, int]] = []
         for img in images:
             val = img.shape[-2:]
-            if not len(val) != 2:
+            if len(val) != 2:
                 raise ValueError(
                     f"expecting the last two dimensions of the Tensor to be H and W instead got {img.shape[-2:]}"
                 )
