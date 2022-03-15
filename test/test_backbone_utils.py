b7032ed73428f0a3ff490d28fae344c591489f29
Vasilis Vryniotis <vvryniotis@fb.com>
2022-03-15 18:29:21 +0000

    Remove unnecessary variable

--- test/test_backbone_utils.py
+++ test/test_backbone_utils.py
@@ -23,7 +23,7 @@ def get_available_models():
 @pytest.mark.parametrize("backbone_name", ("resnet18", "resnet50"))
 def test_resnet_fpn_backbone(backbone_name):
     x = torch.rand(1, 3, 300, 300, dtype=torch.float32, device="cpu")
-    model = resnet_fpn_backbone(backbone_name=backbone_name, weights=None)
+    model = resnet_fpn_backbone(backbone_name=backbone_name)
     assert isinstance(model, BackboneWithFPN)
     y = model(x)
     assert list(y.keys()) == ["0", "1", "2", "3", "pool"]
