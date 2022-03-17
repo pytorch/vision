ec3e1ab8341a4fda2689dd33edcadc9f1085d59c
Yassine Alouini <yalouini@idmog.com>
2022-03-17 17:42:04 +0100

    [ENH] More idomatic dataset code thanks to code review.

--- torchvision/prototype/datasets/_builtin/food101.py
+++ torchvision/prototype/datasets/_builtin/food101.py
@@ -1,10 +1,19 @@
-import functools
 from pathlib import Path
 from typing import Any, Tuple, List, Dict, cast, Optional, BinaryIO
 
-from torchdata.datapipes.iter import IterDataPipe, Filter, Mapper, CSVParser, JsonParser, Demultiplexer
+from torchdata.datapipes.iter import (
+    IterDataPipe,
+    Filter,
+    Mapper,
+    LineReader,
+    JsonParser,
+    Demultiplexer,
+    UnBatcher,
+    IterKeyZipper,
+)
 from torchvision.prototype.datasets.utils import Dataset, DatasetInfo, DatasetConfig, HttpResource, OnlineResource
 from torchvision.prototype.datasets.utils._internal import (
+    MappingIterator,
     hint_shuffling,
     hint_sharding,
     path_comparator,
@@ -19,7 +28,6 @@ class Food101(Dataset):
         return DatasetInfo(
             "food101",
             homepage="https://data.vision.ee.ethz.ch/cvl/datasets_extra/food-101",
-            # Only train and test splits, no validation.
             valid_options=dict(split=("train", "test")),
         )
 
@@ -28,7 +36,6 @@ class Food101(Dataset):
             HttpResource(
                 url="http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz",
                 sha256="d97d15e438b7f4498f96086a4f7e2fa42a32f2712e87d3295441b2b6314053a4",
-                decompress=True,
             )
         ]
 
@@ -41,23 +48,17 @@ class Food101(Dataset):
         else:
             return None
 
-    def _prepare_sample(self, data: Tuple[str, BinaryIO]) -> Dict[str, Any]:
-        image_path, image_buffer = data
-        category = Path(image_path).parent.name
+    def _prepare_sample(self, data: Tuple[str, Tuple[str, BinaryIO]]) -> Dict[str, Any]:
+        id, (path, buffer) = data
         return dict(
-            image=EncodedImage.from_file(image_buffer),
-            label=Label.from_category(category, categories=self.categories),
+            label=Label.from_category(id.split("/", 1)[0], categories=self.categories),
+            path=path,
+            image=EncodedImage.from_file(buffer),
         )
 
-    def _img_in_metadata(self, data: Tuple[str, Any], *, metadata: Dict[str, List[str]]) -> bool:
-        # Check whether or not the file is contained in the list of the splitted metdata.
+    def _image_key(self, data: Tuple[str, Any]) -> str:
         path = Path(data[0])
-        category = Path(path).parent.name
-        if category in metadata:
-            img_ids = {e.split("/")[-1] for e in metadata[category]}
-            return path.stem in img_ids
-        else:
-            return False
+        return path.relative_to(path.parents[1]).with_suffix("").as_posix()
 
     def _make_datapipe(
         self,
@@ -66,21 +67,30 @@ class Food101(Dataset):
         config: DatasetConfig,
     ) -> IterDataPipe[Dict[str, Any]]:
         archive_dp = resource_dps[0]
-        images_dp, metadata_dp = Demultiplexer(
+        images_dp, split_dp = Demultiplexer(
             archive_dp, 2, self._classify_archive, drop_none=True, buffer_size=INFINITE_BUFFER_SIZE
         )
-        metadata_dp = Filter(metadata_dp, path_comparator("name", f"{config.split}.json"))
-        metadata_dp = JsonParser(metadata_dp)
-        metadata = next(iter(Mapper(metadata_dp, getitem(1))))
-        images_dp = Filter(images_dp, functools.partial(self._img_in_metadata, metadata=metadata))
-        images_dp = hint_sharding(images_dp)
-        images_dp = hint_shuffling(images_dp)
-        return Mapper(images_dp, self._prepare_sample)
+        split_dp = Filter(split_dp, path_comparator("name", f"{config.split}.json"))
+        split_dp = JsonParser(split_dp)
+        split_dp = Mapper(split_dp, getitem(1))
+        split_dp: IterDataPipe[List[str]] = MappingIterator(split_dp, drop_key=True)
+        split_dp = UnBatcher(split_dp)
+        split_dp = hint_sharding(split_dp)
+        split_dp = hint_shuffling(split_dp)
+
+        dp = IterKeyZipper(
+            split_dp,
+            images_dp,
+            key_fn=getitem(),
+            ref_key_fn=self._image_key,
+            buffer_size=INFINITE_BUFFER_SIZE,
+        )
+
+        return Mapper(dp, self._prepare_sample)
 
     def _generate_categories(self, root: Path) -> List[str]:
         resources = self.resources(self.default_config)
         dp = resources[0].load(root)
         dp = Filter(dp, path_comparator("name", "classes.txt"))
-        dp = CSVParser(dp, delimiter=" ")
-        categories = [sample[0] for sample in dp]
-        return cast(List[str], categories)
+        dp = LineReader(dp, decode=True, return_path=False)
+        return list(dp)
