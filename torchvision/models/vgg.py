903a3d534c0f8fdf2077f7ffb979a559c30ef6bb
Vasilis Vryniotis <vvryniotis@fb.com>
2022-03-21 20:15:11 +0000

    No initialization on VGG if pretrained

--- torchvision/models/vgg.py
+++ torchvision/models/vgg.py
@@ -97,6 +97,7 @@ cfgs: Dict[str, List[Union[str, int]]] = {
 
 def _vgg(cfg: str, batch_norm: bool, weights: Optional[WeightsEnum], progress: bool, **kwargs: Any) -> VGG:
     if weights is not None:
+        kwargs["init_weights"] = False
         if weights.meta["categories"] is not None:
             _ovewrite_named_param(kwargs, "num_classes", len(weights.meta["categories"]))
     model = VGG(make_layers(cfgs[cfg], batch_norm=batch_norm), **kwargs)
