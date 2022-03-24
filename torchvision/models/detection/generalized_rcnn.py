55d99267f0a80b67967a427ef9c235f49e942c1c
Joao Gomes <jdsgomes@fb.com>
2022-03-24 14:39:00 +0000

    Fix bug

--- torchvision/models/detection/generalized_rcnn.py
+++ torchvision/models/detection/generalized_rcnn.py
@@ -59,7 +59,7 @@ class GeneralizedRCNN(nn.Module):
         """
         if self.training:
             if targets is None:
-                torch._assert(True, "targets should not be none when in training mode")
+                torch._assert(False, "targets should not be none when in training mode")
 
             for target in targets:
                 boxes = target["boxes"]
