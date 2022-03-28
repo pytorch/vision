a7770aefdc3294667dd8aaab57c90efa8355d159
Philip Meier <github.pmeier@posteo.de>
2022-03-28 10:34:47 +0200

    revert test changes and add comment

--- test/test_prototype_builtin_datasets.py
+++ test/test_prototype_builtin_datasets.py
@@ -119,8 +119,12 @@ class TestCommon:
 
         pickle.dumps(dataset)
 
+    # TODO: we need to enforce not only that both a Shuffler and a ShardingFilter are part of the datapipe, but also
+    #  that the Shuffler comes before the ShardingFilter. Early commits in https://github.com/pytorch/vision/pull/5680
+    #  contain a custom test for that, but we opted to wait for a potential solution / test from torchdata for now.
     @parametrize_dataset_mocks(DATASET_MOCKS)
-    def test_shuffling_and_sharding(self, test_home, dataset_mock, config):
+    @pytest.mark.parametrize("annotation_dp_type", (Shuffler, ShardingFilter))
+    def test_has_annotations(self, test_home, dataset_mock, config, annotation_dp_type):
         def scan(graph):
             for node, sub_graph in graph.items():
                 yield node
@@ -130,31 +134,8 @@ class TestCommon:
 
         dataset = datasets.load(dataset_mock.name, **config)
 
-        # The nodes are in reversed order, i.e the last datapipe in the dataset comes first
-        nodes = iter(scan(traverse(dataset)))
-
-        for node in nodes:
-            if isinstance(node, ShardingFilter):
-                break
-            elif isinstance(node, Shuffler):
-                raise AssertionError(
-                    "Found a `Shuffler` after a `ShardingFilter` in the graph. "
-                    "Please place the `dp = hint_shuffling(dp)` call before the `dp = hint_sharding(dp)` call."
-                )
-        else:
-            raise AssertionError(
-                "Found no `ShardingFilter` in the graph. "
-                "Please place a `dp = hint_sharding(dp)` call in the dataset implementation. "
-            )
-
-        for node in nodes:
-            if isinstance(node, Shuffler):
-                break
-        else:
-            raise AssertionError(
-                "Found no `Shuffler` in the graph. "
-                "Please place a `dp = hint_shuffling(dp)` call in the dataset implementation. "
-            )
+        if not any(type(dp) is annotation_dp_type for dp in scan(traverse(dataset))):
+            raise AssertionError(f"The dataset doesn't contain a {annotation_dp_type.__name__}() datapipe.")
 
     @parametrize_dataset_mocks(DATASET_MOCKS)
     def test_save_load(self, test_home, dataset_mock, config):
