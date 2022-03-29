aab8dbb6dee014316f0257fc41dacbe7bbb6644a
Lezwon Castelino <lezwon@users.noreply.github.com>
2022-03-29 07:30:32 +0530

    Apply suggestions from code review
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>

--- torchvision/prototype/datasets/_builtin/sbu.py
+++ torchvision/prototype/datasets/_builtin/sbu.py
@@ -20,20 +20,39 @@ class SBU(Dataset):
             homepage="http://www.cs.virginia.edu/~vicente/sbucaptions/",
         )
 
+        def _preprocess(self, path: pathlib.Path) -> pathlib.Path:
+        folder = OnlineResource._extract(path)
+        data_folder = folder / "dataset"
+        image_folder = data_folder / "images"
+        image_folder.mkdir()
+        broken_urls = []
+        with open(data_folder / "SBU_captioned_photo_dataset_urls.txt") as fh:
+            urls = fh.read().splitlines()
+            for url in tqdm(urls):
+                try:
+                    HttpResource(url).download(image_folder)
+                except Exception:
+                    broken_urls.append(url)
+
+        return folder
+
     def resources(self, config: DatasetConfig) -> List[OnlineResource]:
         return [
             HttpResource(
                 "http://www.cs.virginia.edu/~vicente/sbucaptions/SBUCaptionedPhotoDataset.tar.gz",
                 sha256="2bf37d5e1c9e1c6eae7d5103030d58a7f2117fc5e8c6aa9620f0df165acebf09",
+                preprocess=self._preprocess,
             )
         ]
 
     def _classify_files(self, data: Tuple[str, Any]) -> Optional[int]:
-        path, stream = data
-        if path.endswith("SBU_captioned_photo_dataset_urls.txt"):
-            return SBU.PHOTO_URLS
-        elif path.endswith("SBU_captioned_photo_dataset_captions.txt"):
-            return SBU.PHOTO_CAPTIONS
+        path = pathlib.Path(data[0])
+        if path.parent.name == "images":
+            return 0
+        elif path.name == "SBU_captioned_photo_dataset_urls.txt":
+            return 1
+        elif path.name == "SBU_captioned_photo_dataset_captions.txt":
+            return 2
         else:
             return None
 
@@ -44,20 +63,18 @@ class SBU(Dataset):
         config: DatasetConfig,
     ) -> IterDataPipe[Dict[str, Any]]:
 
-        photo_urls_dp, photo_captions_dp = Demultiplexer(
-            resource_dps[0], 2, self._classify_files, drop_none=True, buffer_size=-1
+        images_dp, urls_dp, captions_dp = Demultiplexer(
+            resource_dps[0], 3, self._classify_files, drop_none=True, buffer_size=INFINITE_BUFFER_SIZE
         )
 
-        photo_urls_dp = LineReader(photo_urls_dp, decode=True, return_path=False)
-        photo_urls_dp = HttpReader(photo_urls_dp)
+        images_dp = hint_shuffling(images_dp)
+        images_dp = hint_sharding(images_dp)
 
-        photo_captions_dp = LineReader(photo_captions_dp, decode=True, return_path=False)
+        urls_dp = LineReader(urls_dp, decode=True, return_path=False)
+        captions_dp = LineReader(captions_dp, decode=True, return_path=False)
+        anns_dp = Zipper(urls_dp, captions_dp)
 
-        dp = Zipper(photo_urls_dp, photo_captions_dp)
-        dp = Filter(dp, lambda data: self._filter_by_content_type(data[0][1]))
-        dp = hint_shuffling(dp)
-        dp = hint_sharding(dp)
-
+        dp = IterKeyZipper(images_dp, anns_dp, path_accessor("name"), buffer_size=INFINITE_BUFFER_SIZE)
         return Mapper(dp, self._prepare_sample)
 
     def _filter_by_content_type(self, stream: StreamWrapper) -> bool:
@@ -66,12 +83,10 @@ class SBU(Dataset):
         except Exception:
             return False
 
-    def _prepare_sample(self, sample: Dict[str, Any]) -> Dict[str, Any]:
-        image, caption = sample
-        _, image = image
-
-        # TODO: check for method to convert to tensor without PIL and Numpy
-        image = pil_image.open(io.BytesIO(image.data))
-        image = np.array(image)
-
-        return {"image": features.Image(image), "caption": caption}
+    def _prepare_sample(self, data: Tuple[Tuple[str, BinaryIO], Tuple[str, str]]) -> Dict[str, Any]:
+        (path, buffer), (_, caption) = data
+        return dict(
+            path=path,
+            image=EncodedImage.from_file(buffer),
+            caption=caption.strip(),
+        )
