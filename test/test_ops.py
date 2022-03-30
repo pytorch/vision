f9f7f18096693f64c0c5192598d65cbe5040bd71
Hu Ye <xiaohuzc@gmail.com>
2022-03-30 21:23:20 +0800

    refactor code

--- test/test_ops.py
+++ test/test_ops.py
@@ -1367,11 +1367,10 @@ class TestUtils:
         assert len(params[0]) == 92
         assert len(params[1]) == 82
 
-    @pytest.mark.parametrize("norm_weight_decay", [None, 0.0])
+    @pytest.mark.parametrize("norm_weight_decay", [None, 0.2]) 
     @pytest.mark.parametrize("norm_layer", [None, nn.LayerNorm])
-    @pytest.mark.parametrize("bias_weight_decay", [None, 0.0])
-    @pytest.mark.parametrize("custom_keys_weight_decay", [None, {"class_token": 0.0, "pos_embedding": 0.0}])
-    def test_set_weight_decay(self, norm_weight_decay, norm_layer, bias_weight_decay, custom_keys_weight_decay):
+    @pytest.mark.parametrize("custom_keys_weight_decay", [None, [("class_token", 0.3), ("pos_embedding", 0.4)]])
+    def test_set_weight_decay(self, norm_weight_decay, norm_layer, custom_keys_weight_decay):
         model = models.VisionTransformer(
             image_size=224,
             patch_size=16,
@@ -1380,37 +1379,31 @@ class TestUtils:
             hidden_dim=8,
             mlp_dim=4,
         )
-        param_groups = ops._utils.set_weight_decay(
+        param_groups = set_weight_decay(
             model,
-            1e-3,
+            0.1,
             norm_weight_decay=norm_weight_decay,
             norm_classes=None if norm_layer is None else [norm_layer],
-            bias_weight_decay=bias_weight_decay,
-            custom_keys_weight_decay=custom_keys_weight_decay,
+            custom_keys_weight_decay=custom_keys_weight_decay
         )
-
-        if norm_weight_decay is None and bias_weight_decay is None and custom_keys_weight_decay is None:
+        
+        if norm_weight_decay is None and custom_keys_weight_decay is None:
             assert len(param_groups) == 1
             assert len(param_groups[0]["params"]) == 20
-
-        if norm_weight_decay is not None and bias_weight_decay is None and custom_keys_weight_decay is None:
+        
+        if norm_weight_decay is not None and custom_keys_weight_decay is None:
             assert len(param_groups) == 2
-            assert len(param_groups[0]["params"]) == 6
-            assert len(param_groups[1]["params"]) == 14
+            param_groups.sort(key=lambda x: x["weight_decay"])
+            assert len(param_groups[0]["params"]) == 14
+            assert len(param_groups[1]["params"]) == 6
 
-        if norm_weight_decay is not None and bias_weight_decay is not None and custom_keys_weight_decay is None:
-            assert len(param_groups) == 3
-            assert len(param_groups[0]["params"]) == 6
-            assert len(param_groups[1]["params"]) == 5
-            assert len(param_groups[2]["params"]) == 9
-
-        if norm_weight_decay is not None and bias_weight_decay is not None and custom_keys_weight_decay is not None:
-            assert len(param_groups) == 5
-            assert len(param_groups[0]["params"]) == 6
-            assert len(param_groups[1]["params"]) == 5
+        if norm_weight_decay is not None and custom_keys_weight_decay is not None:
+            assert len(param_groups) == 4
+            param_groups.sort(key=lambda x: x["weight_decay"])
+            assert len(param_groups[0]["params"]) == 12
+            assert len(param_groups[1]["params"]) == 6
             assert len(param_groups[2]["params"]) == 1
             assert len(param_groups[3]["params"]) == 1
-            assert len(param_groups[4]["params"]) == 7
 
 
 class TestDropBlock:
