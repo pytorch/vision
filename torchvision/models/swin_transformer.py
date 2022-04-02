38279eda500fe6b48855ad63e70a8c4bbcb2a7d9
Hu Ye <xiaohuzc@gmail.com>
2022-04-02 16:55:53 +0800

    Update swin_transformer.py

--- torchvision/models/swin_transformer.py
+++ torchvision/models/swin_transformer.py
@@ -6,11 +6,10 @@ import torch.nn.functional as F
 from torch import nn, Tensor
 
 from ..ops.stochastic_depth import StochasticDepth
-from ..transforms._presets import ImageClassification, InterpolationMode
 from ..utils import _log_api_usage_once
-from ._api import WeightsEnum, Weights
+from ._api import WeightsEnum
 from ._meta import _IMAGENET_CATEGORIES
-from ._utils import handle_legacy_interface, _ovewrite_named_param
+from ._utils import _ovewrite_named_param
 from .convnext import Permute
 from .vision_transformer import MLPBlock
 
