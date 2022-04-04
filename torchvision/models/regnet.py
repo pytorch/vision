c7003de3e800d93e5eaa852b487fde18418d5b23
Yosua Michael Maranatha <yosuamichael@fb.com>
2022-04-04 11:04:04 +0100

    Add regnet_y_128gh_swag weight

--- torchvision/models/regnet.py
+++ torchvision/models/regnet.py
@@ -629,7 +629,19 @@ class RegNet_Y_32GF_Weights(WeightsEnum):
 
 
 class RegNet_Y_128GF_Weights(WeightsEnum):
-    # weights are not available yet.
+    IMAGENET1K_SWAG_V1 = Weights(
+        url="https://download.pytorch.org/models/regnet_y_128gf_swag-c8ce3e52.pth",
+        transforms=partial(
+            ImageClassification, crop_size=384, resize_size=384, interpolation=InterpolationMode.BICUBIC
+        ),
+        meta={
+            **_COMMON_SWAG_META,
+            "num_params": 644812894,
+            # Still mock
+            "acc@1": 88.23,
+            "acc@5": 98.69,
+        },
+    )
     pass
 
 
