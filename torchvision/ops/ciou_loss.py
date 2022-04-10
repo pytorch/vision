1a6b59a1683e5700eed6c1d308eb0c84f53813a6
abhijit_linux <f20190041@goa.bits-pilani.ac.in>
2022-04-10 22:28:21 +0530

    formatting

--- torchvision/ops/ciou_loss.py
+++ torchvision/ops/ciou_loss.py
@@ -1,7 +1,6 @@
 import math
 
 import torch
-from torch import Tensor
 
 from ..utils import _log_api_usage_once
 from .giou_loss import _upcast
@@ -18,8 +17,8 @@ def complete_box_iou_loss(
     Gradient-friendly IoU loss with an additional penalty that is non-zero when the
     boxes do not overlap overlap area, This loss function considers important geometrical
     factors such as  overlap area, normalized central point distance and aspect ratio.
-
     This loss is symmetric, so the boxes1 and boxes2 arguments are interchangeable.
+
     Both sets of boxes are expected to be in ``(x1, y1, x2, y2)`` format with
     ``0 <= x1 < x2`` and ``0 <= y1 < y2``, and The two boxes should have the
     same dimensions.
