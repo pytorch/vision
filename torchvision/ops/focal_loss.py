914e4418516bdc7411e0e765c4449af2d8de5623
Abhijit Deo <72816663+abhi-glitchhg@users.noreply.github.com>
2022-04-10 23:20:15 +0530

    Update focal_loss.py
    updated docstring for the `sigmoid_focal_loss`

--- torchvision/ops/focal_loss.py
+++ torchvision/ops/focal_loss.py
@@ -16,19 +16,20 @@ def sigmoid_focal_loss(
     Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
 
     Args:
-        inputs: A float tensor of arbitrary shape.
+        inputs: (Tensor) A float tensor of arbitrary shape.
                 The predictions for each example.
-        targets: A float tensor with the same shape as inputs. Stores the binary
+        targets: (Tensor) A float tensor with the same shape as inputs. Stores the binary
                 classification label for each element in inputs
                 (0 for the negative class and 1 for the positive class).
-        alpha: (optional) Weighting factor in range (0,1) to balance
+        alpha: (float) Weighting factor in range (0,1) to balance
                 positive vs negative examples or -1 for ignore. Default = 0.25
-        gamma: Exponent of the modulating factor (1 - p_t) to
+        gamma: (float) Exponent of the modulating factor (1 - p_t) to
                balance easy vs hard examples.
-        reduction: 'none' | 'mean' | 'sum'
+        reduction: (string) 'none' | 'mean' | 'sum'
                  'none': No reduction will be applied to the output.
                  'mean': The output will be averaged.
                  'sum': The output will be summed.
+                 Default is 'none'
     Returns:
         Loss tensor with the reduction option applied.
     """
