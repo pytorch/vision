6b5cb3ac06c07a89511e25fbdd482ae39110b63d
abhijit_linux <f20190041@goa.bits-pilani.ac.in>
2022-04-15 11:00:41 +0530

    minor edits

--- torchvision/ops/focal_loss.py
+++ torchvision/ops/focal_loss.py
@@ -21,14 +21,14 @@ def sigmoid_focal_loss(
         targets (Tensor): A float tensor with the same shape as inputs. Stores the binary
                 classification label for each element in inputs
                 (0 for the negative class and 1 for the positive class).
-        alpha (float, optional): Weighting factor in range (0,1) to balance
-                positive vs negative examples or -1 for ignore. Default: 0.25
-        gamma (float, optional): Exponent of the modulating factor (1 - p_t) to
-                balance easy vs hard examples. Default: 2
-        reduction (string, optional): ``'none'`` | ``'mean'`` | ``'sum'``
+        alpha (float): Weighting factor in range (0,1) to balance
+                positive vs negative examples or -1 for ignore. Default: ``0.25``.
+        gamma (float): Exponent of the modulating factor (1 - p_t) to
+                balance easy vs hard examples. Default: ``2``.
+        reduction (string): ``'none'`` | ``'mean'`` | ``'sum'``
                 ``'none'``: No reduction will be applied to the output.
                 ``'mean'``: The output will be averaged.
-                ``'sum'``: The output will be summed. Default: ``'none'``
+                ``'sum'``: The output will be summed. Default: ``'none'``.
     Returns:
         Loss tensor with the reduction option applied.
     """
