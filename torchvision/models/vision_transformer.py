5b6886e8c4fcc075b9cd2ae3d6f0cd5bc2e8b996
Yosua Michael Maranatha <yosuamichael@fb.com>
2022-04-21 10:42:59 +0100

    Remove publication_year and interpolation meta

--- torchvision/models/vision_transformer.py
+++ torchvision/models/vision_transformer.py
@@ -321,17 +321,13 @@ def _vision_transformer(
 _COMMON_META = {
     "task": "image_classification",
     "architecture": "ViT",
-    "publication_year": 2020,
     "categories": _IMAGENET_CATEGORIES,
-    "interpolation": InterpolationMode.BILINEAR,
 }
 
 _COMMON_SWAG_META = {
     **_COMMON_META,
-    "publication_year": 2022,
     "recipe": "https://github.com/facebookresearch/SWAG",
     "license": "https://github.com/facebookresearch/SWAG/blob/main/LICENSE",
-    "interpolation": InterpolationMode.BICUBIC,
 }
 
 
