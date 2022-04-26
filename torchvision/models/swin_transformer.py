09d63f5d00529b367a8286746a929a2ff8cf9eea
Vasilis Vryniotis <datumbox@users.noreply.github.com>
2022-04-26 09:16:00 +0100

    fix linter

--- torchvision/models/swin_transformer.py
+++ torchvision/models/swin_transformer.py
@@ -6,7 +6,6 @@ import torch.nn.functional as F
 from torch import nn, Tensor
 
 from ..ops.stochastic_depth import StochasticDepth
-from ..transforms._presets import InterpolationMode
 from ..utils import _log_api_usage_once
 from ._api import WeightsEnum
 from ._meta import _IMAGENET_CATEGORIES
