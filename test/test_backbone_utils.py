2a15d3a0c4572b5b807d4778d74b1c43c8d0a8e8
Vasilis Vryniotis <vvryniotis@fb.com>
2022-04-27 23:10:38 +0100

    Nomerge

--- test/test_backbone_utils.py
+++ test/test_backbone_utils.py
@@ -22,6 +22,8 @@ def get_available_models():
 
 @pytest.mark.parametrize("backbone_name", ("resnet18", "resnet50"))
 def test_resnet_fpn_backbone(backbone_name):
+    import sys
+    raise Exception(sys.platform)
     x = torch.rand(1, 3, 300, 300, dtype=torch.float32, device="cpu")
     model = resnet_fpn_backbone(backbone_name=backbone_name, weights=None)
     assert isinstance(model, BackboneWithFPN)
