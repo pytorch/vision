6d6bbd19c72dc4322f57ccee6ace9374c220feba
David Berard <dberard@fb.com>
2022-04-27 22:56:52 +0000

    Update on "[testing] enable nvfuser debug flags"
    [ghstack-poisoned]

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -6,4 +6,4 @@ eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
 python -m torch.utils.collect_env
-pytest --junitxml=test-results/junit.xml -v --durations 20
+PYTORCH_NVFUSER_DISABLE_FALLBACK=1 PYTORCH_JIT_LOG_LEVEL=">>graph_fuser" pytest --junitxml=test-results/junit.xml -v --durations 20
