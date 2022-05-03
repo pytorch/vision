972b6398d4bdfa76ef64ea4e67220599fba8132c
Nicolas Hug <contact@nicolas-hug.com>
2022-05-03 10:36:06 +0100

    Update torchvision/models/inception.py

--- torchvision/models/inception.py
+++ torchvision/models/inception.py
@@ -429,7 +429,7 @@ class Inception_V3_Weights(WeightsEnum):
 def inception_v3(*, weights: Optional[Inception_V3_Weights] = None, progress: bool = True, **kwargs: Any) -> Inception3:
     """
     Inception v3 model architecture from
-    `"Rethinking the Inception Architecture for Computer Vision" <http://arxiv.org/abs/1512.00567>`_.
+    `Rethinking the Inception Architecture for Computer Vision <http://arxiv.org/abs/1512.00567>`_.
     The required minimum input size of the model is 75x75.
 
     .. note::
