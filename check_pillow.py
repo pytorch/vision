77aabf772813bfaf47edacf339d2992248e79f0e
Philip Meier <github.pmeier@posteo.de>
2022-05-06 09:18:05 +0200

    more debug output

--- check_pillow.py
+++ check_pillow.py
@@ -12,7 +12,17 @@ tmpdir = tempfile.mkdtemp()
 
 index.fetch_distribution(requirement, tmpdir)
 
+print(f"entries in distmap: {len(index._distmap)}")
+print(f"pillow in distmap: {'pillow' in index._distmap}")
+print(f"Pillow in distmap: {'Pillow' in index._distmap}")
+
 dists = [dist for dist in index._distmap["pillow"] if dist.version.split(".")[0] == "9"]
+
+print(f"Total number of pillow dists: {len(index._distmap['pillow'])}")
+print(f"Number of pillow > 9 dists: {len(dists)}")
+
 sort_keys = [SortKey(*dist.hashcmp) for dist in dists]
 for sort_key in sort_keys:
     print(sort_key)
+
+print("#" * 80)
