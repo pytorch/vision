b1c9f7242e37e45e084f0ec6c7533c6b5eb03bcd
Philip Meier <github.pmeier@posteo.de>
2022-05-06 09:05:13 +0200

    fix

--- .circleci/unittest/windows/scripts/install.sh
+++ .circleci/unittest/windows/scripts/install.sh
@@ -43,7 +43,7 @@ fi
 source "$this_dir/set_cuda_envs.sh"
 
 printf "* Checking Pillow\n"
-"$this_dir/vc_env_helper.bat"python check_pillow.py
+"$this_dir/vc_env_helper.bat" python check_pillow.py
 
 printf "* Installing torchvision\n"
 "$this_dir/vc_env_helper.bat" python setup.py develop
