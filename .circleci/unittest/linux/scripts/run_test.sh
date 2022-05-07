66ffccf5bd0386fea6c6a5d0a7c51d3ab36990ac
David Berard <dberard@fb.com>
2022-05-06 18:59:09 -0700

    Update on "[testing] Test with NVFuser enabled"
    [ghstack-poisoned]

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -5,5 +5,5 @@ set -e
 eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
-PYTORCH_JIT_ENABLE_NVFUSER=1 python -m torch.utils.collect_env
-pytest --junitxml=test-results/junit.xml -v --durations 20
+python -m torch.utils.collect_env
+PYTORCH_JIT_ENABLE_NVFUSER=1 pytest --junitxml=test-results/junit.xml -v --durations 20
