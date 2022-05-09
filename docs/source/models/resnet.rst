fd0eccbf6880f0ce42103472c8298af0078e1ed4
puhuk <wetr235@gmail.com>
2022-05-09 23:59:49 +0900

    To resolve issue #5964
    Add note for resnet architecture

--- docs/source/models/resnet.rst
+++ docs/source/models/resnet.rst
@@ -6,6 +6,12 @@ ResNet
 The ResNet model is based on the `Deep Residual Learning for Image Recognition
 <https://arxiv.org/abs/1512.03385>`_ paper.
 
+.. note::
+    Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+    This variant is also known as ResNet V1.5 and improves accuracy according to
+    https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
 
 Model builders
 --------------
--- torchvision/models/resnet.py
+++ torchvision/models/resnet.py
@@ -575,6 +575,13 @@ class Wide_ResNet101_2_Weights(WeightsEnum):
 def resnet18(*, weights: Optional[ResNet18_Weights] = None, progress: bool = True, **kwargs: Any) -> ResNet:
     """ResNet-18 from `Deep Residual Learning for Image Recognition <https://arxiv.org/pdf/1512.03385.pdf>`__.
 
+    .. note::
+       Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+       while original implementation places the stride at the first 1x1 convolution(self.conv1)
+       according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+       This variant is also known as ResNet V1.5 and improves accuracy according to
+       https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
     Args:
         weights (:class:`~torchvision.models.ResNet18_Weights`, optional): The
             pretrained weights to use. See
