b0dbbd7a6439f2248e194f96ddb58d427670ca56
Philip Meier <github.pmeier@posteo.de>
2022-05-09 09:55:05 +0200

    Fix mypy job failing (#5970)

--- torchvision/models/_api.py
+++ torchvision/models/_api.py
@@ -1,7 +1,6 @@
 import importlib
 import inspect
 import sys
-from collections import OrderedDict
 from dataclasses import dataclass, fields
 from inspect import signature
 from typing import Any, Callable, Dict, cast
@@ -60,7 +59,7 @@ class WeightsEnum(StrEnum):
                 )
         return obj
 
-    def get_state_dict(self, progress: bool) -> OrderedDict:
+    def get_state_dict(self, progress: bool) -> Dict[str, Any]:
         return load_state_dict_from_url(self.url, progress=progress)
 
     def __repr__(self) -> str:
