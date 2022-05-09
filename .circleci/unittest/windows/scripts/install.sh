2b416ab38dbde25a5026a04279de8342710a2be2
Philip Meier <github.pmeier@posteo.de>
2022-05-09 09:25:01 +0200

    cleanup

--- .circleci/unittest/windows/scripts/install.sh
+++ .circleci/unittest/windows/scripts/install.sh
@@ -42,13 +42,5 @@ fi
 
 source "$this_dir/set_cuda_envs.sh"
 
-printf "* Checking Pillow w/ packaging\n"
-"$this_dir/vc_env_helper.bat" pip install --upgrade packaging
-"$this_dir/vc_env_helper.bat" python check_pillow.py
-
-printf "* Checking Pillow w/o packaging\n"
-"$this_dir/vc_env_helper.bat" pip uninstall -y packaging
-"$this_dir/vc_env_helper.bat" python check_pillow.py
-
 printf "* Installing torchvision\n"
 "$this_dir/vc_env_helper.bat" python setup.py develop
