b422cc361eb2e7f4a0c1915a93836b7b233dcdd8
David Berard <dberard@fb.com>
2022-05-16 10:27:11 -0700

    temporaily disable nvfuser
    Waiting on pytorch/pytorch#77471 to fix the breakage, this will stop
    failures for now.
    
    ghstack-source-id: 2392e6b422088cc5a4763a8116107a24ac92f41a
    Pull Request resolved: https://github.com/pytorch/vision/pull/6029

--- .circleci/unittest/windows/scripts/run_test.sh
+++ .circleci/unittest/windows/scripts/run_test.sh
@@ -9,4 +9,4 @@ this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$this_dir/set_cuda_envs.sh"
 
 python -m torch.utils.collect_env
-pytest --junitxml=test-results/junit.xml -v --durations 20
+PYTORCH_JIT_ENABLE_NVFUSER=0 pytest --junitxml=test-results/junit.xml -v --durations 20
