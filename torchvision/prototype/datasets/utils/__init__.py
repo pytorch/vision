1eb702f0b05b1e51d12adff318cf2770486f44ab
Nicolas Hug <contact@nicolas-hug.com>
2022-05-24 16:42:05 +0100

    Removed torchvision/prototype

--- torchvision/prototype/datasets/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import _internal  # usort: skip
-from ._dataset import Dataset
-from ._resource import OnlineResource, HttpResource, GDriveResource, ManualDownloadResource, KaggleDownloadResource
