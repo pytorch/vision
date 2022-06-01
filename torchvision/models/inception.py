27a20d0d38a128c19c6500fa5f24fa4cbe5c2516
Alexander Pivovarov <apivovarov@gmail.com>
2022-06-01 14:36:12 -0700

    Fix Inception transform_input to use Float tensors

--- torchvision/models/inception.py
+++ torchvision/models/inception.py
@@ -94,9 +94,9 @@ class Inception3(nn.Module):
 
     def _transform_input(self, x: Tensor) -> Tensor:
         if self.transform_input:
-            x_ch0 = torch.unsqueeze(x[:, 0], 1) * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
-            x_ch1 = torch.unsqueeze(x[:, 1], 1) * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
-            x_ch2 = torch.unsqueeze(x[:, 2], 1) * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
+            x_ch0 = torch.unsqueeze(x[:, 0], 1) * torch.tensor(0.229 / 0.5) + torch.tensor((0.485 - 0.5) / 0.5)
+            x_ch1 = torch.unsqueeze(x[:, 1], 1) * torch.tensor(0.224 / 0.5) + torch.tensor((0.456 - 0.5) / 0.5)
+            x_ch2 = torch.unsqueeze(x[:, 2], 1) * torch.tensor(0.225 / 0.5) + torch.tensor((0.406 - 0.5) / 0.5)
             x = torch.cat((x_ch0, x_ch1, x_ch2), 1)
         return x
 
