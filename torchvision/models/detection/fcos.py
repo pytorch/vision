0aae00dbe98e244c2d27dcec338f2ac87a546747
Yosua Michael Maranatha <yosuamichael@fb.com>
2022-05-31 19:00:59 -0700

    [fbsync] update paper link for FCOS refence (#6035)
    Summary:
    * update paper link for FCOS refence
    
    * remove 'updated version'
    
    Reviewed By: NicolasHug
    
    Differential Revision: D36760930
    
    fbshipit-source-id: 4916de8cb4ec75166bfbe8a32277606746731381
    
    Co-authored-by: Vasilis Vryniotis <datumbox@users.noreply.github.com>

--- torchvision/models/detection/fcos.py
+++ torchvision/models/detection/fcos.py
@@ -201,7 +201,10 @@ class FCOSClassificationHead(nn.Module):
 
 class FCOSRegressionHead(nn.Module):
     """
-    A regression head for use in FCOS.
+    A regression head for use in FCOS, which combines regression branch and center-ness branch.
+    This can obtain better performance.
+
+    Reference: `FCOS: A simple and strong anchor-free object detector <https://arxiv.org/abs/2006.09214>`_.
 
     Args:
         in_channels (int): number of channels of the input feature
@@ -680,6 +683,7 @@ def fcos_resnet50_fpn(
     Constructs a FCOS model with a ResNet-50-FPN backbone.
 
     Reference: `FCOS: Fully Convolutional One-Stage Object Detection <https://arxiv.org/abs/1904.01355>`_.
+               `FCOS: A simple and strong anchor-free object detector <https://arxiv.org/abs/2006.09214>`_.
 
     The input to the model is expected to be a list of tensors, each of shape ``[C, H, W]``, one for each
     image, and should be in ``0-1`` range. Different images can have different sizes.
