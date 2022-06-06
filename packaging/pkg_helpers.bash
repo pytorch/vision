f6349230a5a5a03c989e0077c2a77b63af259f64
Joao Gomes <jdsgomes@fb.com>
2022-06-06 09:21:58 -0700

    [fbsync] [BE] Unify version computation (#6117)
    Summary:
    * [BE] Unify version computation
    
    Instead of hardcoding dev version in various script, use one from
    `version.txt` if `setup_build_version` is called without arguments
    
    Also, pass `--pre` option to M1 build/test pip install commands to build
    TorchVision against nightly pytorch
    
    * Pin torchvision dependency to a specific pytorch version
    
    Reviewed By: NicolasHug
    
    Differential Revision: D36931835
    
    fbshipit-source-id: b1e4c4be432b4ec1d071677856ca1ac9bc78c7ee

--- packaging/pkg_helpers.bash
+++ packaging/pkg_helpers.bash
@@ -100,7 +100,15 @@ setup_cuda() {
 # Usage: setup_build_version 0.2.0
 setup_build_version() {
   if [[ -z "$BUILD_VERSION" ]]; then
-    export BUILD_VERSION="$1.dev$(date "+%Y%m%d")$VERSION_SUFFIX"
+    if [[ -z "$1" ]]; then
+      SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+      # version.txt for some reason has `a` character after major.minor.rev
+      # command below yields 0.10.0 from version.txt containing 0.10.0a0
+      _VERSION_BASE=$( cut -f 1 -d a "$SCRIPT_DIR/../version.txt" )
+    else
+      _VERSION_BASE="$1"
+    fi
+    export BUILD_VERSION="$_VERSION_BASE.dev$(date "+%Y%m%d")$VERSION_SUFFIX"
   else
     export BUILD_VERSION="$BUILD_VERSION$VERSION_SUFFIX"
   fi
