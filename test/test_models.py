93b3e844a18cd01e18e98be57282e93cc8714995
puhuk <wetr235@gmail.com>
2022-06-14 00:15:26 +0900

    To resolve issue #6155
    Fix warnings in the tests

--- test/test_models.py
+++ test/test_models.py
@@ -870,6 +870,7 @@ def test_quantized_classification_model(model_fn):
         "num_classes": 5,
         "input_shape": (1, 3, 224, 224),
         "quantize": True,
+        "init_weights": True,
     }
     model_name = model_fn.__name__
     kwargs = {**defaults, **_model_params.get(model_name, {})}
