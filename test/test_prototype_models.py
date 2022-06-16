9282ea5e4d5e2e0c329fb3468044197af49b7c27
Yosua Michael Maranatha <yosuamichael@fb.com>
2022-06-16 14:54:37 +0100

    Update the test for raft_stereo

--- test/test_prototype_models.py
+++ test/test_prototype_models.py
@@ -1,28 +1,25 @@
 import pytest
 import torch
-import torch.fx
 import torchvision.prototype.models.depth.stereo.raft_stereo as raft_stereo
 from common_utils import set_rng_seed, cpu_and_gpu
 
 
-@pytest.mark.parametrize("model_builder", (raft_stereo.raft_stereo, raft_stereo.raft_stereo_fast))
-@pytest.mark.parametrize("model_mode", ("standard", "scripted", "fx"))
+@pytest.mark.parametrize("model_builder", (raft_stereo.raft_stereo, raft_stereo.raft_stereo_realtime))
+@pytest.mark.parametrize("model_mode", ("standard", "scripted"))
 @pytest.mark.parametrize("dev", cpu_and_gpu())
 def test_raft_stereo(model_builder, model_mode, dev):
-    # A simple test to make sure the model can do forward pass, jit scriptable, and fx traceable
+    # A simple test to make sure the model can do forward pass and jit scriptable
     set_rng_seed(0)
 
     model = model_builder().eval().to(dev)
-    num_iters = model.num_iters
     if model_mode == "standard":
         model = torch.jit.script(model)
-    elif model_mode == "fx":
-        model = torch.fx.symbolic_trace(model)
 
     img1 = torch.rand(1, 3, 64, 64).to(dev)
     img2 = torch.rand(1, 3, 64, 64).to(dev)
+    num_iters = 3
 
-    preds = model(img1, img2)
+    preds = model(img1, img2, num_iters=num_iters)
 
     assert len(preds) == num_iters, "Number of predictions should be the same as model.num_iters"
 
