b013fdf4e107cca203bc9737858ed0f9e726a38f
Yosua Michael Maranatha <yosuamichael@fb.com>
2022-06-27 13:55:03 -0700

    [fbsync] Added antialias arg to resized crop transform and op (#6193)
    Reviewed By: NicolasHug
    
    Differential Revision: D37450356
    
    fbshipit-source-id: ebbd841dd10609098998d2f2969cd547c36ea035

--- test/test_transforms_tensor.py
+++ test/test_transforms_tensor.py
@@ -447,10 +447,17 @@ class TestResize:
         ],
     )
     @pytest.mark.parametrize("interpolation", [NEAREST, BILINEAR, BICUBIC])
-    def test_resized_crop(self, scale, ratio, size, interpolation, device):
+    @pytest.mark.parametrize("antialias", [None, True, False])
+    def test_resized_crop(self, scale, ratio, size, interpolation, antialias, device):
+
+        if antialias and interpolation == NEAREST:
+            pytest.skip("Can not resize if interpolation mode is NEAREST and antialias=True")
+
         tensor = torch.randint(0, 256, size=(3, 44, 56), dtype=torch.uint8, device=device)
         batch_tensors = torch.randint(0, 256, size=(4, 3, 44, 56), dtype=torch.uint8, device=device)
-        transform = T.RandomResizedCrop(size=size, scale=scale, ratio=ratio, interpolation=interpolation)
+        transform = T.RandomResizedCrop(
+            size=size, scale=scale, ratio=ratio, interpolation=interpolation, antialias=antialias
+        )
         s_transform = torch.jit.script(transform)
         _test_transform_vs_scripted(transform, s_transform, tensor)
         _test_transform_vs_scripted_on_batch(transform, s_transform, batch_tensors)
