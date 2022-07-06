9abedf6f3670e6fe11e71ac154ac3d64a6239a28
Vasilis Vryniotis <vvryniotis@fb.com>
2022-07-06 09:19:33 -0700

    [fbsync] Vectorize box decoding in FCOS (#6203)
    Summary:
    * basic structure
    
    * added constrains
    
    * fixed errors
    
    * thanks to vadim!
    
    * addressing the comments and added docstrign
    
    * Apply suggestions from code review
    
    Reviewed By: jdsgomes
    
    Differential Revision: D37643906
    
    fbshipit-source-id: 13b15e886d23e259a5ed6926677cac55f160f206
    
    Co-authored-by: Vasilis Vryniotis <datumbox@users.noreply.github.com>

--- torchvision/models/detection/fcos.py
+++ torchvision/models/detection/fcos.py
@@ -87,14 +87,12 @@ class FCOSHead(nn.Module):
         loss_cls = sigmoid_focal_loss(cls_logits, gt_classes_targets, reduction="sum")
 
         # regression loss: GIoU loss
-        # TODO: vectorize this instead of using a for loop
-        pred_boxes = [
-            self.box_coder.decode_single(bbox_regression_per_image, anchors_per_image)
-            for anchors_per_image, bbox_regression_per_image in zip(anchors, bbox_regression)
-        ]
+
+        pred_boxes = self.box_coder.decode_all(bbox_regression, anchors)
+
         # amp issue: pred_boxes need to convert float
         loss_bbox_reg = generalized_box_iou_loss(
-            torch.stack(pred_boxes)[foregroud_mask].float(),
+            pred_boxes[foregroud_mask],
             torch.stack(all_gt_boxes_targets)[foregroud_mask],
             reduction="sum",
         )
