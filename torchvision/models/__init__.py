c5b28398cd48d2f3403c7c8eeefbaba9df05fcfe
Ponku <teodor.poncu@gmail.com>
2022-08-05 18:16:12 +0100

    rebased + addresed comments

--- torchvision/models/__init__.py
+++ torchvision/models/__init__.py
@@ -8,11 +8,11 @@ from .mnasnet import *
 from .mobilenet import *
 from .regnet import *
 from .resnet import *
+from .maxvit import *
 from .shufflenetv2 import *
 from .squeezenet import *
 from .vgg import *
 from .vision_transformer import *
 from .swin_transformer import *
-from .maxvit import *
 from . import detection, optical_flow, quantization, segmentation, video
 from ._api import get_model, get_model_weights, get_weight, list_models
