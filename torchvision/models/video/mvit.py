cb62dce8fd9be7fa6adbcae5c2cb0ac66b57f8ab
Vasilis Vryniotis <vvryniotis@fb.com>
2022-08-08 11:00:45 +0100

    Refactor the relative positional code.

--- torchvision/models/video/mvit.py
+++ torchvision/models/video/mvit.py
@@ -24,10 +24,7 @@ __all__ = [
 ]
 
 
-# Reference: https://github.com/facebookresearch/SlowFast/commit/1aebd71a2efad823d52b827a3deaf15a56cf4932#
-
-
-def get_rel_pos(rel_pos: torch.Tensor, d: int) -> torch.Tensor:
+def _interpolate(rel_pos: torch.Tensor, d: int) -> torch.Tensor:
     if rel_pos.shape[0] == d:
         return rel_pos
 
@@ -42,87 +39,62 @@ def get_rel_pos(rel_pos: torch.Tensor, d: int) -> torch.Tensor:
     )
 
 
-def cal_rel_pos_spatial(
+def add_rel_pos(
     attn: torch.Tensor,
     q: torch.Tensor,
     q_shape: Tuple[int, int, int],
     k_shape: Tuple[int, int, int],
     rel_pos_h: torch.Tensor,
     rel_pos_w: torch.Tensor,
+    rel_pos_t: torch.Tensor,
 ) -> torch.Tensor:
+    # Modified code from: https://github.com/facebookresearch/SlowFast/commit/1aebd71a2efad823d52b827a3deaf15a56cf4932
     q_t, q_h, q_w = q_shape
     k_t, k_h, k_w = k_shape
     dh = int(2 * max(q_h, k_h) - 1)
     dw = int(2 * max(q_w, k_w) - 1)
+    dt = int(2 * max(q_t, k_t) - 1)
 
     # Scale up rel pos if shapes for q and k are different.
     q_h_ratio = max(k_h / q_h, 1.0)
     k_h_ratio = max(q_h / k_h, 1.0)
-    dist_h = torch.arange(q_h)[:, None] * q_h_ratio - torch.arange(k_h)[None, :] * k_h_ratio
-    dist_h += (k_h - 1) * k_h_ratio
+    dist_h = torch.arange(q_h)[:, None] * q_h_ratio - (torch.arange(k_h)[None, :] + (1.0 - k_h)) * k_h_ratio
     q_w_ratio = max(k_w / q_w, 1.0)
     k_w_ratio = max(q_w / k_w, 1.0)
-    dist_w = torch.arange(q_w)[:, None] * q_w_ratio - torch.arange(k_w)[None, :] * k_w_ratio
-    dist_w += (k_w - 1) * k_w_ratio
+    dist_w = torch.arange(q_w)[:, None] * q_w_ratio - (torch.arange(k_w)[None, :] + (1.0 - k_w)) * k_w_ratio
+    q_t_ratio = max(k_t / q_t, 1.0)
+    k_t_ratio = max(q_t / k_t, 1.0)
+    dist_t = torch.arange(q_t)[:, None] * q_t_ratio - (torch.arange(k_t)[None, :] + (1.0 - k_t)) * k_t_ratio
 
     # Intepolate rel pos if needed.
-    rel_pos_h = get_rel_pos(rel_pos_h, dh)
-    rel_pos_w = get_rel_pos(rel_pos_w, dw)
+    rel_pos_h = _interpolate(rel_pos_h, dh)
+    rel_pos_w = _interpolate(rel_pos_w, dw)
+    rel_pos_t = _interpolate(rel_pos_t, dt)
     Rh = rel_pos_h[dist_h.long()]
     Rw = rel_pos_w[dist_w.long()]
+    Rt = rel_pos_t[dist_t.long()]
 
-    B, n_head, q_N, dim = q.shape
+    B, n_head, _, dim = q.shape
 
     r_q = q[:, :, 1:].reshape(B, n_head, q_t, q_h, q_w, dim)
     rel_h_q = torch.einsum("bythwc,hkc->bythwk", r_q, Rh)  # [B, H, q_t, qh, qw, k_h]
     rel_w_q = torch.einsum("bythwc,wkc->bythwk", r_q, Rw)  # [B, H, q_t, qh, qw, k_w]
-
-    attn[:, :, 1:, 1:] = (
-        attn[:, :, 1:, 1:].view(B, -1, q_t, q_h, q_w, k_t, k_h, k_w)
-        + rel_h_q[:, :, :, :, :, None, :, None]
-        + rel_w_q[:, :, :, :, :, None, None, :]
-    ).view(B, -1, q_t * q_h * q_w, k_t * k_h * k_w)
-
-    return attn
-
-
-def cal_rel_pos_temporal(
-    attn: torch.Tensor,
-    q: torch.Tensor,
-    q_shape: Tuple[int, int, int],
-    k_shape: Tuple[int, int, int],
-    rel_pos_t: torch.Tensor,
-) -> torch.Tensor:
-    """
-    Temporal Relative Positional Embeddings.
-    """
-    q_t, q_h, q_w = q_shape
-    k_t, k_h, k_w = k_shape
-    dt = int(2 * max(q_t, k_t) - 1)
-    # Intepolate rel pos if needed.
-    rel_pos_t = get_rel_pos(rel_pos_t, dt)
-
-    # Scale up rel pos if shapes for q and k are different.
-    q_t_ratio = max(k_t / q_t, 1.0)
-    k_t_ratio = max(q_t / k_t, 1.0)
-    dist_t = torch.arange(q_t)[:, None] * q_t_ratio - torch.arange(k_t)[None, :] * k_t_ratio
-    dist_t += (k_t - 1) * k_t_ratio
-    Rt = rel_pos_t[dist_t.long()]
-
-    B, n_head, q_N, dim = q.shape
-
-    r_q = q[:, :, 1:].reshape(B, n_head, q_t, q_h, q_w, dim)
     # [B, H, q_t, q_h, q_w, dim] -> [q_t, B, H, q_h, q_w, dim] -> [q_t, B*H*q_h*q_w, dim]
     r_q = r_q.permute(2, 0, 1, 3, 4, 5).reshape(q_t, B * n_head * q_h * q_w, dim)
-
     # [q_t, B*H*q_h*q_w, dim] * [q_t, dim, k_t] = [q_t, B*H*q_h*q_w, k_t] -> [B*H*q_h*q_w, q_t, k_t]
-    rel = torch.matmul(r_q, Rt.transpose(1, 2)).transpose(0, 1)
+    rel_q_t = torch.matmul(r_q, Rt.transpose(1, 2)).transpose(0, 1)
     # [B*H*q_h*q_w, q_t, k_t] -> [B, H, q_t, q_h, q_w, k_t]
-    rel = rel.view(B, n_head, q_h, q_w, q_t, k_t).permute(0, 1, 4, 2, 3, 5)
+    rel_q_t = rel_q_t.view(B, n_head, q_h, q_w, q_t, k_t).permute(0, 1, 4, 2, 3, 5)
 
-    attn[:, :, 1:, 1:] = (
-        attn[:, :, 1:, 1:].view(B, -1, q_t, q_h, q_w, k_t, k_h, k_w) + rel[:, :, :, :, :, :, None, None]
-    ).view(B, -1, q_t * q_h * q_w, k_t * k_h * k_w)
+    # Combine rel pos.
+    rel_pos = (
+        rel_h_q[:, :, :, :, :, None, :, None]
+        + rel_w_q[:, :, :, :, :, None, None, :]
+        + rel_q_t[:, :, :, :, :, :, None, None]
+    ).reshape(B, n_head, q_t * q_h * q_w, k_t * k_h * k_w)
+
+    # Add it to attention
+    attn[:, :, 1:, 1:] += rel_pos
 
     return attn
 
@@ -211,9 +183,7 @@ class Pool(nn.Module):
         return x, (T, H, W)
 
 
-torch.fx.wrap("get_rel_pos")
-torch.fx.wrap("cal_rel_pos_spatial")
-torch.fx.wrap("cal_rel_pos_temporal")
+torch.fx.wrap("add_rel_pos")
 
 
 class MultiscaleAttention(nn.Module):
@@ -312,30 +282,23 @@ class MultiscaleAttention(nn.Module):
         q, k, v = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).transpose(1, 3).unbind(dim=2)
 
         if self.pool_k is not None:
-            k, k_shape = self.pool_k(k, thw)
+            k, k_thw = self.pool_k(k, thw)
         else:
-            k_shape = thw
+            k_thw = thw
         if self.pool_v is not None:
             v = self.pool_v(v, thw)[0]
         if self.pool_q is not None:
             q, thw = self.pool_q(q, thw)
 
         attn = torch.matmul(self.scaler * q, k.transpose(2, 3))
-        if self.rel_pos_h is not None and self.rel_pos_w is not None:
-            attn = cal_rel_pos_spatial(
+        if self.rel_pos_h is not None and self.rel_pos_w is not None and self.rel_pos_t is not None:
+            attn = add_rel_pos(
                 attn,
                 q,
                 thw,
-                k_shape,
+                k_thw,
                 self.rel_pos_h,
                 self.rel_pos_w,
-            )
-        if self.rel_pos_t is not None:
-            attn = cal_rel_pos_temporal(
-                attn,
-                q,
-                thw,
-                k_shape,
                 self.rel_pos_t,
             )
         attn = attn.softmax(dim=-1)
