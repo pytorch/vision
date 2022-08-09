be6d7e080a9b22de59089efd8268f99c19dfa5c9
Vasilis Vryniotis <vvryniotis@fb.com>
2022-08-09 14:02:09 +0100

    Add ported weights.

--- torchvision/models/video/mvit.py
+++ torchvision/models/video/mvit.py
@@ -617,7 +617,34 @@ class MViT_V1_B_Weights(WeightsEnum):
 
 
 class MViT_V2_S_Weights(WeightsEnum):
-    pass
+    KINETICS400_V1 = Weights(
+        url="https://download.pytorch.org/models/mvit_v2_s-ae3be167.pth",
+        transforms=partial(
+            VideoClassification,
+            crop_size=(224, 224),
+            resize_size=(256,),
+            mean=(0.45, 0.45, 0.45),
+            std=(0.225, 0.225, 0.225),
+        ),
+        meta={
+            "min_size": (224, 224),
+            "min_temporal_size": 16,
+            "categories": _KINETICS400_CATEGORIES,
+            "recipe": "https://github.com/facebookresearch/SlowFast/blob/main/MODEL_ZOO.md",
+            "_docs": (
+                "The weights were ported from the paper. The accuracies are estimated on video-level "
+                "with parameters `frame_rate=7.5`, `clips_per_video=5`, and `clip_len=16`"
+            ),
+            "num_params": 34537744,
+            "_metrics": {
+                "Kinetics-400": {
+                    "acc@1": 80.757,
+                    "acc@5": 94.665,
+                }
+            },
+        },
+    )
+    DEFAULT = KINETICS400_V1
 
 
 @register_model()
