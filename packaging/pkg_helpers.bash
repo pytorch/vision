c31db6d640fc92fcabbd8ae7212eb979096ca0be
Andrey Talman <atalman@fb.com>
2022-08-12 11:10:17 -0400

    Using conda metapackage when installing cuda 11.6 and 11.7 (#6371)
    * Testing conda metapackage
    
    * Testing
    
    * Testing pytorch-cuda
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Using new metapackage
    
    * Testing
    
    * Simplifing logic
    
    * Further simplifying

--- packaging/pkg_helpers.bash
+++ packaging/pkg_helpers.bash
@@ -261,8 +261,11 @@ setup_conda_cudatoolkit_constraint() {
     export CONDA_BUILD_VARIANT="cpu"
   else
     case "$CU_VERSION" in
+      cu117)
+        export CONDA_CUDATOOLKIT_CONSTRAINT="- pytorch-cuda=11.7 # [not osx]"
+        ;;
       cu116)
-        export CONDA_CUDATOOLKIT_CONSTRAINT="- cuda >=11.6,<11.7 # [not osx]"
+        export CONDA_CUDATOOLKIT_CONSTRAINT="- pytorch-cuda=11.6 # [not osx]"
         ;;
       cu113)
         export CONDA_CUDATOOLKIT_CONSTRAINT="- cudatoolkit >=11.3,<11.4 # [not osx]"
@@ -290,8 +293,11 @@ setup_conda_cudatoolkit_plain_constraint() {
     export CMAKE_USE_CUDA=0
   else
     case "$CU_VERSION" in
+      cu117)
+        export CONDA_CUDATOOLKIT_CONSTRAINT="pytorch-cuda=11.7"
+        ;;
       cu116)
-        export CONDA_CUDATOOLKIT_CONSTRAINT="cuda=11.6"
+        export CONDA_CUDATOOLKIT_CONSTRAINT="pytorch-cuda=11.6"
         ;;
       cu113)
         export CONDA_CUDATOOLKIT_CONSTRAINT="cudatoolkit=11.3"
