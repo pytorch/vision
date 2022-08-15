763a74a9e0ee80fac1fee0df43f3cc042ee387e2
Philip Meier <github.pmeier@posteo.de>
2022-08-15 02:54:05 +0200

    remove TODO from BoundingBox.to_format()

--- torchvision/prototype/features/_bounding_box.py
+++ torchvision/prototype/features/_bounding_box.py
@@ -60,9 +60,6 @@ class BoundingBox(_Feature):
         )
 
     def to_format(self, format: Union[str, BoundingBoxFormat]) -> BoundingBox:
-        # TODO: this is useful for developing and debugging but we should remove or at least revisit this before we
-        #  promote this out of the prototype state
-
         # import at runtime to avoid cyclic imports
         from torchvision.prototype.transforms.functional import convert_bounding_box_format
 
