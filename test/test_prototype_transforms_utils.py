6f22eda410d01d1761e7bbdd84466fa60280d73c
Philip Meier <github.pmeier@posteo.de>
2022-08-18 16:12:41 +0200

    use concreate examples in test rather than abstract ones

--- test/test_prototype_transforms_utils.py
+++ test/test_prototype_transforms_utils.py
@@ -1,24 +1,44 @@
+import PIL.Image
 import pytest
-from torchvision.prototype.transforms._utils import has_all, has_any
+
+import torch
+
+from test_prototype_transforms_functional import make_bounding_box, make_image, make_segmentation_mask
+
+from torchvision.prototype import features
+from torchvision.prototype.transforms._utils import has_all, has_any, is_simple_tensor
+from torchvision.prototype.transforms.functional import to_image_pil
+
+
+IMAGE = make_image(color_space=features.ColorSpace.RGB)
+BOUNDING_BOX = make_bounding_box(format=features.BoundingBoxFormat.XYXY, image_size=IMAGE.image_size)
+SEGMENTATION_MASK = make_segmentation_mask(size=IMAGE.image_size)
 
 
 @pytest.mark.parametrize(
     ("sample", "types", "expected"),
     [
-        ((0, 0.0, ""), (int,), True),
-        ((0, 0.0, ""), (float,), True),
-        ((0, 0.0, ""), (str,), True),
-        ((0, 0.0, ""), (int, float), True),
-        ((0, 0.0, ""), (int, str), True),
-        ((0, 0.0, ""), (float, str), True),
-        (("",), (int, float), False),
-        ((0.0,), (int, str), False),
-        ((0,), (float, str), False),
-        ((0, 0.0, ""), (int, float, str), True),
-        ((), (int, float, str), False),
-        ((0, 0.0, ""), (lambda obj: isinstance(obj, int),), True),
-        ((0, 0.0, ""), (lambda _: False,), False),
-        ((0, 0.0, ""), (lambda _: True,), True),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (features.Image,), True),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (features.BoundingBox,), True),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (features.SegmentationMask,), True),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (features.Image, features.BoundingBox), True),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (features.Image, features.SegmentationMask), True),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (features.BoundingBox, features.SegmentationMask), True),
+        ((SEGMENTATION_MASK,), (features.Image, features.BoundingBox), False),
+        ((BOUNDING_BOX,), (features.Image, features.SegmentationMask), False),
+        ((IMAGE,), (features.BoundingBox, features.SegmentationMask), False),
+        (
+            (IMAGE, BOUNDING_BOX, SEGMENTATION_MASK),
+            (features.Image, features.BoundingBox, features.SegmentationMask),
+            True,
+        ),
+        ((), (features.Image, features.BoundingBox, features.SegmentationMask), False),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (lambda obj: isinstance(obj, features.Image),), True),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (lambda _: False,), False),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (lambda _: True,), True),
+        ((IMAGE,), (features.Image, PIL.Image.Image, is_simple_tensor), True),
+        ((torch.Tensor(IMAGE),), (features.Image, PIL.Image.Image, is_simple_tensor), True),
+        ((to_image_pil(IMAGE),), (features.Image, PIL.Image.Image, is_simple_tensor), True),
     ],
 )
 def test_has_any(sample, types, expected):
@@ -28,23 +48,35 @@ def test_has_any(sample, types, expected):
 @pytest.mark.parametrize(
     ("sample", "types", "expected"),
     [
-        ((0, 0.0, ""), (int,), True),
-        ((0, 0.0, ""), (float,), True),
-        ((0, 0.0, ""), (str,), True),
-        ((0, 0.0, ""), (int, float), True),
-        ((0, 0.0, ""), (int, str), True),
-        ((0, 0.0, ""), (float, str), True),
-        ((0, 0.0, ""), (int, float, str), True),
-        ((0.0, ""), (int, float), False),
-        ((0.0, ""), (int, str), False),
-        ((0, ""), (float, str), False),
-        ((0, 0.0, ""), (int, float, str), True),
-        ((0.0, ""), (int, float, str), False),
-        ((0, ""), (int, float, str), False),
-        ((0, 0.0), (int, float, str), False),
-        ((0, 0.0, ""), (lambda obj: isinstance(obj, (int, float, str)),), True),
-        ((0, 0.0, ""), (lambda _: False,), False),
-        ((0, 0.0, ""), (lambda _: True,), True),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (features.Image,), True),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (features.BoundingBox,), True),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (features.SegmentationMask,), True),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (features.Image, features.BoundingBox), True),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (features.Image, features.SegmentationMask), True),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (features.BoundingBox, features.SegmentationMask), True),
+        (
+            (IMAGE, BOUNDING_BOX, SEGMENTATION_MASK),
+            (features.Image, features.BoundingBox, features.SegmentationMask),
+            True,
+        ),
+        ((BOUNDING_BOX, SEGMENTATION_MASK), (features.Image, features.BoundingBox), False),
+        ((BOUNDING_BOX, SEGMENTATION_MASK), (features.Image, features.SegmentationMask), False),
+        ((IMAGE, SEGMENTATION_MASK), (features.BoundingBox, features.SegmentationMask), False),
+        (
+            (IMAGE, BOUNDING_BOX, SEGMENTATION_MASK),
+            (features.Image, features.BoundingBox, features.SegmentationMask),
+            True,
+        ),
+        ((BOUNDING_BOX, SEGMENTATION_MASK), (features.Image, features.BoundingBox, features.SegmentationMask), False),
+        ((IMAGE, SEGMENTATION_MASK), (features.Image, features.BoundingBox, features.SegmentationMask), False),
+        ((IMAGE, BOUNDING_BOX), (features.Image, features.BoundingBox, features.SegmentationMask), False),
+        (
+            (IMAGE, BOUNDING_BOX, SEGMENTATION_MASK),
+            (lambda obj: isinstance(obj, (features.Image, features.BoundingBox, features.SegmentationMask)),),
+            True,
+        ),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (lambda _: False,), False),
+        ((IMAGE, BOUNDING_BOX, SEGMENTATION_MASK), (lambda _: True,), True),
     ],
 )
 def test_has_all(sample, types, expected):
