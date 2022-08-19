cbfc2db8e378b72ad9cee8ac8318816149b819f5
Philip Meier <github.pmeier@posteo.de>
2022-08-19 12:02:31 +0200

    add dispatcher for erase image kernels

--- torchvision/prototype/transforms/functional/_augment.py
+++ torchvision/prototype/transforms/functional/_augment.py
@@ -1,6 +1,9 @@
+from typing import Any
+
 import PIL.Image
 
 import torch
+from torchvision.prototype import features
 from torchvision.transforms import functional_tensor as _FT
 from torchvision.transforms.functional import pil_to_tensor, to_pil_image
 
@@ -14,3 +17,13 @@ def erase_image_pil(
     t_img = pil_to_tensor(img)
     output = erase_image_tensor(t_img, i=i, j=j, h=h, w=w, v=v, inplace=inplace)
     return to_pil_image(output, mode=img.mode)
+
+
+def erase(inpt: Any, i: int, j: int, h: int, w: int, v: torch.Tensor, inplace: bool = False):
+    if isinstance(inpt, torch.Tensor):
+        output = erase_image_tensor(inpt, i=i, j=j, h=h, w=w, v=v, inplace=inplace)
+        if isinstance(inpt, features.Image):
+            output = features.Image.new_like(inpt, output)
+        return output
+    else:  # isinstance(inpt, PIL.Image.Image):
+        return erase_image_pil(inpt, i=i, j=j, h=h, w=w, v=v, inplace=inplace)
