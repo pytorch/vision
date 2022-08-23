940fe90945cad1f47b2f132dab78adb87027573e
Vasilis Vryniotis <datumbox@users.noreply.github.com>
2022-08-23 09:53:10 +0100

    Fix linter

--- torchvision/csrc/io/image/cpu/decode_png.cpp
+++ torchvision/csrc/io/image/cpu/decode_png.cpp
@@ -59,14 +59,17 @@ torch::Tensor decode_png(
   reader.ptr = png_const_bytep(datap) + 8;
   reader.count = datap_len - 8;
 
-  auto read_callback =
-      [](png_structp png_ptr, png_bytep output, png_size_t bytes) {
-        auto reader = static_cast<Reader*>(png_get_io_ptr(png_ptr));
-        TORCH_CHECK(reader->count >= bytes, "Out of bound read in decode_png. Probably, the input image is corrupted");
-        std::copy(reader->ptr, reader->ptr + bytes, output);
-        reader->ptr += bytes;
-        reader->count -= bytes;
-      };
+  auto read_callback = [](png_structp png_ptr,
+                          png_bytep output,
+                          png_size_t bytes) {
+    auto reader = static_cast<Reader*>(png_get_io_ptr(png_ptr));
+    TORCH_CHECK(
+        reader->count >= bytes,
+        "Out of bound read in decode_png. Probably, the input image is corrupted");
+    std::copy(reader->ptr, reader->ptr + bytes, output);
+    reader->ptr += bytes;
+    reader->count -= bytes;
+  };
   png_set_sig_bytes(png_ptr, 8);
   png_set_read_fn(png_ptr, &reader, read_callback);
   png_read_info(png_ptr, info_ptr);
