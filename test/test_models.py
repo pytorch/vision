9c7e1cba4cb065581d916b64f6c27ac89871a10e
Nicolas Hug <nicolashug@fb.com>
2022-08-23 06:54:23 -0700

    [fbsync] Add SwinV2 (#6246)
    Summary:
    * init submit
    
    * fix typo
    
    * support ufmt and mypy
    
    * fix 2 unittest errors
    
    * fix ufmt issue
    
    * Apply suggestions from code review
    
    * unify codes
    
    * fix meshgrid indexing
    
    * fix a bug
    
    * fix type check
    
    * add type_annotation
    
    * add slow model
    
    * fix device issue
    
    * fix ufmt issue
    
    * add expect pickle file
    
    * fix jit script issue
    
    * fix type check
    
    * keep consistent argument order
    
    * add support for pretrained_window_size
    
    * avoid code duplication
    
    * a better code reuse
    
    * update window_size argument
    
    * make permute and flatten operations modular
    
    * add PatchMergingV2
    
    * modify expect.pkl
    
    * use None as default argument value
    
    * fix type check
    
    * fix indent
    
    * fix window_size (temporarily)
    
    * remove "v2_" related prefix and add v2 builder
    
    * remove v2 builder
    
    * keep default value consistent with official repo
    
    * deprecate dropout
    
    * deprecate pretrained_window_size
    
    * fix dynamic padding edge case
    
    * remove unused imports
    
    * remove doc modification
    
    * Revert "deprecate dropout"
    
    This reverts commit 8a13f932815ae25655c07430d52929f86b1ca479.
    
    * Revert "fix dynamic padding edge case"
    
    This reverts commit 1c7579cb1bd7bf2f0f94907f39bee6ed707a97a8.
    
    * remove unused kwargs
    
    * add downsample docs
    
    * revert block default value
    
    * revert argument order change
    
    * explicitly specify start_dim
    
    * add small and base variants
    
    * add expect files and slow_models
    
    * Add model weights and documentation for swin v2
    
    * fix lint
    
    * fix end of files line
    
    Reviewed By: datumbox
    
    Differential Revision: D38824237
    
    fbshipit-source-id: d94082c210c26665e70bdf8967ef72cbe3ed4a8a
    
    Co-authored-by: Vasilis Vryniotis <datumbox@users.noreply.github.com>
    Co-authored-by: Vasilis Vryniotis <datumbox@users.noreply.github.com>
    Co-authored-by: Joao Gomes <jdsgomes@fb.com>

--- test/test_models.py
+++ test/test_models.py
@@ -332,6 +332,9 @@ slow_models = [
     "swin_t",
     "swin_s",
     "swin_b",
+    "swin_v2_t",
+    "swin_v2_s",
+    "swin_v2_b",
 ]
 for m in slow_models:
     _model_params[m] = {"input_shape": (1, 3, 64, 64)}
