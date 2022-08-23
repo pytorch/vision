ce3e5f1ed8e843be02897884d8cd6ca4dee9e83d
Philip Meier <github.pmeier@posteo.de>
2022-08-23 16:04:01 +0200

    improve todo comment

--- torchvision/prototype/transforms/functional/_deprecated.py
+++ torchvision/prototype/transforms/functional/_deprecated.py
@@ -10,7 +10,9 @@ from ._meta import convert_color_space, convert_color_space_image_pil
 
 
 # TODO: this was copied from `torchvision.prototype.transforms._utils`. Given that this is not related to pytree / the
-#  Transform object, we should probably move it to `functional._utils`.
+#  Transform object, we should probably move it to somewhere else. One possibility is `functional._utils` so both the
+#  functionals and the transforms have access to it. We could also move it `features` since it literally checks for the
+#  _Feature type.
 def is_simple_tensor(inpt: Any) -> bool:
     return isinstance(inpt, torch.Tensor) and not isinstance(inpt, features._Feature)
 
