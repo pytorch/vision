852367192e148d82c06cc9deceb1fba60ab84fd4
Philip Meier <github.pmeier@posteo.de>
2022-08-25 14:12:19 +0200

    add aliases for hflip and vflip

--- torchvision/prototype/transforms/functional/__init__.py
+++ torchvision/prototype/transforms/functional/__init__.py
@@ -68,6 +68,7 @@ from ._geometry import (
     five_crop,
     five_crop_image_pil,
     five_crop_image_tensor,
+    hflip,
     horizontal_flip,
     horizontal_flip_bounding_box,
     horizontal_flip_image_pil,
@@ -106,6 +107,7 @@ from ._geometry import (
     vertical_flip_image_pil,
     vertical_flip_image_tensor,
     vertical_flip_segmentation_mask,
+    vflip,
 )
 from ._misc import gaussian_blur, gaussian_blur_image_pil, gaussian_blur_image_tensor, normalize, normalize_image_tensor
 from ._type_conversion import decode_image_with_pil, decode_video_with_av, to_image_pil, to_image_tensor
