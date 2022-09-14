de0ca59f4e4194e93d78a78bc22f4471e61c6811
Philip Meier <github.pmeier@posteo.de>
2022-09-14 09:28:18 +0200

    rename module _segmentation_mask.py -> _mask.py

--- torchvision/prototype/features/__init__.py
+++ torchvision/prototype/features/__init__.py
@@ -3,4 +3,4 @@ from ._encoded import EncodedData, EncodedImage, EncodedVideo
 from ._feature import _Feature, is_simple_tensor
 from ._image import ColorSpace, Image
 from ._label import Label, OneHotLabel
-from ._segmentation_mask import Mask
+from ._mask import Mask
