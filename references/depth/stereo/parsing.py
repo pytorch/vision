faa14365bb7f6c3cce9444af33854eb0cd628e6c
TeodorPoncu <teodor.poncu@gmail.com>
2022-09-19 18:20:29 +0000

    change if-else to dict

--- references/depth/stereo/parsing.py
+++ references/depth/stereo/parsing.py
@@ -59,64 +59,30 @@ def make_eval_transform(args: argparse.Namespace) -> torch.nn.Module:
 
 
 def make_dataset(dataset_name: str, dataset_root: str, transforms: torch.nn.Module) -> torch.utils.data.Dataset:
-    valid_datasets = [
-        "crestereo",
-        "carla-highres",
-        "instereo2k",
-        "sintel",
-        "sceneflow-monkaa",
-        "sceneflow-flyingthings",
-        "sceneflow-driving",
-        "fallingthings",
-        "eth3d-train",
-        "eth3d-test",
-        "kitti2015-train",
-        "kitti2015-test",
-        "kitti2012-train",
-        "kitti2012-test",
-        "middlebury2014-other",
-        "middlebury2014-train",
-        "middlebury2014-test",
-        "middlebury2014-train-ambient",
-    ]
+    valid_datasets = {
+        "crestereo": CREStereo(root=dataset_root, transforms=transforms),
+        "carla-highres": CarlaStereo(root=dataset_root, transforms=transforms),
+        "instereo2k": InStereo2k(root=dataset_root, transforms=transforms),
+        "sintel": SintelStereo(root=dataset_root, transforms=transforms),
+        "sceneflow-monkaa": SceneFlowStereo(root=dataset_root, transforms=transforms, split="monkaa", pass_name="both"),
+        "sceneflow-flyingthings": SceneFlowStereo(root=dataset_root, transforms=transforms, split="flyingthings", pass_name="both"),
+        "sceneflow-driving": SceneFlowStereo(root=dataset_root, transforms=transforms, split="driving", pass_name="both"),
+        "fallingthings": FallingThingsStereo(root=dataset_root, transforms=transforms, variant="both"),
+        "eth3d-train": ETH3DStereo(root=dataset_root, transforms=transforms, split="train"),
+        "eth3d-test": ETH3DStereo(root=dataset_root, transforms=transforms, split="test"),
+        "kitti2015-train": Kitti2015Stereo(root=dataset_root, transforms=transforms, split="train"),
+        "kitti2015-test": Kitti2015Stereo(root=dataset_root, transforms=transforms, split="test"),
+        "kitti2012-train": Kitti2012Stereo(root=dataset_root, transforms=transforms, split="train"),
+        "kitti2012-test": Kitti2012Stereo(root=dataset_root, transforms=transforms, split="test"),
+        "middlebury2014-other": Middlebury2014Stereo(root=dataset_root, transforms=transforms, split="additional", use_ambient_views=True, calibration="both"),
+        "middlebury2014-train": Middlebury2014Stereo(root=dataset_root, transforms=transforms, split="train", calibration="perfect"),
+        "middlebury2014-test": Middlebury2014Stereo(root=dataset_root, transforms=transforms, split="test", calibration="perfect"),
+        "middlebury2014-train-ambient": Middlebury2014Stereo(root=dataset_root, transforms=transforms, split="train", use_ambient_views=True, calibration="perfect"),
+    }
 
+    # raise a key-error just to inform the user about which datasets are valid
     if dataset_name not in valid_datasets:
-        raise ValueError(f"Invalid dataset name: {dataset_name}. Valid datasets are: {valid_datasets}")
+        raise KeyError(f"Invalid dataset name: {dataset_name}. Valid datasets are: {valid_datasets.keys()}")
 
-    if dataset_name == "crestereo":
-        return CREStereo(root=dataset_root, transforms=transforms)
-    elif dataset_name == "carla-highres":
-        return CarlaStereo(root=dataset_root, transforms=transforms)
-    elif dataset_name == "instereo2k":
-        return InStereo2k(root=dataset_root, transforms=transforms)
-    elif dataset_name == "sintel":
-        return SintelStereo(root=dataset_root, transforms=transforms)
-    elif dataset_name == "sceneflow-monkaa":
-        return SceneFlowStereo(root=dataset_root, transforms=transforms, split="monkaa", pass_name="both")
-    elif dataset_name == "sceneflow-flyingthings":
-        return SceneFlowStereo(root=dataset_root, transforms=transforms, split="flyingthings", pass_name="both")
-    elif dataset_name == "sceneflow-driving":
-        return SceneFlowStereo(root=dataset_root, transforms=transforms, split="driving", pass_name="both")
-    elif dataset_name == "fallingthings":
-        return FallingThingsStereo(root=dataset_root, transforms=transforms, variant="both")
-    elif dataset_name == "eth3d-train":
-        return ETH3DStereo(root=dataset_root, transforms=transforms, split="train")
-    elif dataset_name == "eth3d-test":
-        return ETH3DStereo(root=dataset_root, transforms=transforms, split="test")
-    elif dataset_name == "kitti2015-train":
-        return Kitti2015Stereo(root=dataset_root, transforms=transforms, split="train")
-    elif dataset_name == "kitti2015-test":
-        return Kitti2015Stereo(root=dataset_root, transforms=transforms, split="test")
-    elif dataset_name == "kitti2012-train":
-        return Kitti2012Stereo(root=dataset_root, transforms=transforms, split="train")
-    elif dataset_name == "kitti2012-test":
-        return Kitti2012Stereo(root=dataset_root, transforms=transforms, split="test")
-    elif dataset_name == "middlebury2014-train-ambient":
-        return Middlebury2014Stereo(root=dataset_root, transforms=transforms, split="train", use_ambient_views=True, calibration="perfect")
-    elif dataset_name == "middlebury2014-train":
-        return Middlebury2014Stereo(root=dataset_root, transforms=transforms, split="train", calibration="perfect")
-    elif dataset_name == "middlebury2014-test":
-        return Middlebury2014Stereo(root=dataset_root, transforms=transforms, split="test", calibration="perfect")
-    elif dataset_name == "middlebury2014-other":
-        return Middlebury2014Stereo(root=dataset_root, transforms=transforms, split="additional", use_ambient_views=True, calibration="both")
+    return valid_datasets[dataset_name]
         
