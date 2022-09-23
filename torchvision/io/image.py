c48cbb295792dcfd076b86d4a257d69e6fc67a14
Ambuj Pawar <your_email@abc.example>
2022-09-23 16:50:39 +0200

    add limitation of decode_jpeg in the function docstring

--- torchvision/io/image.py
+++ torchvision/io/image.py
@@ -72,7 +72,7 @@ def decode_png(input: torch.Tensor, mode: ImageReadMode = ImageReadMode.UNCHANGE
         input (Tensor[1]): a one dimensional uint8 tensor containing
             the raw bytes of the PNG image.
         mode (ImageReadMode): the read mode used for optionally
-            converting the image. Default: ``ImageReadMode.UNCHANGED``.
+            converting the image. The mode Default: ``ImageReadMode.UNCHANGED``.
             See `ImageReadMode` class for more information on various
             available modes.
 
@@ -137,7 +137,9 @@ def decode_jpeg(
             the raw bytes of the JPEG image. This tensor must be on CPU,
             regardless of the ``device`` parameter.
         mode (ImageReadMode): the read mode used for optionally
-            converting the image. Default: ``ImageReadMode.UNCHANGED``.
+            converting the image. The supported modes are: ``ImageReadMode.UNCHANGED``,
+            ``ImageReadMode.GRAY`` and ``ImageReadMode.RGB``
+            Default: ``ImageReadMode.UNCHANGED``.
             See ``ImageReadMode`` class for more information on various
             available modes.
         device (str or torch.device): The device on which the decoded image will
