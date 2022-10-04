e2c0aea0a31fc3436a0f2c31e880eafd28a2a163
BowenBao <bowbao@microsoft.com>
2022-10-04 13:08:56 -0700

    add comma

--- torchvision/ops/_register_onnx_ops.py
+++ torchvision/ops/_register_onnx_ops.py
@@ -46,7 +46,7 @@ def _register_custom_op():
         if aligned:
             warnings.warn(
                 "ROIAlign with aligned=True is only supported in opset >= 16. "
-                "Please export with opset 16 or higher or use aligned=False."
+                "Please export with opset 16 or higher, or use aligned=False."
             )
         sampling_ratio = _process_sampling_ratio_for_roi_align(g, sampling_ratio)
         return g.op(
