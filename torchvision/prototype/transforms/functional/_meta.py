c9ba0fd60b7ab464955ec35396347ca26a59dfe5
Vasilis Vryniotis <vvryniotis@fb.com>
2022-10-07 11:49:24 +0100

    Remove comments

--- torchvision/prototype/transforms/functional/_meta.py
+++ torchvision/prototype/transforms/functional/_meta.py
@@ -6,15 +6,9 @@ from torchvision.prototype import features
 from torchvision.prototype.features import BoundingBoxFormat, ColorSpace
 from torchvision.transforms import functional_pil as _FP, functional_tensor as _FT
 
+
 get_dimensions_image_tensor = _FT.get_dimensions
 get_dimensions_image_pil = _FP.get_dimensions
-
-
-# The three functions below are here for BC. Whether we want to have two different kernels and how they and the
-# compound version should be named is still under discussion: https://github.com/pytorch/vision/issues/6491
-# Given that these kernels should also support boxes, masks, and videos, it is unlikely that there name will stay.
-# They will either be deprecated or simply aliased to the new kernels if we have reached consensus about the issue
-# detailed above.
 
 
 def get_dimensions(image: features.ImageTypeJIT) -> List[int]:
