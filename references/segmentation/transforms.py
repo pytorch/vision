669b1baa43dbfd99e28bc8888d640919977229b5
Vasilis Vryniotis <vvryniotis@fb.com>
2022-10-11 12:31:13 +0100

    Restoring original reference transforms so that test can run

--- references/segmentation/transforms.py
+++ references/segmentation/transforms.py
@@ -1,23 +1,28 @@
+import random
+
+import numpy as np
 import torch
 
-from torchvision.prototype import features, transforms
-from torchvision.prototype.transforms import functional as F
+from torchvision import transforms as T
+from torchvision.prototype import features, transforms as PT
+from torchvision.prototype.transforms import functional as PF
+from torchvision.transforms import functional as F
 
 
-class WrapIntoFeatures(transforms.Transform):
+class WrapIntoFeatures(PT.Transform):
     def forward(self, sample):
         image, mask = sample
-        return F.to_image_tensor(image), features.Mask(F.pil_to_tensor(mask).squeeze(0), dtype=torch.int64)
+        return PF.to_image_tensor(image), features.Mask(PF.pil_to_tensor(mask).squeeze(0), dtype=torch.int64)
 
 
-class PadIfSmaller(transforms.Transform):
+class PadIfSmaller(PT.Transform):
     def __init__(self, size, fill=0):
         super().__init__()
         self.size = size
-        self.fill = transforms._geometry._setup_fill_arg(fill)
+        self.fill = PT._geometry._setup_fill_arg(fill)
 
     def _get_params(self, sample):
-        _, height, width = transforms._utils.query_chw(sample)
+        _, height, width = PT._utils.query_chw(sample)
         padding = [0, 0, max(self.size - width, 0), max(self.size - height, 0)]
         needs_padding = any(padding)
         return dict(padding=padding, needs_padding=needs_padding)
@@ -27,6 +32,103 @@ class PadIfSmaller(transforms.Transform):
             return inpt
 
         fill = self.fill[type(inpt)]
-        fill = F._geometry._convert_fill_arg(fill)
+        fill = PF._geometry._convert_fill_arg(fill)
 
-        return F.pad(inpt, padding=params["padding"], fill=fill)
+        return PF.pad(inpt, padding=params["padding"], fill=fill)
+
+
+# Original Transforms can be removed:
+
+
+def pad_if_smaller(img, size, fill=0):
+    min_size = min(img.size)
+    if min_size < size:
+        ow, oh = img.size
+        padh = size - oh if oh < size else 0
+        padw = size - ow if ow < size else 0
+        img = F.pad(img, (0, 0, padw, padh), fill=fill)
+    return img
+
+
+class Compose:
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image, target):
+        for t in self.transforms:
+            image, target = t(image, target)
+        return image, target
+
+
+class RandomResize:
+    def __init__(self, min_size, max_size=None):
+        self.min_size = min_size
+        if max_size is None:
+            max_size = min_size
+        self.max_size = max_size
+
+    def __call__(self, image, target):
+        size = random.randint(self.min_size, self.max_size)
+        image = F.resize(image, size)
+        target = F.resize(target, size, interpolation=T.InterpolationMode.NEAREST)
+        return image, target
+
+
+class RandomHorizontalFlip:
+    def __init__(self, flip_prob):
+        self.flip_prob = flip_prob
+
+    def __call__(self, image, target):
+        if random.random() < self.flip_prob:
+            image = F.hflip(image)
+            target = F.hflip(target)
+        return image, target
+
+
+class RandomCrop:
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, image, target):
+        image = pad_if_smaller(image, self.size)
+        target = pad_if_smaller(target, self.size, fill=255)
+        crop_params = T.RandomCrop.get_params(image, (self.size, self.size))
+        image = F.crop(image, *crop_params)
+        target = F.crop(target, *crop_params)
+        return image, target
+
+
+class CenterCrop:
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, image, target):
+        image = F.center_crop(image, self.size)
+        target = F.center_crop(target, self.size)
+        return image, target
+
+
+class PILToTensor:
+    def __call__(self, image, target):
+        image = F.pil_to_tensor(image)
+        target = torch.as_tensor(np.array(target), dtype=torch.int64)
+        return image, target
+
+
+class ConvertImageDtype:
+    def __init__(self, dtype):
+        self.dtype = dtype
+
+    def __call__(self, image, target):
+        image = F.convert_image_dtype(image, self.dtype)
+        return image, target
+
+
+class Normalize:
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, image, target):
+        image = F.normalize(image, mean=self.mean, std=self.std)
+        return image, target
