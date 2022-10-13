85e6fe0074c4f3a260481986439f239c6454770e
vfdev-5 <vfdev.5@gmail.com>
2022-10-13 12:49:32 +0000

    Revert "Use softmax in _get_gaussian_kernel1d"
    This reverts commit eb8fba36302d2da9e06e6f40afaaf901b276a771.

--- torchvision/transforms/functional_tensor.py
+++ torchvision/transforms/functional_tensor.py
@@ -730,7 +730,8 @@ def _get_gaussian_kernel1d(kernel_size: int, sigma: float) -> Tensor:
     ksize_half = (kernel_size - 1) * 0.5
 
     x = torch.linspace(-ksize_half, ksize_half, steps=kernel_size)
-    kernel1d = torch.softmax(-0.5 * (x / sigma).pow(2), dim=0)
+    pdf = torch.exp(-0.5 * (x / sigma).pow(2))
+    kernel1d = pdf / pdf.sum()
 
     return kernel1d
 
