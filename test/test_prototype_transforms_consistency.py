514a0319a873eb5254bc359aec588f47b1b115f0
Yosua Michael Maranatha <yosuamichael@meta.com>
2022-10-17 11:10:37 -0700

    [fbsync] improve test id for consistency tests (#6763)
    Reviewed By: NicolasHug
    
    Differential Revision: D40427475
    
    fbshipit-source-id: 209a4b7af54d80fbaf0ca4bc7e1a91bea6f028a0

--- test/test_prototype_transforms_consistency.py
+++ test/test_prototype_transforms_consistency.py
@@ -575,9 +575,11 @@ def check_call_consistency(prototype_transform, legacy_transform, images=None, s
 @pytest.mark.parametrize(
     ("config", "args_kwargs"),
     [
-        pytest.param(config, args_kwargs, id=f"{config.legacy_cls.__name__}({args_kwargs})")
+        pytest.param(
+            config, args_kwargs, id=f"{config.legacy_cls.__name__}-{idx:0{len(str(len(config.args_kwargs)))}d}"
+        )
         for config in CONSISTENCY_CONFIGS
-        for args_kwargs in config.args_kwargs
+        for idx, args_kwargs in enumerate(config.args_kwargs)
     ],
 )
 def test_call_consistency(config, args_kwargs):
