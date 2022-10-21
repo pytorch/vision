4523fadae8f6e2eb0e05e40da7f1870a13c09bc7
Vasilis Vryniotis <vvryniotis@fb.com>
2022-10-21 17:13:50 +0100

    Adding the UniformTemporalSubsample class.

--- torchvision/prototype/transforms/functional/_temporal.py
+++ torchvision/prototype/transforms/functional/_temporal.py
@@ -4,8 +4,8 @@ from torchvision.prototype import features
 
 
 def uniform_temporal_subsample(
-    inpt: features.TensorVideoTypeJIT, num_samples: int, temporal_dim: int = -4
-) -> features.InputTypeJIT:
+    inpt: features.VideoTypeJIT, num_samples: int, temporal_dim: int = -4
+) -> features.VideoTypeJIT:
     if isinstance(inpt, torch.Tensor) and (torch.jit.is_scripting() or not isinstance(inpt, features.Video)):
         return uniform_temporal_subsample_video(inpt, num_samples, temporal_dim=temporal_dim)
     else:  # isinstance(inpt, features.Video)
@@ -18,6 +18,7 @@ def uniform_temporal_subsample(
 
 
 def uniform_temporal_subsample_video(video: torch.Tensor, num_samples: int, temporal_dim: int = -4) -> torch.Tensor:
+    # Reference: https://github.com/facebookresearch/pytorchvideo/blob/a0a131e/pytorchvideo/transforms/functional.py#L19
     t_max = video.size(temporal_dim) - 1
     indices = torch.linspace(0, t_max, num_samples, device=video.device).clamp_(0, t_max).long()
     return torch.index_select(video, temporal_dim, indices)
