d9dbb6aa4832723464c46bca2cc9a5822e396c3a
Vasilis Vryniotis <vvryniotis@fb.com>
2022-10-24 12:10:27 +0100

    Adding comments and optimizations.

--- torchvision/prototype/transforms/functional/_color.py
+++ torchvision/prototype/transforms/functional/_color.py
@@ -2,7 +2,7 @@ import torch
 from torchvision.prototype import features
 from torchvision.transforms import functional_pil as _FP, functional_tensor as _FT
 
-from ._meta import _rgb_to_gray, get_dimensions_image_tensor, get_num_channels_image_tensor, convert_dtype_image_tensor
+from ._meta import _rgb_to_gray, convert_dtype_image_tensor, get_dimensions_image_tensor, get_num_channels_image_tensor
 
 
 def _blend(image1: torch.Tensor, image2: torch.Tensor, ratio: float) -> torch.Tensor:
@@ -269,12 +269,17 @@ def adjust_gamma_image_tensor(image: torch.Tensor, gamma: float, gain: float = 1
     if gamma < 0:
         raise ValueError("Gamma should be a non-negative real number")
 
+    # The input image is either assumed to be at [0, 1] scale (if float) or is converted to that scale (if integer).
+    # Since the gamma is non-negative, the output remains at [0, 1] scale.
     if not torch.is_floating_point(image):
         output = convert_dtype_image_tensor(image, torch.float32).pow_(gamma)
     else:
         output = image.pow(gamma)
 
-    output = output.mul_(gain).clamp_(0.0, 1.0)
+    if gain != 1.0:
+        # The clamp operation is needed only if mutiplication is performed. It's only when gain != 1, that the scale
+        # of the output can go beyond [0, 1].
+        output = output.mul_(gain).clamp_(0.0, 1.0)
 
     return convert_dtype_image_tensor(output, image.dtype)
 
