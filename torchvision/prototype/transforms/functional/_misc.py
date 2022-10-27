719eb7cc77761959f85210d51aa67c75b314258d
Vasilis Vryniotis <vvryniotis@meta.com>
2022-10-27 02:05:41 -0700

    [fbsync] [prototype] Minor improvements on functional (#6832)
    Summary:
    * Minor improvements on functional.
    
    * Restore `_split_alpha`.
    
    * Revert "Restore `_split_alpha`."
    
    This reverts commit 2286120be6d4af2a3c9b52b605d87611ec70fe06.
    
    Reviewed By: YosuaMichael
    
    Differential Revision: D40722902
    
    fbshipit-source-id: 3a574939365abd1b74ed3a558b4354b1c40fc883

--- torchvision/prototype/transforms/functional/_misc.py
+++ torchvision/prototype/transforms/functional/_misc.py
@@ -67,9 +67,9 @@ def normalize(
     return normalize_image_tensor(inpt, mean=mean, std=std, inplace=inplace)
 
 
-def _get_gaussian_kernel1d(kernel_size: int, sigma: float) -> torch.Tensor:
+def _get_gaussian_kernel1d(kernel_size: int, sigma: float, dtype: torch.dtype, device: torch.device) -> torch.Tensor:
     lim = (kernel_size - 1) / (2 * math.sqrt(2) * sigma)
-    x = torch.linspace(-lim, lim, steps=kernel_size)
+    x = torch.linspace(-lim, lim, steps=kernel_size, dtype=dtype, device=device)
     kernel1d = torch.softmax(-x.pow_(2), dim=0)
     return kernel1d
 
@@ -77,8 +77,8 @@ def _get_gaussian_kernel1d(kernel_size: int, sigma: float) -> torch.Tensor:
 def _get_gaussian_kernel2d(
     kernel_size: List[int], sigma: List[float], dtype: torch.dtype, device: torch.device
 ) -> torch.Tensor:
-    kernel1d_x = _get_gaussian_kernel1d(kernel_size[0], sigma[0]).to(device, dtype=dtype)
-    kernel1d_y = _get_gaussian_kernel1d(kernel_size[1], sigma[1]).to(device, dtype=dtype)
+    kernel1d_x = _get_gaussian_kernel1d(kernel_size[0], sigma[0], dtype, device)
+    kernel1d_y = _get_gaussian_kernel1d(kernel_size[1], sigma[1], dtype, device)
     kernel2d = kernel1d_y.unsqueeze(-1) * kernel1d_x
     return kernel2d
 
