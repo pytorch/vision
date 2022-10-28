728c7cacac0a7a300feb9903d1ebb0bb4b393f9b
Ambuj Pawar <your_email@abc.example>
2022-10-28 11:40:57 +0200

    WIP: Random Mixup for detection

--- torchvision/prototype/transforms/_mixup.py
+++ torchvision/prototype/transforms/_mixup.py
@@ -1,4 +1,18 @@
 # TODO: Consturct a Mixup transform
+import random
+
+from torchvision.prototype import features
+from torchvision.prototype.transforms import AutoAugmentPolicy, functional as F, InterpolationMode, Transform
+
 
 class _Mixup:
-    pass
+    def __init__(
+        self,
+        *,
+        alpha: float = 0.9,
+        beta: float = 0.1,
+    ) -> None:
+        super().__init__()
+
+    # Fetch a random image from the dataset
+    random_index = random.randint(0, )
