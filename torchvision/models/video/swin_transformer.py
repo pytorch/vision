8eddca8a5d03eff87cae83213d1fe2a34e0da3ce
Aditya Oke <okeaditya315@gmail.com>
2022-11-16 22:47:32 +0530

    Fix url recipe

--- torchvision/models/video/swin_transformer.py
+++ torchvision/models/video/swin_transformer.py
@@ -518,7 +518,7 @@ class Swin3D_T_Weights(WeightsEnum):
         ),
         meta={
             **_COMMON_META,
-            "recipe": "https://github.com/pytorch/vision/tree/main/references/video_classification#s3d",
+            "recipe": "https://github.com/SwinTransformer/Video-Swin-Transformer#kinetics-400",
             "_docs": (
                 "The weights were ported from the paper. The accuracies are estimated on video-level "
                 "with parameters `frame_rate=15`, `clips_per_video=12`, and `clip_len=32`"
@@ -549,7 +549,7 @@ class Swin3D_S_Weights(WeightsEnum):
         ),
         meta={
             **_COMMON_META,
-            "recipe": "https://github.com/pytorch/vision/tree/main/references/video_classification#s3d",
+            "recipe": "https://github.com/SwinTransformer/Video-Swin-Transformer#kinetics-400",
             "_docs": (
                 "The weights were ported from the paper. The accuracies are estimated on video-level "
                 "with parameters `frame_rate=15`, `clips_per_video=12`, and `clip_len=32`"
@@ -580,7 +580,7 @@ class Swin3D_B_Weights(WeightsEnum):
         ),
         meta={
             **_COMMON_META,
-            "recipe": "https://github.com/pytorch/vision/tree/main/references/video_classification#s3d",
+            "recipe": "https://github.com/SwinTransformer/Video-Swin-Transformer#kinetics-400",
             "_docs": (
                 "The weights were ported from the paper. The accuracies are estimated on video-level "
                 "with parameters `frame_rate=15`, `clips_per_video=12`, and `clip_len=32`"
@@ -607,7 +607,7 @@ class Swin3D_B_Weights(WeightsEnum):
         ),
         meta={
             **_COMMON_META,
-            "recipe": "https://github.com/pytorch/vision/tree/main/references/video_classification#s3d",
+            "recipe": "https://github.com/SwinTransformer/Video-Swin-Transformer#kinetics-400",
             "_docs": (
                 "The weights were ported from the paper. The accuracies are estimated on video-level "
                 "with parameters `frame_rate=15`, `clips_per_video=12`, and `clip_len=32`"
