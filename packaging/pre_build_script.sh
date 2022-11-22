37ded3a17c0eb4cabcc59497c04e3d6e3acd4654
Omkar Salpekar <osalpekar@fb.com>
2022-11-22 11:04:33 -0800

    Fix extensions in wheels build

---
+++ packaging/pre_build_script.sh
@@ -0,0 +1,19 @@
+if [[ "$(uname)" == Darwin || "$OSTYPE" == "msys" ]]; then
+  # Install libpng from Anaconda (defaults)
+  conda install ${CONDA_CHANNEL_FLAGS} libpng "jpeg<=9b" -y
+  conda install -yq ffmpeg=4.2 -c pytorch
+  conda install -yq wget
+else
+  # Install native CentOS libJPEG, freetype and GnuTLS
+  yum install -y libjpeg-turbo-devel freetype gnutls
+  # Download all the dependencies required to compile image and video_reader
+  # extensions
+  mkdir -p ext_libraries
+  pushd ext_libraries
+  popd
+  export PATH="$(pwd)/ext_libraries/bin:$PATH"
+  # Point to custom libraries
+  export LD_LIBRARY_PATH=$(pwd)/ext_libraries/lib:$LD_LIBRARY_PATH
+  export TORCHVISION_INCLUDE=$(pwd)/ext_libraries/include
+  export TORCHVISION_LIBRARY=$(pwd)/ext_libraries/lib
+fi
--- test/smoke_test.py
+++ test/smoke_test.py
@@ -1,5 +1,7 @@
 """Run smoke tests"""
 
 import torchvision
+from torchvision.io import read_image
 
 print("torchvision version is ", torchvision.__version__)
+img = read_image("pytorch/vision/test/assets/encode_jpeg/grace_hopper_517x606.jpg")
