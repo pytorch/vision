2126c0d6ddc1d0df1bcfb9fe2322dbc1259b9f0e
Philip Meier <github.pmeier@posteo.de>
2022-11-22 14:09:13 +0100

    more debug output

--- test/test_gpu_ci.py
+++ test/test_gpu_ci.py
@@ -5,11 +5,14 @@ import pytest
 
 
 @pytest.mark.parametrize("device", cpu_and_gpu())
-def test_cpu_and_gpu(device):
+def test_cpu_and_gpu(request, device):
     print(os.getenv("GITHUB_ACTIONS"))
+    for mark in request.node.own_markers:
+        print(mark)
     assert device != "cpu", "This should not be run on a GPU machine"
 
 
 @needs_cuda
-def test_needs_cuda():
-    assert True
+def test_needs_cuda(request):
+    for mark in request.node.own_markers:
+        print(mark)
