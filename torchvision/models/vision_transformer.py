c34c1e6dc5bc97271fb6c44587e5bed94cc40dcc
Yassine Alouini <yassinealouini@outlook.com>
2022-11-27 15:05:03 +0100

    Remove useless change.

--- torchvision/models/vision_transformer.py
+++ torchvision/models/vision_transformer.py
@@ -113,6 +113,7 @@ class EncoderBlock(nn.Module):
         x, _ = self.self_attention(query=x, key=x, value=x, need_weights=False)
         x = self.dropout(x)
         x = x + input
+
         y = self.ln_2(x)
         y = self.mlp(y)
         return x + y
