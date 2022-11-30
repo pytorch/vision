474d87b8a942eb0d5f22f5d98120c6f8961c798e
Omkar Salpekar <osalpekar@gmail.com>
2022-11-29 19:50:25 -0500

    [Nova] Fix extensions in wheels build (#6972)
    * Fix extensions in wheels build
    
    * pass pre-script from caller
    
    * add pre-script to run relocate on wheel
    
    * install auditwheel for relocate
    
    * reorder numpy install
    
    * use jpeg80 for py3.9 and py3.10 mac builds
    
    * revert back to jpeg9
    
    * printing the found jpeg libs during build time and reintro circle for comparison
    
    * more debug logs, remove extra linux builds
    
    * typo fix
    
    * brew uninstall conflicting jpeg libraries on mac machines
    
    * fix path for unittests
    
    * ufmt

--- test/smoke_test.py
+++ test/smoke_test.py
@@ -1,5 +1,12 @@
 """Run smoke tests"""
 
-import torchvision
+import os
 
+import torchvision
+from torchvision.io import read_image
+
+image_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "assets", "encode_jpeg", "grace_hopper_517x606.jpg"
+)
 print("torchvision version is ", torchvision.__version__)
+img = read_image(image_path)
