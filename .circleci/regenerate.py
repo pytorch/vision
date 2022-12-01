0fae9f487d320b27b55b3a08689a60a810d58d24
Yosua Michael Maranatha <yosuamichael@fb.com>
2022-12-01 09:18:39 +0000

    Run with original machine type for baseline

--- .circleci/regenerate.py
+++ .circleci/regenerate.py
@@ -270,7 +270,7 @@ def unittest_workflows(indentation=6):
                 else:
                     job["cu_version"] = "cpu"
                     if os_type == "linux":
-                        job["machine_type"] = "xlarge"
+                        job["machine_type"] = "2xlarge+"
                     elif os_type == "macos":
                         job["machine_type"] = "large"
 
