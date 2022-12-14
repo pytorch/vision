f5c3b83602ea919ec288074f78815ac4eaacec7c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-14 15:40:04 +0000

    Adding tarfile member sanitization to extractall()

--- torchvision/datasets/sbu.py
+++ torchvision/datasets/sbu.py
@@ -100,7 +100,26 @@ class SBU(VisionDataset):
 
         # Extract file
         with tarfile.open(os.path.join(self.root, self.filename), "r:gz") as tar:
-            tar.extractall(path=self.root)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=self.root)
 
         # Download individual photos
         with open(os.path.join(self.root, "dataset", "SBU_captioned_photo_dataset_urls.txt")) as fh:
