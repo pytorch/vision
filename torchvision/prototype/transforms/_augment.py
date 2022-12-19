99de2327fd6bd9e33866afcf6b2181443b33800b
Philip Meier <github.pmeier@posteo.de>
2022-12-19 13:10:24 +0100

    create base class

--- torchvision/prototype/transforms/_augment.py
+++ torchvision/prototype/transforms/_augment.py
@@ -1,17 +1,17 @@
 import math
 import numbers
 import warnings
-from typing import Any, Callable, cast, Dict, List, Optional, Tuple, Type, Union
+from typing import Any, cast, Dict, List, Optional, Tuple, Union
 
 import PIL.Image
+
 import torch
-from torch.utils._pytree import tree_flatten, tree_unflatten, TreeSpec
 from torchvision.ops import masks_to_boxes
 from torchvision.prototype import datapoints
-from torchvision.prototype.transforms import functional as F, InterpolationMode, Transform
+from torchvision.prototype.transforms import functional as F, InterpolationMode
 
-from ._transform import _RandomApplyTransform
-from .utils import check_type, has_any, is_simple_tensor, query_chw, query_spatial_size
+from ._transform import _DetectionBatchTransform, _RandomApplyTransform
+from .utils import has_any, is_simple_tensor, query_chw, query_spatial_size
 
 
 class RandomErasing(_RandomApplyTransform):
@@ -190,73 +190,6 @@ class RandomCutmix(_BaseMixupCutmix):
             return inpt
 
 
-def flatten_and_extract_data(
-    inputs: Any, **types_or_checks: Tuple[Union[Type, Callable[[Any], bool]], ...]
-) -> Tuple[Tuple[List[Any], TreeSpec, List[Dict[str, int]]], List[Dict[str, Any]]]:
-    batch = inputs if len(inputs) > 1 else inputs[0]
-    flat_batch = []
-    sample_specs = []
-
-    offset = 0
-    batch_idcs = []
-    batch_data = []
-    for sample_idx, sample in enumerate(batch):
-        flat_sample, sample_spec = tree_flatten(sample)
-        flat_batch.extend(flat_sample)
-        sample_specs.append(sample_spec)
-
-        sample_types_or_checks = types_or_checks.copy()
-        sample_idcs = {}
-        sample_data = {}
-        for flat_idx, item in enumerate(flat_sample, offset):
-            if not sample_types_or_checks:
-                break
-
-            for key, types_or_checks_ in sample_types_or_checks.items():
-                if check_type(item, types_or_checks_):
-                    break
-            else:
-                continue
-
-            del sample_types_or_checks[key]
-            sample_idcs[key] = flat_idx
-            sample_data[key] = item
-
-        if sample_types_or_checks:
-            # TODO: improve message
-            raise TypeError(f"Sample at index {sample_idx} in the batch is missing {sample_types_or_checks.keys()}`")
-
-        batch_idcs.append(sample_idcs)
-        batch_data.append(sample_data)
-        offset += len(flat_sample)
-
-    batch_spec = TreeSpec(list, context=None, children_specs=sample_specs)
-
-    return (flat_batch, batch_spec, batch_idcs), batch_data
-
-
-def unflatten_and_insert_data(
-    flat_batch_with_spec: Tuple[List[Any], TreeSpec, List[Dict[str, int]]],
-    batch: List[Dict[str, Any]],
-) -> Any:
-    flat_batch, batch_spec, batch_idcs = flat_batch_with_spec
-
-    for sample_idx, sample_idcs in enumerate(batch_idcs):
-        for key, flat_idx in sample_idcs.items():
-            inpt = flat_batch[flat_idx]
-            item = batch[sample_idx][key]
-
-            if not is_simple_tensor(inpt) and is_simple_tensor(item):
-                if isinstance(inpt, datapoints._datapoint.Datapoint):
-                    item = type(inpt).wrap_like(inpt, item)
-                elif isinstance(inpt, PIL.Image.Image):
-                    item = F.to_image_pil(item)
-
-            flat_batch[flat_idx] = item
-
-    return tree_unflatten(flat_batch, batch_spec)
-
-
 class SimpleCopyPaste(_RandomApplyTransform):
     def __init__(
         self,
@@ -395,7 +328,7 @@ class SimpleCopyPaste(_RandomApplyTransform):
         return unflatten_and_insert_data(flat_batch_with_spec, output_images, output_targets)
 
 
-class MixupDetection(Transform):
+class MixupDetection(_DetectionBatchTransform):
     _transformed_types = (is_simple_tensor, datapoints.Image, PIL.Image)
 
     def __init__(
@@ -414,7 +347,7 @@ class MixupDetection(Transform):
         return dict(ratio=float(self._dist.sample()))
 
     def forward(self, *inputs: Any) -> Any:
-        flat_batch_with_spec, batch = flatten_and_extract_data(
+        flat_batch_with_spec, batch = self._flatten_and_extract_data(
             inputs,
             image=(datapoints.Image, PIL.Image.Image, is_simple_tensor),
             boxes=(datapoints.BoundingBox,),
@@ -422,20 +355,14 @@ class MixupDetection(Transform):
         )
         self._check_inputs(flat_batch_with_spec[0])
 
-        for sample in batch:
-            image = sample.pop("image")
-            if isinstance(image, PIL.Image.Image):
-                image = F.pil_to_tensor(image)
-            elif isinstance(image, datapoints.Image):
-                image = image.as_subclass(torch.Tensor)
-            sample["image"] = image
+        batch = self._to_image_tensor(batch)
 
         batch_output = [
             self._mixup(sample, sample_rolled, self._get_params([])["ratio"])
             for sample, sample_rolled in zip(batch, batch[-1:] + batch[:-1])
         ]
 
-        return unflatten_and_insert_data(flat_batch_with_spec, batch_output)
+        return self._unflatten_and_insert_data(flat_batch_with_spec, batch_output)
 
     def _mixup(self, sample_1: Dict[str, Any], sample_2: Dict[str, Any], ratio: float) -> Dict[str, Any]:
         if ratio >= 1.0:
--- torchvision/prototype/transforms/_transform.py
+++ torchvision/prototype/transforms/_transform.py
@@ -2,10 +2,13 @@ import enum
 from typing import Any, Callable, Dict, List, Tuple, Type, Union
 
 import PIL.Image
+
 import torch
 from torch import nn
-from torch.utils._pytree import tree_flatten, tree_unflatten
-from torchvision.prototype.transforms.utils import check_type
+from torch.utils._pytree import tree_flatten, tree_unflatten, TreeSpec
+from torchvision.prototype import datapoints
+from torchvision.prototype.transforms import functional as F
+from torchvision.prototype.transforms.utils import check_type, is_simple_tensor
 from torchvision.utils import _log_api_usage_once
 
 
@@ -83,3 +86,85 @@ class _RandomApplyTransform(Transform):
         ]
 
         return tree_unflatten(flat_outputs, spec)
+
+
+class _DetectionBatchTransform(Transform):
+    @staticmethod
+    def _flatten_and_extract_data(
+        inputs: Any, **types_or_checks: Tuple[Union[Type, Callable[[Any], bool]], ...]
+    ) -> Tuple[Tuple[List[Any], TreeSpec, List[Dict[str, int]]], List[Dict[str, Any]]]:
+        batch = inputs if len(inputs) > 1 else inputs[0]
+        flat_batch = []
+        sample_specs = []
+
+        offset = 0
+        batch_idcs = []
+        batch_data = []
+        for sample_idx, sample in enumerate(batch):
+            flat_sample, sample_spec = tree_flatten(sample)
+            flat_batch.extend(flat_sample)
+            sample_specs.append(sample_spec)
+
+            sample_types_or_checks = types_or_checks.copy()
+            sample_idcs = {}
+            sample_data = {}
+            for flat_idx, item in enumerate(flat_sample, offset):
+                if not sample_types_or_checks:
+                    break
+
+                for key, types_or_checks_ in sample_types_or_checks.items():
+                    if check_type(item, types_or_checks_):
+                        break
+                else:
+                    continue
+
+                del sample_types_or_checks[key]
+                sample_idcs[key] = flat_idx
+                sample_data[key] = item
+
+            if sample_types_or_checks:
+                # TODO: improve message
+                raise TypeError(
+                    f"Sample at index {sample_idx} in the batch is missing {sample_types_or_checks.keys()}`"
+                )
+
+            batch_idcs.append(sample_idcs)
+            batch_data.append(sample_data)
+            offset += len(flat_sample)
+
+        batch_spec = TreeSpec(list, context=None, children_specs=sample_specs)
+
+        return (flat_batch, batch_spec, batch_idcs), batch_data
+
+    @staticmethod
+    def _to_image_tensor(batch: List[Dict[str, Any]], *, key: str = "image") -> List[Dict[str, Any]]:
+        for sample in batch:
+            image = sample.pop(key)
+            if isinstance(image, PIL.Image.Image):
+                image = F.pil_to_tensor(image)
+            elif isinstance(image, datapoints.Image):
+                image = image.as_subclass(torch.Tensor)
+            sample[key] = image
+        return batch
+
+    @staticmethod
+    def _unflatten_and_insert_data(
+        flat_batch_with_spec: Tuple[List[Any], TreeSpec, List[Dict[str, int]]],
+        batch: List[Dict[str, Any]],
+    ) -> Any:
+        flat_batch, batch_spec, batch_idcs = flat_batch_with_spec
+
+        for sample_idx, sample_idcs in enumerate(batch_idcs):
+            for key, flat_idx in sample_idcs.items():
+                inpt = flat_batch[flat_idx]
+                item = batch[sample_idx][key]
+
+                if not is_simple_tensor(inpt) and is_simple_tensor(item):
+                    if isinstance(inpt, datapoints._datapoint.Datapoint):
+                        item = type(inpt).wrap_like(inpt, item)
+                    elif isinstance(inpt, PIL.Image.Image):
+                        item = F.to_image_pil(item)
+
+                flat_batch[flat_idx] = item
+
+        return tree_unflatten(flat_batch, batch_spec)
