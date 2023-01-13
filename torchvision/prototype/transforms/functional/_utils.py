5011a1947daa8d933e7d1634cb005763eeeef6a4
Philip Meier <github.pmeier@posteo.de>
2023-01-13 09:56:45 +0100

    remove typeguard

--- torchvision/prototype/transforms/functional/_utils.py
+++ torchvision/prototype/transforms/functional/_utils.py
@@ -3,8 +3,6 @@ from typing import Any
 import torch
 from torchvision.prototype.datapoints._datapoint import Datapoint
 
-from typing_extensions import TypeGuard
 
-
-def is_simple_tensor(inpt: Any) -> TypeGuard[torch.Tensor]:
+def is_simple_tensor(inpt: Any) -> bool:
     return isinstance(inpt, torch.Tensor) and not isinstance(inpt, Datapoint)
