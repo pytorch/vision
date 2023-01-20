e83ad89472c1f78e3a793794c1269a08d3b34e83
Nicolas Hug <contact@nicolas-hug.com>
2023-01-20 15:27:19 +0000

    Undo changes to Grayscale, will address in another PR

--- torchvision/prototype/transforms/_deprecated.py
+++ torchvision/prototype/transforms/_deprecated.py
@@ -28,6 +28,7 @@ class ToTensor(Transform):
         return _F.to_tensor(inpt)
 
 
+# TODO: in other PR (?) undeprecate those and make them use _rgb_to_gray?
 class Grayscale(Transform):
     _transformed_types = (
         datapoints.Image,
@@ -37,6 +38,23 @@ class Grayscale(Transform):
     )
 
     def __init__(self, num_output_channels: Literal[1, 3] = 1) -> None:
+        deprecation_msg = (
+            f"The transform `Grayscale(num_output_channels={num_output_channels})` "
+            f"is deprecated and will be removed in a future release."
+        )
+        if num_output_channels == 1:
+            replacement_msg = (
+                "transforms.ConvertImageColorSpace(old_color_space=ColorSpace.RGB, color_space=ColorSpace.GRAY)"
+            )
+        else:
+            replacement_msg = (
+                "transforms.Compose(\n"
+                "    transforms.ConvertImageColorSpace(old_color_space=ColorSpace.RGB, color_space=ColorSpace.GRAY),\n"
+                "    transforms.ConvertImageColorSpace(old_color_space=ColorSpace.GRAY, color_space=ColorSpace.RGB),\n"
+                ")"
+            )
+        warnings.warn(f"{deprecation_msg} Instead, please use\n\n{replacement_msg}")
+
         super().__init__()
         self.num_output_channels = num_output_channels
 
@@ -45,8 +63,7 @@ class Grayscale(Transform):
     ) -> Union[datapoints.ImageType, datapoints.VideoType]:
         output = _F.rgb_to_grayscale(inpt, num_output_channels=self.num_output_channels)
         if isinstance(inpt, (datapoints.Image, datapoints.Video)):
-            # TODO: Q: is the wrapping still needed? Is the type ignore still needed?
-            output = inpt.wrap_like(inpt, output)  # type: ignore[arg-type]
+            output = inpt.wrap_like(inpt, output, color_space=datapoints.ColorSpace.GRAY)  # type: ignore[arg-type]
         return output
 
 
@@ -59,6 +76,18 @@ class RandomGrayscale(_RandomApplyTransform):
     )
 
     def __init__(self, p: float = 0.1) -> None:
+        warnings.warn(
+            "The transform `RandomGrayscale(p=...)` is deprecated and will be removed in a future release. "
+            "Instead, please use\n\n"
+            "transforms.RandomApply(\n"
+            "    transforms.Compose(\n"
+            "        transforms.ConvertImageColorSpace(old_color_space=ColorSpace.RGB, color_space=ColorSpace.GRAY),\n"
+            "        transforms.ConvertImageColorSpace(old_color_space=ColorSpace.GRAY, color_space=ColorSpace.RGB),\n"
+            "    )\n"
+            "    p=...,\n"
+            ")"
+        )
+
         super().__init__(p=p)
 
     def _get_params(self, flat_inputs: List[Any]) -> Dict[str, Any]:
@@ -70,6 +99,5 @@ class RandomGrayscale(_RandomApplyTransform):
     ) -> Union[datapoints.ImageType, datapoints.VideoType]:
         output = _F.rgb_to_grayscale(inpt, num_output_channels=params["num_input_channels"])
         if isinstance(inpt, (datapoints.Image, datapoints.Video)):
-            # TODO: Same as the other TODO above
-            output = inpt.wrap_like(inpt, output)  # type: ignore[arg-type]
+            output = inpt.wrap_like(inpt, output, color_space=datapoints.ColorSpace.GRAY)  # type: ignore[arg-type]
         return output
