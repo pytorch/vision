da5cf4312282387ad46e5b7c0c4e3f695b96f03d
Nicolas Hug <contact@nicolas-hug.com>
2023-01-25 10:14:40 +0000

    ufmt

--- torchvision/models/__init__.py
+++ torchvision/models/__init__.py
@@ -15,8 +15,8 @@ from .vision_transformer import *
 from .swin_transformer import *
 from .maxvit import *
 from . import detection, optical_flow, quantization, segmentation, video
-from ._api import get_model, get_model_builder, get_model_weights, get_weight, list_models
-# We're making these public for packages like torchgeo who are interested in
-# using them https://github.com/pytorch/vision/issues/7094
+
+# We're making Weights and WeightsEnum public for packages like torchgeo who are
+# interested in using them https://github.com/pytorch/vision/issues/7094
 # TODO: we could / should document them publicly as well?
-from ._api import Weights, WeightsEnum
+from ._api import get_model, get_model_builder, get_model_weights, get_weight, list_models, Weights, WeightsEnum
