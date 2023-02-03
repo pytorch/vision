43b3c6af5da9b49dff3465116105c2cdc4a78445
Philip Meier <github.pmeier@posteo.de>
2023-02-03 16:55:48 +0100

    remove default value from LabelToOneHot

--- torchvision/prototype/transforms/_type_conversion.py
+++ torchvision/prototype/transforms/_type_conversion.py
@@ -15,15 +15,12 @@ from torchvision.prototype.transforms.utils import is_simple_tensor
 class LabelToOneHot(Transform):
     _transformed_types = (datapoints.Label,)
 
-    def __init__(self, num_categories: int = -1):
+    def __init__(self, num_categories: int):
         super().__init__()
         self.num_categories = num_categories
 
     def _transform(self, inpt: datapoints.Label, params: Dict[str, Any]) -> datapoints.OneHotLabel:
-        num_categories = self.num_categories
-        if num_categories == -1 and inpt.categories is not None:
-            num_categories = len(inpt.categories)
-        output = one_hot(inpt.as_subclass(torch.Tensor), num_classes=num_categories)
+        output = one_hot(inpt.as_subclass(torch.Tensor), num_classes=self.num_categories)
         return datapoints.OneHotLabel(output, categories=inpt.categories)
 
     def extra_repr(self) -> str:
