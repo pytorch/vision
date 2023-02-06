b4921e3d982079b807f5a89d67b3f5ed0becf51c
Nicolas Hug <nicolashug@meta.com>
2023-02-06 15:43:07 +0000

    Error message

--- torchvision/transforms/functional.py
+++ torchvision/transforms/functional.py
@@ -1619,12 +1619,16 @@ def _check_antialias(
             interpolation == InterpolationMode.BILINEAR or interpolation == InterpolationMode.BICUBIC
         ):
             warnings.warn(
-                "The default value of the antialias parameter will change from None to True in v0.17, "
+                "The default value of the antialias parameter of all the resizing transforms "
+                "(Resize(), RandomResizedCrop(), etc.) "
+                "will change from None to True in v0.17, "
                 "in order to be consistent across the PIL and Tensor backends. "
                 "To suppress this warning, directly pass "
                 "antialias=True (recommended, future default), antialias=None (current default, "
                 "which means False for Tensors and True for PIL), "
-                "or antialias=False (only works on Tensors - PIL will still use antialiasing)."
+                "or antialias=False (only works on Tensors - PIL will still use antialiasing). "
+                "This also applies if you are using the inference transforms from the models weights: "
+                "update the call to weights.transforms(antialias=True)."
             )
         antialias = None
 
