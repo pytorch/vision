6691acdd7025de90e97abaea4e5e382c206243e4
Philip Meier <github.pmeier@posteo.de>
2023-02-17 13:58:12 +0100

    try run v2 tests in a separate step for macos

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -8,7 +8,15 @@ conda activate ./env
 python -m torch.utils.collect_env
 
 case "$(uname -s)" in
-    Darwin*) IGNORE='--ignore-glob=test/*v2*';;
-    *) IGNORE=''
+  Darwin*)
+    # The largest macOS runner is not able to handle the regular test suite plus the transforms v2 tests at the same
+    # time due to insufficient resources. Thus, we ignore the transforms v2 tests at first and run them in a separate
+    # step afterwards.
+    GLOB='test/test_transforms_v2*'
+    pytest --junitxml=test-results/junit.xml -v --durations 20 --ignore-glob="${GLOB}"
+    eval "pytest --junitxml=test-results/junit-transforms-v2.xml -v --durations 20 ${GLOB}"
+    ;;
+  *)
+    pytest --junitxml=test-results/junit.xml -v --durations 20
+    ;;
 esac
-pytest --junitxml=test-results/junit.xml -v --durations 20 $IGNORE
