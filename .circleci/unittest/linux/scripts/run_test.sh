a3cb43d01e63438af4f16bb4691cc7f439bdcb27
Philip Meier <github.pmeier@posteo.de>
2023-02-17 12:50:00 +0100

    try ignoring v2 tests on macos

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -6,4 +6,9 @@ eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
 python -m torch.utils.collect_env
-pytest --junitxml=test-results/junit.xml -v --durations 20
+
+case "$(uname -s)" in
+    Darwin*) IGNORE='--ignore-glob=test/*v2*';;
+    *) IGNORE=''
+esac
+pytest --junitxml=test-results/junit.xml -v --durations 20 $IGNORE
