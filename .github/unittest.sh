11a2d6869e75e2038d292a87e2843e17f08beb1a
Philip Meier <github.pmeier@posteo.de>
2023-02-20 15:39:00 +0100

    refactor script

--- .github/unittest.sh
+++ .github/unittest.sh
@@ -5,41 +5,33 @@ set -euo pipefail
 echo '::group::Prepare conda'
 CONDA_PATH=$(which conda)
 eval "$(${CONDA_PATH} shell.bash hook)"
-# The `setuptools` package installed through `conda` includes a patch that errors if something is installed
-# through `setuptools` while the `CONDA_BUILD` environment variable is set.
-# https://github.com/AnacondaRecipes/setuptools-feedstock/blob/f5d8d256810ce28fc0cf34170bc34e06d3754041/recipe/patches/0002-disable-downloads-inside-conda-build.patch
-# (Although we are not using the `-c conda-forge` channel, the patch is equivalent but not public for
-# `setuptools` from the `-c defaults` channel)
-# Since we aren't using `conda build` here, we unset it to avoid installation problems later
-# TODO: investigate where `CONDA_BUILD` is set and maybe fix unset it there
-unset CONDA_BUILD
 echo '::endgroup::'
 
-echo '::group::Set PyTorch conda channel'
+echo '::group::Set PyTorch conda channel and wheel index'
 # TODO: Can we maybe have this as environment variable in the job template? For example, `IS_RELEASE`.
 if [[ (${GITHUB_EVENT_NAME} = 'pull_request' && (${GITHUB_BASE_REF} = 'release'*)) || (${GITHUB_REF} = 'refs/heads/release'*) ]]; then
-  POSTFIX=test
+  CHANNEL_ID=test
 else
-  POSTFIX=nightly
+  CHANNEL_ID=nightly
 fi
-PYTORCH_CHANNEL=pytorch-"${POSTFIX}"
-echo "${PYTORCH_CHANNEL}"
-echo '::endgroup::'
+PYTORCH_CONDA_CHANNEL=pytorch-"${CHANNEL_ID}"
+echo "${PYTORCH_CONDA_CHANNEL}"
 
-echo '::group::Set PyTorch GPU mutex'
 case $GPU_ARCH_TYPE in
   cpu)
-    PYTORCH_MUTEX=cpuonly
+    GPU_ARCH_ID="cpu"
     ;;
   cuda)
-    PYTORCH_MUTEX="pytorch-cuda=${GPU_ARCH_VERSION}"
+    VERSION_WITHOUT_DOT=$(echo "${GPU_ARCH_VERSION}" | sed 's/\.//')
+    GPU_ARCH_ID="cu${VERSION_WITHOUT_DOT}"
     ;;
   *)
     echo "Unknown GPU_ARCH_TYPE=${GPU_ARCH_TYPE}"
     exit 1
     ;;
 esac
-echo "${PYTORCH_MUTEX}"
+PYTORCH_WHEEL_INDEX="https://download.pytorch.org/whl/${CHANNEL_ID}/${GPU_ARCH_ID}"
+echo "${PYTORCH_WHEEL_INDEX}"
 echo '::endgroup::'
 
 echo '::group::Create build environment'
@@ -47,20 +39,25 @@ conda create \
   --name ci \
   --quiet --yes \
   python="${PYTHON_VERSION}" pip \
-  setuptools ninja \
-  libpng jpeg \
-  Pillow numpy requests
+  ninja libpng jpeg \
+  -c "${PYTORCH_CONDA_CHANNEL}" \
+  -c conda-forge
 conda activate ci
-pip install 'av<10'
+pip install --progress-bar=off --upgrade setuptools
 echo '::endgroup::'
 
 echo '::group::Install PyTorch'
-conda install \
-  --quiet --yes \
-  -c "${PYTORCH_CHANNEL}" \
-  -c nvidia \
-  pytorch \
-  "${PYTORCH_MUTEX}"
+# Due to the supply chain attack in Dec 2022 (https://pytorch.org/blog/compromised-nightly-dependency/), we host all
+# third-party dependencies on Linux on our own indices and *don't* install them from PyPI.
+case "$(uname -s)" in
+    Linux*)
+      INDEX_TYPE="index-url"
+      ;;
+    *)
+      INDEX_TYPE="extra-index-url"
+esac
+
+pip install --progress-bar=off torch "--${INDEX_TYPE}=${PYTORCH_WHEEL_INDEX}"
 
 if [[ $GPU_ARCH_TYPE = 'cuda' ]]; then
   python3 -c "import torch; exit(not torch.cuda.is_available())"
