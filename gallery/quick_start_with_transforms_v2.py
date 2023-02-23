9f54794f75d5529bc69cca9a7e26ff11a09e3d37
vfdev-5 <vfdev.5@gmail.com>
2023-02-23 12:46:49 +0100

    Added classification pipeline

--- gallery/quick_start_with_transforms_v2.py
+++ gallery/quick_start_with_transforms_v2.py
@@ -3,47 +3,51 @@
 Quick-start with Transforms API v2
 ==================================
 
-This example illustrates how to update user's data-augmentations pipeline with
+This example illustrates how to update existing user's data augmentations pipeline with
 new Torchvision Transforms API v2 (:ref:`image transformations <transforms>`).
 
 """
 
-a = 1
+#######################################
+# Classification pipeline
+# -----------------------
+# For the classification task, let us take as example ImageNet
+# training data augmentation pipeline (TODO: add a link to ref script).
+# The only change needed is to replace the imported module `transforms` by `v2`:
+
+import torch
+from torchvision.transforms import autoaugment
+from torchvision.transforms import InterpolationMode
+
+mode = InterpolationMode.BILINEAR
+aa_policy = autoaugment.AutoAugmentPolicy("imagenet")
+mean = (0.485, 0.456, 0.406)
+std = (0.229, 0.224, 0.225)
 
 #######################################
-# Transforming images on GPU
-# --------------------------
-# Most transforms natively support tensors on top of PIL images (to visualize
-# the effect of the transforms, you may refer to see
-# :ref:`sphx_glr_auto_examples_plot_transforms.py`).
-# Using tensor images, we can run the transforms on GPUs if cuda is available!
 #
 #   .. code-block:: diff
 #
-#       - a = 10
-#       + a = 12
+#       - from torchvision.transforms import transforms
+#       + from torchvision.transforms import v2 as transforms
+#
+#
+#       t = transforms.Compose([
+#           transforms.RandomResizedCrop(224, interpolation=mode),
+#           transforms.RandomHorizontalFlip(),
+#       -   autoaugment.AutoAugment(policy=aa_policy, interpolation=mode),
+#       +   transforms.AutoAugment(policy=aa_policy, interpolation=mode),
+#           transforms.PILToTensor(),  # optionally, we can use transforms.ToImageTensor() instead
+#           transforms.ConvertImageDtype(torch.float),
+#           transforms.Normalize(mean=mean, std=std),
+#           transforms.RandomErasing()
+#       ])
 #
 
-"""
-various features that are now supported by the
-:ref:`image transformations <transforms>` on Tensor images. In particular, we
-show how image transforms can be performed on GPU, and how one can also script
-them using JIT compilation.
+#######################################
+# Object detection pipeline
+# -------------------------
+# TODO
 
-Prior to v0.8.0, transforms in torchvision have traditionally been PIL-centric
-and presented multiple limitations due to that. Now, since v0.8.0, transforms
-implementations are Tensor and PIL compatible, and we can achieve the following
-new features:
-
-- transform multi-band torch tensor images (with more than 3-4 channels)
-- torchscript transforms together with your model for deployment
-- support for GPU acceleration
-- batched transformation such as for videos
-- read and decode data directly as torch tensor with torchscript support (for PNG and JPEG image formats)
-
-.. note::
-    These features are only possible with **Tensor** images.
-
-"""
 
 
