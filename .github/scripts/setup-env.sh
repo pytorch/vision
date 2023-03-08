e6708542135afc3a11dfa302c84c17dc8a27b96d
Philip Meier <github.pmeier@posteo.de>
2023-03-08 22:28:01 +0100

    Revert "try bash -x"
    This reverts commit 3a3b300831bc36eddb04aefffb79f70d98439fcf.

--- .github/scripts/setup-env.sh
+++ .github/scripts/setup-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
 # Prepare conda
 CONDA_PATH=$(which conda)
--- .github/scripts/unittest.sh
+++ .github/scripts/unittest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
 ./.github/scripts/setup-env.sh
 
