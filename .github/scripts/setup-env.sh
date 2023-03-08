3a3b300831bc36eddb04aefffb79f70d98439fcf
Philip Meier <github.pmeier@posteo.de>
2023-03-08 22:16:50 +0100

    try bash -x

--- .github/scripts/setup-env.sh
+++ .github/scripts/setup-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euxo pipefail
 
 # Prepare conda
 CONDA_PATH=$(which conda)
--- .github/scripts/unittest.sh
+++ .github/scripts/unittest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euxo pipefail
 
 ./.github/scripts/setup-env.sh
 
