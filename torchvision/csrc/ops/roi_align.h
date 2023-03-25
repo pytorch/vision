61c777860e81f0eec456b816fecea6aa8586e5ec
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared13302.05.pnb1.facebook.com>
2023-03-25 04:30:52 -0700

    2023-03-25 nightly release (d4adf08988339a7da81a19ba390d78a629e45c4d)

--- torchvision/csrc/ops/roi_align.h
+++ torchvision/csrc/ops/roi_align.h
@@ -15,6 +15,15 @@ VISION_API at::Tensor roi_align(
     int64_t sampling_ratio,
     bool aligned);
 
+VISION_API at::Tensor roi_align_symint(
+    const at::Tensor& input,
+    const at::Tensor& rois,
+    double spatial_scale,
+    c10::SymInt pooled_height,
+    c10::SymInt pooled_width,
+    int64_t sampling_ratio,
+    bool aligned);
+
 namespace detail {
 
 at::Tensor _roi_align_backward(
@@ -27,6 +36,19 @@ at::Tensor _roi_align_backward(
     int64_t channels,
     int64_t height,
     int64_t width,
+    int64_t sampling_ratio,
+    bool aligned);
+
+at::Tensor _roi_align_backward_symint(
+    const at::Tensor& grad,
+    const at::Tensor& rois,
+    double spatial_scale,
+    c10::SymInt pooled_height,
+    c10::SymInt pooled_width,
+    c10::SymInt batch_size,
+    c10::SymInt channels,
+    c10::SymInt height,
+    c10::SymInt width,
     int64_t sampling_ratio,
     bool aligned);
 
