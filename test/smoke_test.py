0ff64a886ab207d75720de522221d39dd77cc90f
Nicolas Hug <nicolashug@meta.com>
2023-03-30 08:10:33 -0700

    [fbsync] Extend TorchVision smoke tests to MPS (#7365)
    Reviewed By: vmoens
    
    Differential Revision: D44416562
    
    fbshipit-source-id: dcb12c77a56d8a257e6c49416be67f89d6dca9ee

--- test/smoke_test.py
+++ test/smoke_test.py
@@ -59,6 +59,8 @@ def main() -> None:
     smoke_test_torchvision_resnet50_classify()
     if torch.cuda.is_available():
         smoke_test_torchvision_resnet50_classify("cuda")
+    if torch.backends.mps.is_available():
+        smoke_test_torchvision_resnet50_classify("mps")
 
 
 if __name__ == "__main__":
