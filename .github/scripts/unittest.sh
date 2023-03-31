c585d29b3eeb8d16e17df416c97450428a3fa747
Philip Meier <github.pmeier@posteo.de>
2023-03-31 09:27:30 +0200

    [REVERTME] only run smoke tests

--- .github/scripts/unittest.sh
+++ .github/scripts/unittest.sh
@@ -9,8 +9,10 @@ CONDA_PATH=$(which conda)
 eval "$(${CONDA_PATH} shell.bash hook)"
 conda activate ci
 
-echo '::group::Install testing utilities'
-pip install --progress-bar=off pytest pytest-mock pytest-cov
-echo '::endgroup::'
+python test/smoke_test.py
 
-pytest --junit-xml="${RUNNER_TEST_RESULTS_DIR}/test-results.xml" -v --durations=25
+#echo '::group::Install testing utilities'
+#pip install --progress-bar=off pytest pytest-mock pytest-cov
+#echo '::endgroup::'
+#
+#pytest --junit-xml="${RUNNER_TEST_RESULTS_DIR}/test-results.xml" -v --durations=25
