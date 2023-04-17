95b34de8aedcc07289965dc36ab88c4e396d14db
Liron <liron@tapwithus.com>
2023-04-17 16:07:06 +0300

    use local parameters and fix nested tuple

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -462,12 +462,12 @@ def test_resize_size_equals_small_edge_size(height, width):
     result = t(img)
     assert max(result.size) == max_size
 
-def test_resize_equal_input_output_sizes(height, width):
+def test_resize_equal_input_output_sizes():
     # Regression test for https://github.com/pytorch/vision/issues/7518
     height, width = 28, 27
     img = Image.new("RGB", size=(width, height))
 
-    t = transforms.Resize(((height, width)), antialias=True)
+    t = transforms.Resize((height, width), antialias=True)
     result = t(img)
     assert result is img
 
