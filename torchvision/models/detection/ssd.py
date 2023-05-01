eaa1f39035572533fba68a01125992ce763834bb
Junji Hashimoto <junji.hashimoto@gmail.com>
2023-05-02 05:37:27 +0900

    Add SSD512 object detection

--- torchvision/models/detection/ssd.py
+++ torchvision/models/detection/ssd.py
@@ -22,6 +22,7 @@ from .transform import GeneralizedRCNNTransform
 __all__ = [
     "SSD300_VGG16_Weights",
     "ssd300_vgg16",
+    "ssd512_vgg16",
 ]
 
 
@@ -582,6 +583,7 @@ def ssd300_vgg16(
     num_classes: Optional[int] = None,
     weights_backbone: Optional[VGG16_Weights] = VGG16_Weights.IMAGENET1K_FEATURES,
     trainable_backbone_layers: Optional[int] = None,
+    highres: bool = False,
     **kwargs: Any,
 ) -> SSD:
     """The SSD300 model is based on the `SSD: Single Shot MultiBox Detector
@@ -635,6 +637,7 @@ def ssd300_vgg16(
         trainable_backbone_layers (int, optional): number of trainable (not frozen) layers starting from final block.
             Valid values are between 0 and 5, with 5 meaning all backbone layers are trainable. If ``None`` is
             passed (the default) this value is set to 4.
+        highres (bool, optional): By default (False), input-resolution is 300x300. If True is passed, it is 512x512.
         **kwargs: parameters passed to the ``torchvision.models.detection.SSD``
             base class. Please refer to the `source code
             <https://github.com/pytorch/vision/blob/main/torchvision/models/detection/ssd.py>`_
@@ -661,12 +664,19 @@ def ssd300_vgg16(
 
     # Use custom backbones more appropriate for SSD
     backbone = vgg16(weights=weights_backbone, progress=progress)
-    backbone = _vgg_extractor(backbone, False, trainable_backbone_layers)
-    anchor_generator = DefaultBoxGenerator(
-        [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
-        scales=[0.07, 0.15, 0.33, 0.51, 0.69, 0.87, 1.05],
-        steps=[8, 16, 32, 64, 100, 300],
-    )
+    backbone = _vgg_extractor(backbone, highres, trainable_backbone_layers)
+    if highres:
+        anchor_generator = DefaultBoxGenerator(
+            [[2], [2, 3], [2, 3], [2, 3], [2, 3], [2], [2]],
+            scales=[0.04, 0.1, 0.26, 0.42, 0.58, 0.74, 0.9, 1.06],
+            steps=[8, 16, 32, 64, 128, 256, 512],
+        )
+    else:
+        anchor_generator = DefaultBoxGenerator(
+            [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+            scales=[0.07, 0.15, 0.33, 0.51, 0.69, 0.87, 1.05],
+            steps=[8, 16, 32, 64, 100, 300],
+        )
 
     defaults = {
         # Rescale the input in a way compatible to the backbone
@@ -680,3 +690,43 @@ def ssd300_vgg16(
         model.load_state_dict(weights.get_state_dict(progress=progress))
 
     return model
+
+@register_model()
+@handle_legacy_interface(
+    weights=("pretrained", None),
+    weights_backbone=("pretrained_backbone", VGG16_Weights.IMAGENET1K_FEATURES),
+)
+def ssd512_vgg16(
+    *,
+    weights: Optional[WeightsEnum] = None,
+    progress: bool = True,
+    num_classes: Optional[int] = None,
+    weights_backbone: Optional[VGG16_Weights] = VGG16_Weights.IMAGENET1K_FEATURES,
+    trainable_backbone_layers: Optional[int] = None,
+    **kwargs: Any,
+) -> SSD:
+    """The SSD512 model is based on the `SSD: Single Shot MultiBox Detector
+    <https://arxiv.org/abs/1512.02325>`_ paper
+
+    .. betastatus:: detection module
+
+    The interface is based on ssd300_vgg16.
+
+    Args:
+        weights (:class:`~torchvision.models._api.WeightsEnum`, optional): The pretrained
+                weights to use. By default, no pre-trained weights are used.
+        progress (bool, optional): If True, displays a progress bar of the download to stderr
+            Default is True.
+        num_classes (int, optional): number of output classes of the model (including the background)
+        weights_backbone (:class:`~torchvision.models.VGG16_Weights`, optional): The pretrained weights for the
+            backbone
+        trainable_backbone_layers (int, optional): number of trainable (not frozen) layers starting from final block.
+            Valid values are between 0 and 5, with 5 meaning all backbone layers are trainable. If ``None`` is
+            passed (the default) this value is set to 4.
+        **kwargs: parameters passed to the ``torchvision.models.detection.SSD``
+            base class. Please refer to the `source code
+            <https://github.com/pytorch/vision/blob/main/torchvision/models/detection/ssd.py>`_
+            for more details about this class.
+
+    """
+    return ssd300_vgg16(weights, progress, num_classes, weights_backbone, trainable_backbone_layers, True, **kwargs)
