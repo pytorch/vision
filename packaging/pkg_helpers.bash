96d0a6e027347ea3804bb555116b9ace927e5c6e
pbialecki <piotr.bialecki@hotmail.de>
2023-05-22 22:24:07 -0700

    remove CUDA 11.7 builds; add 11.8

--- packaging/pkg_helpers.bash
+++ packaging/pkg_helpers.bash
@@ -62,14 +62,6 @@ setup_cuda() {
       fi
       export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6;9.0"
       ;;
-    cu117)
-      if [[ "$OSTYPE" == "msys" ]]; then
-        export CUDA_HOME="C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.7"
-      else
-        export CUDA_HOME=/usr/local/cuda-11.7/
-      fi
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
-      ;;
     cpu)
       ;;
     rocm*)
@@ -266,9 +258,6 @@ setup_conda_cudatoolkit_constraint() {
       cu118)
         export CONDA_CUDATOOLKIT_CONSTRAINT="- pytorch-cuda=11.8 # [not osx]"
         ;;
-      cu117)
-        export CONDA_CUDATOOLKIT_CONSTRAINT="- pytorch-cuda=11.7 # [not osx]"
-        ;;
       cpu)
         export CONDA_CUDATOOLKIT_CONSTRAINT=""
         export CONDA_BUILD_VARIANT="cpu"
@@ -294,9 +283,6 @@ setup_conda_cudatoolkit_plain_constraint() {
         ;;
       cu118)
         export CONDA_CUDATOOLKIT_CONSTRAINT="pytorch-cuda=11.8"
-        ;;
-      cu117)
-        export CONDA_CUDATOOLKIT_CONSTRAINT="pytorch-cuda=11.7"
         ;;
       cpu)
         export CONDA_CUDATOOLKIT_CONSTRAINT=""
