f30e4136a63985cf9afc5fca028e246ce5d6ebd4
Philip Meier <github.pmeier@posteo.de>
2023-05-24 11:59:57 +0200

    [REVERTME] disable unrelated CI

--- .github/scripts/unittest.sh
+++ .github/scripts/unittest.sh
@@ -11,4 +11,5 @@ echo '::group::Install testing utilities'
 pip install --progress-bar=off pytest pytest-mock pytest-cov
 echo '::endgroup::'
 
-pytest --junit-xml="${RUNNER_TEST_RESULTS_DIR}/test-results.xml" -v --durations=25
+python test/smoke_test.py
+#pytest --junit-xml="${RUNNER_TEST_RESULTS_DIR}/test-results.xml" -v --durations=25
