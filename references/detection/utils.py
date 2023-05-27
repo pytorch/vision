a40c4eb589fba59ed59b0acd623b0e684aad47b1
kafmws <wdxdesperado@qq.com>
2023-05-27 21:25:46 +0800

    make function reduce_dict always return tensors

--- references/detection/utils.py
+++ references/detection/utils.py
@@ -90,23 +90,23 @@ def reduce_dict(input_dict, average=True):
         average (bool): whether to do average or sum
     Reduce the values in the dictionary from all processes so that all processes
     have the averaged results. Returns a dict with the same fields as
-    input_dict, after reduction.
+    input_dict, after reduction. Always return tensors for consistency of return types.
     """
+    names = []
+    values = []
+    # sort the keys so that they are consistent across processes.
+    # nothing to sync, but we still convert to tensor for consistency with the distributed case.
+    for k in sorted(input_dict.keys()):
+        names.append(k)
+        values.append(torch.tensor(input_dict[k], device="cuda"))
     world_size = get_world_size()
-    if world_size < 2:
-        return input_dict
-    with torch.inference_mode():
-        names = []
-        values = []
-        # sort the keys so that they are consistent across processes
-        for k in sorted(input_dict.keys()):
-            names.append(k)
-            values.append(input_dict[k])
-        values = torch.stack(values, dim=0)
-        dist.all_reduce(values)
-        if average:
-            values /= world_size
-        reduced_dict = {k: v for k, v in zip(names, values)}
+    if world_size >= 2:
+        with torch.no_grad():
+            values = torch.stack(values, dim=0)
+            dist.all_reduce(values)
+            if average:
+                values /= world_size
+    reduced_dict = {k: v for k, v in zip(names, values)}
     return reduced_dict
 
 
