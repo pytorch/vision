09c07a084e31a378aed3511f9fbab5e0a057ed01
Philip Meier <github.pmeier@posteo.de>
2023-07-17 09:50:17 +0200

    cleanup detection references

--- references/detection/coco_utils.py
+++ references/detection/coco_utils.py
@@ -198,9 +198,7 @@ def get_coco_api_from_dataset(dataset):
             break
         if isinstance(dataset, torch.utils.data.Subset):
             dataset = dataset.dataset
-    if isinstance(dataset, torchvision.datasets.CocoDetection) or isinstance(
-        getattr(dataset, "_dataset", None), torchvision.datasets.CocoDetection
-    ):
+    if isinstance(dataset, torchvision.datasets.CocoDetection):
         return dataset.coco
     return convert_to_coco_api(dataset)
 
