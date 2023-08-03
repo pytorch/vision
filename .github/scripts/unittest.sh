b391a6ab923ea9b9fcee92c29ad244e1e4e01df3
Nicolas Hug <contact@nicolas-hug.com>
2023-08-03 17:03:12 +0100

    test all of test_image.py?

--- .github/scripts/unittest.sh
+++ .github/scripts/unittest.sh
@@ -12,4 +12,4 @@ pip install --progress-bar=off pytest pytest-mock pytest-cov
 echo '::endgroup::'
 
 # pytest --junit-xml="${RUNNER_TEST_RESULTS_DIR}/test-results.xml" -v --durations=25
-pytest test/test_image.py -k turbo --junit-xml="${RUNNER_TEST_RESULTS_DIR}/test-results.xml" -v -s --durations=25
+pytest test/test_image.py --junit-xml="${RUNNER_TEST_RESULTS_DIR}/test-results.xml" -v -s --durations=25
