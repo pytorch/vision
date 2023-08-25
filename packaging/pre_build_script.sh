32b83569c4e1ced2d6f431ad4e0dd962a2aa80be
Nicolas Hug <nicolashug@meta.com>
2023-08-25 06:38:15 -0700

    [fbsync] Use libjpeg-turbo for tests and builds (#7672)
    Summary: Co-authored-by: Philip Meier <github.pmeier@posteo.de>
    
    Reviewed By: matteobettini
    
    Differential Revision: D48642301
    
    fbshipit-source-id: bbf318b1568fdc24796c4e6649d010bdfbd27a44

--- packaging/pre_build_script.sh
+++ packaging/pre_build_script.sh
@@ -13,8 +13,8 @@ fi
 
 if [[ "$(uname)" == Darwin || "$OSTYPE" == "msys" ]]; then
   # Install libpng from Anaconda (defaults)
-  conda install libpng "jpeg<=9b" -yq
-  conda install -yq ffmpeg=4.2 -c pytorch
+  conda install libpng -yq
+  conda install -yq ffmpeg=4.2 libjpeg-turbo -c pytorch
 
   # Copy binaries to be included in the wheel distribution
   if [[ "$OSTYPE" == "msys" ]]; then
