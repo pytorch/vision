cf3ca684361fee8fcd8c954f6fade348778c1c8e
Nicolas Hug <nicolashug@meta.com>
2023-08-25 06:38:15 -0700

    [fbsync] Dispatcher -> Functional (#7829)
    Reviewed By: matteobettini
    
    Differential Revision: D48642298
    
    fbshipit-source-id: fc7c521bde09fea9450b5ac2b11b5b055432db65

--- torchvision/transforms/v2/_transform.py
+++ torchvision/transforms/v2/_transform.py
@@ -30,8 +30,8 @@ class Transform(nn.Module):
     def _get_params(self, flat_inputs: List[Any]) -> Dict[str, Any]:
         return dict()
 
-    def _call_kernel(self, dispatcher: Callable, inpt: Any, *args: Any, **kwargs: Any) -> Any:
-        kernel = _get_kernel(dispatcher, type(inpt), allow_passthrough=True)
+    def _call_kernel(self, functional: Callable, inpt: Any, *args: Any, **kwargs: Any) -> Any:
+        kernel = _get_kernel(functional, type(inpt), allow_passthrough=True)
         return kernel(inpt, *args, **kwargs)
 
     def _transform(self, inpt: Any, params: Dict[str, Any]) -> Any:
