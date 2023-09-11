dbc2ede8d91a22e14e52f5a07b0ddff1ab78cd6b
Philip Meier <github.pmeier@posteo.de>
2023-09-11 09:29:10 +0200

    remove deprecated functional modules

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -2,7 +2,6 @@ import math
 import os
 import random
 import re
-import textwrap
 import warnings
 from functools import partial
 
@@ -25,7 +24,7 @@ try:
 except ImportError:
     stats = None
 
-from common_utils import assert_equal, assert_run_python_script, cycle_over, float_dtypes, int_dtypes
+from common_utils import assert_equal, cycle_over, float_dtypes, int_dtypes
 
 
 GRACE_HOPPER = get_file_path_2(
@@ -2248,36 +2247,6 @@ def test_random_grayscale_with_grayscale_input():
     image_pil = F.to_pil_image(image_tensor)
     output_pil = transform(image_pil)
     torch.testing.assert_close(F.pil_to_tensor(output_pil), image_tensor)
-
-
-# TODO: remove in 0.17 when we can delete functional_pil.py and functional_tensor.py
-@pytest.mark.parametrize(
-    "import_statement",
-    (
-        "from torchvision.transforms import functional_pil",
-        "from torchvision.transforms import functional_tensor",
-        "from torchvision.transforms.functional_tensor import resize",
-        "from torchvision.transforms.functional_pil import resize",
-    ),
-)
-@pytest.mark.parametrize("from_private", (True, False))
-def test_functional_deprecation_warning(import_statement, from_private):
-    if from_private:
-        import_statement = import_statement.replace("functional", "_functional")
-        source = f"""
-        import warnings
-
-        with warnings.catch_warnings():
-            warnings.simplefilter("error")
-            {import_statement}
-        """
-    else:
-        source = f"""
-        import pytest
-        with pytest.warns(UserWarning, match="removed in 0.17"):
-            {import_statement}
-        """
-    assert_run_python_script(textwrap.dedent(source))
 
 
 if __name__ == "__main__":
