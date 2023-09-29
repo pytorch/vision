b171192de8ec916141b8fca4ebbbca114df492f5
Philip Meier <github.pmeier@posteo.de>
2023-09-29 14:46:26 +0200

    [REVERTME] temporarily re-indent resize core functionality for review

--- torchvision/transforms/v2/functional/_geometry.py
+++ torchvision/transforms/v2/functional/_geometry.py
@@ -227,54 +227,56 @@ def resize_image(
     if numel == 0:
         return image.reshape(output_shape)
 
-    image = image.reshape(-1, num_channels, old_height, old_width)
+    # FIXME: remove this before merge. This is just here to get a proper diff view
+    if True:
+        image = image.reshape(-1, num_channels, old_height, old_width)
 
-    dtype = image.dtype
-    acceptable_dtypes = [torch.float32, torch.float64]
-    if interpolation == InterpolationMode.NEAREST or interpolation == InterpolationMode.NEAREST_EXACT:
-        # uint8 dtype can be included for cpu and cuda input if nearest mode
-        acceptable_dtypes.append(torch.uint8)
-    elif image.device.type == "cpu":
-        # uint8 dtype support for bilinear and bicubic is limited to cpu and
-        # according to our benchmarks, non-AVX CPUs should still prefer u8->f32->interpolate->u8 path for bilinear
-        if (interpolation == InterpolationMode.BILINEAR and "AVX2" in torch.backends.cpu.get_cpu_capability()) or (
-            interpolation == InterpolationMode.BICUBIC
-        ):
+        dtype = image.dtype
+        acceptable_dtypes = [torch.float32, torch.float64]
+        if interpolation == InterpolationMode.NEAREST or interpolation == InterpolationMode.NEAREST_EXACT:
+            # uint8 dtype can be included for cpu and cuda input if nearest mode
             acceptable_dtypes.append(torch.uint8)
+        elif image.device.type == "cpu":
+            # uint8 dtype support for bilinear and bicubic is limited to cpu and
+            # according to our benchmarks, non-AVX CPUs should still prefer u8->f32->interpolate->u8 path for bilinear
+            if (interpolation == InterpolationMode.BILINEAR and "AVX2" in torch.backends.cpu.get_cpu_capability()) or (
+                interpolation == InterpolationMode.BICUBIC
+            ):
+                acceptable_dtypes.append(torch.uint8)
 
-    strides = image.stride()
-    if image.is_contiguous(memory_format=torch.channels_last) and image.shape[0] == 1 and numel != strides[0]:
-        # There is a weird behaviour in torch core where the output tensor of `interpolate()` can be allocated as
-        # contiguous even though the input is un-ambiguously channels_last (https://github.com/pytorch/pytorch/issues/68430).
-        # In particular this happens for the typical torchvision use-case of single CHW images where we fake the batch dim
-        # to become 1CHW. Below, we restride those tensors to trick torch core into properly allocating the output as
-        # channels_last, thus preserving the memory format of the input. This is not just for format consistency:
-        # for uint8 bilinear images, this also avoids an extra copy (re-packing) of the output and saves time.
-        # TODO: when https://github.com/pytorch/pytorch/issues/68430 is fixed (possibly by https://github.com/pytorch/pytorch/pull/100373),
-        # we should be able to remove this hack.
-        new_strides = list(strides)
-        new_strides[0] = numel
-        image = image.as_strided((1, num_channels, old_height, old_width), new_strides)
+        strides = image.stride()
+        if image.is_contiguous(memory_format=torch.channels_last) and image.shape[0] == 1 and numel != strides[0]:
+            # There is a weird behaviour in torch core where the output tensor of `interpolate()` can be allocated as
+            # contiguous even though the input is un-ambiguously channels_last (https://github.com/pytorch/pytorch/issues/68430).
+            # In particular this happens for the typical torchvision use-case of single CHW images where we fake the batch dim
+            # to become 1CHW. Below, we restride those tensors to trick torch core into properly allocating the output as
+            # channels_last, thus preserving the memory format of the input. This is not just for format consistency:
+            # for uint8 bilinear images, this also avoids an extra copy (re-packing) of the output and saves time.
+            # TODO: when https://github.com/pytorch/pytorch/issues/68430 is fixed (possibly by https://github.com/pytorch/pytorch/pull/100373),
+            # we should be able to remove this hack.
+            new_strides = list(strides)
+            new_strides[0] = numel
+            image = image.as_strided((1, num_channels, old_height, old_width), new_strides)
 
-    need_cast = dtype not in acceptable_dtypes
-    if need_cast:
-        image = image.to(dtype=torch.float32)
+        need_cast = dtype not in acceptable_dtypes
+        if need_cast:
+            image = image.to(dtype=torch.float32)
 
-    image = interpolate(
-        image,
-        size=[new_height, new_width],
-        mode=interpolation.value,
-        align_corners=align_corners,
-        antialias=antialias,
-    )
+        image = interpolate(
+            image,
+            size=[new_height, new_width],
+            mode=interpolation.value,
+            align_corners=align_corners,
+            antialias=antialias,
+        )
 
-    if need_cast:
-        if interpolation == InterpolationMode.BICUBIC and dtype == torch.uint8:
-            # This path is hit on non-AVX archs, or on GPU.
-            image = image.clamp_(min=0, max=255)
-        if dtype in (torch.uint8, torch.int8, torch.int16, torch.int32, torch.int64):
-            image = image.round_()
-        image = image.to(dtype=dtype)
+        if need_cast:
+            if interpolation == InterpolationMode.BICUBIC and dtype == torch.uint8:
+                # This path is hit on non-AVX archs, or on GPU.
+                image = image.clamp_(min=0, max=255)
+            if dtype in (torch.uint8, torch.int8, torch.int16, torch.int32, torch.int64):
+                image = image.round_()
+            image = image.to(dtype=dtype)
 
     return image.reshape(output_shape)
 
