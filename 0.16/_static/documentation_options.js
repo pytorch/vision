c0239bb7b8f879c9e378d5be556e02c4177b76ef
Nicolas Hug <nh.nicolas.hug@gmail.com>
2023-10-04 18:48:28 +0100

    0.16 docs (#8018)

--- 0.16/_static/documentation_options.js
+++ 0.16/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.16',
+    VERSION: 'main',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
