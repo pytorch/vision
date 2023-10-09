1f50d634f76b7e2cb9aa9037386cc2b440b6ceee
pytorchbot <pytorchbot@pytorch.com>
2023-10-09 11:34:41 +0000

    2023-10-09 nightly release (16967f0920a67d264f1a680c389e16cd94ef7e12)

--- test/test_transforms_v2.py
+++ test/test_transforms_v2.py
@@ -52,6 +52,12 @@ from torchvision.transforms.v2.functional._geometry import _get_perspective_coef
 from torchvision.transforms.v2.functional._utils import _get_kernel, _register_kernel_internal
 
 
+# FIXME: Put this back once https://github.com/pytorch/vision/issues/8030
+# and https://github.com/pytorch/pytorch/issues/110857 are addressed
+# turns all warnings into errors for this module
+# pytestmark = pytest.mark.filterwarnings("error")
+
+
 @pytest.fixture(autouse=True)
 def fix_rng_seed():
     set_rng_seed(0)
@@ -537,10 +543,6 @@ def reference_affine_bounding_boxes_helper(bounding_boxes, *, affine_matrix, new
         format=format,
         canvas_size=canvas_size,
     )
-
-
-# turns all warnings into errors for this module
-pytestmark = pytest.mark.filterwarnings("error")
 
 
 class TestResize:
