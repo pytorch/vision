aadda3845822ef38fda4c740715b93dffbe27bee
Nicolas Hug <contact@nicolas-hug.com>
2023-10-27 15:11:06 +0100

    Remove optest as we can't xfail parametrized tests

--- test/test_ops.py
+++ test/test_ops.py
@@ -117,9 +117,8 @@ class RoIOpTester(ABC):
             torch.float32,
             torch.float64,
         ),
-        # ids=str,
+        ids=str,
     )
-    @pytest.mark.opcheck_only_one()
     def test_forward(self, device, contiguous, x_dtype, rois_dtype=None, deterministic=False, **kwargs):
         if device == "mps" and x_dtype is torch.float64:
             pytest.skip("MPS does not support float64")
@@ -187,7 +186,6 @@ class RoIOpTester(ABC):
     @pytest.mark.parametrize("seed", range(10))
     @pytest.mark.parametrize("device", cpu_and_cuda_and_mps())
     @pytest.mark.parametrize("contiguous", (True, False))
-    @pytest.mark.opcheck_only_one()
     def test_backward(self, seed, device, contiguous, deterministic=False):
         atol = self.mps_backward_atol if device == "mps" else 1e-05
         dtype = self.mps_dtype if device == "mps" else self.dtype
@@ -370,15 +368,6 @@ class TestPSRoIPool(RoIOpTester):
 
     def test_boxes_shape(self):
         self._helper_boxes_shape(ops.ps_roi_pool)
-
-
-optests.generate_opcheck_tests(
-    testcase=TestPSRoIPool,
-    namespaces=["torchvision"],
-    failures_dict_path=os.path.join(os.path.dirname(__file__), "optests_failures_dict.json"),
-    additional_decorators=[],
-    test_utils=OPTESTS,
-)
 
 
 def bilinear_interpolate(data, y, x, snap_border=False):
