7ef6aaa1170dcdc8d3c854af72303c0e7e41530e
atalman <atalman@fb.com>
2023-12-01 09:49:01 -0800

    remove_typo

--- test/smoke_test.py
+++ test/smoke_test.py
@@ -13,7 +13,7 @@ SCRIPT_DIR = Path(__file__).parent
 
 def smoke_test_torchvision() -> None:
     print(
-        "Is torchvision usable?",ƒ
+        "Is torchvision usable?",
         all(x is not None for x in [torch.ops.image.decode_png, torch.ops.torchvision.roi_align]),
     )
 
