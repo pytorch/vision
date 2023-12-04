e092477bacea16a74979857cb1e9b98e7168aeec
Philip Meier <github.pmeier@posteo.de>
2023-12-04 10:40:12 +0100

    add documentation

--- torchvision/datasets/imagenette.py
+++ torchvision/datasets/imagenette.py
@@ -9,22 +9,41 @@ from .vision import VisionDataset
 
 
 class Imagenette(VisionDataset):
+    """`Imagenette <https://github.com/fastai/imagenette#imagenette-1`_ image classification dataset.
+
+    Args:
+        root (string): Root directory of the Imagenette dataset.
+        split (string, optional): The dataset split. Supports ``"train"`` (default), and ``"val"``.
+        size (string, optional): The image size. Supports ``"full"`` (default), ``"320px"``, and ``"160px"``.
+        download (bool, optional): If ``True``, downloads the dataset components and places them in ``root``. Already
+            downloaded archives are not downloaded again.
+        transform (callable, optional): A function/transform that  takes in an PIL image and returns a transformed
+            version, e.g. ``transforms.RandomCrop``.
+        target_transform (callable, optional): A function/transform that takes in the target and transforms it.
+
+     Attributes:
+        classes (list): List of the class name tuples.
+        class_to_idx (dict): Dict with items (class name, class index).
+        wnids (list): List of the WordNet IDs.
+        wnid_to_idx (dict): Dict with items (WordNet ID, class index).
+    """
+
     _ARCHIVES = {
         "full": ("https://s3.amazonaws.com/fast-ai-imageclas/imagenette2.tgz", "fe2fc210e6bb7c5664d602c3cd71e612"),
         "320px": ("https://s3.amazonaws.com/fast-ai-imageclas/imagenette2-320.tgz", "3df6f0d01a2c9592104656642f5e78a3"),
         "160px": ("https://s3.amazonaws.com/fast-ai-imageclas/imagenette2-160.tgz", "e793b78cc4c9e9a4ccc0c1155377a412"),
     }
     _WNID_TO_CLASS = {
-        "n01440764": "tench, Tinca tinca",
-        "n02102040": "English springer, English springer spaniel",
-        "n02979186": "cassette player",
-        "n03000684": "chain saw, chainsaw",
-        "n03028079": "church, church building",
-        "n03394916": "French horn, horn",
-        "n03417042": "garbage truck, dustcart",
-        "n03425413": "gas pump, gasoline pump, petrol pump, island dispenser",
-        "n03445777": "golf ball",
-        "n03888257": "parachute, chute",
+        "n01440764": ("tench", "Tinca tinca"),
+        "n02102040": ("English springer", "English springer spaniel"),
+        "n02979186": ("cassette player",),
+        "n03000684": ("chain saw", "chainsaw"),
+        "n03028079": ("church", "church building"),
+        "n03394916": ("French horn", "horn"),
+        "n03417042": ("garbage truck", "dustcart"),
+        "n03425413": ("gas pump", "gasoline pump", "petrol pump", "island dispenser"),
+        "n03445777": ("golf ball",),
+        "n03888257": ("parachute", "chute"),
     }
 
     def __init__(
@@ -48,11 +67,13 @@ class Imagenette(VisionDataset):
         if download:
             self._download()
         elif not self._check_exists():
-            raise RuntimeError("Dataset not found or corrupted. You can use download=True to download it")
+            raise RuntimeError("Dataset not found. You can use download=True to download it.")
 
         self.wnids, self.wnid_to_idx = find_classes(self._image_root)
         self.classes = [self._WNID_TO_CLASS[wnid] for wnid in self.wnids]
-        self.class_to_idx = {self._WNID_TO_CLASS[wnid]: idx for wnid, idx in self.wnid_to_idx.items()}
+        self.class_to_idx = {
+            class_name: idx for wnid, idx in self.wnid_to_idx.items() for class_name in self._WNID_TO_CLASS[wnid]
+        }
         self._samples = make_dataset(self._image_root, self.wnid_to_idx, extensions=".jpeg")
 
     def _check_exists(self) -> bool:
