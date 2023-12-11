99e8b79da2868575fb2a57cd559ba40703f72307
Nicolas Hug <contact@nicolas-hug.com>
2023-12-11 15:46:12 +0000

    Explicitly ignore all video tests

--- .github/scripts/unittest.sh
+++ .github/scripts/unittest.sh
@@ -12,4 +12,5 @@ pip install --progress-bar=off pytest pytest-mock pytest-cov expecttest
 echo '::endgroup::'
 
 python test/smoke_test.py
-pytest --junit-xml="${RUNNER_TEST_RESULTS_DIR}/test-results.xml" -v --durations=25
+
+pytest --ignore-glob="*test_video*" --junit-xml="${RUNNER_TEST_RESULTS_DIR}/test-results.xml" -v --durations=25
