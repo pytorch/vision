6ace7a011dbae0f2b453b035669336bd618182a1
pytorchbot <pytorchbot@pytorch.com>
2023-12-12 11:34:32 +0000

    2023-12-12 nightly release (44a9beaf0c8b26bb31892c0f1714c84742663ad7)

--- .github/scripts/unittest.sh
+++ .github/scripts/unittest.sh
@@ -12,4 +12,6 @@ pip install --progress-bar=off pytest pytest-mock pytest-cov expecttest
 echo '::endgroup::'
 
 python test/smoke_test.py
-pytest --junit-xml="${RUNNER_TEST_RESULTS_DIR}/test-results.xml" -v --durations=25
+
+# We explicitly ignore the video tests until we resolve https://github.com/pytorch/vision/issues/8162
+pytest --ignore-glob="*test_video*" --junit-xml="${RUNNER_TEST_RESULTS_DIR}/test-results.xml" -v --durations=25
