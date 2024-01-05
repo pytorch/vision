8ca3b51c0d0e23c5a9d7d07b945d42a299d522d2
Ahmad Sharif <ahmads@fb.com>
2024-01-05 08:49:57 -0800

    Addressed comments

--- test/test_internet.py
+++ test/test_internet.py
@@ -6,6 +6,7 @@ cleanly ignored in FB internal test infra.
 """
 
 import os
+import pathlib
 from urllib.error import URLError
 
 import pytest
@@ -13,36 +14,40 @@ import torchvision.datasets.utils as utils
 
 
 class TestDatasetUtils:
-    @pytest.mark.parametrize("temp_dir", ("tmpdir", "tmp_path"))
-    def test_download_url(self, temp_dir, request):
-        temp_dir = request.getfixturevalue(temp_dir)
+    @pytest.mark.parametrize("use_pathlib", (True, False))
+    def test_download_url(self, tmpdir, use_pathlib):
+        if use_pathlib:
+            tmpdir = pathlib.Path(tmpdir)
         url = "http://github.com/pytorch/vision/archive/master.zip"
         try:
-            utils.download_url(url, temp_dir)
-            assert len(os.listdir(temp_dir)) != 0
+            utils.download_url(url, tmpdir)
+            assert len(os.listdir(tmpdir)) != 0
         except URLError:
             pytest.skip(f"could not download test file '{url}'")
 
-    @pytest.mark.parametrize("temp_dir", ("tmpdir", "tmp_path"))
-    def test_download_url_retry_http(self, temp_dir, request):
-        temp_dir = request.getfixturevalue(temp_dir)
+    @pytest.mark.parametrize("use_pathlib", (True, False))
+    def test_download_url_retry_http(self, tmpdir, use_pathlib):
+        if use_pathlib:
+            tmpdir = pathlib.Path(tmpdir)
         url = "https://github.com/pytorch/vision/archive/master.zip"
         try:
-            utils.download_url(url, temp_dir)
-            assert len(os.listdir(temp_dir)) != 0
+            utils.download_url(url, tmpdir)
+            assert len(os.listdir(tmpdir)) != 0
         except URLError:
             pytest.skip(f"could not download test file '{url}'")
 
-    @pytest.mark.parametrize("temp_dir", ("tmpdir", "tmp_path"))
-    def test_download_url_dont_exist(self, temp_dir, request):
-        temp_dir = request.getfixturevalue(temp_dir)
+    @pytest.mark.parametrize("use_pathlib", (True, False))
+    def test_download_url_dont_exist(self, tmpdir, use_pathlib):
+        if use_pathlib:
+            tmpdir = pathlib.Path(tmpdir)
         url = "http://github.com/pytorch/vision/archive/this_doesnt_exist.zip"
         with pytest.raises(URLError):
-            utils.download_url(url, temp_dir)
+            utils.download_url(url, tmpdir)
 
-    @pytest.mark.parametrize("temp_dir", ("tmpdir", "tmp_path"))
-    def test_download_url_dispatch_download_from_google_drive(self, mocker, temp_dir, request):
-        temp_dir = request.getfixturevalue(temp_dir)
+    @pytest.mark.parametrize("use_pathlib", (True, False))
+    def test_download_url_dispatch_download_from_google_drive(self, mocker, tmpdir, use_pathlib):
+        if use_pathlib:
+            tmpdir = pathlib.Path(tmpdir)
         url = "https://drive.google.com/file/d/1GO-BHUYRuvzr1Gtp2_fqXRsr9TIeYbhV/view"
 
         id = "1GO-BHUYRuvzr1Gtp2_fqXRsr9TIeYbhV"
@@ -50,9 +55,9 @@ class TestDatasetUtils:
         md5 = "md5"
 
         mocked = mocker.patch("torchvision.datasets.utils.download_file_from_google_drive")
-        utils.download_url(url, temp_dir, filename, md5)
+        utils.download_url(url, tmpdir, filename, md5)
 
-        mocked.assert_called_once_with(id, os.path.expanduser(temp_dir), filename, md5)
+        mocked.assert_called_once_with(id, os.path.expanduser(tmpdir), filename, md5)
 
 
 if __name__ == "__main__":
