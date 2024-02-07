2cacb30218d493905b13c8bc76e5e2db1aebf112
mantasu <mantix7@gmail.com>
2024-02-07 11:13:26 +0000

    Allow to_image to handle image paths

--- torchvision/transforms/v2/functional/_type_conversion.py
+++ torchvision/transforms/v2/functional/_type_conversion.py
@@ -6,9 +6,11 @@ import torch
 from torchvision import tv_tensors
 from torchvision.transforms import functional as _F
 
+from ....io import read_image
+
 
 @torch.jit.unused
-def to_image(inpt: Union[torch.Tensor, PIL.Image.Image, np.ndarray]) -> tv_tensors.Image:
+def to_image(inpt: Union[torch.Tensor, PIL.Image.Image, np.ndarray, str]) -> tv_tensors.Image:
     """See :class:`~torchvision.transforms.v2.ToImage` for details."""
     if isinstance(inpt, np.ndarray):
         output = torch.from_numpy(np.atleast_3d(inpt)).permute((2, 0, 1)).contiguous()
@@ -16,9 +18,12 @@ def to_image(inpt: Union[torch.Tensor, PIL.Image.Image, np.ndarray]) -> tv_tenso
         output = pil_to_tensor(inpt)
     elif isinstance(inpt, torch.Tensor):
         output = inpt
+    elif isinstance(inpt, str):
+        output = read_image(inpt)
     else:
         raise TypeError(
-            f"Input can either be a pure Tensor, a numpy array, or a PIL image, but got {type(inpt)} instead."
+            f"Input can either be a pure Tensor, a numpy array, a PIL image, "
+            f"or a string representing image path, but got {type(inpt)} instead."
         )
     return tv_tensors.Image(output)
 
