41a35a4cafbecf24503323dcb31817f1366d2c70
Nicolas Hug <contact@nicolas-hug.com>
2024-03-15 13:45:37 +0000

    mypy?

--- torchvision/transforms/v2/_misc.py
+++ torchvision/transforms/v2/_misc.py
@@ -1,5 +1,5 @@
 import warnings
-from typing import Any, Callable, Dict, List, Optional, Sequence, Type, Union
+from typing import Any, Callable, Dict, List, Optional, Sequence, Type, Union, Tuple
 
 import PIL.Image
 
@@ -350,7 +350,7 @@ class SanitizeBoundingBoxes(Transform):
     def __init__(
         self,
         min_size: float = 1.0,
-        labels_getter: Union[Callable[[Any], Optional[torch.Tensor]], str, None] = "default",
+        labels_getter: Union[Callable[[Any], Optional[Union[torch.Tensor, Tuple[torch.tensor]]]], str, None] = "default",
     ) -> None:
         super().__init__()
 
