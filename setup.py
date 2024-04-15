e46a0bbe329737b27015573c8066a0f33d14b876
Nicolas Hug <contact@nicolas-hug.com>
2024-04-15 11:15:34 +0100

    LAENFJLAENF

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ requirements = [
 ]
 
 # Excluding 8.3.* because of https://github.com/pytorch/vision/issues/4934
-pillow_ver = " >= 5.3.0, !=8.3.*"
+pillow_ver = " >= 5.3.0, !=8.3.*, <10.3.0"
 pillow_req = "pillow-simd" if get_dist("pillow-simd") is not None else "pillow"
 requirements.append(pillow_req + pillow_ver)
 
