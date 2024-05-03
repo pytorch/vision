7be488e663cfd5af65375ed5e73e14f9d130ed95
Nicolas Hug <contact@nicolas-hug.com>
2024-05-03 18:36:05 +0100

    Return batch if GIF has multiple images

--- torchvision/csrc/io/image/cpu/decode_gif.cpp
+++ torchvision/csrc/io/image/cpu/decode_gif.cpp
@@ -14,10 +14,10 @@ typedef struct reader_helper_t {
 } reader_helper_t;
 
 // Reader function for libgif. This reads `len` bytes and writes them into
-// `buf`. The data is read from the UserData field of the `gif` paramater which
+// `buf`. The data is read from the UserData field of the `gifFile` paramater which
 // was set when DGifOpen() was called.
-int read_from_tensor(GifFileType* gif, GifByteType* buf, int len) {
-    reader_helper_t* reader_helper = (reader_helper_t *)gif->UserData;
+int read_from_tensor(GifFileType* gifFile, GifByteType* buf, int len) {
+    reader_helper_t* reader_helper = (reader_helper_t *)gifFile->UserData;
     auto num_encoded_bytes = (int)reader_helper->encoded_bytes->numel();
     auto data_ptr = reader_helper->encoded_bytes->data_ptr<GifByteType>();
 
@@ -32,16 +32,20 @@ int read_from_tensor(GifFileType* gif, GifByteType* buf, int len) {
 }
 
 torch::Tensor decode_gif(const torch::Tensor& encoded_bytes) {
+    // LibGif docs: https://giflib.sourceforge.net/intro.html
+    // Refer over there for more details on the libgif API, API ref, and a
+    // detailed description of the GIF format.
 
     int error = D_GIF_SUCCEEDED;
 
-    // The other entrypoints of libgif are DGifOpenFileName and
-    // DGifOpenFileHandle but we don't want to use those since we are reading
-    // encoded bytes from a tensor (for consistency with existing jpeg and png
-    // decoders). Using DGifOpen is the only way to read from a custom source.
-    // For that we need to provide a reader function that reads from the tensor,
-    // and we keep track of the number of bytes read via the reader_helper
-    // struct.
+    // We're using DGidOpen. The other entrypoints of libgif are
+    // DGifOpenFileName and DGifOpenFileHandle but we don't want to use those,
+    // since we need to read the encoded bytes from a tensor (for consistency
+    // with existing jpeg and png decoders). Using DGifOpen is the only way to
+    // read from a custom source.
+    // For that we need to provide a reader function `read_from_tensor` that
+    // reads from the tensor, and we keep track of the number of bytes read so
+    // far via the reader_helper struct.
     // Note: it does seem like this is involving an extra copy (which happens in
     // read_from_tensor: from the tensor to `buf`), compared to using
     // DGifOpenFileName() or DGifOpenFileHandle().
@@ -62,29 +66,47 @@ torch::Tensor decode_gif(const torch::Tensor& encoded_bytes) {
     }
 
     ColorMapObject* commonMap = gifFile->SColorMap;
-    std::cout << ": " << gifFile->SWidth << "x" << gifFile->SHeight << std::endl;
-    std::cout << "Number of images: " << gifFile->ImageCount << std::endl;
 
-    const SavedImage& saved = gifFile->SavedImages[0];  // TODO: handle multple images?
-    const GifImageDesc& desc = saved.ImageDesc;
-    const ColorMapObject* colorMap = desc.ColorMap ? desc.ColorMap : commonMap;
-    // TODO What are Left and Top used for?
-    std::cout << desc.Width << "x" << desc.Height << "+" << desc.Left << "," << desc.Top
-        << ", has local colorMap: " << (desc.ColorMap ? "Yes" : "No") << std::endl;
+    // Note:
+    // - We're assuming and enforcing that all images have the same height and width
+    // - We're ignoring the canvas width and height and assume shape is that of the first image
+    // - We're ignoring the top and left offsets and enforce them to be 0.
+    // This is for simplicity. All of those may be revisited in the future if need be.
 
-    auto out = torch::empty({3, int64_t(desc.Height), int64_t(desc.Width)}, torch::kU8);
+    // TODO: Transparency???
 
-    for (int h = 0; h < desc.Height; h++) {
-        for (int w = 0; w < desc.Width; w++) {
-            int c = saved.RasterBits[h * desc.Width + w];
-            if (colorMap) { // TODO Can there be no colormap????
+    auto out_h = gifFile->SavedImages[0].ImageDesc.Height;
+    auto out_w = gifFile->SavedImages[0].ImageDesc.Width;
+    auto num_images = gifFile->ImageCount;
+
+    auto out = torch::empty({int64_t(num_images), 3, int64_t(out_h), int64_t(out_w)}, torch::kU8);
+
+    for(int i = 0; i < num_images; i++) {
+        const SavedImage& img = gifFile->SavedImages[i];
+        const GifImageDesc& desc = img.ImageDesc;
+        const ColorMapObject* colorMap = desc.ColorMap ? desc.ColorMap : commonMap;
+
+        TORCH_CHECK(
+            desc.Width == out_w && desc.Height == out_h,
+            "All images in the gif should have the same dimensions."
+        );
+        TORCH_CHECK(colorMap != nullptr, "ColorMap is missing.");
+        TORCH_CHECK(
+            desc.Top == 0 && desc.Left == 0,
+            "Images are expected to have top and left offsets of 0."
+        );
+
+        for (int h = 0; h < desc.Height; h++) {
+            for (int w = 0; w < desc.Width; w++) {
+                auto c = img.RasterBits[h * desc.Width + w];
                 GifColorType rgb = colorMap->Colors[c];
-                out.index_put_({0, h, w}, rgb.Red);
-                out.index_put_({1, h, w}, rgb.Green);
-                out.index_put_({2, h, w}, rgb.Blue);
+                out.index_put_({i, 0, h, w}, rgb.Red);
+                out.index_put_({i, 1, h, w}, rgb.Green);
+                out.index_put_({i, 2, h, w}, rgb.Blue);
             }
         }
     }
+    out = out.squeeze(0);  // remove batch dim if there's only one image
 
     DGifCloseFile(gifFile, &error);
     TORCH_CHECK(error == D_GIF_SUCCEEDED, "DGifCloseFile() failed - ", error);
--- torchvision/csrc/io/image/cpu/decode_image.cpp
+++ torchvision/csrc/io/image/cpu/decode_image.cpp
@@ -24,14 +24,15 @@ torch::Tensor decode_image(
 
   const uint8_t jpeg_signature[3] = {255, 216, 255}; // == "\xFF\xD8\xFF"
   const uint8_t png_signature[4] = {137, 80, 78, 71}; // == "\211PNG"
-  const uint8_t gif_signature[3] = {0x47, 0x49, 0x46}; // == "GIF"
+  const uint8_t gif_signature_1[6] = {0x47, 0x49, 0x46, 0x38, 0x39, 0x61}; // == "GIF89a"
+  const uint8_t gif_signature_2[6] = {0x47, 0x49, 0x46, 0x38, 0x37, 0x61}; // == "GIF87a"
 
   if (memcmp(jpeg_signature, datap, 3) == 0) {
     return decode_jpeg(data, mode, apply_exif_orientation);
   } else if (memcmp(png_signature, datap, 4) == 0) {
     return decode_png(
         data, mode, /*allow_16_bits=*/false, apply_exif_orientation);
-  } else if (memcmp(gif_signature, datap, 3) == 0) {
+  } else if (memcmp(gif_signature_1, datap, 6) == 0 || memcmp(gif_signature_2, datap, 6) == 0) {
     return decode_gif(data);
   } else {
     TORCH_CHECK(
