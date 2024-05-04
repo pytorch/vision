07294a5c8d3ff67ec5b8bab44ab4208709f200cf
Nicolas Hug <contact@nicolas-hug.com>
2024-05-04 14:46:14 +0100

    Comments

--- torchvision/csrc/io/image/cpu/decode_gif.cpp
+++ torchvision/csrc/io/image/cpu/decode_gif.cpp
@@ -1,8 +1,5 @@
 #include "decode_gif.h"
 #include "giflib/gif_lib.h"
-#include <stdio.h> // TODO: Remove
-#include <iostream> // TODO: Remove?
-
 
 namespace vision {
 namespace image {
@@ -12,10 +9,11 @@ typedef struct reader_helper_t {
   int num_bytes_read; // number of bytes read so far in the tensor
 } reader_helper_t;
 
-// Reader function for libgif. This reads `len` bytes and writes them into
-// `buf`. The data is read from the UserData field of the `gifFile` paramater
-// which was set when DGifOpen() was called.
+// That function is used by GIFLIB routines to read the encoded bytes.
+// This reads `len` bytes and writes them into `buf`. The data is read from the
+// input tensor passed to decode_gif().
 int read_from_tensor(GifFileType* gifFile, GifByteType* buf, int len) {
+  // the UserData field was set in DGifOpen()
   reader_helper_t* reader_helper = (reader_helper_t*)gifFile->UserData;
   auto num_encoded_bytes = (int)reader_helper->encoded_bytes->numel();
   auto data_ptr = reader_helper->encoded_bytes->data_ptr<GifByteType>();
@@ -40,15 +38,21 @@ torch::Tensor decode_gif(const torch::Tensor& encoded_bytes) {
 
   // We're using DGidOpen. The other entrypoints of libgif are
   // DGifOpenFileName and DGifOpenFileHandle but we don't want to use those,
-  // since we need to read the encoded bytes from a tensor (for consistency
-  // with existing jpeg and png decoders). Using DGifOpen is the only way to
-  // read from a custom source.
+  // since we need to read the encoded bytes from a tensor of encoded bytes, not
+  // from a file (for consistency with existing jpeg and png decoders). Using
+  // DGifOpen is the only way to read from a custom source.
   // For that we need to provide a reader function `read_from_tensor` that
-  // reads from the tensor, and we keep track of the number of bytes read so
-  // far via the reader_helper struct.
-  // Note: it does seem like this is involving an extra copy (which happens in
-  // read_from_tensor: from the tensor to `buf`), compared to using
-  // DGifOpenFileName() or DGifOpenFileHandle().
+  // reads from the tensor, and we have to keep track of the number of bytes
+  // read so far: this is why we need the reader_helper struct.
+
+  // TODO: We are potentially doing an unnecessary copy of the encoded bytes:
+  // - 1 copy in from file to tensor (in read_file())
+  // - 1 copy from tensor to GIFLIB buffers (in read_from_tensor())
+  // Since we're vendoring GIFLIB we can potentially modify the calls to
+  // InternalRead() and just set the `buf` pointer to the tensor data directly.
+  // That might even save allocation of those buffers.
+  // If we do that, we'd have to make sure the buffers are never written to by
+  // GIFLIB, otherwise we'd be overridding the tensor data.
   reader_helper_t reader_helper;
   reader_helper.encoded_bytes = &encoded_bytes;
   reader_helper.num_bytes_read = 0;
