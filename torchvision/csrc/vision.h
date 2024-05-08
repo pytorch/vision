c5b14a382dc540ad5999a280fea813a3482f408f
Nicolas Hug <contact@nicolas-hug.com>
2024-05-08 11:54:21 +0100

    Remove #pragma comment linker

--- torchvision/csrc/vision.h
+++ torchvision/csrc/vision.h
@@ -8,9 +8,9 @@ VISION_API int64_t cuda_version();
 
 namespace detail {
 extern "C" VISION_INLINE_VARIABLE auto _register_ops = &cuda_version;
-#ifdef HINT_MSVC_LINKER_INCLUDE_SYMBOL
-#pragma comment(linker, "/include:_register_ops")
-#endif
+// #ifdef HINT_MSVC_LINKER_INCLUDE_SYMBOL
+// #pragma comment(linker, "/include:_register_ops")
+// #endif
 
 } // namespace detail
 } // namespace vision
