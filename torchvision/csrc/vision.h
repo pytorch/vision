65325bd353f840a56885c05e5a0174ed3754c542
Nicolas Hug <contact@nicolas-hug.com>
2024-05-09 11:56:30 +0100

    Remove VISION_INLINE_VARIABLE

--- torchvision/csrc/vision.h
+++ torchvision/csrc/vision.h
@@ -7,6 +7,6 @@ namespace vision {
 VISION_API int64_t cuda_version();
 
 namespace detail {
-extern "C" VISION_INLINE_VARIABLE auto _register_ops = &cuda_version;
+extern "C" inline auto _register_ops = &cuda_version;
 } // namespace detail
 } // namespace vision
