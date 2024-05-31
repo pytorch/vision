c0d048a4b7f3119e7ff962a9c1a259bcacbfb9e0
Nicolas Hug <contact@nicolas-hug.com>
2024-05-31 10:25:10 +0100

    test against numpy 2.0

--- .github/scripts/unittest.sh
+++ .github/scripts/unittest.sh
@@ -7,6 +7,10 @@ set -euo pipefail
 # Activate conda environment
 eval "$($(which conda) shell.bash hook)" && conda deactivate && conda activate ci
 
+echo '::group::Install numpy nightly'
+pip install "numpy==2.0.0rc2"
+python -c "import numpy; print(numpy.__version__)"
+
 echo '::group::Install testing utilities'
 # TODO: remove the <8 constraint on pytest when https://github.com/pytorch/vision/issues/8238 is closed
 pip install --progress-bar=off "pytest<8" pytest-mock pytest-cov expecttest!=0.2.0 requests
