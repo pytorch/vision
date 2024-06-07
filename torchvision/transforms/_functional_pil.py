ddd9b4d0396e1be9dfea56e36eff3c4cc62fac38
Nicolas Hug <nicolashug@meta.com>
2024-06-07 10:05:14 -0700

    [fbsync] Make adjust_hue() work with numpy 2.0 (#8463)
    Reviewed By: vmoens
    
    Differential Revision: D58283860
    
    fbshipit-source-id: 37261b4d0ca78666ef01d9b04937bd02870f81e2

--- torchvision/transforms/_functional_pil.py
+++ torchvision/transforms/_functional_pil.py
@@ -109,9 +109,9 @@ def adjust_hue(img: Image.Image, hue_factor: float) -> Image.Image:
     h, s, v = img.convert("HSV").split()
 
     np_h = np.array(h, dtype=np.uint8)
-    # uint8 addition take cares of rotation across boundaries
-    with np.errstate(over="ignore"):
-        np_h += np.uint8(hue_factor * 255)
+    # This will over/underflow, as desired
+    np_h += np.array(hue_factor * 255).astype(np.uint8)
+
     h = Image.fromarray(np_h, "L")
 
     img = Image.merge("HSV", (h, s, v)).convert(input_mode)
