7c06376735696dae36406abefc640c5f74709b1f
Huy Do <huydhn@gmail.com>
2024-06-07 20:13:27 -0700

    Debug

---
+++ packaging/env_var_script.sh
@@ -0,0 +1,6 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+MACOSX_DEPLOYMENT_TARGET=11.0
