10cbad7e45e46112224d18a1d4b1525959135d3c
Nicolas Hug <contact@nicolas-hug.com>
2024-07-15 11:48:47 +0100

    Add xfail

--- test/test_transforms_v2.py
+++ test/test_transforms_v2.py
@@ -2085,6 +2085,8 @@ class TestToDtype:
     def test_image_correctness(self, input_dtype, output_dtype, device, scale):
         if input_dtype.is_floating_point and output_dtype == torch.int64:
             pytest.xfail("float to int64 conversion is not supported")
+        if input_dtype == torch.uint8 and output_dtype == torch.uint16 and device == "cuda":
+            pytest.xfail("uint8 to uint16 conversion is not supported on cuda")
 
         input = make_image(dtype=input_dtype, device=device)
 
