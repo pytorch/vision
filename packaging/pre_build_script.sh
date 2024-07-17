dc92133a39c31527a9c81c41e52774ab747ff24f
Nicolas Hug <contact@nicolas-hug.com>
2024-07-17 12:11:59 +0100

    Remove useless stuff from pre_build_script

--- packaging/pre_build_script.sh
+++ packaging/pre_build_script.sh
@@ -29,21 +29,8 @@ else
     conda install -yq ffmpeg=4.2 libjpeg-turbo -c pytorch-nightly
   fi
 
-  # Install native CentOS libJPEG, freetype and GnuTLS
   yum install -y libjpeg-turbo-devel freetype gnutls
-
-  # Download all the dependencies required to compile image and video_reader
-  # extensions
-  mkdir -p ext_libraries
-  pushd ext_libraries
-  popd
-  export PATH="$(pwd)/ext_libraries/bin:$PATH"
   pip install auditwheel
-
-  # Point to custom libraries
-  export LD_LIBRARY_PATH=$(pwd)/ext_libraries/lib:$LD_LIBRARY_PATH
-  export TORCHVISION_INCLUDE=$(pwd)/ext_libraries/include
-  export TORCHVISION_LIBRARY=$(pwd)/ext_libraries/lib
 fi
 
 pip install numpy pyyaml future ninja
