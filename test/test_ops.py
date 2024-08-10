e948fcafe109989ee388a9db77b4281dc31853e4
Nicolas Hug <contact@nicolas-hug.com>
2024-08-10 16:34:57 +0100

    Skip flaky test_autocast_cpu test

--- test/test_ops.py
+++ test/test_ops.py
@@ -512,6 +512,9 @@ class TestRoIAlign(RoIOpTester):
     @pytest.mark.parametrize("x_dtype", (torch.float, torch.bfloat16))
     @pytest.mark.parametrize("rois_dtype", (torch.float, torch.bfloat16))
     def test_autocast_cpu(self, aligned, deterministic, x_dtype, rois_dtype):
+        if aligned and x_dtype == torch.bfloat16:
+            pytest.skip("1/5000 flaky failure")
+
         with torch.cpu.amp.autocast():
             self.test_forward(
                 torch.device("cpu"),
