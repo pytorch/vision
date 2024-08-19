0a0f34b4d2c5c7fccf98a8db53ebb917630a7ec3
Nicolas Hug <nh.nicolas.hug@gmail.com>
2024-08-19 12:45:12 +0100

    Cleanup video decoder build stuff (#8602)

--- torchvision/io/_load_gpu_decoder.py
+++ torchvision/io/_load_gpu_decoder.py
@@ -2,7 +2,7 @@ from ..extension import _load_library
 
 
 try:
-    _load_library("Decoder")
+    _load_library("gpu_decoder")
     _HAS_GPU_VIDEO_DECODER = True
 except (ImportError, OSError):
     _HAS_GPU_VIDEO_DECODER = False
