81f341db6e3a262eaf258413c3853162a7efbd57
pytorchbot <pytorchbot@pytorch.com>
2024-08-27 11:34:45 +0000

    2024-08-27 nightly release (4c5ae7853e825446dd208aa2f2ff4d372777e79e)

--- torchvision/datasets/moving_mnist.py
+++ torchvision/datasets/moving_mnist.py
@@ -66,7 +66,7 @@ class MovingMNIST(VisionDataset):
     def __getitem__(self, idx: int) -> torch.Tensor:
         """
         Args:
-            index (int): Index
+            idx (int): Index
         Returns:
             torch.Tensor: Video frames (torch Tensor[T, C, H, W]). The `T` is the number of frames.
         """
