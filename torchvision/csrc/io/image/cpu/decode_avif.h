404645a3cbe2b80eadc59d221c5cd9a08f7f1cc1
Nicolas Hug <contact@nicolas-hug.com>
2024-08-28 12:22:04 +0100

    lint

--- torchvision/csrc/io/image/cpu/decode_avif.h
+++ torchvision/csrc/io/image/cpu/decode_avif.h
@@ -6,7 +6,9 @@
 namespace vision {
 namespace image {
 
-C10_EXPORT torch::Tensor decode_avif(const torch::Tensor& encoded_data, ImageReadMode mode = IMAGE_READ_MODE_UNCHANGED);
+C10_EXPORT torch::Tensor decode_avif(
+    const torch::Tensor& encoded_data,
+    ImageReadMode mode = IMAGE_READ_MODE_UNCHANGED);
 
 } // namespace image
 } // namespace vision
