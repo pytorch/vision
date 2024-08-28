f6d23e3a2f00681bf2b83de9440abf43c0a344ef
Nicolas Hug <contact@nicolas-hug.com>
2024-08-28 15:19:27 +0100

    better exception handling

--- torchvision/csrc/io/image/cpu/decode_heic.cpp
+++ torchvision/csrc/io/image/cpu/decode_heic.cpp
@@ -27,33 +27,42 @@ torch::Tensor decode_heic(const torch::Tensor& encoded_data) {
       encoded_data.dim(),
       " dims.");
 
-  heif::Context ctx;
-  ctx.read_from_memory_without_copy(
-      encoded_data.data_ptr<uint8_t>(), encoded_data.numel());
+  int height = 0;
+  int width = 0;
+  int stride = 0;
+  uint8_t* decoded_data = nullptr;
+  heif::Image img;
 
-  heif::ImageHandle handle = ctx.get_primary_image_handle();
-  heif::Image img =
-      handle.decode_image(heif_colorspace_RGB, heif_chroma_interleaved_RGB);
+  try {
+    heif::Context ctx;
+    ctx.read_from_memory_without_copy(
+        encoded_data.data_ptr<uint8_t>(), encoded_data.numel());
 
-  int stride;
-  uint8_t* decoded_data = img.get_plane(heif_channel_interleaved, &stride);
+    heif::ImageHandle handle = ctx.get_primary_image_handle();
+    height = handle.get_height();
+    width = handle.get_width();
+
+    img = handle.decode_image(heif_colorspace_RGB, heif_chroma_interleaved_RGB);
+
+    decoded_data = img.get_plane(heif_channel_interleaved, &stride);
+  } catch (const heif::Error& err) {
+    TORCH_CHECK(false, "decode_heif failed: ", err.get_message());
+  }
   TORCH_CHECK(decoded_data != nullptr, "Something went wrong during decoding.");
 
-  auto out =
-      torch::empty({handle.get_height(), handle.get_width(), 3}, torch::kUInt8);
+  auto out = torch::empty({height, width, 3}, torch::kUInt8);
   auto out_ptr = out.data_ptr<uint8_t>();
 
-  // decoded_data is *almost* the raw decoded data: for some image, there may be
-  // some padding at the end of each row, i.e. when stride != row_size. So we
-  // can't copy decoded_data into the tensor's memory directly, we have to copy
-  // row by row.
-  // Oh, and if you think you can take a shortcut when stride == row_size and
-  // just do:
-  // out = torch::from_blob(decoded_data, ...)
+  // decoded_data is *almost* the raw decoded data, but not quite: for some
+  // image, there may be some padding at the end of each row, i.e. when stride
+  // != row_size. So we can't copy decoded_data into the tensor's memory
+  // directly, we have to copy row by row. Oh, and if you think you can take a
+  // shortcut when stride == row_size and just do:
+  // out =  torch::from_blob(decoded_data, ...)
   // you can't, because decoded_data is owned by the heif::Image object and gets
   // freed when it gets out of scope!
-  auto row_size = handle.get_width() * 3;
-  for (auto i = 0; i < handle.get_height(); i++) {
+  auto row_size = width * 3;
+  for (auto i = 0; i < height; i++) {
     memcpy(out_ptr, decoded_data, row_size);
     out_ptr += row_size;
     decoded_data += stride;
