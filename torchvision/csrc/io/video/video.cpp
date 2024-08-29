5471aa3a31678a317d59db62b52fc3b2a11d014b
Chenheli Hua <huachenheli@meta.com>
2024-08-29 14:26:41 -0700

    Further reduce torchvision decoder log spam.
    Reviewed By: wqfish
    
    Differential Revision: D61992169
    
    fbshipit-source-id: 853322a3add4c8f99c348b98c23f94e4feee8853

--- torchvision/csrc/io/video/video.cpp
+++ torchvision/csrc/io/video/video.cpp
@@ -244,7 +244,6 @@ void Video::_init(std::string stream, int64_t numThreads) {
   streamsMetadata.insert("cc", ccMetadata);
 
   succeeded = setCurrentStream(stream);
-  LOG(INFO) << "\nDecoder inited with: " << succeeded << "\n";
   if (std::get<1>(current_stream) != -1) {
     LOG(INFO)
         << "Stream index set to " << std::get<1>(current_stream)
