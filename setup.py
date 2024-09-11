1249d0b09db224e052c0c30ee7ff6e87d0209ee7
atalman <atalman@fb.com>
2024-09-11 08:30:01 -0700

    lint

--- setup.py
+++ setup.py
@@ -366,7 +366,7 @@ def make_image_extension():
         else:
             warnings.warn("Building torchvision without AVIF support")
 
-    if USE_NVJPEG and ( torch.cuda.is_available() or FORCE_CUDA ):
+    if USE_NVJPEG and (torch.cuda.is_available() or FORCE_CUDA):
         nvjpeg_found = CUDA_HOME is not None and (Path(CUDA_HOME) / "include/nvjpeg.h").exists()
 
         if nvjpeg_found:
