95eb1cd5ec1a0f3ccdd271a4a4c1e34ecc3feb04
Thomas Martin <thomas@goldfishsound.com>
2024-10-25 19:17:42 +0200

    Initial commit for deform_conv2d for MPS

---
+++ Deform_conv2d_kernals.metal
@@ -0,0 +1,11 @@
+//
+//  Deform_conv2d_kernals.metal
+//  torchvision
+//
+//  Created by Thomas Martin on 14/10/2024.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+
---
+++ build_xcode/VisionTests/VisionTests.mm
@@ -0,0 +1,36 @@
+//
+//  VisionTests.m
+//  VisionTests
+//
+//  Created by Thomas Martin on 12/10/2024.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VisionTests : XCTestCase
+
+@end
+
+@implementation VisionTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
