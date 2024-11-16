970183d062c58a1079db8f4ed3b8c1504b85ef68
Thomas Martin <thomas@goldfishsound.com>
2024-11-16 13:52:05 +0100

    Removing framework dir and included files.

--- framework/include/torchvision/io/image/image.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "cpu/decode_image.h"
-#include "cpu/decode_jpeg.h"
-#include "cpu/decode_png.h"
-#include "cpu/encode_jpeg.h"
-#include "cpu/encode_png.h"
-#include "cpu/read_write_file.h"
-#include "cuda/decode_jpeg_cuda.h"
--- framework/include/torchvision/ops/mps/mps_helpers.h
+++
@@ -1,6 +0,0 @@
-constexpr int threadsPerBlock = 512;
-
-template <typename T>
-constexpr inline T ceil_div(T n, T m) {
-  return (n + m - 1) / m;
-}
