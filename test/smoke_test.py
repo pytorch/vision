d3df9e2a82faf13cc460218f4ab7c01d155c3f02
pytorchbot <pytorchbot@pytorch.com>
2024-11-28 15:26:32 +0000

    2024-11-28 nightly release (22e86bd3ef3d297ef2e1ce84bbc0602a6f8a9b28)

--- test/smoke_test.py
+++ test/smoke_test.py
@@ -102,8 +102,9 @@ def main() -> None:
         smoke_test_torchvision_resnet50_classify("cuda")
 
         # TODO: remove once pytorch/pytorch#110436 is resolved
-        if sys.version_info < (3, 12, 0):
-            smoke_test_compile()
+        # Temporary Disabling compile test. Untill triton with Manylinux2014 is available
+        # if sys.version_info < (3, 12, 0):
+        #    smoke_test_compile()
 
     if torch.backends.mps.is_available():
         smoke_test_torchvision_resnet50_classify("mps")
