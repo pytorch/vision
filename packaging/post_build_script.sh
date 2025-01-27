3f9a7b244a3a879bd935599c290b3472ea2ba7ec
atalman <atalman@fb.com>
2025-01-27 10:33:03 -0800

    Test auditwheel

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+pip install auditwheel
+
+if [[ ${ARCH} == "aarch64" ]]; then
+    auditwheel repair --plat manylinux_2_28_aarch64 pytorch/vision/dist/*.whl
+fi
+
 LD_LIBRARY_PATH="/usr/local/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH" python packaging/wheel/relocate.py
 
 pip install torchvision-extra-decoders
