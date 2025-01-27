f47b6c54cdc07a2544453cc2b2079dfdcc919237
atalman <atalman@fb.com>
2025-01-27 11:12:29 -0800

    test

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -2,7 +2,7 @@
 pip install auditwheel
 
 if [[ ${ARCH} == "aarch64" ]]; then
-    auditwheel repair --plat manylinux_2_28_aarch64 pytorch/vision/dist/*.whl
+    auditwheel repair --plat manylinux_2_28_aarch64 dist/*.whl
 fi
 
 LD_LIBRARY_PATH="/usr/local/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH" python packaging/wheel/relocate.py
