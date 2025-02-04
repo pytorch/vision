c0445210a2d1dd67e211f30765e793c6e1c0eb52
Andrey Talman <atalman@fb.com>
2025-02-04 11:12:07 -0500

    Update pre_build_script.sh

--- packaging/pre_build_script.sh
+++ packaging/pre_build_script.sh
@@ -37,7 +37,7 @@ else
     conda install -yq ffmpeg=4.2 libjpeg-turbo -c pytorch-nightly
   fi
 
-  conda install -y libwebp ${CONDA_CHANNEL}
+  conda install -v libwebp ${CONDA_CHANNEL}
   conda install libjpeg-turbo -c pytorch
   yum install -y freetype gnutls
   pip install auditwheel
