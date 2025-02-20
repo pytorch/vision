f3b94cf20123d041d4ab3bd1d8ec69937c61c69d
Nicolas Hug <nicolashug@fb.com>
2025-02-20 09:21:09 -0800

    Add comment

--- torchvision/ops/boxes.py
+++ torchvision/ops/boxes.py
@@ -78,6 +78,7 @@ def batched_nms(
         _log_api_usage_once(batched_nms)
     # Benchmarks that drove the following thresholds are at
     # https://github.com/pytorch/vision/issues/1311#issuecomment-781329339
+    # and https://github.com/pytorch/vision/pull/8925
     if boxes.numel() > (4000 if boxes.device.type == "cpu" else 100_000) and not torchvision._is_tracing():
         return _batched_nms_vanilla(boxes, scores, idxs, iou_threshold)
     else:
