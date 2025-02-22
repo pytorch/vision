f968f58e303e4eadd20406b3264273e883c9f3be
GdoongMathew <gdoongmathew@gmail.com>
2025-02-23 00:12:19 +0800

    Docs: update `pycocotools` url in `CocoDetection` and `CocoCaptions`.

--- torchvision/datasets/coco.py
+++ torchvision/datasets/coco.py
@@ -10,8 +10,8 @@ from .vision import VisionDataset
 class CocoDetection(VisionDataset):
     """`MS Coco Detection <https://cocodataset.org/#detection-2016>`_ Dataset.
 
-    It requires the `pycocotools` to be installed <https://github.com/ppwwyyxx/cocoapi>`_.
-    You can install it via ``pip install pycocotools``, ``conda install conda-forge::pycocotools``.
+    It requires `pycocotools <https://github.com/ppwwyyxx/cocoapi>`_ to be installed,
+    which could be installed via ``pip install pycocotools`` or ``conda install conda-forge::pycocotools``.
 
     Args:
         root (str or ``pathlib.Path``): Root directory where images are downloaded to.
@@ -66,8 +66,8 @@ class CocoDetection(VisionDataset):
 class CocoCaptions(CocoDetection):
     """`MS Coco Captions <https://cocodataset.org/#captions-2015>`_ Dataset.
 
-    It requires the `pycocotools` to be installed <https://github.com/ppwwyyxx/cocoapi>`_.
-    You can install it via ``pip install pycocotools``, ``conda install conda-forge::pycocotools``.
+    It requires `pycocotools <https://github.com/ppwwyyxx/cocoapi>`_ to be installed,
+    which could be installed via ``pip install pycocotools`` or ``conda install conda-forge::pycocotools``.
 
     Args:
         root (str or ``pathlib.Path``): Root directory where images are downloaded to.
