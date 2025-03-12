521f1a0b0e302043389f40f76b83d581c6970821
Nicolas Hug <contact@nicolas-hug.com>
2025-03-12 11:15:09 +0000

    Only set channel on linux

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 LD_LIBRARY_PATH="/usr/local/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH" python packaging/wheel/relocate.py
 
-pip install torchvision-extra-decoders --pre --index-url https://download.pytorch.org/whl/nightly/cpu
+if [[ "$(uname)" == "Linux" ]]; then
+    extra_decoders_channel="--pre --index-url https://download.pytorch.org/whl/nightly/cpu"
+else
+    extra_decoders_channel=""
+fi
+
+pip install torchvision-extra-decoders $extra_decoders_channel
