733064b7407fd06123ee7f848cf6450c3b2e8339
Nicolas Hug <nh.nicolas.hug@gmail.com>
2025-03-17 13:18:29 +0000

    Install torchvision-extra-decoders from nightly channel (#8963)

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 LD_LIBRARY_PATH="/usr/local/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH" python packaging/wheel/relocate.py
 
-pip install torchvision-extra-decoders
+if [[ "$(uname)" == "Linux" && "$(uname -m)" != "aarch64" ]]; then
+    extra_decoders_channel="--pre --index-url https://download.pytorch.org/whl/nightly/cpu"
+else
+    extra_decoders_channel=""
+fi
+
+pip install torchvision-extra-decoders $extra_decoders_channel
