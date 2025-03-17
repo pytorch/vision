3a2e90b68d197d9541f4821f482825d42975df46
Nicolas Hug <contact@nicolas-hug.com>
2025-03-17 12:25:05 +0000

    Try again

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 LD_LIBRARY_PATH="/usr/local/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH" python packaging/wheel/relocate.py
 
-if [[ "$(uname)" == "Linux" && "$ARCH" != "aarch64" ]]; then
+if [[ "$(uname)" == "Linux" && "$(uname -m)" != "aarch64" ]]; then
     extra_decoders_channel="--pre --index-url https://download.pytorch.org/whl/nightly/cpu"
 else
     extra_decoders_channel=""
