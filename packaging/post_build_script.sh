40089622446ff2b4fd99e144a5a2e70bd538a96d
Nicolas Hug <contact@nicolas-hug.com>
2025-03-18 10:22:03 +0000

    Protect CUDA_HOME

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-LD_LIBRARY_PATH="/usr/local/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH" python packaging/wheel/relocate.py
+if [ -n "${CUDA_HOME:-}" ]; then
+    LD_LIBRARY_PATH="/usr/local/lib:${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}"
+fi
+
+python packaging/wheel/relocate.py
 
 if [[ "$(uname)" == "Linux" && "$(uname -m)" != "aarch64" ]]; then
     extra_decoders_channel="--pre --index-url https://download.pytorch.org/whl/nightly/cpu"
