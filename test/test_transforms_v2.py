9f5b3f2c3007a87d3eb86a87d873ed78c0eaeb58
GdoongMathew <gdoongmathew@gmail.com>
2025-03-26 20:48:12 +0800

    fix: Add type checking in `posterize_image`. (#8993)
    Co-authored-by: Nicolas Hug <contact@nicolas-hug.com>
    Co-authored-by: Nicolas Hug <nh.nicolas.hug@gmail.com>

--- test/test_transforms_v2.py
+++ test/test_transforms_v2.py
@@ -4594,6 +4594,14 @@ class TestPosterize:
 
         assert_equal(actual, expected)
 
+    @pytest.mark.parametrize("bits", [-1, 9, 2.1])
+    def test_error_functional(self, bits):
+        with pytest.raises(
+            TypeError,
+            match=re.escape(f"bits must be a positive integer in the range [0, 8], got {bits} instead."),
+        ):
+            F.posterize(make_image(dtype=torch.uint8), bits=bits)
+
 
 class TestSolarize:
     def _make_threshold(self, input, *, factor=0.5):
