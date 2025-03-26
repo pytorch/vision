ff8bf332e371fe3ba4a209bef1494596c011651a
Nicolas Hug <contact@nicolas-hug.com>
2025-03-26 10:55:41 +0000

    Fix

--- torchvision/io/_video_deprecation_warning.py
+++ torchvision/io/_video_deprecation_warning.py
@@ -3,10 +3,10 @@ import warnings
 
 def _raise_video_deprecation_warning():
     warnings.warn(
-        UserWarning,
         "The video decoding and encoding capabilities of torchvision "
         "are deprecated from version 0.22 and will be removed in version 0.24. "
         "We recommend that you migrate to TorchCodec, where we'll consolidate "
         "the future decoding/encoding capabilities of PyTorch: "
         "https://github.com/pytorch/torchcodec",
+        UserWarning,
     )
