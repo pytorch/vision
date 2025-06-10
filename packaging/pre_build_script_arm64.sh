fdef4a618ad1969a3d1e619a487b6dbef1ce1ad6
alinpahontu2912 <alin_pahontu@yahoo.com>
2025-06-10 17:34:32 +0300

    retrigger

--- packaging/pre_build_script_arm64.sh
+++ packaging/pre_build_script_arm64.sh
@@ -45,7 +45,7 @@ cp -r "$DEPENDENCIES_DIR/Library/bin/"* "$SRC_PATH/torchvision"
 ls -l "$DEPENDENCIES_DIR/Library/"
 ls -l "$DEPENDENCIES_DIR/Library/include"
 ls -l "$DEPENDENCIES_DIR/Library/bin/"
-ls -l "$SRC_PATH/torchvision"
+ls -l "$SRC_PATH"
 
 # Source directory
 cd "$SRC_PATH" || exit
