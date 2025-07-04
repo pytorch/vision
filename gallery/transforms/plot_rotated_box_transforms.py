89097424c68d303e3f74ed899b07d2962e20d940
Scott Schneider <scott.a.s@gmail.com>
2025-07-03 18:16:58 -0700

    Add transforms, add iterations

--- gallery/transforms/plot_rotated_box_transforms.py
+++ gallery/transforms/plot_rotated_box_transforms.py
@@ -42,67 +42,87 @@ orig_box = tv_tensors.BoundingBoxes(
     ],
     format="CXCYWHR",
     canvas_size=(orig_img.size[1], orig_img.size[0]),
-    clamping_mode="hard",
 )
-# TODO: why is this necessary?
+
+# Note that this conversion is only necessary because our utilities for drawing
+# the bounding boxes expect this format. This conversion is not necessary in
+# actual training pipelines. TODO: verify with @NicolasHug, @AntoineSimoulin
 orig_box = v2.ConvertBoundingBoxFormat("xyxyxyxy")(orig_box)
 
-plot([(orig_img, orig_box)])
+plot([(orig_img, orig_box)], bbox_width=10)
 
 # %%
-# Image Rotation
-# ---------------
-# We can rotate the image itself, and the already rotated bounding boxes are
-# rotated appropriately.
-
-out_img, out_box = v2.RandomRotation(degrees=(0, 180), expand=True)(orig_img, orig_box)
-plot([(out_img, out_box)])
+# Rotation
+# --------------
+rotater = v2.RandomRotation(degrees=(0, 180), expand=True)
+rotated_imgs = [rotater((orig_img, orig_box)) for _ in range(4)]
+plot([(orig_img, orig_box)] + rotated_imgs, bbox_width=10)
 
 # %%
-# Image Padding
+# Padding
 # -------------
 # The rotated bounding boxes also respect padding transforms.
 padded_imgs_and_boxes = [
-    v2.Pad(padding=padding)(orig_img, orig_box) for padding in (10, 30, 50, 100)
+    v2.Pad(padding=padding)(orig_img, orig_box)
+    for padding in (30, 50, 100, 200)
 ]
-plot([(orig_img, orig_box)] + padded_imgs_and_boxes)
+plot([(orig_img, orig_box)] + padded_imgs_and_boxes, bbox_width=10)
 
 # %%
-# Image Resizing
+# Resizing
 # --------------
-# The rotated bounding boxes are resized along with the image.
-resized_imgs_and_boxes = [v2.Resize(size=size)(orig_img, orig_box) for size in (30, 50, 100, orig_img.size)]
-plot([(orig_img, orig_box)] + resized_imgs_and_boxes)
+# Note that the bounding box looking bigger in the small images is an artifact,
+# not reality. It is due to the the fact that we specify a fixed-size for the
+# width of the lines to draw. When the image is, say, only 30 pixels wide, a
+# line that is 3 pixels wide is relatively large. We could potentially try to
+# tweak the plotting function to avoid this appearance.
+resized_imgs = [
+    v2.Resize(size=size)(orig_img, orig_box)
+    for size in (30, 50, 100, orig_img.size)
+]
+plot([(orig_img, orig_box)] + resized_imgs, bbox_width=3)
 
 # %%
-# Image Rotation
-# --------------
-rotater = v2.RandomRotation(degrees=(0, 180))
-rotated_imgs = [rotater((orig_img, orig_box)) for _ in range(4)]
-plot([(orig_img, orig_box)] + rotated_imgs)
+# Perspective
+# -----------
+perspective_transformer = v2.RandomPerspective(distortion_scale=0.6, p=1.0)
+perspective_imgs = [perspective_transformer(orig_img, orig_box) for _ in range(4)]
+plot([(orig_img, orig_box)] + perspective_imgs, bbox_width=10)
 
 # %%
 # Elastic Transform
 # -----------------
-plot([v2.ElasticTransform(alpha=250.0)(orig_img, orig_box)])
+elastic_imgs = [
+    v2.ElasticTransform(alpha=alpha)(orig_img, orig_box)
+    for alpha in (100.0, 500.0, 1000.0, 2000.0)
+]
+plot([(orig_img, orig_box)] + elastic_imgs, bbox_width=10)
 
 # %%
-# Clamping Modes
-# --------------
-# Explain hard and soft, with appropriate links to documentation. Talk about
-# defaults. Link to to-be-written-tutorial on mode-setting in general.
-soft_box = orig_box.clone()
-soft_box.clamping_mode = "soft"
+# Crop & Clamping Modes
+# ---------------------
+# This section doubles as the example for Crop and for explaining clamping
+# modes. My rationale for doing at both at once: any meaningful examples for
+# cropping are going to impact the bounding box, and the only way to make
+# sense of that is to also explain clamping modes. We should cover:
+#
+# * Clamping mode kinds: hard, soft, None. Behavior of each, when to use them.
+# * Clamping mode defaults for: bounding boxes, functionals, transforms in
+#   general, ClampingBoundingBoxes() specifically.
+assert orig_box.clamping_mode == "soft"
+hard_box = orig_box.clone()
+hard_box.clamping_mode = "hard"
 
-hard_center_crops_and_boxes = [
+soft_center_crops_and_boxes = [
     v2.CenterCrop(size=size)(orig_img, orig_box)
     for size in (800, 1200, 2000, orig_img.size)
 ]
 
-soft_center_crops_and_boxes = [
-    v2.CenterCrop(size=size)(orig_img, soft_box)
+hard_center_crops_and_boxes = [
+    v2.CenterCrop(size=size)(orig_img, hard_box)
     for size in (800, 1200, 2000, orig_img.size)
 ]
 
-plot([[(orig_img, orig_box)] + hard_center_crops_and_boxes,
-      [(orig_img, soft_box)] + soft_center_crops_and_boxes])
+plot([[(orig_img, orig_box)] + soft_center_crops_and_boxes,
+      [(orig_img, hard_box)] + hard_center_crops_and_boxes],
+     bbox_width=10)
