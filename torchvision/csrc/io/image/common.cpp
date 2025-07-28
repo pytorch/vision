960d94a942a09e3b6f9c5d110ccff8d69135c7c2
Nicolas Hug <nicolashug@meta.com>
2025-07-28 06:47:11 -0700

    [fbsync] [ROCm][Windows] Fix compile torchvision image for ROCm (#8897)
    Reviewed By: scotts
    
    Differential Revision: D77997074
    
    fbshipit-source-id: 06cdc349f360270a90d05fde2623bfacab729313

--- torchvision/csrc/io/image/common.cpp
+++ torchvision/csrc/io/image/common.cpp
@@ -1,6 +1,14 @@
 
 #include "common.h"
 
+// If we are in a Windows environment, we need to define
+// initialization functions for the _custom_ops extension
+#ifdef _WIN32
+void* PyInit_image(void) {
+  return nullptr;
+}
+#endif
+
 namespace vision {
 namespace image {
 
