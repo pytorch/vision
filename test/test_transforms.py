8c76f9c8454079eeaff55b910ff49ac63deb3c23
Nicolas Hug <nicolashug@meta.com>
2025-07-29 07:11:44 -0700

    TorchVision sync: tests
    Summary:
    This is part of the GH -> fbcode sync for torchvision.
    Some changes were made to the torchvision codebase on fbcode that weren't reflected on GitHub. That created a desync between both code-bases which makes it impossible for us to import changes from GH into fbcode. This diff aligns with fbcode codebase with the one that's on GH, based from commit  997348dec3 on main. This will allow us to continue the syncing process.
    
    ```
    ~/dev/vision (997348dec3*) » git diff origin/fbsync torchvision/test/ > d
    ```
    (excluding the cpp changes)
    
    Reviewed By: AntoineSimoulin
    
    Differential Revision: D79163069
    
    fbshipit-source-id: c7d191ca5c96a05eb41f7ff5ed351cd0c4809499

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -12,10 +12,10 @@ import torchvision.transforms as transforms
 import torchvision.transforms._functional_tensor as F_t
 import torchvision.transforms.functional as F
 from PIL import Image
-from torch._utils_internal import get_file_path_2 # @manual=fbcode//caffe2:utils_internal
+from torch._utils_internal import get_file_path_2
 
 try:
-    import accimage # @manual=fbcode//pytorch/accimage:accimage
+    import accimage
 except ImportError:
     accimage = None
 
