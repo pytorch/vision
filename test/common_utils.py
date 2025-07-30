b79a1fbe24251256e6952015497c9d405f7a7c0b
Nicolas Hug <nicolashug@meta.com>
2025-07-30 01:14:56 -0700

    [fbsync] A temporary fix to windows unittests failing in `INaturalistTestCase`. (#9007)
    Reviewed By: AntoineSimoulin
    
    Differential Revision: D79175051
    
    fbshipit-source-id: 9e8e6e9168f9e3cd79803e5de15f884582fc013e

--- test/common_utils.py
+++ test/common_utils.py
@@ -34,14 +34,12 @@ OSS_CI_GPU_NO_CUDA_MSG = "We're in an OSS GPU machine, and this test doesn't nee
 
 @contextlib.contextmanager
 def get_tmp_dir(src=None, **kwargs):
-    tmp_dir = tempfile.mkdtemp(**kwargs)
-    if src is not None:
-        os.rmdir(tmp_dir)
-        shutil.copytree(src, tmp_dir)
-    try:
+    with tempfile.TemporaryDirectory(
+        **kwargs,
+    ) as tmp_dir:
+        if src is not None:
+            shutil.copytree(src, tmp_dir)
         yield tmp_dir
-    finally:
-        shutil.rmtree(tmp_dir)
 
 
 def set_rng_seed(seed):
