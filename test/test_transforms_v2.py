9ffbd250bff10d737f857f554117da10ae35301c
pytorchbot <pytorchbot@pytorch.com>
2025-08-26 11:35:19 +0000

    2025-08-26 nightly release (7a936daee8429915e6e1208838f6d2f89a4da78a)

--- test/test_transforms_v2.py
+++ test/test_transforms_v2.py
@@ -7016,6 +7016,29 @@ def test_parallelogram_to_bounding_boxes(input_size, device):
     actual = _parallelogram_to_bounding_boxes(parallelogram)
     torch.testing.assert_close(actual, expected)
 
+    # Test the transformation of a simple parallelogram.
+    #              1
+    #    1-2      /   2
+    #   / /  ->  /   /
+    # 4-3       4   /
+    #              3
+    #
+    #          1
+    # 1-2       \ 2
+    #   \ \  ->  \  \
+    #    4-3       4 \
+    #                 3
+    parallelogram = torch.tensor(
+        [[0, 4, 3, 1, 5, 1, 2, 4], [0, 1, 2, 1, 5, 4, 3, 4]],
+        dtype=torch.float32,
+    )
+    expected = torch.tensor(
+        [[0, 4, 4, 0, 5, 1, 1, 5], [0, 1, 1, 0, 5, 4, 4, 5]],
+        dtype=torch.float32,
+    )
+    actual = _parallelogram_to_bounding_boxes(parallelogram)
+    torch.testing.assert_close(actual, expected)
+
 
 @pytest.mark.parametrize("image_type", (PIL.Image, torch.Tensor, tv_tensors.Image))
 @pytest.mark.parametrize("data_augmentation", ("hflip", "lsj", "multiscale", "ssd", "ssdlite"))
