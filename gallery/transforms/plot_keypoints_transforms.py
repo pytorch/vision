28605ad1b754be0c364a56b45247aa7408df4940
Antoine Simoulin <antoinesimoulin@fb.com>
2025-09-10 11:53:47 -0700

    lint

--- gallery/transforms/plot_keypoints_transforms.py
+++ gallery/transforms/plot_keypoints_transforms.py
@@ -3,8 +3,8 @@
 Transforms on KeyPoints
 ===============================================================
 
-This example illustrates how to define and use keypoints. 
-For this tutorial, we use this picture of a ceramic figure from the pre-columbian period. 
+This example illustrates how to define and use keypoints.
+For this tutorial, we use this picture of a ceramic figure from the pre-columbian period.
 The image is specified "public domain" (https://www.metmuseum.org/art/collection/search/502727).
 
 .. note::
