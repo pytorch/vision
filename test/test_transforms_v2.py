70fb3936b77587efa0ee0989320d2b9c4fa2aadc
Justin Davis <judavis@nvidia.com>
2025-12-18 14:39:36 -0800

    fix import changes

--- test/test_transforms_v2.py
+++ test/test_transforms_v2.py
@@ -21,7 +21,6 @@ import torchvision.ops
 import torchvision.transforms.v2 as transforms
 
 from common_utils import (
-    assert_close,
     assert_equal,
     cache,
     cpu_and_cuda,
@@ -43,6 +42,7 @@ from common_utils import (
 )
 
 from torch import nn
+from torch.testing import assert_close
 from torch.utils._pytree import tree_flatten, tree_map
 from torch.utils.data import DataLoader, default_collate
 from torchvision import tv_tensors
