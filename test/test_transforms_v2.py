4dc990f1a619f2c4b9c57a803ed462d48a243a15
Zhitao Yu <zhitao@fb.com>
2026-02-19 18:41:36 -0800

    Add test to verify inplace parameter works

--- test/test_transforms_v2.py
+++ test/test_transforms_v2.py
@@ -4440,6 +4440,36 @@ class TestConvertBoundingBoxFormat:
 
         torch.testing.assert_close(actual, expected)
 
+    @pytest.mark.parametrize(
+        "old_format",
+        [tv_tensors.BoundingBoxFormat.XYWH, tv_tensors.BoundingBoxFormat.CXCYWH],
+    )
+    @pytest.mark.parametrize("dtype", [torch.float32, torch.int64])
+    @pytest.mark.parametrize("device", cpu_and_cuda())
+    def test_xywh_cxcywh_direct_conversion_inplace(self, old_format, dtype, device):
+        bounding_boxes = make_bounding_boxes(format=old_format, dtype=dtype, device=device)
+        input_tensor = bounding_boxes.as_subclass(torch.Tensor).clone()
+
+        if old_format == tv_tensors.BoundingBoxFormat.XYWH:
+            convert_fn = _xywh_to_cxcywh
+        else:
+            convert_fn = _cxcywh_to_xywh
+
+        input_clone = input_tensor.clone()
+        output_inplace_false = convert_fn(input_clone, inplace=False)
+        assert output_inplace_false.data_ptr() != input_clone.data_ptr()
+
+        input_clone = input_tensor.clone()
+        input_data_ptr = input_clone.data_ptr()
+
+        output_inplace = convert_fn(input_clone, inplace=True)
+
+        assert output_inplace.data_ptr() == input_data_ptr
+
+        assert output_inplace is input_clone
+
+        torch.testing.assert_close(output_inplace, output_inplace_false)
+
     def test_cxcywh_to_xyxy_odd_dimensions(self):
         # Non-regression test for https://github.com/pytorch/vision/issues/8887
         # Integer bounding boxes with odd width/height produced incorrect results
