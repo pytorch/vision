f562aa36db347b13c0d3a7865d9aa141800988e5
adabeyta <aabeyta@redhat.com>
2026-08-04 23:48:17 +0000

    Port ps_roi_align to stable ABI. (CUDA)

--- setup.py
+++ setup.py
@@ -185,6 +185,9 @@ STABLE_SOURCES.add(CSRS_DIR / ("ops/hip/nms_kernel.hip" if IS_ROCM else "ops/cud
 STABLE_SOURCES.add(
     CSRS_DIR / ("ops/hip/box_iou_rotated_kernel.hip" if IS_ROCM else "ops/cuda/box_iou_rotated_kernel.cu")
 )
+STABLE_SOURCES.add(
+    CSRS_DIR / ("ops/hip/ps_roi_align_kernel.hip" if IS_ROCM else "ops/cuda/ps_roi_align_kernel.cu")
+)
 
 
 def _not_stable(paths):
