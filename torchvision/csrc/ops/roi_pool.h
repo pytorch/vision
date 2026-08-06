c77c6cf31f8f6c3fe59ca092af7d02198868240d
pytorchbot <pytorchbot@pytorch.com>
2026-08-06 12:14:11 +0000

    2026-08-06 nightly release (9f1d924526c8b5ff094dd8bfda1f47b51a7f3553)

--- torchvision/csrc/ops/roi_pool.h
+++ torchvision/csrc/ops/roi_pool.h
@@ -1,31 +1,26 @@
 #pragma once
 
-#include <ATen/ATen.h>
+#include <torch/csrc/stable/tensor.h>
 #include "../macros.h"
+
+#include <tuple>
 
 namespace vision {
 namespace ops {
 
-VISION_API std::tuple<at::Tensor, at::Tensor> roi_pool(
-    const at::Tensor& input,
-    const at::Tensor& rois,
+VISION_API std::tuple<torch::stable::Tensor, torch::stable::Tensor> roi_pool(
+    const torch::stable::Tensor& input,
+    const torch::stable::Tensor& rois,
     double spatial_scale,
     int64_t pooled_height,
     int64_t pooled_width);
 
-VISION_API std::tuple<at::Tensor, at::Tensor> roi_pool_symint(
-    const at::Tensor& input,
-    const at::Tensor& rois,
-    double spatial_scale,
-    c10::SymInt pooled_height,
-    c10::SymInt pooled_width);
-
 namespace detail {
 
-at::Tensor _roi_pool_backward(
-    const at::Tensor& grad,
-    const at::Tensor& rois,
-    const at::Tensor& argmax,
+torch::stable::Tensor _roi_pool_backward(
+    const torch::stable::Tensor& grad,
+    const torch::stable::Tensor& rois,
+    const torch::stable::Tensor& argmax,
     double spatial_scale,
     int64_t pooled_height,
     int64_t pooled_width,
@@ -33,18 +28,6 @@ at::Tensor _roi_pool_backward(
     int64_t channels,
     int64_t height,
     int64_t width);
-
-at::Tensor _roi_pool_backward_symint(
-    const at::Tensor& grad,
-    const at::Tensor& rois,
-    const at::Tensor& argmax,
-    double spatial_scale,
-    c10::SymInt pooled_height,
-    c10::SymInt pooled_width,
-    c10::SymInt batch_size,
-    c10::SymInt channels,
-    c10::SymInt height,
-    c10::SymInt width);
 
 } // namespace detail
 
