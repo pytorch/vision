5ebb1b52d4c4078b8b1293761512f0139709cf81
pytorchbot <pytorchbot@pytorch.com>
2026-08-21 11:41:20 +0000

    2026-08-21 nightly release (e6f04004f0e0f759b44329d9ee6751a55abf0f83)

--- torchvision/_autograd_registrations.py
+++ torchvision/_autograd_registrations.py
@@ -87,6 +87,8 @@ def _ps_roi_align_setup_context(ctx, inputs, output):
 
 
 def _ps_roi_align_backward(ctx, grad_output, _grad_channel_mapping):
+    if grad_output.device.type == "mps":
+        torch._prims_common.alert_not_deterministic("ps_roi_align_backward_kernel")
     rois, channel_mapping = ctx.saved_tensors
     batch_size, channels, height, width = ctx.input_shape
     grad_input = torch.ops.torchvision._ps_roi_align_backward(
@@ -117,6 +119,8 @@ def _ps_roi_pool_setup_context(ctx, inputs, output):
 
 
 def _ps_roi_pool_backward(ctx, grad_output, _grad_channel_mapping):
+    if grad_output.device.type == "mps":
+        torch._prims_common.alert_not_deterministic("ps_roi_pool_backward_kernel")
     rois, channel_mapping = ctx.saved_tensors
     batch_size, channels, height, width = ctx.input_shape
     grad_input = torch.ops.torchvision._ps_roi_pool_backward(
