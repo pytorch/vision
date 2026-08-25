cafed4f06c7da48fc4dd6c8fe4367eeeb9daed47
pytorchbot <pytorchbot@pytorch.com>
2026-08-25 11:41:50 +0000

    2026-08-25 nightly release (541c08303e9bbc9524bbd60108775612d3af6112)

--- setup.py
+++ setup.py
@@ -185,6 +185,11 @@ STABLE_SOURCES.add(CSRS_DIR / ("ops/hip/nms_kernel.hip" if IS_ROCM else "ops/cud
 STABLE_SOURCES.add(
     CSRS_DIR / ("ops/hip/box_iou_rotated_kernel.hip" if IS_ROCM else "ops/cuda/box_iou_rotated_kernel.cu")
 )
+STABLE_SOURCES.add(CSRS_DIR / ("ops/hip/roi_pool_kernel.hip" if IS_ROCM else "ops/cuda/roi_pool_kernel.cu"))
+STABLE_SOURCES.add(CSRS_DIR / ("ops/hip/roi_align_kernel.hip" if IS_ROCM else "ops/cuda/roi_align_kernel.cu"))
+STABLE_SOURCES.add(CSRS_DIR / ("ops/hip/deform_conv2d_kernel.hip" if IS_ROCM else "ops/cuda/deform_conv2d_kernel.cu"))
+STABLE_SOURCES.add(CSRS_DIR / ("ops/hip/ps_roi_pool_kernel.hip" if IS_ROCM else "ops/cuda/ps_roi_pool_kernel.cu"))
+STABLE_SOURCES.add(CSRS_DIR / ("ops/hip/ps_roi_align_kernel.hip" if IS_ROCM else "ops/cuda/ps_roi_align_kernel.cu"))
 
 
 def _not_stable(paths):
