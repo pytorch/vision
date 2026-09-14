57281b6a335b31589256fd5d8b215de459dd588c
Adrian Abeyta <aabeyta@redhat.com>
2026-09-14 03:02:45 -0500

    Replace stable_permute and index_select shims with upstream torch::stable wrappers. (#9660)

--- torchvision/csrc/io/image/cpu/decode_jpeg.cpp
+++ torchvision/csrc/io/image/cpu/decode_jpeg.cpp
@@ -259,7 +259,7 @@ torch::stable::Tensor decode_jpeg(
 
   jpeg_finish_decompress(&cinfo);
   jpeg_destroy_decompress(&cinfo);
-  auto output = stable_permute(*tensor, {2, 0, 1});
+  auto output = torch::stable::permute(*tensor, {2, 0, 1});
 
   if (apply_exif_orientation) {
     return exif_orientation_transform(output, exif_orientation);
